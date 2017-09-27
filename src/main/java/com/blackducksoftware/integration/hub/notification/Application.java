d64df769abd2b5dd78aee67a0f0f2104a2c14823
Paulo Santos <psantos@blackducksoftware.com>
2017-09-27 16:34:04 -0400

    fix the build to setup the logger and set the spring configuration file

--- src/main/java/com/blackducksoftware/integration/hub/notification/Application.java
+++ src/main/java/com/blackducksoftware/integration/hub/notification/Application.java
@@ -1,8 +1,23 @@
 package com.blackducksoftware.integration.hub.notification;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class Application {
 
+    private final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    @PostConstruct
+    void init() {
+        logger.debug("STARTED THE NOTIFICATION ENGINE");
+    }
+
+    public static void main(final String[] args) {
+        new SpringApplicationBuilder(Application.class).logStartupInfo(false).run(args);
+    }
 }
---
+++ src/main/resources/notification_engine.sh
@@ -0,0 +1,110 @@
+#! /bin/sh
+#*******************************************************************************
+# Copyright (C) 2017 Black Duck Software, Inc.
+# http://www.blackducksoftware.com/
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements. See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership. The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#*******************************************************************************
+
+PID_FILE=hub_notification_engine.pid
+TITLE="Hub Notification Engine"
+MAX_ATTEMPTS=20
+PROGRAM="$0"
+DIR=`dirname "$PROGRAM"`
+DIRECTORY_NAME="hub-notification-engine"
+
+checkIsRunning() {
+  if [ -f $DIR/$PID_FILE ]; then
+    local running_pid=$(ps -eo pid,args | grep java | grep $DIRECTORY_NAME | awk '{print $1}')
+    local saved_pid=`cat $DIR/$PID_FILE`
+    if [ ! -z $running_pid  ]; then
+        if [ "$saved_pid" -eq "$running_pid" ]; then
+            return 1
+        fi
+    fi
+    return 0
+  fi
+  return 0
+}
+
+startExtension() {
+  checkIsRunning
+  returnValue=$?
+  if [ "$returnValue" -eq "1" ]; then
+      echo "$TITLE is already running"
+      return 0;
+  fi
+  echo "Starting $TITLE"
+  $DIR/$DIRECTORY_NAME &
+  sleep 1s
+  local pid=$(ps -eo pid,args | grep java | grep $DIRECTORY_NAME | awk '{print $1}')
+  echo "$pid" > $DIR/$PID_FILE
+  echo "Started $TITLE with PID: $pid"
+}
+
+stopExtension() {
+  checkIsRunning
+  returnValue=$?
+  if [ "$returnValue" -eq "1" ]; then
+     local pid=`cat $DIR/$PID_FILE`
+     echo "Stopping $TITLE with PID: $pid"
+     for index in `seq 1 $MAX_ATTEMPTS`; do
+       checkIsRunning
+       stillRunning=$?
+       if [ "$stillRunning" -eq "0" ]; then
+         rm -f $PID_FILE
+         echo "Stopped $TITLE with PID: $pid"
+         return 0;
+       fi
+       kill $pid
+       sleep .5
+     done
+  fi
+  return 1
+}
+
+extensionStatus () {
+  checkIsRunning
+  returnValue=$?
+  if [ "$returnValue" -eq "1" ]; then
+     local pid=`cat $DIR/$PID_FILE`
+     echo "$TITLE is running with PID: $pid"
+  else
+     echo "$TITLE is not running"
+  fi
+}
+
+case "$1" in
+    start)
+          startExtension
+          ;;
+    stop)
+          stopExtension
+          ;;
+    restart)
+          stopExtension
+          startExtension
+          ;;
+    status)
+          extensionStatus
+          ;;
+    *)
+          echo "Usage: start|stop|restart|status"
+          exit 1
+          ;;
+esac
