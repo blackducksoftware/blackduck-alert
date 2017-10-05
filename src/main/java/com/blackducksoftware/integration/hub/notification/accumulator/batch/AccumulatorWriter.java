b36e216310dbd5af9ba9163e14f1563db58d9fa4
Paulo Santos <psantos@blackducksoftware.com>
2017-10-05 15:00:06 -0400

    Remove the unused logger

--- src/main/java/com/blackducksoftware/integration/hub/notification/accumulator/batch/AccumulatorWriter.java
+++ src/main/java/com/blackducksoftware/integration/hub/notification/accumulator/batch/AccumulatorWriter.java
@@ -3,8 +3,6 @@ package com.blackducksoftware.integration.hub.notification.accumulator.batch;
 import java.util.Date;
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemWriter;
 
 import com.blackducksoftware.integration.hub.dataservice.notification.model.NotificationContentItem;
@@ -16,7 +14,6 @@ import com.blackducksoftware.integration.hub.notification.processor.event.Notifi
 import com.blackducksoftware.integration.hub.report.api.PolicyRule;
 
 public class AccumulatorWriter implements ItemWriter<DBStoreEvent> {
-    private final static Logger logger = LoggerFactory.getLogger(AccumulatorWriter.class);
     private final NotificationRepository notificationRepository;
 
     public AccumulatorWriter(final NotificationRepository notificationRepository) {
