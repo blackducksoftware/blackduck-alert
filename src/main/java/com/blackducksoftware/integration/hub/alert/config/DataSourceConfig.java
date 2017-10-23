98ea1d7ae15dcd82ebc14de27e4cb6f0ab91aedf
jrichard <jrichard@blackducksoftware.com>
2017-10-23 15:40:54 -0400

    Updating schema creation

--- src/main/java/com/blackducksoftware/integration/hub/alert/config/DataSourceConfig.java
+++ src/main/java/com/blackducksoftware/integration/hub/alert/config/DataSourceConfig.java
@@ -41,7 +41,8 @@ public class DataSourceConfig {
     @Bean
     public DataSource dataSource() {
         final EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
-        final EmbeddedDatabase dataSource = builder.setType(EmbeddedDatabaseType.H2).addScript("db/create-notification-schema.sql").addScript("db/create-notification-db.sql").addScript("db/email/create-tables.sql").build();
+        final EmbeddedDatabase dataSource = builder.setType(EmbeddedDatabaseType.H2).addScript("db/create-notification-schema.sql").addScript("db/create-notification-db.sql").addScript("db/email/create-configuration-schema.sql")
+                .addScript("db/email/create-tables.sql").build();
         return dataSource;
     }
 
