b186050928fa46b5ad78e990cbcb3724f055af4e
jrichard <jrichard@blackducksoftware.com>
2017-11-02 11:42:29 -0400

    Adding npm to the gradle build

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -6,7 +6,22 @@ const ReactDOM = require('react-dom');
 class App extends React.Component {
     render() {
         return (
-            <h1>Hello World!!!</h1>
+            <div>
+                <h1>Hub Alert Global Config</h1>
+                <input type="text" name="hubUrl"></input><br />
+                <input type="text" name="hubUsername"></input><br />
+                <input type="password" name="hubPassword"></input><br />
+                <input type="number" name="hubTimeout"></input><br />
+                <input type="checkbox" name="hubAlwaysTrustCertificate"></input><br />
+                
+                <input type="text" name="hubProxyHost"></input><br />
+                <input type="number" name="hubProxyPort"></input><br />
+                <input type="text" name="hubProxyUsername"></input><br />
+                <input type="text" name="hubProxyPassword"></input><br />
+                
+                <input type="text" name="accumulatorCron"></input><br />
+                <input type="text" name="dailyDigestCron"></input><br /> 
+            </div>
         )
     }
 }
