b2fd05576b6ea5cd710de799240322b49f7fdc92
jrichard <jrichard@blackducksoftware.com>
2017-11-06 12:16:49 -0500

    Working on field errors

--- src/main/js/component/GlobalConfiguration.js
+++ src/main/js/component/GlobalConfiguration.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Field from '../field/Field';
 
 class GlobalConfiguration extends React.Component {
 	constructor(props) {
@@ -123,49 +124,29 @@ class GlobalConfiguration extends React.Component {
                 <h1>Global Configuration</h1>
                 <form onSubmit={this.handleSubmit.bind(this)}>
 	                <h2>Hub Configuration</h2>
-	                <label>Url</label>
-	                <input type="text" name="hubUrl" value={this.state.hubUrl} onChange={this.handleChange}></input><br />
-	                <div name="hubUrlError" value={this.state.hubUrlError}></div>
+	                <Field label="Url" type="text" name="hubUrl" value={this.state.hubUrl} onChange={this.handleChange} errorName="hubUrlError" errorValue={this.state.hubUrlError}></Field>
 	                
-	                <label>Username</label>
-	                <input type="text" name="hubUsername" value={this.state.hubUsername} onChange={this.handleChange}></input><br />
-	                <div name="hubUsernameError" value={this.state.hubUsernameError}></div>
+	                <Field label="Username" type="text" name="hubUsername" value={this.state.hubUsername} onChange={this.handleChange} errorName="hubUsernameError" errorValue={this.state.hubUsernameError}></Field>
 	                
-	                <label>Password</label>
-	                <input type="password" name="hubPassword" value={this.state.hubPassword} onChange={this.handleChange}></input><br />
+	                <Field label="Password" type="password" name="hubPassword" value={this.state.hubPassword} onChange={this.handleChange} ></Field>
 	                
-	                <label>Timeout</label>
-	                <input type="number" name="hubTimeout" value={this.state.hubTimeout} onChange={this.handleChange}></input><br />
-	                <div name="hubTimeoutError" value={this.state.hubTimeoutError}></div>
+	                <Field label="Timeout" type="number" name="hubTimeout" value={this.state.hubTimeout} onChange={this.handleChange} errorName="hubTimeoutError" errorValue={this.state.hubTimeoutError}></Field>
 	                
-	                <label>Trust Https Certificates</label>
-	                <input type="checkbox" name="hubAlwaysTrustCertificate" checked={this.state.hubAlwaysTrustCertificate} onChange={this.handleChange}></input><br />
-	                <div name="hubAlwaysTrustCertificateError" value={this.state.hubAlwaysTrustCertificateError}></div>
+	                <Field label="Trust Https Certificates" type="checkbox" name="hubAlwaysTrustCertificate" value={this.state.hubAlwaysTrustCertificate} onChange={this.handleChange} errorName="hubAlwaysTrustCertificateError" errorValue={this.state.hubAlwaysTrustCertificateError}></Field>
 	                
 	                <h2>Proxy Configuration</h2>
-	                <label>Host Name</label>
-	                <input type="text" name="hubProxyHost" value={this.state.hubProxyHost} onChange={this.handleChange}></input><br />
-	                <div name="hubProxyHostError" value={this.state.hubProxyHostError}></div>
+                    <Field label="Host Name" type="text" name="hubProxyHost" value={this.state.hubProxyHost} onChange={this.handleChange} errorName="hubProxyHostError" errorValue={this.state.hubProxyHostError}></Field>
 	                
-	                <label>Port</label>
-	                <input type="number" name="hubProxyPort" value={this.state.hubProxyPort} onChange={this.handleChange}></input><br />
-	                <div name="hubProxyPortError" value={this.state.hubProxyPortError}></div>
+	                <Field label="Port" type="number" name="hubPassword" value={this.state.hubProxyPort} onChange={this.handleChange} errorName="hubProxyPortError" errorValue={this.state.hubProxyPortError}></Field>
 	                
-	                <label>Username</label>
-	                <input type="text" name="hubProxyUsername" value={this.state.hubProxyUsername} onChange={this.handleChange}></input><br />
-	                <div name="hubProxyUsernameError" value={this.state.hubProxyUsernameError}></div>
+	                <Field label="Username" type="text" name="hubProxyUsername" value={this.state.hubProxyUsername} onChange={this.handleChange} errorName="hubProxyUsernameError" errorValue={this.state.hubProxyUsernameError}></Field>
 	                
-	                <label>Password</label>
-	                <input type="password" name="hubProxyPassword" value={this.state.hubProxyPassword} onChange={this.handleChange}></input><br />
+	                <Field label="Password" type="text" name="hubProxyPassword" value={this.state.hubProxyPassword} onChange={this.handleChange} ></Field>
 	                
 	                <h2>Scheduling Configuration</h2>
-	                <label>Accumulator Cron</label>
-	                <input type="text" name="accumulatorCron" value={this.state.accumulatorCron} onChange={this.handleChange}></input><br />
-	                <div name="accumulatorCronError" value={this.state.accumulatorCronError}></div>
+                    <Field label="Accumulator Cron" type="text" name="accumulatorCron" value={this.state.accumulatorCron} onChange={this.handleChange} errorName="accumulatorCronError" errorValue={this.state.accumulatorCronError}></Field>
 	                
-	                <label>Daily Digest Cron</label>
-	                <input type="text" name="dailyDigestCron" value={this.state.dailyDigestCron} onChange={this.handleChange}></input><br /> 
-	                <div name="dailyDigestCronError" value={this.state.dailyDigestCronError}></div>
+	                <Field label="Daily Digest Cron" type="text" name="dailyDigestCron" value={this.state.dailyDigestCron} onChange={this.handleChange} errorName="dailyDigestCronError" errorValue={this.state.dailyDigestCronError}></Field>
 	                
 	                <input type="submit" value="Save"></input>
 	                <input type="button" value="Test" onClick={this.handleTestSubmit.bind(this)}></input>
