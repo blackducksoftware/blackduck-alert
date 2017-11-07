8edfd9f20e31675dedc54ca4d16fefffbccfa976
Paulo Santos <psantos@blackducksoftware.com>
2017-11-07 15:12:46 -0500

    Add the columns for the notification table that were missing

--- src/main/resources/db/create-notification-db.sql
+++ src/main/resources/db/create-notification-db.sql
@@ -1,10 +1,13 @@
 CREATE TABLE alert.notification_events (
 	id INTEGER PRIMARY KEY,
+	hub_user VARCHAR,
 	event_key VARCHAR,
     created_at TIMESTAMP,
     notification_type VARCHAR,
     project_name VARCHAR,
+    project_url VARCHAR,
     project_version VARCHAR,
+    project_version_url VARCHAR,
     component_name VARCHAR,
     component_version VARCHAR,
     person VARCHAR,
