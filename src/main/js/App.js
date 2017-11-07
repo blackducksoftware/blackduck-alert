a16c5f6acc80e2ff35ece72e469c16ef9fc239f9
Brian Mandel <bamandel@gmail.com>
2017-11-07 17:53:04 -0500

    UI looking better with nested tabs

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -6,6 +6,7 @@ import GlobalConfiguration from './component/GlobalConfiguration';
 import EmailConfiguration from './component/EmailConfiguration';
 import HipChatConfiguration from './component/HipChatConfiguration';
 import SlackConfiguration from './component/SlackConfiguration';
+
 import styles from '../css/main.css';
 
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
@@ -13,7 +14,10 @@ import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 class App extends React.Component {
     constructor() {
         super();
-        this.state = {index: 0};
+        this.state = {
+            mainIndex: 0,
+            channelIndex: 0
+        };
     }
 
     render() {
@@ -23,18 +27,31 @@ class App extends React.Component {
                     <h1>Black Duck Alert</h1>
                 </div>
                 
-                <Tabs selectedIndex={this.state.index} onSelect={index => this.setState({ index })}>
+                <Tabs selectedIndex={this.state.mainIndex} onSelect={index => this.setState({ mainIndex: index })}>
                     <TabList className={styles.table}>
-                        <Tab>Hub settings</Tab>
-                        <Tab>Channel configuration</Tab>
+                        <Tab className={styles.tabSpacing}>Hub settings</Tab>
+                        <Tab className={styles.tabSpacing}>Channel configuration</Tab>
                     </TabList>
                     <TabPanel className={styles.tabContent}>
                         <GlobalConfiguration />
                     </TabPanel>
                     <TabPanel className={styles.tabContent}>
-                        <EmailConfiguration />
-                        <HipChatConfiguration />
-                        <SlackConfiguration />
+                        <Tabs selectedIndex={this.state.channelIndex} onSelect={index => this.setState({ channelIndex: index })}>
+                            <TabList className={styles.table}>
+                                <Tab>Email</Tab>
+                                <Tab>Hipchat</Tab>
+                                <Tab>Slack</Tab>
+                            </TabList>
+                            <TabPanel>
+                                <EmailConfiguration />
+                            </TabPanel>
+                            <TabPanel>
+                                <HipChatConfiguration />
+                            </TabPanel>
+                            <TabPanel>
+                                <SlackConfiguration />
+                            </TabPanel>
+                        </Tabs>
                     </TabPanel>
                 </Tabs>
             </div>
