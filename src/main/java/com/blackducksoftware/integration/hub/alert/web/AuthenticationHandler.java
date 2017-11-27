299fe6956d92f496089d7d028dfa3d8b41f6c2b1
jrichard <jrichard@blackducksoftware.com>
2017-11-27 14:05:13 -0500

    Removing the commented code

--- src/main/java/com/blackducksoftware/integration/hub/alert/web/AuthenticationHandler.java
+++ src/main/java/com/blackducksoftware/integration/hub/alert/web/AuthenticationHandler.java
@@ -34,7 +34,6 @@ public class AuthenticationHandler extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests().anyRequest().permitAll().and().logout().logoutSuccessUrl("/");
-        // http.authorizeRequests().antMatchers("/configuration/**", "/api/**", "/verify").authenticated();
     }
 
 }
