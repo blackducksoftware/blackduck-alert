a968d900cf76364a2d903226ce802dad64a8b51a
Gavin Killough <gkillough@blackducksoftware.com>
2017-12-01 10:03:10 -0500

    Delete unused SQL files.

--- src/main/resources/db/hipchat/create-tables.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE alert.hipchat_config (
-    id INTEGER PRIMARY KEY,
-    api_key VARCHAR,
-    room_id INTEGER,
-    notify BOOLEAN,
-    color VARCHAR
-);
--- src/main/resources/db/user/create-user-db.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE alert.hub_users (
-    id INTEGER PRIMARY KEY,
-    hub_username VARCHAR
-);
