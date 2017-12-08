acc14b50fcd6670d1e256ce4bfe883cf770e678b
Gavin Killough <gkillough@blackducksoftware.com>
2017-12-08 11:37:23 -0500

    Add filtering functionality and fix APIs broken by changes.

---
+++ src/main/java/com/blackducksoftware/integration/hub/alert/digest/filter/NotificationPostProcessor.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (C) 2017 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.blackducksoftware.integration.hub.alert.digest.filter;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.blackducksoftware.integration.hub.alert.datasource.entity.CommonDistributionConfigEntity;
+import com.blackducksoftware.integration.hub.alert.datasource.entity.ConfiguredProjectEntity;
+import com.blackducksoftware.integration.hub.alert.datasource.entity.repository.ConfiguredProjectsRepository;
+import com.blackducksoftware.integration.hub.alert.datasource.relation.DistributionProjectRelation;
+import com.blackducksoftware.integration.hub.alert.datasource.relation.repository.DistributionProjectRepository;
+import com.blackducksoftware.integration.hub.alert.digest.model.ProjectData;
+
+@Component
+public class NotificationPostProcessor {
+    private final DistributionProjectRepository distributionProjectRepository;
+    private final ConfiguredProjectsRepository configuredProjectsRepository;
+
+    @Autowired
+    public NotificationPostProcessor(final DistributionProjectRepository distributionProjectRepository, final ConfiguredProjectsRepository configuredProjectsRepository) {
+        this.distributionProjectRepository = distributionProjectRepository;
+        this.configuredProjectsRepository = configuredProjectsRepository;
+    }
+
+    public Set<CommonDistributionConfigEntity> getApplicableConfigurations(final Collection<CommonDistributionConfigEntity> distributionConfigurations, final ProjectData projectData) {
+        final Set<CommonDistributionConfigEntity> applicableConfigurations = new HashSet<>();
+        distributionConfigurations.forEach(distributionConfig -> {
+            // TODO wrap the distribution config and call isApplicable()
+            if (distributionConfig.getFilterByProject()) {
+                final Set<CommonDistributionConfigEntity> filteredConfigurations = getApplicableConfigurationsFilteredByProject(distributionConfig, projectData);
+                applicableConfigurations.addAll(filteredConfigurations);
+            } else {
+                applicableConfigurations.add(distributionConfig);
+            }
+        });
+        return applicableConfigurations;
+    }
+
+    public Set<CommonDistributionConfigEntity> getApplicableConfigurationsFilteredByProject(final CommonDistributionConfigEntity distributionEntity, final ProjectData projectData) {
+        final Set<CommonDistributionConfigEntity> applicableConfigurations = new HashSet<>();
+        final List<DistributionProjectRelation> foundRelations = distributionProjectRepository.findByDistributionConfigId(distributionEntity.getId());
+        foundRelations.forEach(relation -> {
+            final ConfiguredProjectEntity foundEntity = configuredProjectsRepository.findOne(relation.getProjectId());
+            if (foundEntity != null && foundEntity.getProjectName().equals(projectData.getProjectName())) {
+                applicableConfigurations.add(distributionEntity);
+            }
+        });
+        return applicableConfigurations;
+    }
+
+}
