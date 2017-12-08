5d33533dcae12791a802073fcb75fdac282496b2
Brian Mandel <bamandel@gmail.com>
2017-12-08 13:29:21 -0500

    Channel tests updated

---
+++ src/test/java/com/blackducksoftware/integration/hub/alert/TestProperties.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2017 Black Duck Software Inc.
+ * http://www.blackducksoftware.com/
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Black Duck Software ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Black Duck Software.
+ */
+package com.blackducksoftware.integration.hub.alert;
+
+import java.util.Properties;
+
+import org.junit.Assume;
+
+public class TestProperties {
+    private final ResourceLoader resourceLoader;
+    private Properties properties;
+
+    public TestProperties() {
+        resourceLoader = new ResourceLoader();
+    }
+
+    public Properties getProperties() {
+        if (properties == null) {
+            try {
+                properties = resourceLoader.loadProperties(ResourceLoader.DEFAULT_PROPERTIES_FILE_LOCATION);
+                if (properties.isEmpty()) {
+                    populatePropertiesFromEnv();
+                }
+            } catch (final Exception ex) {
+                System.out.println("Couldn't load " + ResourceLoader.DEFAULT_PROPERTIES_FILE_LOCATION + " file!");
+            }
+        }
+
+        return properties;
+    }
+
+    private void populatePropertiesFromEnv() {
+        for (final TestPropertyKey key : TestPropertyKey.values()) {
+            final String prop = System.getenv(key.getPropertyKey());
+            if (prop != null && !prop.isEmpty()) {
+                properties.put(key.getPropertyKey(), prop);
+            }
+        }
+    }
+
+    public String getProperty(final TestPropertyKey propertyKey) {
+        return getProperty(propertyKey.getPropertyKey());
+    }
+
+    public String getProperty(final String propertyKey) {
+        assumeTrue(propertyKey);
+        return getProperties().getProperty(propertyKey);
+    }
+
+    public void assumeTrue(final TestPropertyKey propertyKey) {
+        assumeTrue(propertyKey.getPropertyKey());
+    }
+
+    public void assumeTrue(final String propertyKey) {
+        Assume.assumeTrue(getProperties().containsKey(propertyKey));
+    }
+
+    public boolean containsKey(final TestPropertyKey propertyKey) {
+        return containsKey(propertyKey.getPropertyKey());
+    }
+
+    public boolean containsKey(final String propertyKey) {
+        return getProperties().containsKey(propertyKey);
+    }
+}
--- src/test/java/com/blackducksoftware/integration/hub/alert/channel/hipchat/HipChatChannelTestIT.java
+++ src/test/java/com/blackducksoftware/integration/hub/alert/channel/hipchat/HipChatChannelTestIT.java
@@ -14,69 +14,41 @@ package com.blackducksoftware.integration.hub.alert.channel.hipchat;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
 
-import org.junit.Assume;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import com.blackducksoftware.integration.hub.alert.TestGlobalProperties;
-import com.blackducksoftware.integration.hub.alert.channel.RestChannelTest;
+import com.blackducksoftware.integration.hub.alert.TestPropertyKey;
+import com.blackducksoftware.integration.hub.alert.channel.ChannelTest;
 import com.blackducksoftware.integration.hub.alert.datasource.entity.distribution.HipChatDistributionConfigEntity;
 import com.blackducksoftware.integration.hub.alert.datasource.entity.global.GlobalHipChatConfigEntity;
 import com.blackducksoftware.integration.hub.alert.datasource.entity.repository.global.GlobalHubRepository;
-import com.blackducksoftware.integration.hub.alert.digest.DigestTypeEnum;
-import com.blackducksoftware.integration.hub.alert.digest.model.CategoryData;
-import com.blackducksoftware.integration.hub.alert.digest.model.ItemData;
 import com.blackducksoftware.integration.hub.alert.digest.model.ProjectData;
-import com.blackducksoftware.integration.hub.notification.processor.NotificationCategoryEnum;
 
-public class HipChatChannelTestIT extends RestChannelTest {
+public class HipChatChannelTestIT extends ChannelTest {
 
     @Test
     public void sendMessageTestIT() throws IOException {
-        Assume.assumeTrue(properties.containsKey("hipchat.api.key"));
-        Assume.assumeTrue(properties.containsKey("hipchat.room.id"));
-
         final GlobalHubRepository mockedGlobalRepository = Mockito.mock(GlobalHubRepository.class);
         final TestGlobalProperties globalProperties = new TestGlobalProperties(mockedGlobalRepository);
         HipChatChannel hipChatChannel = new HipChatChannel(gson, globalProperties, null, null, null);
 
-        final HashMap<NotificationCategoryEnum, CategoryData> categoryMap = new HashMap<>();
-        categoryMap.put(NotificationCategoryEnum.POLICY_VIOLATION, createMockPolicyViolation());
-        categoryMap.put(NotificationCategoryEnum.MEDIUM_VULNERABILITY, createMockVulnerability());
-
-        final ProjectData data = new ProjectData(DigestTypeEnum.REAL_TIME, "Integration Test Project Name", "Integration Test Project Version Name", categoryMap);
+        final ProjectData data = createProjectData("Integration test project");
         final HipChatEvent event = new HipChatEvent(data, null);
-        final HipChatDistributionConfigEntity config = new HipChatDistributionConfigEntity(Integer.parseInt(properties.getProperty("hipchat.room.id")), false, "random");
+        final int roomId = Integer.parseInt(properties.getProperty(TestPropertyKey.TEST_HIPCHAT_ROOM_ID));
+        final boolean notify = false;
+        final String color = "random";
+        final HipChatDistributionConfigEntity config = new HipChatDistributionConfigEntity(roomId, notify, color);
 
         hipChatChannel = Mockito.spy(hipChatChannel);
-        Mockito.doReturn(new GlobalHipChatConfigEntity(properties.getProperty("hipchat.api.key"))).when(hipChatChannel).getGlobalConfigEntity();
+        Mockito.doReturn(new GlobalHipChatConfigEntity(properties.getProperty(TestPropertyKey.TEST_HIPCHAT_API_KEY))).when(hipChatChannel).getGlobalConfigEntity();
 
         hipChatChannel.sendMessage(event, config);
 
         final String responseLine = getLineContainingText("Successfully sent a HipChat message!");
 
         assertTrue(!responseLine.isEmpty());
-    }
-
-    private CategoryData createMockPolicyViolation() {
-        final HashMap<String, Object> dataMap = new HashMap<>();
-        dataMap.put("COMPONENT", "comp");
-        dataMap.put("VERSION", "version in violation");
-        dataMap.put("RULE", "my policy rule");
-
-        return new CategoryData("POLICY_VIOLATION", Arrays.asList(new ItemData(dataMap)), 1);
-    }
-
-    private CategoryData createMockVulnerability() {
-        final HashMap<String, Object> dataMap = new HashMap<>();
-        dataMap.put("COMPONENT", "vuln comp");
-        dataMap.put("VERSION", "vuln ver");
-        dataMap.put("COUNT", 7);
-
-        return new CategoryData("MEDIUM_VULNERABILITY", Arrays.asList(new ItemData(dataMap)), 1);
     }
 
 }
