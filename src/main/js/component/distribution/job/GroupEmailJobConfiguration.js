f0a5b311dcd6aa5e28c13133515f48e0d7dc2d89
Paulo Santos <psantos@blackducksoftware.com>
2017-12-11 13:52:09 -0500

    initial change to add a property for selected groups

--- src/main/js/component/distribution/job/GroupEmailJobConfiguration.js
+++ src/main/js/component/distribution/job/GroupEmailJobConfiguration.js
@@ -18,30 +18,44 @@ export default class GroupEmailJobConfiguration extends BaseJobConfiguration {
 		super.handleStateValues('groupValue', optionsList);
 	}
 
-	render() {		
-		var groupOptions= new Array();
-
-		if (this.props.groups != null && this.props.groups != undefined && this.props.groups.length > 0) {
-			var rawGroups = this.props.groups;
+    initializeValues() {
+        super.initializeValues();
+        const { groups, selectedGroups } = this.props;
+        let groupOptions= new Array();
+        if (groups != null && groups != undefined && groups.length > 0) {
+			var rawGroups = groups;
 			for (var index in rawGroups) {
 				groupOptions.push({
 					label: rawGroups[index].name,
 					id: rawGroups[index].url
 				});
 			}
+            
 		}
+        this.state.groupOptions = groupOptions;
+        this.state.groupValue = groups;
+    }
+
+	render() {
+        const { groupOptions } = this.state;
+        let options;
+        if(groupOptions) {
+            options = groupOptions;
+        } else {
+            options = new Array();
+        }
 
 		let errorDiv = null;
 		if (this.props.groupError) {
 			errorDiv = <p className={fieldError} name="groupError">{this.props.groupError}</p>;
 		}
-		let content = 
+		let content =
 					<div>
 						<label className={fieldLabel}>Group</label>
 						<Typeahead className={typeAheadField}
 							onChange={this.handleGroupsChanged}
 						    clearButton
-						    options={groupOptions}
+						    options={options}
 						    placeholder='Choose the Hub user group'
 						    selected={this.state.groupValue}
 						  />
