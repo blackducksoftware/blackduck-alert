0231c334519712973c78872fff9bcbdc0d0dd26b
jrichard <jrichard@blackducksoftware.com>
2017-12-12 12:45:46 -0500

    Adding the include all project checkbox

--- src/main/js/component/distribution/JobAddModal.js
+++ src/main/js/component/distribution/JobAddModal.js
@@ -85,11 +85,11 @@ export default class JobAddModal extends Component {
 		if (typeValues != null && typeValues.length > 0) {
 			var type = typeValues[0];
 			if (type.id === 'Group Email') {
-				currentJobConfig = <GroupEmailJobConfiguration buttonsFixed={false} waitingForGroups={this.props.waitingForGroups} groups={this.props.groups}  waitingForProjects={this.props.waitingForProjects} projects={this.props.projects} handleCancel={this.props.onModalClose} groupError={this.props.groupError} projectTableMessage={this.props.projectTableMessage} />;
+				currentJobConfig = <GroupEmailJobConfiguration buttonsFixed={false} includeAllProjects={this.props.includeAllProjects} waitingForGroups={this.props.waitingForGroups} groups={this.props.groups}  waitingForProjects={this.props.waitingForProjects} projects={this.props.projects} handleCancel={this.props.onModalClose} groupError={this.props.groupError} projectTableMessage={this.props.projectTableMessage} />;
 			} else if (type.id === 'HipChat') {
-				currentJobConfig = <HipChatJobConfiguration buttonsFixed={false} waitingForProjects={this.props.waitingForProjects} projects={this.props.projects} handleCancel={this.props.onModalClose} projectTableMessage={this.props.projectTableMessage} />;
+				currentJobConfig = <HipChatJobConfiguration buttonsFixed={false} includeAllProjects={this.props.includeAllProjects} waitingForProjects={this.props.waitingForProjects} projects={this.props.projects} handleCancel={this.props.onModalClose} projectTableMessage={this.props.projectTableMessage} />;
 			} else if (type.id === 'Slack') {
-				currentJobConfig = <SlackJobConfiguration buttonsFixed={false} waitingForProjects={this.props.waitingForProjects} projects={this.props.projects} handleCancel={this.props.onModalClose} projectTableMessage={this.props.projectTableMessage} />;
+				currentJobConfig = <SlackJobConfiguration buttonsFixed={false} includeAllProjects={this.props.includeAllProjects} waitingForProjects={this.props.waitingForProjects} projects={this.props.projects} handleCancel={this.props.onModalClose} projectTableMessage={this.props.projectTableMessage} />;
 			}
 		}
 		return currentJobConfig;
