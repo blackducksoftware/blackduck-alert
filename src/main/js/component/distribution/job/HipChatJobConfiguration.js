fdd9c625a1f7f8ac3d5165dd1bca4c2136ab2a24
jrichard <jrichard@blackducksoftware.com>
2017-12-12 15:33:42 -0500

    Updating the Hipchat configurations

--- src/main/js/component/distribution/job/HipChatJobConfiguration.js
+++ src/main/js/component/distribution/job/HipChatJobConfiguration.js
@@ -2,6 +2,7 @@
 import React from 'react';
 
 import TextInput from '../../../field/input/TextInput';
+import CheckboxInput from '../../../field/input/CheckboxInput';
 
 import BaseJobConfiguration from './BaseJobConfiguration';
 
@@ -11,8 +12,12 @@ export default class HipChatJobConfiguration extends BaseJobConfiguration {
 	}
 
 	render() {
-		let content = 
-			<TextInput label="Room Name" name="roomName" value={this.props.roomName} onChange={this.props.handleRoomNameChange} errorName="roomNameError" errorValue={this.props.roomNameError}></TextInput>
+		let content = <div>
+							<TextInput label="Room Name" name="roomName" value={this.props.roomName} onChange={this.handleChange} errorName="roomNameError" errorValue={this.props.roomNameError}></TextInput>
+							<CheckboxInput label="Notify" name="notify" value={this.props.notify} onChange={this.handleChange} errorName="notifyError" errorValue={this.props.notifyError}></CheckboxInput>
+							<TextInput label="Color" name="color" value={this.props.color} onChange={this.handleChange} errorName="colorError" errorValue={this.props.colorError}></TextInput>
+						</div>;
+		
 		return super.render(content);
 	}
 }
