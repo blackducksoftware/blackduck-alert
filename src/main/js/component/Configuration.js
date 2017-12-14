e55831828fe0144aab13f9ac731cb937cdd800fe
jrichard <jrichard@blackducksoftware.com>
2017-12-14 13:32:21 -0500

    Fixing job saving and loading

--- src/main/js/component/Configuration.js
+++ src/main/js/component/Configuration.js
@@ -101,7 +101,7 @@ export default class Configuration extends Component {
 			});
 			if (response.ok) {
 				return response.json().then(json => {
-					var values = {};
+					var values = self.state.values;
 					values.id = json.id;
 					self.setState({
 						values,
