9ff95d0acbcfcad636f9fa59897f1134e2e84a8a
Gavin Killough <gkillough@blackducksoftware.com>
2018-01-11 11:08:03 -0500

    Add serial version UID tests in entities/relations that were missing them.

---
+++ src/test/java/com/blackducksoftware/integration/hub/alert/datasource/relation/DistributionProjectRelationTest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2018 Black Duck Software Inc.
+ * http://www.blackducksoftware.com/
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Black Duck Software ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Black Duck Software.
+ */
+package com.blackducksoftware.integration.hub.alert.datasource.relation;
+
+import org.junit.Test;
+
+public class DistributionProjectRelationTest {
+    @Test
+    public void getSerialversionuidTest() {
+        DistributionProjectRelation.getSerialversionuid();
+    }
+
+}
