54ad51e02bb646acf3074f167484a793c725f748
Brian Mandel <bmandel@blackducksoftware.com>
2018-01-22 17:00:57 -0500

    Fix for okhttp leaked connection

--- src/main/java/com/blackducksoftware/integration/hub/alert/channel/hipchat/HipChatChannel.java
+++ src/main/java/com/blackducksoftware/integration/hub/alert/channel/hipchat/HipChatChannel.java
@@ -41,6 +41,7 @@ import com.blackducksoftware.integration.hub.alert.AlertConstants;
 import com.blackducksoftware.integration.hub.alert.audit.repository.AuditEntryRepositoryWrapper;
 import com.blackducksoftware.integration.hub.alert.channel.ChannelFreemarkerTemplatingService;
 import com.blackducksoftware.integration.hub.alert.channel.ChannelRestConnectionFactory;
+import com.blackducksoftware.integration.hub.alert.channel.ChannelRestFactory;
 import com.blackducksoftware.integration.hub.alert.channel.DistributionChannel;
 import com.blackducksoftware.integration.hub.alert.channel.SupportedChannels;
 import com.blackducksoftware.integration.hub.alert.channel.hipchat.repository.distribution.HipChatDistributionConfigEntity;
@@ -56,10 +57,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 import freemarker.template.TemplateException;
-import okhttp3.HttpUrl;
 import okhttp3.Request;
-import okhttp3.RequestBody;
-import okhttp3.Response;
 
 @Component
 public class HipChatChannel extends DistributionChannel<HipChatEvent, GlobalHipChatConfigEntity, HipChatDistributionConfigEntity> {
@@ -105,27 +103,22 @@ public class HipChatChannel extends DistributionChannel<HipChatEvent, GlobalHipC
         final RestConnection connection = restConnectionFactory.createUnauthenticatedRestConnection(apiUrl);
         if (connection != null) {
             final String jsonString = getJsonString(message, senderName, config.getNotify(), config.getColor());
-            final RequestBody body = connection.createJsonRequestBody(jsonString);
 
             final List<String> urlSegments = Arrays.asList("v2", "room", config.getRoomId().toString(), "notification");
-            final HttpUrl httpUrl = connection.createHttpUrl(urlSegments);
 
-            final Map<String, String> map = new HashMap<>();
-            map.put("Authorization", "Bearer " + getGlobalConfigEntity().getApiKey());
-            map.put("Content-Type", "application/json");
+            final Map<String, String> requestProperties = new HashMap<>();
+            requestProperties.put("Authorization", "Bearer " + getGlobalConfigEntity().getApiKey());
+            requestProperties.put("Content-Type", "application/json");
 
-            final Request request = connection.createPostRequest(httpUrl, map, body);
+            final ChannelRestFactory channelRestFactory = new ChannelRestFactory(connection, logger);
+            final Request request = channelRestFactory.createRequest(urlSegments, jsonString, requestProperties);
             try {
-                logger.info("Attempting to send a HipChat message...");
-                final Response response = connection.createResponse(request);
-                logger.info("Successfully sent a HipChat message!");
-                if (logger.isTraceEnabled()) {
-                    logger.trace("Response: " + response.toString());
-                }
-                return Integer.toString(response.code());
+                channelRestFactory.createResponse(request);
             } catch (final IntegrationException e) {
-                throw new IntegrationRestException(HttpStatus.BAD_REQUEST.value(), "Failed to send a HipChat message", e.getMessage(), e);
+                return e.getMessage();
             }
+
+            return "Succesfully sent HipChat message!";
         } else {
             throw new IntegrationRestException(HttpStatus.INTERNAL_SERVER_ERROR.value(), "No message will be sent because a connection was not established.", "No message will be sent because a connection was not established.");
         }
