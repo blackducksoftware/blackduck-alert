d120ccea252f232e95e60ba654c296237ae3fdae
Brian Mandel <bmandel@blackducksoftware.com>
2018-01-22 16:04:22 -0500

    Added more tests for channels

---
+++ src/test/java/com/blackducksoftware/integration/hub/alert/channel/SuppoertedChannelsTest.java
@@ -0,0 +1,16 @@
+package com.blackducksoftware.integration.hub.alert.channel;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SuppoertedChannelsTest {
+
+    @Test
+    public void testSupportedChannels() {
+        assertEquals("email_group_channel", SupportedChannels.EMAIL_GROUP);
+        assertEquals("hipchat_channel", SupportedChannels.HIPCHAT);
+        assertEquals("slack_channel", SupportedChannels.SLACK);
+    }
+
+}
