28f988f6164182b83ed456ae76f150bd7847c34a
Brian Mandel <bmandel@blackducksoftware.com>
2018-01-25 15:23:46 -0500

    Added more channel tests

---
+++ src/test/java/com/blackducksoftware/integration/hub/alert/channel/ChannelRestFactoryTest.java
@@ -0,0 +1,55 @@
+package com.blackducksoftware.integration.hub.alert.channel;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.blackducksoftware.integration.hub.alert.OutputLogger;
+import com.blackducksoftware.integration.hub.alert.TestGlobalProperties;
+import com.blackducksoftware.integration.hub.alert.config.GlobalProperties;
+import com.blackducksoftware.integration.hub.rest.exception.IntegrationRestException;
+
+import okhttp3.Request;
+
+public class ChannelRestFactoryTest {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private OutputLogger outputLogger;
+
+    @Before
+    public void init() throws IOException {
+        outputLogger = new OutputLogger();
+    }
+
+    @After
+    public void cleanup() throws IOException {
+        outputLogger.cleanup();
+    }
+
+    @Test
+    public void testResponseException() throws IntegrationRestException, IOException {
+        final ChannelRestFactory channelRestFactory = new ChannelRestFactory("https:url", new TestGlobalProperties(), logger);
+        final Request request = channelRestFactory.createRequest("https:url", "", new HashMap<>());
+        channelRestFactory.sendRequest(request);
+
+        assertTrue(outputLogger.isLineContainingText("There was a problem sending request"));
+    }
+
+    @Test
+    public void testResponseException2() throws IntegrationRestException, IOException {
+        final GlobalProperties globalProperties = Mockito.mock(GlobalProperties.class);
+        final ChannelRestFactory channelRestFactory = new ChannelRestFactory("https:url", globalProperties, logger);
+        final Request request = channelRestFactory.createRequest("https:url", "", new HashMap<>());
+        channelRestFactory.sendRequest(request);
+
+        assertTrue(outputLogger.isLineContainingText("There was a problem sending request"));
+    }
+}
--- src/test/java/com/blackducksoftware/integration/hub/alert/channel/SupportedChannelsTest.java
+++ src/test/java/com/blackducksoftware/integration/hub/alert/channel/SupportedChannelsTest.java
@@ -4,13 +4,14 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class SupportedChannelsTest {
+// Extends SupportedChannels so that it counts towards the code coverage
+public class SupportedChannelsTest extends SupportedChannels {
 
     @Test
     public void testSupportedChannels() {
-        assertEquals("email_group_channel", SupportedChannels.EMAIL_GROUP);
-        assertEquals("hipchat_channel", SupportedChannels.HIPCHAT);
-        assertEquals("slack_channel", SupportedChannels.SLACK);
+        assertEquals("email_group_channel", EMAIL_GROUP);
+        assertEquals("hipchat_channel", HIPCHAT);
+        assertEquals("slack_channel", SLACK);
     }
 
 }
