6c7632a17643da1ccacb7ded9a21a95797e6bc73
Gavin Killough <gkillough@blackducksoftware.com>
2018-02-06 16:29:20 -0500

    Change '/' to be a GET mapping.

--- src/main/java/com/blackducksoftware/integration/hub/alert/web/controller/HomeController.java
+++ src/main/java/com/blackducksoftware/integration/hub/alert/web/controller/HomeController.java
@@ -28,12 +28,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = { "/" })
+    @GetMapping(value = { "/" })
     public String index() {
         return "index";
     }
