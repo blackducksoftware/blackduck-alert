c5c320550dd605db1c70d4d52a125942502e0e63
Gavin Killough <gkillough@blackducksoftware.com>
2018-02-06 09:40:26 -0500

    Add test button to global HipChat config.

--- src/main/js/component/server/HipChatConfiguration.js
+++ src/main/js/component/server/HipChatConfiguration.js
@@ -32,8 +32,9 @@ HipChatConfiguration.propTypes = {
 
 HipChatConfiguration.defaultProps = {
     headerText: 'HipChat Configuration',
-    configButtonTest: false,
-    baseUrl: '/configuration/global/hipchat'
+    configButtonTest: true,
+    baseUrl: '/configuration/global/hipchat',
+    testUrl: '/configuration/global/hipchat/test'
 };
 
 export default HipChatConfiguration;
