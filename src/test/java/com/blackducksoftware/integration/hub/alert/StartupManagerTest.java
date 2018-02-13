b5611d7b01067f1e32ad6a66fb8d16b97feb2433
Brian Mandel <bmandel@blackducksoftware.com>
2018-02-13 18:23:39 -0500

    Additional tests

---
+++ src/test/java/com/blackducksoftware/integration/hub/alert/StartupManagerTest.java
@@ -0,0 +1,67 @@
+package com.blackducksoftware.integration.hub.alert;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.blackducksoftware.integration.hub.alert.config.AccumulatorConfig;
+import com.blackducksoftware.integration.hub.alert.config.DailyDigestBatchConfig;
+import com.blackducksoftware.integration.hub.alert.config.PurgeConfig;
+import com.blackducksoftware.integration.hub.alert.scheduling.mock.MockGlobalSchedulingEntity;
+import com.blackducksoftware.integration.hub.alert.scheduling.repository.global.GlobalSchedulingConfigEntity;
+import com.blackducksoftware.integration.hub.alert.scheduling.repository.global.GlobalSchedulingRepositoryWrapper;
+
+public class StartupManagerTest {
+    private OutputLogger outputLogger;
+
+    @Before
+    public void init() throws IOException {
+        outputLogger = new OutputLogger();
+    }
+
+    @After
+    public void cleanup() throws IOException {
+        if (outputLogger != null) {
+            outputLogger.cleanup();
+        }
+    }
+
+    @Test
+    public void testLogConfiguration() throws IOException {
+        final TestGlobalProperties testGlobalProperties = new TestGlobalProperties();
+        final TestGlobalProperties mockTestGlobalProperties = Mockito.spy(testGlobalProperties);
+        // Mockito.doReturn(null).when(mockTestGlobalProperties).getHubConfig();
+        final StartupManager startupManager = new StartupManager(null, mockTestGlobalProperties, null, null, null);
+
+        startupManager.logConfiguration();
+        assertTrue(outputLogger.isLineContainingText("Hub API Key:        **********"));
+    }
+
+    @Test
+    public void testInitializeCronJobs() throws IOException {
+        final AccumulatorConfig accumulatorConfig = Mockito.mock(AccumulatorConfig.class);
+        Mockito.doNothing().when(accumulatorConfig).scheduleJobExecution(Mockito.anyString());
+        Mockito.doReturn(1L).when(accumulatorConfig).getMillisecondsToNextRun();
+        final DailyDigestBatchConfig dailyDigestBatchConfig = Mockito.mock(DailyDigestBatchConfig.class);
+        Mockito.doNothing().when(dailyDigestBatchConfig).scheduleJobExecution(Mockito.anyString());
+        Mockito.doReturn("time").when(dailyDigestBatchConfig).getFormatedNextRunTime();
+        final PurgeConfig purgeConfig = Mockito.mock(PurgeConfig.class);
+        Mockito.doNothing().when(purgeConfig).scheduleJobExecution(Mockito.anyString());
+        Mockito.doReturn("time").when(purgeConfig).getFormatedNextRunTime();
+        final GlobalSchedulingRepositoryWrapper globalSchedulingRepositoryWrapper = Mockito.mock(GlobalSchedulingRepositoryWrapper.class);
+        final MockGlobalSchedulingEntity mockGlobalSchedulingEntity = new MockGlobalSchedulingEntity();
+        final GlobalSchedulingConfigEntity entity = mockGlobalSchedulingEntity.createGlobalEntity();
+        Mockito.when(globalSchedulingRepositoryWrapper.save(Mockito.any(GlobalSchedulingConfigEntity.class))).thenReturn(entity);
+        final StartupManager startupManager = new StartupManager(globalSchedulingRepositoryWrapper, null, accumulatorConfig, dailyDigestBatchConfig, purgeConfig);
+
+        startupManager.initializeCronJobs();
+
+        final String expectedLog = entity.toString();
+        assertTrue(outputLogger.isLineContainingText(expectedLog));
+    }
+}
--- src/test/java/com/blackducksoftware/integration/hub/alert/config/AccumulatorConfigTest.java
+++ src/test/java/com/blackducksoftware/integration/hub/alert/config/AccumulatorConfigTest.java
@@ -1,6 +1,11 @@
 package com.blackducksoftware.integration.hub.alert.config;
 
+import org.mockito.Mockito;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import com.blackducksoftware.integration.hub.alert.accumulator.AccumulatorProcessor;
 import com.blackducksoftware.integration.hub.alert.accumulator.AccumulatorReader;
@@ -24,12 +29,33 @@ public class AccumulatorConfigTest extends CommonConfigTest<AccumulatorReader, A
     }
 
     @Override
-    public AccumulatorConfig getConfigWithPassedParams(final SimpleJobLauncher simpleJobLauncher) {
+    public AccumulatorConfig getConfigWithSimpleJobLauncher(final SimpleJobLauncher simpleJobLauncher) {
         return new AccumulatorConfig(simpleJobLauncher, null, null, null, null, null, null, null, null);
     }
 
     @Override
-    public void testCreateStep() {
+    public AccumulatorConfig getConfigWithTaskScheduler(final TaskScheduler taskScheduler) {
+        return new AccumulatorConfig(null, null, null, null, null, null, null, taskScheduler, null);
+    }
+
+    @Override
+    public AccumulatorConfig getConfigWithParams(final StepBuilderFactory stepBuilderFactory, final TaskExecutor taskExecutor, final PlatformTransactionManager platformTransactionManager) {
+        return new AccumulatorConfig(null, null, stepBuilderFactory, taskExecutor, null, platformTransactionManager, null, null, null);
+    }
+
+    @Override
+    public AccumulatorReader getMockReader() {
+        return Mockito.mock(AccumulatorReader.class);
+    }
+
+    @Override
+    public AccumulatorProcessor getMockProcessor() {
+        return Mockito.mock(AccumulatorProcessor.class);
+    }
+
+    @Override
+    public AccumulatorWriter getMockWriter() {
+        return Mockito.mock(AccumulatorWriter.class);
     }
 
 }
