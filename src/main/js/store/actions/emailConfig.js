cc11d2554bfd0a17f0813ac43a618e78549cd9a7
Paulo Santos <psantos@blackducksoftware.com>
2018-02-27 07:11:43 -0500

    Fix the email config to save the data to the server.

--- src/main/js/store/actions/emailConfig.js
+++ src/main/js/store/actions/emailConfig.js
@@ -9,6 +9,27 @@ import {
 
 const CONFIG_URL = '/api/configuration/global/email';
 
+function scrubConfig(config) {
+    return {
+        mailSmtpHost: config.mailSmtpHost,
+        mailSmtpFrom: config.mailSmtpFrom,
+        mailSmtpAuth: config.mailSmtpAuth,
+        mailSmtpUser: config.mailSmtpUser,
+        mailSmtpPassword: config.mailSmtpPassword,
+        mailSmtpPasswordIsSet: config.mailSmtpPasswordIsSet,
+        mailSmtpPort: config.mailSmtpPort,
+        mailSmtpConnectionTimeout: config.mailSmtpConnectionTimeout,
+        mailSmtpTimeout: config.mailSmtpTimeout,
+        mailSmtpLocalhost: config.mailSmtpLocalhost,
+        mailSmtpEhlo: config.mailSmtpEhlo,
+        mailSmtpDnsNotify: config.mailSmtpDnsNotify,
+        mailSmtpDnsRet: config.mailSmtpDnsRet,
+        mailSmtpAllow8bitmime: config.mailSmtpAllow8bitmime,
+        mailSmtpSendPartial: config.mailSmtpSendPartial,
+        id: config.id
+    };
+}
+
 /**
  * Triggers Email Config Fetching reducer
  * @returns {{type}}
@@ -75,13 +96,34 @@ export function getEmailConfig() {
 export function updateEmailConfig(config) {
     return (dispatch) => {
         dispatch(updatingEmailConfig());
-
+        const method = config.id ? 'PUT' : 'POST';
+        const body = scrubConfig(config);
         fetch(CONFIG_URL, {
-            method: 'POST',
-            credentials: 'include'
+            credentials: 'include',
+            method,
+            body: JSON.stringify(body),
+            headers: {
+                'content-type': 'application/json'
+            }
         })
-            .then((response) => response.json())
-            .then((body) => { dispatch(emailConfigUpdated(body[0])); })
-            .catch(console.error);
+        .then((response) => {
+            if(response.ok) {
+                response.json().then((body) => dispatch(emailConfigUpdated({id: body.id, ...config})));
+            } else {
+                response.json()
+                    .then((data) => {
+                        console.log('data', data.message);
+                        switch(response.status) {
+                            case 400:
+                                return dispatch(configError(data.message, data.errors));
+                            case 412:
+                                return dispatch(configError(data.message, data.errors));
+                            default:
+                                dispatch(configError(data.message, null));
+                        }
+                    });
+            }
+        })
+        .catch(console.error);
     }
 };
