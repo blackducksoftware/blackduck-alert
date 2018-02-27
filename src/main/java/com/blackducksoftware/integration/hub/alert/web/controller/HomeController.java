476b8e0565c7c998b9bb2d0a2704d0647cd32471
Paulo Santos <psantos@blackducksoftware.com>
2018-02-27 08:16:57 -0500

    Fix the path resolution from the UI to avoid the error pages

--- src/main/java/com/blackducksoftware/integration/hub/alert/web/controller/HomeController.java
+++ src/main/java/com/blackducksoftware/integration/hub/alert/web/controller/HomeController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping(value = { "/" }, produces = MediaType.TEXT_HTML_VALUE)
+    @GetMapping(value = { "/", "/channels/**", "/providers/**", "/general/**" }, produces = MediaType.TEXT_HTML_VALUE)
     public String index() {
         return "index";
     }
