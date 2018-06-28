cea698411ee328de1ea17b1c364f7593d04a8aa4
psantos1113 <psantos@blackducksoftware.com>
2018-06-28 19:45:39 -0400

    Update kubernetes deployment files

--- deployment/kubernetes/delete.sh
+++ deployment/kubernetes/delete.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-kubectl -n blackduck delete configmap blackduck-alert-config
-kubectl -n blackduck delete deployment alert
-kubectl -n blackduck delete service alert
+kubectl -n blackduck-alert delete configmap blackduck-alert-config
+kubectl -n blackduck-alert delete deployment alert
+kubectl -n blackduck-alert delete service alert
+kubectl -n blackduck-alert delete deployment cfssl
+kubectl -n blackduck-alert delete service cfssl
+kubectl delete namespace blackduck-alert
