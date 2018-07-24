9d571586b94bb37add067c6cd2b062bca8b88560
psantos1113 <psantos@blackducksoftware.com>
2018-07-24 09:42:47 -0400

    Clean up logging and fix writing the notifications

--- src/main/java/com/blackducksoftware/integration/alert/common/accumulator/PollingAccumulator.java
+++ src/main/java/com/blackducksoftware/integration/alert/common/accumulator/PollingAccumulator.java
@@ -25,6 +25,8 @@ package com.blackducksoftware.integration.alert.common.accumulator;
 
 import java.io.File;
 import java.io.IOException;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
@@ -36,6 +38,7 @@ import org.springframework.scheduling.TaskScheduler;
 
 import com.blackducksoftware.integration.alert.common.exception.AlertException;
 import com.blackducksoftware.integration.alert.workflow.scheduled.ScheduledTask;
+import com.blackducksoftware.integration.rest.connection.RestConnection;
 
 public abstract class PollingAccumulator extends ScheduledTask implements Accumulator {
 
@@ -62,15 +65,39 @@ public abstract class PollingAccumulator extends ScheduledTask implements Accumu
 
     @Override
     public void accumulate() {
+        logger.info(createLoggerMessage("### Accumulator Starting Operation..."));
         try {
+            if (!getSearchRangeFilePath().exists()) {
+                initializeSearchRangeFile();
+            }
             final Pair<Date, Date> dateRange = createDateRange(getSearchRangeFilePath());
-            final String nextSearchStartTime = accumulate(dateRange);
-            FileUtils.write(getSearchRangeFilePath(), nextSearchStartTime, ENCODING);
+            final Date nextSearchStartTime = accumulate(dateRange);
+            final String nextSearchStartString = formatDate(nextSearchStartTime);
+            logger.info(createLoggerMessage("Accumulator Next Range Start Time: {} "), nextSearchStartString);
+            FileUtils.write(getSearchRangeFilePath(), nextSearchStartString, ENCODING);
         } catch (final IOException | AlertException ex) {
-            logger.error("Error occurred accumulating data! ", ex);
+            logger.error(createLoggerMessage("Error occurred accumulating data! "), ex);
+        } finally {
+            final Long seconds = TimeUnit.MILLISECONDS.toSeconds(getMillisecondsToNextRun());
+            logger.debug(createLoggerMessage("Accumulator next run: {} seconds"), seconds);
+            logger.info(createLoggerMessage("### Accumulator Finished Operation."));
         }
-        final Long seconds = TimeUnit.MILLISECONDS.toSeconds(getMillisecondsToNextRun());
-        logger.debug("Accumulator next run: {} seconds", seconds);
+    }
+
+    public void initializeSearchRangeFile() throws IOException {
+        ZonedDateTime zonedDate = ZonedDateTime.now();
+        zonedDate = zonedDate.withZoneSameInstant(ZoneOffset.UTC);
+        zonedDate = zonedDate.withSecond(0).withNano(0);
+        final Date date = Date.from(zonedDate.toInstant());
+        FileUtils.write(getSearchRangeFilePath(), formatDate(date), ENCODING);
+    }
+
+    public String formatDate(final Date date) {
+        return RestConnection.formatDate(date);
+    }
+
+    public String createLoggerMessage(final String messageFormat) {
+        return String.format("[ %s ] %s", getName(), messageFormat);
     }
 
     @Override
@@ -94,7 +121,7 @@ public abstract class PollingAccumulator extends ScheduledTask implements Accumu
 
     protected abstract Pair<Date, Date> createDateRange(final File lastRunFile) throws AlertException;
 
-    protected abstract String accumulate(Pair<Date, Date> dateRange) throws AlertException;
+    protected abstract Date accumulate(Pair<Date, Date> dateRange) throws AlertException;
 
 }
 
