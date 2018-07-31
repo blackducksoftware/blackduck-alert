9c150cdb161c665d17df2d46f893896fdb3585c2
jrichard <jrichard@synopsys.com>
2018-07-31 16:22:28 -0400

    Auto refresh update

---
+++ src/main/js/store/reducers/refresh.js
@@ -0,0 +1,27 @@
+import {REFRESH_DISABLE, REFRESH_ENABLE, SERIALIZE} from '../actions/types';
+
+const initialState = {
+    autoRefresh: false
+};
+
+const refresh = (state = initialState, action) => {
+    switch (action.type) {
+        case REFRESH_DISABLE:
+            return Object.assign({}, state, {
+                autoRefresh: false
+            });
+
+        case REFRESH_ENABLE:
+            return Object.assign({}, state, {
+                autoRefresh: true
+            });
+
+        case SERIALIZE:
+            return initialState;
+
+        default:
+            return state;
+    }
+};
+
+export default refresh;
