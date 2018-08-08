db2b42a87fd6c89c2a4ea1ccf75781e32bb63841
psantos1113 <psantos@blackducksoftware.com>
2018-08-08 10:38:50 -0400

    Get the channel and provider names into the about model

--- src/test/java/com/blackducksoftware/integration/alert/common/model/AboutModelTest.java
+++ src/test/java/com/blackducksoftware/integration/alert/common/model/AboutModelTest.java
@@ -2,6 +2,9 @@ package com.blackducksoftware.integration.alert.common.model;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
 import com.blackducksoftware.integration.alert.web.model.AboutModel;
@@ -13,11 +16,15 @@ public class AboutModelTest {
         final String version = "1.2.3";
         final String description = "description";
         final String gitHubUrl = "https://www.google.com";
+        final List<String> channelList = Arrays.asList("channel_1", "channel_2", "channel_3");
+        final List<String> providerList = Arrays.asList("provider_1", "provider_2", "provider_3");
 
-        final AboutModel model = new AboutModel(version, description, gitHubUrl);
+        final AboutModel model = new AboutModel(version, description, gitHubUrl, providerList, channelList);
 
         assertEquals(version, model.getVersion());
         assertEquals(description, model.getDescription());
         assertEquals(gitHubUrl, model.getProjectUrl());
+        assertEquals(providerList, model.getProviderList());
+        assertEquals(channelList, model.getChannelList());
     }
 }
