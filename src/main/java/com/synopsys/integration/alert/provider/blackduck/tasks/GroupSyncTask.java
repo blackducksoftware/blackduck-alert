abbf48f7dad7e8bedcdeaf3658d244a006dfdfe0
jrichard <jrichard@synopsys.com>
2018-09-05 14:10:30 -0400

    Adding the relations between Users and groups and Users and Projects

--- src/main/java/com/synopsys/integration/alert/provider/blackduck/tasks/GroupSyncTask.java
+++ src/main/java/com/synopsys/integration/alert/provider/blackduck/tasks/GroupSyncTask.java
@@ -25,10 +25,13 @@ package com.synopsys.integration.alert.provider.blackduck.tasks;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,48 +41,45 @@ import org.springframework.stereotype.Component;
 import com.synopsys.integration.alert.database.entity.DatabaseEntity;
 import com.synopsys.integration.alert.database.provider.blackduck.data.BlackDuckGroupEntity;
 import com.synopsys.integration.alert.database.provider.blackduck.data.BlackDuckGroupRepositoryAccessor;
+import com.synopsys.integration.alert.database.provider.blackduck.data.BlackDuckUserEntity;
+import com.synopsys.integration.alert.database.provider.blackduck.data.BlackDuckUserRepositoryAccessor;
 import com.synopsys.integration.alert.database.provider.blackduck.data.relation.UserGroupRelationRepositoryAccessor;
 import com.synopsys.integration.alert.provider.blackduck.BlackDuckProperties;
 import com.synopsys.integration.alert.provider.blackduck.tasks.model.GroupData;
+import com.synopsys.integration.blackduck.api.core.HubView;
 import com.synopsys.integration.blackduck.api.generated.discovery.ApiDiscovery;
 import com.synopsys.integration.blackduck.api.generated.view.UserGroupView;
-import com.synopsys.integration.blackduck.rest.BlackduckRestConnection;
+import com.synopsys.integration.blackduck.api.generated.view.UserView;
 import com.synopsys.integration.blackduck.service.HubService;
-import com.synopsys.integration.blackduck.service.HubServicesFactory;
 import com.synopsys.integration.exception.IntegrationException;
-import com.synopsys.integration.log.Slf4jIntLogger;
 
 @Component
 public class GroupSyncTask extends SyncTask<GroupData> {
     private final Logger logger = LoggerFactory.getLogger(GroupSyncTask.class);
-    private final BlackDuckProperties blackDuckProperties;
+    private final BlackDuckUserRepositoryAccessor blackDuckUserRepositoryAccessor;
     private final BlackDuckGroupRepositoryAccessor blackDuckGroupRepositoryAccessor;
     private final UserGroupRelationRepositoryAccessor userGroupRelationRepositoryAccessor;
 
     @Autowired
-    public GroupSyncTask(final TaskScheduler taskScheduler, final BlackDuckProperties blackDuckProperties, final BlackDuckGroupRepositoryAccessor blackDuckGroupRepositoryAccessor,
+    public GroupSyncTask(final TaskScheduler taskScheduler, final BlackDuckProperties blackDuckProperties, final BlackDuckUserRepositoryAccessor blackDuckUserRepositoryAccessor,
+        final BlackDuckGroupRepositoryAccessor blackDuckGroupRepositoryAccessor,
         final UserGroupRelationRepositoryAccessor userGroupRelationRepositoryAccessor) {
-        super(taskScheduler, "blackduck-sync-group-task");
-        this.blackDuckProperties = blackDuckProperties;
+        super(taskScheduler, "blackduck-sync-group-task", blackDuckProperties);
+        this.blackDuckUserRepositoryAccessor = blackDuckUserRepositoryAccessor;
         this.blackDuckGroupRepositoryAccessor = blackDuckGroupRepositoryAccessor;
         this.userGroupRelationRepositoryAccessor = userGroupRelationRepositoryAccessor;
     }
 
     @Override
-    public Set<GroupData> getCurrentData() throws IOException, IntegrationException {
-        final Optional<BlackduckRestConnection> optionalConnection = blackDuckProperties.createRestConnectionAndLogErrors(logger);
-        if (optionalConnection.isPresent()) {
-            try (final BlackduckRestConnection restConnection = optionalConnection.get()) {
-                if (restConnection != null) {
-                    final HubServicesFactory hubServicesFactory = blackDuckProperties.createBlackDuckServicesFactory(restConnection, new Slf4jIntLogger(logger));
-                    final HubService hubService = hubServicesFactory.createHubService();
-                    final List<UserGroupView> groupResponses = hubService.getAllResponses(ApiDiscovery.USERGROUPS_LINK_RESPONSE);
-                    final Set<GroupData> groups = groupResponses.stream().map(groupView -> new GroupData(groupView.name, groupView.active, groupView._meta.href)).collect(Collectors.toSet());
-                    return groups;
-                }
-            }
-        }
-        return null;
+    public List<UserGroupView> getHubViews(final HubService hubService) throws IntegrationException {
+        return hubService.getAllResponses(ApiDiscovery.USERGROUPS_LINK_RESPONSE);
+    }
+
+    @Override
+    public Map<GroupData, ? extends HubView> getCurrentData(final List<? extends HubView> hubViews) {
+        final List<UserGroupView> userGroupViews = (List<UserGroupView>) hubViews;
+        final Map<GroupData, ? extends HubView> groupMap = userGroupViews.stream().collect(Collectors.toMap(groupView -> new GroupData(groupView.name, groupView.active, groupView._meta.href), Function.identity()));
+        return groupMap;
     }
 
     @Override
@@ -97,19 +97,18 @@ public class GroupSyncTask extends SyncTask<GroupData> {
     }
 
     @Override
-    public List<Long> getEntityIdsToRemove(final List<? extends DatabaseEntity> storedEntities, final Set<GroupData> dataToRemove) {
+    public List<BlackDuckGroupEntity> getEntitiesToRemove(final List<? extends DatabaseEntity> storedEntities, final Set<GroupData> dataToRemove) {
         final List<BlackDuckGroupEntity> blackDuckGroupEntities = (List<BlackDuckGroupEntity>) storedEntities;
-        final List<Long> blackDuckGroupIdsToRemove = blackDuckGroupEntities.stream()
-                                                         .filter(blackDuckGroupEntity -> {
-                                                                 Optional<GroupData> found = dataToRemove.stream()
-                                                                                                 .filter(data -> data.getName().equals(blackDuckGroupEntity.getName()))
-                                                                                                 .findFirst();
-                                                                 return found.isPresent();
-                                                             }
-                                                         )
-                                                         .map(blackDuckGroupEntity -> blackDuckGroupEntity.getId())
-                                                         .collect(Collectors.toList());
-        return blackDuckGroupIdsToRemove;
+        final List<BlackDuckGroupEntity> blackDuckGroupsToRemove = blackDuckGroupEntities.stream()
+                                                                       .filter(blackDuckGroupEntity -> {
+                                                                               Optional<GroupData> found = dataToRemove.stream()
+                                                                                                               .filter(data -> data.getName().equals(blackDuckGroupEntity.getName()))
+                                                                                                               .findFirst();
+                                                                               return found.isPresent();
+                                                                           }
+                                                                       )
+                                                                       .collect(Collectors.toList());
+        return blackDuckGroupsToRemove;
     }
 
     @Override
@@ -122,6 +121,34 @@ public class GroupSyncTask extends SyncTask<GroupData> {
     public DatabaseEntity createAndSaveEntity(final GroupData data) {
         return blackDuckGroupRepositoryAccessor.saveEntity(new BlackDuckGroupEntity(data.getName(), data.getActive(), data.getHref()));
     }
+
+    @Override
+    public void addRelations(final Map<GroupData, ? extends HubView> currentDataMap, final List<? extends DatabaseEntity> storedEntities, final HubService hubService) throws IOException, IntegrationException {
+        final List<BlackDuckGroupEntity> blackDuckGroupEntities = (List<BlackDuckGroupEntity>) storedEntities;
+
+        for (final Map.Entry<GroupData, ? extends HubView> entry : currentDataMap.entrySet()) {
+            final GroupData groupData = entry.getKey();
+            final UserGroupView userGroupView = (UserGroupView) entry.getValue();
+            final Optional<BlackDuckGroupEntity> optionalBlackDuckGroupEntity = blackDuckGroupEntities.stream().filter(blackDuckGroupEntity -> blackDuckGroupEntity.getName().equals(groupData.getName())).findFirst();
+            final BlackDuckGroupEntity groupEntity = optionalBlackDuckGroupEntity.get();
+
+            final List<BlackDuckUserEntity> storedUsers = (List<BlackDuckUserEntity>) blackDuckUserRepositoryAccessor.readEntities();
+
+            final List<UserView> usersForThisGroup = hubService.getAllResponses(userGroupView, UserGroupView.USERS_LINK_RESPONSE);
+            for (final UserView userView : usersForThisGroup) {
+                if (StringUtils.isNotBlank(userView.email)) {
+                    final Optional<BlackDuckUserEntity> matchingUser = storedUsers.stream().filter(blackDuckUserEntity -> blackDuckUserEntity.getEmailAddress().equals(userView.email)).findFirst();
+                    if (matchingUser.isPresent()) {
+                        final BlackDuckUserEntity userEntity = matchingUser.get();
+                        try {
+                            userGroupRelationRepositoryAccessor.addUserGroupRelation(userEntity.getId(), groupEntity.getId());
+                        } catch (final Exception e) {
+                            logger.error("COULD NOT SAVE THIS RELATION {}", e.getMessage());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 }
-
-
