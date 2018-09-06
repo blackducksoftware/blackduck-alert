91a8c8d5de002558b336164652f7650f55d9668e
psantos1113 <psantos@blackducksoftware.com>
2018-09-06 16:00:45 -0400

    add a test for CollectorLookup

--- src/test/java/com/synopsys/integration/alert/common/workflow/processor/CollectorLookupTest.java
+++ src/test/java/com/synopsys/integration/alert/common/workflow/processor/CollectorLookupTest.java
@@ -1,4 +1,37 @@
 package com.synopsys.integration.alert.common.workflow.processor;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import com.synopsys.integration.alert.common.enumeration.FormatType;
+
 public class CollectorLookupTest {
+
+    @Test
+    public void testFindFormat() {
+        final FormatType formatType = FormatType.DIGEST;
+        final TopicCollector topicCollector = Mockito.mock(TopicCollector.class);
+        Mockito.when(topicCollector.getFormat()).thenReturn(formatType);
+        final CollectorLookup collectorLookup = new CollectorLookup(Arrays.asList(topicCollector));
+        final Optional<TopicCollector> actualTopicCollector = collectorLookup.findCollector(formatType);
+        assertTrue(actualTopicCollector.isPresent());
+        assertEquals(topicCollector, actualTopicCollector.get());
+    }
+
+    @Test
+    public void testFormatNotFound() {
+        final FormatType formatType = FormatType.DIGEST;
+        final TopicCollector topicCollector = Mockito.mock(TopicCollector.class);
+        Mockito.when(topicCollector.getFormat()).thenReturn(formatType);
+        final CollectorLookup collectorLookup = new CollectorLookup(Arrays.asList(topicCollector));
+        final Optional<TopicCollector> actualTopicCollector = collectorLookup.findCollector(FormatType.DEFAULT);
+        assertFalse(actualTopicCollector.isPresent());
+    }
 }
