59eee17b254144b4a0171e9351b18a9adf45ba4b
jrichard <jrichard@synopsys.com>
2018-09-12 13:52:33 -0400

    Updating test

--- src/test/java/com/synopsys/integration/alert/channel/email/EmailChannelTestIT.java
+++ src/test/java/com/synopsys/integration/alert/channel/email/EmailChannelTestIT.java
@@ -2,6 +2,8 @@ package com.synopsys.integration.alert.channel.email;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Date;
 
 import org.junit.Test;
@@ -13,10 +15,14 @@ import com.synopsys.integration.alert.TestAlertProperties;
 import com.synopsys.integration.alert.TestBlackDuckProperties;
 import com.synopsys.integration.alert.TestPropertyKey;
 import com.synopsys.integration.alert.channel.ChannelTest;
+import com.synopsys.integration.alert.channel.email.mock.MockEmailEntity;
 import com.synopsys.integration.alert.channel.event.ChannelEvent;
 import com.synopsys.integration.alert.common.digest.model.DigestModel;
+import com.synopsys.integration.alert.common.digest.model.ProjectData;
 import com.synopsys.integration.alert.database.audit.AuditEntryRepository;
+import com.synopsys.integration.alert.database.channel.email.EmailGlobalConfigEntity;
 import com.synopsys.integration.alert.database.entity.NotificationContent;
+import com.synopsys.integration.alert.database.provider.blackduck.GlobalBlackDuckConfigEntity;
 import com.synopsys.integration.alert.database.provider.blackduck.GlobalBlackDuckRepository;
 import com.synopsys.integration.alert.database.provider.blackduck.data.BlackDuckProjectRepositoryAccessor;
 import com.synopsys.integration.alert.database.provider.blackduck.data.BlackDuckUserRepositoryAccessor;
@@ -35,46 +41,49 @@ public class EmailChannelTestIT extends ChannelTest {
         final AuditEntryRepository auditEntryRepository = Mockito.mock(AuditEntryRepository.class);
         final GlobalBlackDuckRepository globalRepository = Mockito.mock(GlobalBlackDuckRepository.class);
 
+        final GlobalBlackDuckConfigEntity globalConfig = new GlobalBlackDuckConfigEntity(300, properties.getProperty(TestPropertyKey.TEST_BLACKDUCK_PROVIDER_API_KEY), properties.getProperty(TestPropertyKey.TEST_BLACKDUCK_PROVIDER_URL));
+        Mockito.when(globalRepository.findAll()).thenReturn(Arrays.asList(globalConfig));
+
         final TestAlertProperties testAlertProperties = new TestAlertProperties();
         final TestBlackDuckProperties globalProperties = new TestBlackDuckProperties(globalRepository, testAlertProperties);
         globalProperties.setBlackDuckUrl(properties.getProperty(TestPropertyKey.TEST_BLACKDUCK_PROVIDER_URL));
+
+        final String trustCert = properties.getProperty(TestPropertyKey.TEST_BLACKDUCK_PROVIDER_TRUST_HTTPS_CERT);
+        if (trustCert != null) {
+            testAlertProperties.setAlertTrustCertificate(Boolean.valueOf(trustCert));
+        }
 
         final BlackDuckUserRepositoryAccessor blackDuckUserRepositoryAccessor = new MockBlackDuckUserRepositoryAccessor();
         final BlackDuckProjectRepositoryAccessor blackDuckProjectRepositoryAccessor = new MockBlackDuckProjectRepositoryAccessor();
         final UserProjectRelationRepositoryAccessor userProjectRelationRepositoryAccessor = new MockUserProjectRelationRepositoryAccessor();
 
-        //TODO JR fix test
-        //        final BlackDuckUserEntity userEntity = (BlackDuckUserEntity) blackDuckUserRepositoryAccessor.saveEntity(new BlackDuckUserEntity("noreply@blackducksoftware.com", false));
-        //        final BlackDuckGroupEntity groupEntity = (BlackDuckGroupEntity) blackDuckGroupRepositoryAccessor.saveEntity(new BlackDuckGroupEntity("IntegrationTest", true, "Href"));
-        //        userGroupRelationRepositoryAccessor.addUserGroupRelation(new UserGroupRelation(userEntity.getId(), groupEntity.getId()));
-        //
-        //        EmailGroupChannel emailChannel = new EmailGroupChannel(gson, testAlertProperties, globalProperties, auditEntryRepository, null, null, null, blackDuckUserRepositoryAccessor, blackDuckGroupRepositoryAccessor,
-        //            userGroupRelationRepositoryAccessor);
-        //        final Collection<ProjectData> projectData = createProjectData("Manual test project");
-        //        final DigestModel digestModel = new DigestModel(projectData);
-        //        final NotificationContent notificationContent = new NotificationContent(new Date(), "provider", "notificationType", contentConverter.getJsonString(digestModel));
-        //        final ChannelEvent event = new ChannelEvent(EmailGroupChannel.COMPONENT_NAME, RestConstants.formatDate(notificationContent.getCreatedAt()), notificationContent.getProvider(), notificationContent.getNotificationType(),
-        //            notificationContent.getContent(), 1L, 1L);
-        //
-        //        final String smtpHost = properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_HOST);
-        //        final String smtpFrom = properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_FROM);
-        //        final String smtpUser = properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_USER);
-        //        final String smtpPassword = properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_PASSWORD);
-        //        final Boolean smtpEhlo = Boolean.valueOf(properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_EHLO));
-        //        final Boolean smtpAuth = Boolean.valueOf(properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_AUTH));
-        //        final Integer smtpPort = Integer.valueOf(properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_PORT));
-        //
-        //        final EmailGlobalConfigEntity emailGlobalConfigEntity = new EmailGlobalConfigEntity(smtpHost, smtpUser, smtpPassword, smtpPort, null, null, null, smtpFrom, null, null, null, smtpEhlo, smtpAuth, null, null, null, null, null, null,
-        //            null,
-        //            null, null, null,
-        //            null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
-        //
-        //        emailChannel = Mockito.spy(emailChannel);
-        //        Mockito.doReturn(emailGlobalConfigEntity).when(emailChannel).getGlobalConfigEntity();
-        //
-        //        final MockEmailEntity mockEmailEntity = new MockEmailEntity();
-        //        mockEmailEntity.setGroupName(properties.getProperty(TestPropertyKey.TEST_EMAIL_GROUP));
-        //        emailChannel.sendAuditedMessage(event, mockEmailEntity.createEntity());
+        EmailGroupChannel emailChannel = new EmailGroupChannel(gson, testAlertProperties, globalProperties, auditEntryRepository, null, null, null, blackDuckUserRepositoryAccessor, blackDuckProjectRepositoryAccessor,
+            userProjectRelationRepositoryAccessor);
+        final Collection<ProjectData> projectData = createProjectData("Manual test project");
+        final DigestModel digestModel = new DigestModel(projectData);
+        final NotificationContent notificationContent = new NotificationContent(new Date(), "provider", "notificationType", contentConverter.getJsonString(digestModel));
+        final ChannelEvent event = new ChannelEvent(EmailGroupChannel.COMPONENT_NAME, RestConstants.formatDate(notificationContent.getCreatedAt()), notificationContent.getProvider(), notificationContent.getNotificationType(),
+            notificationContent.getContent(), 1L, 1L);
+
+        final String smtpHost = properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_HOST);
+        final String smtpFrom = properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_FROM);
+        final String smtpUser = properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_USER);
+        final String smtpPassword = properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_PASSWORD);
+        final Boolean smtpEhlo = Boolean.valueOf(properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_EHLO));
+        final Boolean smtpAuth = Boolean.valueOf(properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_AUTH));
+        final Integer smtpPort = Integer.valueOf(properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_PORT));
+
+        final EmailGlobalConfigEntity emailGlobalConfigEntity = new EmailGlobalConfigEntity(smtpHost, smtpUser, smtpPassword, smtpPort, null, null, null, smtpFrom, null, null, null, smtpEhlo, smtpAuth, null, null, null, null, null, null,
+            null,
+            null, null, null,
+            null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
+
+        emailChannel = Mockito.spy(emailChannel);
+        Mockito.doReturn(emailGlobalConfigEntity).when(emailChannel).getGlobalConfigEntity();
+
+        final MockEmailEntity mockEmailEntity = new MockEmailEntity();
+        mockEmailEntity.setGroupName(properties.getProperty(TestPropertyKey.TEST_EMAIL_GROUP));
+        emailChannel.sendAuditedMessage(event, mockEmailEntity.createEntity());
     }
 
     @Test
