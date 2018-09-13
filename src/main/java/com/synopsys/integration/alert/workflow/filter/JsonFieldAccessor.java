f2d6acf554ebf96793e9e7d74c0e88c55f9cc760
Gavin Killough <gavink@synopsys.com>
2018-09-13 11:45:22 -0400

    Create JsonFieldAccessor to minimize number of gson calls.

---
+++ src/main/java/com/synopsys/integration/alert/workflow/filter/JsonFieldAccessor.java
@@ -0,0 +1,87 @@
+package com.synopsys.integration.alert.workflow.filter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import com.synopsys.integration.alert.common.exception.AlertException;
+import com.synopsys.integration.alert.common.field.HierarchicalField;
+import com.synopsys.integration.alert.common.field.ObjectHierarchicalField;
+import com.synopsys.integration.alert.common.field.StringHierarchicalField;
+
+public class JsonFieldAccessor {
+    private Map<StringHierarchicalField, List<String>> stringMappings = new HashMap<>();
+    private Map<ObjectHierarchicalField, List<Object>> objectMappings = new HashMap<>();
+
+    public JsonFieldAccessor(final Map<HierarchicalField, List<Object>> fieldToDataMap) {
+        initializeSubMappings(fieldToDataMap, stringMappings, StringHierarchicalField.class);
+    }
+
+    public List<String> get(final StringHierarchicalField field) {
+        return getFrom(stringMappings, field);
+    }
+
+    public List<Object> get(final ObjectHierarchicalField field) {
+        return getFrom(objectMappings, field);
+    }
+
+    public <T> List<T> get(final ObjectHierarchicalField field, final Class<T> expectedClazz) throws AlertException {
+        final List<Object> objectList = get(field);
+
+        final List<T> typedList = new ArrayList<>();
+        for (final Object object : objectList) {
+            if (expectedClazz.isAssignableFrom(object.getClass())) {
+                typedList.add(expectedClazz.cast(object));
+            } else {
+                throw new AlertException(String.format("Invalid data type '%s' for field '%s'.", expectedClazz.getName(), field.getLabel()));
+            }
+        }
+        return typedList;
+    }
+
+    public Optional<String> getFirst(final StringHierarchicalField field) {
+        return getFirst(get(field));
+    }
+
+    public Optional<Object> getFirst(final ObjectHierarchicalField field) {
+        return getFirst(get(field));
+    }
+
+    public <T> Optional<T> getFirst(final ObjectHierarchicalField field, final Class<T> expectedClass) throws AlertException {
+        return getFirst(get(field, expectedClass));
+    }
+
+    private <K, V> List<V> getFrom(final Map<K, List<V>> map, final K field) {
+        if (map.containsKey(field)) {
+            return map.get(field);
+        }
+        return Collections.emptyList();
+    }
+
+    private <K, V> Optional<V> getFirst(final List<V> list) {
+        if (!list.isEmpty()) {
+            final V firstValue = list.get(0);
+            return Optional.of(firstValue);
+        }
+        return Optional.empty();
+    }
+
+    private <K, V> void initializeSubMappings(final Map<HierarchicalField, List<Object>> fieldToDataMap, Map<K, List<V>> subMap, final Class<K> targetKeyClass) {
+        for (final Map.Entry<HierarchicalField, List<Object>> entry : fieldToDataMap.entrySet()) {
+            final HierarchicalField key = entry.getKey();
+            if (targetKeyClass.isAssignableFrom(key.getClass())) {
+                final List<V> newValues = new ArrayList<>();
+
+                final List<Object> values = entry.getValue();
+                for (final Object value : values) {
+                    newValues.add((V) value);
+                }
+                subMap.put((K) key, newValues);
+            }
+        }
+
+    }
+}
