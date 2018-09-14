05dd41019ffa1910d1aadbaaa0b25bd7bc4aaa89
psantos1113 <psantos@blackducksoftware.com>
2018-09-14 08:06:52 -0400

    some minor improvements to performance

--- src/main/java/com/synopsys/integration/alert/workflow/processor/JobProcessor.java
+++ src/main/java/com/synopsys/integration/alert/workflow/processor/JobProcessor.java
@@ -1,3 +1,26 @@
+/**
+ * blackduck-alert
+ *
+ * Copyright (C) 2018 Black Duck Software, Inc.
+ * http://www.blackducksoftware.com/
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package com.synopsys.integration.alert.workflow.processor;
 
 import java.util.Collection;
@@ -5,6 +28,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -54,37 +78,41 @@ public class JobProcessor {
         return topicContentList;
     }
 
-    private Collection<NotificationContent> filterNotifications(final CommonDistributionConfig jobConfiguration, final Collection<NotificationContent> notificationCollection) {
+    private Collection<NotificationContent> filterNotifications(final ProviderDescriptor providerDescriptor, final CommonDistributionConfig jobConfiguration, final Collection<NotificationContent> notificationCollection) {
         final Predicate<NotificationContent> providerFilter = (notificationContent) -> jobConfiguration.getProviderName().equals(notificationContent.getProvider());
         final Collection<NotificationContent> providerNotifications = applyFilter(notificationCollection, providerFilter);
-        final Collection<NotificationContent> filteredNotificationList = notificationFilter.extractApplicableNotifications(jobConfiguration, providerNotifications);
+        final Collection<NotificationContent> filteredNotificationList = notificationFilter.extractApplicableNotifications(providerDescriptor.getProviderContentTypes(), jobConfiguration, providerNotifications);
         return filteredNotificationList;
     }
 
     private List<TopicContent> collectTopics(final CommonDistributionConfig jobConfiguration, final Collection<NotificationContent> notificationCollection) {
+        final Optional<ProviderDescriptor> providerDescriptor = providerDescriptors.parallelStream().filter(descriptor -> jobConfiguration.getProviderName().equals(descriptor.getName())).findFirst();
 
-        final Collection<NotificationContent> notificationsForJob = filterNotifications(jobConfiguration, notificationCollection);
-        if (notificationsForJob.isEmpty()) {
+        if (!providerDescriptor.isPresent()) {
             return Collections.emptyList();
-        }
+        } else {
+            final Collection<NotificationContent> notificationsForJob = filterNotifications(providerDescriptor.get(), jobConfiguration, notificationCollection);
+            if (notificationsForJob.isEmpty()) {
+                return Collections.emptyList();
+            }
 
-        final FormatType formatType = FormatType.valueOf(jobConfiguration.getFormatType());
-        final Map<String, TopicCollector> collectorMap = new HashMap<>();
+            final FormatType formatType = FormatType.valueOf(jobConfiguration.getFormatType());
+            final Map<String, TopicCollector> collectorMap = new HashMap<>();
 
-        for (final ProviderDescriptor providerDescriptor : providerDescriptors) {
-            final Set<TopicCollector> providerTopicCollectors = providerDescriptor.createTopicCollectors();
+            //TODO need better performance to map the notification type to the processor
+            final Set<TopicCollector> providerTopicCollectors = providerDescriptor.get().createTopicCollectors();
             for (final TopicCollector collector : providerTopicCollectors) {
                 for (final String notificationType : collector.getSupportedNotificationTypes()) {
                     collectorMap.put(notificationType, collector);
                 }
             }
+
+            notificationsForJob.parallelStream()
+                .filter(notificationContent -> collectorMap.containsKey(notificationContent.getNotificationType()))
+                .forEach(notificationContent -> collectorMap.get(notificationContent.getNotificationType()).insert(notificationContent));
+
+            return collectorMap.values().parallelStream().flatMap(collector -> collector.collect(formatType).stream()).collect(Collectors.toList());
         }
-
-        notificationsForJob.parallelStream()
-            .filter(notificationContent -> collectorMap.containsKey(notificationContent.getNotificationType()))
-            .forEach(notificationContent -> collectorMap.get(notificationContent.getNotificationType()).insert(notificationContent));
-
-        return collectorMap.values().parallelStream().flatMap(collector -> collector.collect(formatType).stream()).collect(Collectors.toList());
     }
 
     //TODO notificationFilter also has this should it be made common?
