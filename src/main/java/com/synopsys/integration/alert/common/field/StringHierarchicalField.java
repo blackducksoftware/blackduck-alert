6d7d817d3cf17445b83cd0c2f58e6361ed87c52c
Gavin Killough <gavink@synopsys.com>
2018-09-21 10:39:56 -0400

    Correct field hierarchy for HierarchicalFields.

--- src/main/java/com/synopsys/integration/alert/common/field/StringHierarchicalField.java
+++ src/main/java/com/synopsys/integration/alert/common/field/StringHierarchicalField.java
@@ -25,19 +25,31 @@ package com.synopsys.integration.alert.common.field;
 
 import java.lang.reflect.Type;
 import java.util.Collection;
+import java.util.Optional;
 
 import com.google.gson.reflect.TypeToken;
 import com.synopsys.integration.alert.common.enumeration.FieldContentIdentifier;
 
 public class StringHierarchicalField extends HierarchicalField {
-
     private static final Type TYPE = new TypeToken<String>() {}.getType();
+
+    private final String configNameMapping;
 
     public StringHierarchicalField(final Collection<String> pathToField, final String innerMostFieldName, final FieldContentIdentifier contentIdentifier, final String label) {
         super(pathToField, innerMostFieldName, contentIdentifier, label, TYPE);
+        this.configNameMapping = null;
     }
 
     public StringHierarchicalField(final Collection<String> pathToField, final String innerMostFieldName, final FieldContentIdentifier contentIdentifier, final String label, final String configNameMapping) {
-        super(pathToField, innerMostFieldName, contentIdentifier, label, configNameMapping, TYPE);
+        super(pathToField, innerMostFieldName, contentIdentifier, label, TYPE);
+        this.configNameMapping = configNameMapping;
+    }
+
+    public Optional<String> getConfigNameMapping() {
+        return Optional.ofNullable(configNameMapping);
+    }
+
+    public boolean isFilterable() {
+        return configNameMapping != null;
     }
 }
