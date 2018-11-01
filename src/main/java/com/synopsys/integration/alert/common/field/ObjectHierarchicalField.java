e946bfa42c636a8cf603a8439af75b2f8f2cb068
Gavin Killough <gavink@synopsys.com>
2018-11-01 14:20:36 -0400

    Remove excess whitespace.

--- src/main/java/com/synopsys/integration/alert/common/field/ObjectHierarchicalField.java
+++ src/main/java/com/synopsys/integration/alert/common/field/ObjectHierarchicalField.java
@@ -29,7 +29,6 @@ import java.util.Collection;
 import com.synopsys.integration.alert.common.enumeration.FieldContentIdentifier;
 
 public class ObjectHierarchicalField extends HierarchicalField {
-
     public ObjectHierarchicalField(final Collection<String> pathToField, final String innerMostFieldName, final FieldContentIdentifier contentIdentifier, final String label, final Type type) {
         super(pathToField, innerMostFieldName, contentIdentifier, label, type);
     }
