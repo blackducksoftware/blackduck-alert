e85c2c57ed73128fdb01f42cd9538b40a40b846f
psantos1113 <psantos@blackducksoftware.com>
2018-11-05 08:41:04 -0500

    Capture the system errors and display in a popover

--- src/main/js/AboutInfoFooter.js
+++ src/main/js/AboutInfoFooter.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import '../css/footer.scss';
+import {OverlayTrigger, Popover} from 'react-bootstrap'
 
-import { getAboutInfo } from './store/actions/about';
+import {getAboutInfo} from './store/actions/about';
 
 class AboutInfoFooter extends React.Component {
     constructor(props) {
@@ -14,8 +15,29 @@ class AboutInfoFooter extends React.Component {
         this.props.getAboutInfo();
     }
 
+    createErrorComponent() {
+        const {startupErrors} = this.props;
+
+        const message = startupErrors ? startupErrors : "none";
+        const divClassName = startupErrors ? "errorStatus" : "validStatus";
+        const iconClassName = startupErrors ? "fa fa-exclamation-triangle" : "fa fa-check-circle";
+
+        const popover = (<Popover id="system-errors-popover" title="System Errors">{message}</Popover>);
+        return (
+            <div className="statusPopover">
+                <OverlayTrigger
+                    container={this}
+                    trigger="click"
+                    placement="top"
+                    overlay={popover}>
+                    <div className={divClassName}><span className={iconClassName}></span></div>
+                </OverlayTrigger>
+            </div>);
+    }
+
     render() {
-        const { version, projectUrl } = this.props;
+        const {version, projectUrl} = this.props;
+        const errorComponent = this.createErrorComponent();
         return (
             <div className="footer">
                 <a className="productName" alt={projectUrl} href={projectUrl}>
@@ -27,6 +49,7 @@ class AboutInfoFooter extends React.Component {
                     <a id="aboutLink" href='http://www.blackducksoftware.com'>Black Duck Software, Inc</a>
                     &nbsp;All rights reserved.
                 </span>
+                {errorComponent}
             </div>
         );
     }
@@ -36,21 +59,24 @@ AboutInfoFooter.propTypes = {
     fetching: PropTypes.bool,
     version: PropTypes.string.isRequired,
     description: PropTypes.string,
-    projectUrl: PropTypes.string.isRequired
+    projectUrl: PropTypes.string.isRequired,
+    startupErrors: PropTypes.string
 };
 
 AboutInfoFooter.defaultProps = {
     fetching: false,
     version: '',
     description: '',
-    projectUrl: ''
+    projectUrl: '',
+    startupErrors: ''
 };
 
 const mapStateToProps = state => ({
     fetching: state.about.fetching,
     version: state.about.version,
     description: state.about.description,
-    projectUrl: state.about.projectUrl
+    projectUrl: state.about.projectUrl,
+    startupErrors: state.about.startupErrors
 });
 
 const mapDispatchToProps = dispatch => ({
