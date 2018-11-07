addf3453c69765385d89ea2f0070c2bd62786c14
psantos1113 <psantos@blackducksoftware.com>
2018-11-07 16:48:26 -0500

    Add the icons for the messages

--- src/main/js/component/common/SystemMessage.js
+++ src/main/js/component/common/SystemMessage.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import '../../../css/footer.scss';
 
 class SystemMessage extends Component {
 
@@ -9,7 +10,24 @@ class SystemMessage extends Component {
 
     render() {
         const {createdAt, content, severity} = this.props;
-        return (<div>{severity} {createdAt} {content}</div>);
+
+
+        return (<div>
+            <span className={this.getIcon()} aria-hidden="true"/> {createdAt}
+            <div>{content}</div>
+        </div>);
+    }
+
+    getIcon() {
+        const {severity} = this.props;
+        const errorIcon = "fa fa-exclamation-triangle";
+        if (severity == 'ERROR') {
+            return `${errorIcon} errorStatus`;
+        } else if (severity == 'WARNING') {
+            return `${errorIcon} warningStatus`
+        } else {
+            return "fa fa-check-circle validStatus"
+        }
     }
 }
 
