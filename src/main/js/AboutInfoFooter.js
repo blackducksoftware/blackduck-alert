448e91c435496b9aaf03074309e276402d1b8015
psantos1113 <psantos@blackducksoftware.com>
2018-11-07 15:55:33 -0500

    set the key of the system message objects

--- src/main/js/AboutInfoFooter.js
+++ src/main/js/AboutInfoFooter.js
@@ -5,9 +5,9 @@ import PropTypes from 'prop-types';
 import '../css/footer.scss';
 import {Overlay, Popover} from 'react-bootstrap'
 
+import SystemMessage from './component/common/SystemMessage';
 import {getAboutInfo} from './store/actions/about';
 import {getLatestMessages} from './store/actions/system';
-import SystemMessage from "./component/common/SystemMessage";
 
 class AboutInfoFooter extends React.Component {
     constructor(props) {
@@ -16,7 +16,7 @@ class AboutInfoFooter extends React.Component {
             showOverlay: false
         };
         this.createErrorComponent = this.createErrorComponent.bind(this);
-        this.createErrorList = this.createErrorList.bind(this);
+        this.createMessageList = this.createMessageList.bind(this);
         this.handleOverlayButton = this.handleOverlayButton.bind(this);
         this.reload = this.reload.bind(this);
     }
@@ -29,7 +29,7 @@ class AboutInfoFooter extends React.Component {
 
     componentWillReceiveProps(nextProps) {
         const {latestMessages} = nextProps;
-        const showOverlay = latestMessages && latestMessages.length > 0 ? true : false;
+        const showOverlay = this.hasErrorMessages(latestMessages);
         this.setState({showOverlay: showOverlay});
         if (!nextProps.fetching) {
             this.startAutoReload();
@@ -37,10 +37,9 @@ class AboutInfoFooter extends React.Component {
     }
 
     createErrorComponent() {
-        const errorMessages = this.createErrorList();
-        const showOverlay = errorMessages ? true : false;
-        const divClassName = showOverlay ? "errorStatus" : "validStatus";
-        const iconClassName = showOverlay ? "fa fa-exclamation-triangle" : "fa fa-check-circle";
+        const errorMessages = this.createMessageList();
+        const iconColor = this.getIconColor();
+        const iconClassName = this.getFontAwesomeIcon();
         const popover = (<Popover id="system-errors-popover" className="popoverContent" title="System Messages">{errorMessages}</Popover>);
         const overlayComponent = (
             <Overlay
@@ -58,23 +57,65 @@ class AboutInfoFooter extends React.Component {
                 <div ref={button => {
                     this.target = button
                 }} onClick={this.handleOverlayButton}>
-                    <div className={divClassName}><span className={iconClassName}></span></div>
+                    <div className={iconColor}><span className={iconClassName}></span></div>
                 </div>
                 {overlayComponent}
             </div>
         );
     }
 
-    createErrorList() {
+    createMessageList() {
         const {latestMessages} = this.props;
         if (latestMessages && latestMessages.length > 0) {
-            return latestMessages.map((message) => {
-                return (<SystemMessage createdAt={message.createdAt} content={message.content} severity={message.severity}/>);
+            return latestMessages.map((message, index) => {
+                const itemKey = `system_message_${message.createdAt}`;
+                return (<SystemMessage key={itemKey} createdAt={message.createdAt} content={message.content} severity={message.severity}/>);
             });
         } else {
             return null;
         }
     }
+
+    hasErrorMessages(messages) {
+        return this.containsSeverity(messages, 'ERROR');
+    }
+
+    hasWarninigMessages(messages) {
+        return this.containsSeverity(messages, 'WARNING');
+    }
+
+    containsSeverity(messages, severity) {
+        if (messages && messages.length > 0) {
+            if (messages.find(message => message.severity === severity)) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    getFontAwesomeIcon() {
+        const {latestMessages} = this.props;
+        if (this.hasErrorMessages(latestMessages) || this.hasWarninigMessages(latestMessages)) {
+            return "fa fa-exclamation-triangle"
+        } else {
+            return "fa fa-check-circle"
+        }
+    }
+
+    getIconColor() {
+        const {latestMessages} = this.props;
+        if (this.hasErrorMessages(latestMessages)) {
+            return "errorStatus"
+        } else if (this.hasWarninigMessages(latestMessages)) {
+            return "warningStatus"
+        } else {
+            return "validStatus"
+        }
+    }
+
 
     handleOverlayButton() {
         this.setState({showOverlay: !this.state.showOverlay});
