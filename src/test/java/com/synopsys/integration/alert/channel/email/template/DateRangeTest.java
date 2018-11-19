8646930c9a2db3e0571c74c0910425c4f3d8ebaa
jrichard <jrichard@synopsys.com>
2018-11-19 17:55:27 -0500

    Updating DateRange to use static methods to create a new instance

--- src/test/java/com/synopsys/integration/alert/channel/email/template/DateRangeTest.java
+++ src/test/java/com/synopsys/integration/alert/channel/email/template/DateRangeTest.java
@@ -6,13 +6,15 @@ import java.util.Date;
 
 import org.junit.Test;
 
+import com.synopsys.integration.alert.common.model.DateRange;
+
 public class DateRangeTest {
 
     @Test
     public void testGetTimes() {
         final Date start = new Date(300);
         final Date end = new Date(500);
-        final DateRange dateRange = new DateRange(start, end);
+        final DateRange dateRange = DateRange.of(start, end);
 
         assertEquals(start, dateRange.getStart());
         assertEquals(end, dateRange.getEnd());
