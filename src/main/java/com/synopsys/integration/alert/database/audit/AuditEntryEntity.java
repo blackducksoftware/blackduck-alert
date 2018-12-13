0f7bf2109eb2aba98f18bc6f4ff08d39e0677baf
jrichard <jrichard@synopsys.com>
2018-12-13 17:40:39 -0500

    Fixing copy paste error

--- src/main/java/com/synopsys/integration/alert/database/audit/AuditEntryEntity.java
+++ src/main/java/com/synopsys/integration/alert/database/audit/AuditEntryEntity.java
@@ -28,7 +28,6 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -70,11 +69,9 @@ public class AuditEntryEntity extends DatabaseEntity {
     private String errorStackTrace;
 
     @OneToMany
-    @JoinColumn(name = "audit_entry_id")
     private List<AuditNotificationRelation> auditNotificationRelations;
 
     @ManyToOne
-    @JoinColumn(name = "common_config_id", insertable = false, updatable = false)
     private CommonDistributionConfigEntity commonDistributionConfigEntity;
 
     public AuditEntryEntity() {
