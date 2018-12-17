2d1f4c098f48b7a4f36d9cd6144b595adea2d371
psantos1113 <psantos@blackducksoftware.com>
2018-12-17 15:48:50 -0500

    update the tests to get more to compile

--- src/test/java/com/synopsys/integration/alert/channel/DescriptorTestConfigTest.java
+++ src/test/java/com/synopsys/integration/alert/channel/DescriptorTestConfigTest.java
@@ -1,64 +1,103 @@
 package com.synopsys.integration.alert.channel;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.support.DefaultConversionService;
 
 import com.google.gson.Gson;
 import com.synopsys.integration.alert.AlertIntegrationTest;
 import com.synopsys.integration.alert.TestProperties;
+import com.synopsys.integration.alert.channel.event.DistributionEvent;
 import com.synopsys.integration.alert.common.ContentConverter;
 import com.synopsys.integration.alert.common.descriptor.ChannelDescriptor;
-import com.synopsys.integration.alert.database.entity.DatabaseEntity;
-import com.synopsys.integration.alert.mock.model.MockRestModelUtil;
+import com.synopsys.integration.alert.common.descriptor.config.context.DescriptorActionApi;
+import com.synopsys.integration.alert.common.enumeration.ConfigContextEnum;
+import com.synopsys.integration.alert.database.api.configuration.ConfigurationAccessor;
+import com.synopsys.integration.alert.web.model.FieldModel;
+import com.synopsys.integration.alert.web.model.TestConfigModel;
+import com.synopsys.integration.exception.IntegrationException;
 
-public abstract class DescriptorTestConfigTest<R extends CommonDistributionConfig, E extends DistributionChannelConfigEntity, GE extends GlobalChannelConfigEntity, EP extends ChannelEventProducer> extends AlertIntegrationTest {
+public abstract class DescriptorTestConfigTest extends AlertIntegrationTest {
     protected Gson gson;
-    protected EP channelEventProducer;
+    protected DistributionEvent channelEvent;
+
     protected ContentConverter contentConverter;
     protected TestProperties properties;
 
-    public abstract EP createChannelEventProducer();
+    @Autowired
+    protected ConfigurationAccessor configurationAccessor;
+
+    protected ConfigurationAccessor.ConfigurationModel global_config;
+    protected ConfigurationAccessor.ConfigurationModel distribution_config;
+
+    public abstract DistributionEvent createChannelEvent();
 
     @Before
     public void init() {
         gson = new Gson();
         contentConverter = new ContentConverter(gson, new DefaultConversionService());
         properties = new TestProperties();
-        channelEventProducer = createChannelEventProducer();
-        cleanGlobalRepository();
-        cleanDistributionRepositories();
+        channelEvent = createChannelEvent();
     }
 
-    public abstract void cleanGlobalRepository();
+    @After
+    public void cleanupTest() throws Exception {
+        if (global_config != null) {
+            configurationAccessor.deleteConfiguration(global_config);
+        }
 
-    public abstract void cleanDistributionRepositories();
+        if (distribution_config != null) {
+            configurationAccessor.deleteConfiguration(distribution_config);
+        }
+    }
 
-    public abstract void saveGlobalConfiguration();
+    public abstract Map<String, String> createGlobalConfiguration();
+
+    public abstract Map<String, String> createDistributionConfiguration();
+
+    public Map<String, String> getAllConfigurationMap() {
+        final Map<String, String> globalConfiguration = createGlobalConfiguration();
+        final Map<String, String> distributionConfiguration = createDistributionConfiguration();
+        final Map<String, String> combinedMap = new HashMap<>(globalConfiguration.size() + distributionConfiguration.size());
+        combinedMap.putAll(globalConfiguration);
+        combinedMap.putAll(distributionConfiguration);
+        return combinedMap;
+    }
 
     public abstract ChannelDescriptor getDescriptor();
 
-    public abstract MockRestModelUtil<R> getMockRestModelUtil();
-
-    public abstract DatabaseEntity getDistributionEntity();
-
-    public abstract void testCreateChannelEvent() throws Exception;
+    public abstract FieldModel getFieldModel();
 
     @Test
     public void testSendTestMessage() throws Exception {
-        // FIXME fix this test
-        //        saveGlobalConfiguration();
-        //        final DescriptorActionApi descriptorActionApi = getDescriptor().getRestApi(ActionApiType.CHANNEL_DISTRIBUTION_CONFIG);
-        //        final DescriptorActionApi spyDescriptorConfig = Mockito.spy(descriptorActionApi);
-        //        final Config restModel = getMockRestModelUtil().createRestModel();
-        //        try {
-        //            spyDescriptorConfig.testConfig(new TestConfigModel(restModel));
-        //        } catch (final IntegrationException e) {
-        //            e.printStackTrace();
-        //            Assert.fail();
-        //        }
-        //
-        //        Mockito.verify(spyDescriptorConfig).testConfig(Mockito.any());
+        final DescriptorActionApi descriptorActionApi = getDescriptor().getRestApi(ConfigContextEnum.DISTRIBUTION);
+        final DescriptorActionApi spyDescriptorConfig = Mockito.spy(descriptorActionApi);
+        final FieldModel restModel = getFieldModel();
+        try {
+            spyDescriptorConfig.testConfig(new TestConfigModel(restModel));
+        } catch (final IntegrationException e) {
+            e.printStackTrace();
+            Assert.fail();
+        }
+
+        Mockito.verify(spyDescriptorConfig).testConfig(Mockito.any());
     }
 
+    @Test
+    public void testCreateChannelEvent() throws Exception {
+        final DistributionEvent channelEvent = this.createChannelEvent();
+
+        assertEquals(Long.valueOf(1L), channelEvent.getConfigId());
+        assertEquals(36, channelEvent.getEventId().length());
+        assertEquals(getDescriptor().getDestinationName(), channelEvent.getDestination());
+    }
 }
--- src/test/java/com/synopsys/integration/alert/channel/email/EmailChannelDescriptorTestIT.java
+++ src/test/java/com/synopsys/integration/alert/channel/email/EmailChannelDescriptorTestIT.java
@@ -1,29 +1,31 @@
 package com.synopsys.integration.alert.channel.email;
 
-import static org.junit.Assert.assertEquals;
-
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
-import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.synopsys.integration.alert.TestPropertyKey;
 import com.synopsys.integration.alert.channel.DescriptorTestConfigTest;
 import com.synopsys.integration.alert.channel.email.descriptor.EmailDescriptor;
-import com.synopsys.integration.alert.channel.email.mock.MockEmailEntity;
-import com.synopsys.integration.alert.channel.email.mock.MockEmailRestModel;
+import com.synopsys.integration.alert.channel.email.descriptor.EmailDistributionUIConfig;
+import com.synopsys.integration.alert.channel.event.DistributionEvent;
+import com.synopsys.integration.alert.common.configuration.FieldAccessor;
 import com.synopsys.integration.alert.common.descriptor.ChannelDescriptor;
+import com.synopsys.integration.alert.common.enumeration.ConfigContextEnum;
+import com.synopsys.integration.alert.common.enumeration.EmailPropertyKeys;
 import com.synopsys.integration.alert.common.enumeration.FormatType;
-import com.synopsys.integration.alert.common.enumeration.FrequencyType;
+import com.synopsys.integration.alert.common.exception.AlertDatabaseConstraintException;
 import com.synopsys.integration.alert.common.model.AggregateMessageContent;
+import com.synopsys.integration.alert.common.model.DateRange;
 import com.synopsys.integration.alert.common.model.LinkableItem;
-import com.synopsys.integration.alert.database.channel.email.EmailDistributionRepositoryAccessor;
-import com.synopsys.integration.alert.database.channel.email.EmailGlobalConfigEntity;
-import com.synopsys.integration.alert.database.channel.email.EmailGlobalRepository;
-import com.synopsys.integration.alert.database.channel.email.EmailGroupDistributionConfigEntity;
+import com.synopsys.integration.alert.database.api.configuration.ConfigurationAccessor;
+import com.synopsys.integration.alert.database.api.configuration.ConfigurationFieldModel;
 import com.synopsys.integration.alert.database.entity.DatabaseEntity;
 import com.synopsys.integration.alert.database.provider.blackduck.data.BlackDuckProjectEntity;
 import com.synopsys.integration.alert.database.provider.blackduck.data.BlackDuckProjectRepositoryAccessor;
@@ -31,12 +33,14 @@ import com.synopsys.integration.alert.database.provider.blackduck.data.BlackDuck
 import com.synopsys.integration.alert.database.provider.blackduck.data.BlackDuckUserRepositoryAccessor;
 import com.synopsys.integration.alert.database.provider.blackduck.data.relation.UserProjectRelation;
 import com.synopsys.integration.alert.database.provider.blackduck.data.relation.UserProjectRelationRepositoryAccessor;
-import com.synopsys.integration.alert.mock.model.MockRestModelUtil;
-import com.synopsys.integration.alert.web.channel.model.EmailDistributionConfig;
+import com.synopsys.integration.alert.provider.blackduck.BlackDuckProvider;
+import com.synopsys.integration.alert.web.model.FieldModel;
+import com.synopsys.integration.alert.web.model.FieldValueModel;
+import com.synopsys.integration.rest.RestConstants;
 
-public class EmailChannelDescriptorTestIT extends DescriptorTestConfigTest<EmailDistributionConfig, EmailGroupDistributionConfigEntity, EmailGlobalConfigEntity, EmailEventProducer> {
-    @Autowired
-    private EmailDistributionRepositoryAccessor emailDistributionRepositoryAccessor;
+public class EmailChannelDescriptorTestIT extends DescriptorTestConfigTest {
+
+    public static final String SUBJECT_LINE = "Alert It Test";
     @Autowired
     private BlackDuckProjectRepositoryAccessor blackDuckProjectRepositoryAccessor;
     @Autowired
@@ -44,14 +48,10 @@ public class EmailChannelDescriptorTestIT extends DescriptorTestConfigTest<Email
     @Autowired
     private UserProjectRelationRepositoryAccessor userProjectRelationRepositoryAccessor;
     @Autowired
-    private EmailGlobalRepository emailGlobalRepository;
-    @Autowired
     private EmailDescriptor emailDescriptor;
-    @Autowired
-    private GlobalBlackDuckRepository globalBlackDuckRepository;
 
     @Before
-    public void testSetup() {
+    public void testSetup() throws Exception {
         final DatabaseEntity project1 = blackDuckProjectRepositoryAccessor.saveEntity(new BlackDuckProjectEntity("Project one", "", "", ""));
         final DatabaseEntity project2 = blackDuckProjectRepositoryAccessor.saveEntity(new BlackDuckProjectEntity("Project two", "", "", ""));
         final DatabaseEntity project3 = blackDuckProjectRepositoryAccessor.saveEntity(new BlackDuckProjectEntity("Project three", "", "", ""));
@@ -66,67 +66,53 @@ public class EmailChannelDescriptorTestIT extends DescriptorTestConfigTest<Email
         final UserProjectRelation userProjectRelation3 = new UserProjectRelation(user2.getId(), project3.getId());
         final UserProjectRelation userProjectRelation4 = new UserProjectRelation(user3.getId(), project4.getId());
         userProjectRelationRepositoryAccessor.deleteAndSaveAll(new HashSet<>(Arrays.asList(userProjectRelation1, userProjectRelation2, userProjectRelation3, userProjectRelation4)));
-        final GlobalBlackDuckConfigEntity blackDuckConfigEntity = new GlobalBlackDuckConfigEntity(300,
-                properties.getProperty(TestPropertyKey.TEST_BLACKDUCK_PROVIDER_API_KEY),
-                properties.getProperty(TestPropertyKey.TEST_BLACKDUCK_PROVIDER_URL));
-        globalBlackDuckRepository.deleteAll();
-        globalBlackDuckRepository.save(blackDuckConfigEntity);
+
+        final String blackDuckTimeoutKey = TestPropertyKey.TEST_BLACKDUCK_PROVIDER_TIMEOUT.getPropertyKey();
+        final ConfigurationFieldModel blackDuckTimeoutField = ConfigurationFieldModel.create(blackDuckTimeoutKey);
+        blackDuckTimeoutField.setFieldValue(properties.getProperty(TestPropertyKey.TEST_BLACKDUCK_PROVIDER_TIMEOUT));
+
+        final String blackDuckApiKey = TestPropertyKey.TEST_BLACKDUCK_PROVIDER_API_KEY.getPropertyKey();
+        final ConfigurationFieldModel blackDuckApiField = ConfigurationFieldModel.create(blackDuckApiKey);
+        blackDuckApiField.setFieldValue(properties.getProperty(TestPropertyKey.TEST_BLACKDUCK_PROVIDER_API_KEY));
+
+        final String blackDuckProviderUrlKey = TestPropertyKey.TEST_BLACKDUCK_PROVIDER_URL.getPropertyKey();
+        final ConfigurationFieldModel blackDuckProviderUrlField = ConfigurationFieldModel.create(blackDuckProviderUrlKey);
+        blackDuckProviderUrlField.setFieldValue(properties.getProperty(TestPropertyKey.TEST_BLACKDUCK_PROVIDER_URL));
+
+        final ConfigurationAccessor.ConfigurationModel global_config = configurationAccessor
+                                                                           .createConfiguration(BlackDuckProvider.COMPONENT_NAME, ConfigContextEnum.GLOBAL, List.of(blackDuckTimeoutField, blackDuckApiField, blackDuckProviderUrlField));
     }
 
     @Override
-    @Test
-    public void testCreateChannelEvent() throws Exception {
+    public Map<String, String> createGlobalConfiguration() {
+        return null;
+    }
+
+    @Override
+    public Map<String, String> createDistributionConfiguration() {
+        return null;
+    }
+
+    @Override
+    public DistributionEvent createChannelEvent() {
         final LinkableItem subTopic = new LinkableItem("subTopic", "Alert has sent this test message", null);
         final AggregateMessageContent content = new AggregateMessageContent("testTopic", "", null, subTopic, Collections.emptyList());
-        final DatabaseEntity distributionEntity = getDistributionEntity();
-        final String subjectLine = "Alert It Test";
-        final EmailDistributionConfig jobConfig = new EmailDistributionConfig("1", String.valueOf(distributionEntity.getId()), getDescriptor().getDestinationName(), "Test Job", "provider", FrequencyType.DAILY.name(), "true",
-                "", subjectLine, "", true, Collections.emptyList(), Collections.emptyList(), FormatType.DIGEST.name());
+        List<ConfigurationAccessor.ConfigurationModel> models = List.of();
+        try {
+            models = configurationAccessor.getConfigurationsByDescriptorName(EmailGroupChannel.COMPONENT_NAME);
+        } catch (final AlertDatabaseConstraintException e) {
+            e.printStackTrace();
+        }
 
-        final EmailChannelEvent channelEvent = channelEventProducer.createChannelEvent(jobConfig, content);
+        final Map<String, ConfigurationFieldModel> fieldMap = new HashMap<>();
+        for (final ConfigurationAccessor.ConfigurationModel model : models) {
+            fieldMap.putAll(model.getCopyOfKeyToFieldMap());
+        }
 
-        assertEquals(Long.valueOf(1L), channelEvent.getCommonDistributionConfigId());
-        assertEquals(36, channelEvent.getEventId().length());
-        assertEquals(getDescriptor().getDestinationName(), channelEvent.getDestination());
-        assertEquals(subjectLine, channelEvent.getSubjectLine());
-    }
-
-    @Override
-    public DatabaseEntity getDistributionEntity() {
-        final MockEmailEntity mockEmailEntity = new MockEmailEntity();
-        final EmailGroupDistributionConfigEntity emailGroupDistributionConfigEntity = mockEmailEntity.createEntity();
-        return emailDistributionRepositoryAccessor.saveEntity(emailGroupDistributionConfigEntity);
-    }
-
-    @Override
-    public EmailEventProducer createChannelEventProducer() {
-        return new EmailEventProducer(blackDuckProjectRepositoryAccessor, blackDuckUserRepositoryAccessor, userProjectRelationRepositoryAccessor);
-    }
-
-    @Override
-    public void cleanGlobalRepository() {
-        emailGlobalRepository.deleteAll();
-    }
-
-    @Override
-    public void cleanDistributionRepositories() {
-        emailDistributionRepositoryAccessor.deleteAll();
-    }
-
-    @Override
-    public void saveGlobalConfiguration() {
-        cleanGlobalRepository();
-        final String smtpHost = properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_HOST);
-        final String smtpFrom = properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_FROM);
-        final String smtpUser = properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_USER);
-        final String smtpPassword = properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_PASSWORD);
-        final Boolean smtpEhlo = Boolean.valueOf(properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_EHLO));
-        final Boolean smtpAuth = Boolean.valueOf(properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_AUTH));
-        final Integer smtpPort = Integer.valueOf(properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_PORT));
-        final EmailGlobalConfigEntity emailGlobalConfigEntity = new EmailGlobalConfigEntity(smtpHost, smtpUser, smtpPassword, smtpPort, null, null, null, smtpFrom, null, null, null, smtpEhlo, smtpAuth, null, null, null, null, null, null,
-                null, null, null, null,
-                null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null);
-        emailGlobalRepository.save(emailGlobalConfigEntity);
+        final FieldAccessor fieldAccessor = new FieldAccessor(fieldMap);
+        final String createdAt = RestConstants.formatDate(DateRange.createCurrentDateTimestamp());
+        final DistributionEvent event = new DistributionEvent("1L", EmailGroupChannel.COMPONENT_NAME, createdAt, BlackDuckProvider.COMPONENT_NAME, FormatType.DEFAULT.name(), content, fieldAccessor);
+        return event;
     }
 
     @Override
@@ -135,8 +121,30 @@ public class EmailChannelDescriptorTestIT extends DescriptorTestConfigTest<Email
     }
 
     @Override
-    public MockRestModelUtil<EmailDistributionConfig> getMockRestModelUtil() {
-        return new MockEmailRestModel();
+    public FieldModel getFieldModel() {
+        final Map<String, FieldValueModel> valueMap = new HashMap<>();
+        final String smtpHost = properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_HOST);
+        final String smtpFrom = properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_FROM);
+        final String smtpUser = properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_USER);
+        final String smtpPassword = properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_PASSWORD);
+        final Boolean smtpEhlo = Boolean.valueOf(properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_EHLO));
+        final Boolean smtpAuth = Boolean.valueOf(properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_AUTH));
+        final Integer smtpPort = Integer.valueOf(properties.getProperty(TestPropertyKey.TEST_EMAIL_SMTP_PORT));
+
+        valueMap.put(EmailPropertyKeys.JAVAMAIL_HOST_KEY.getPropertyKey(), new FieldValueModel(List.of(String.valueOf(smtpHost)), true));
+        valueMap.put(EmailPropertyKeys.JAVAMAIL_FROM_KEY.getPropertyKey(), new FieldValueModel(List.of(String.valueOf(smtpFrom)), true));
+        valueMap.put(EmailPropertyKeys.JAVAMAIL_USER_KEY.getPropertyKey(), new FieldValueModel(List.of(String.valueOf(smtpUser)), true));
+        valueMap.put(EmailPropertyKeys.JAVAMAIL_PASSWORD_KEY.getPropertyKey(), new FieldValueModel(List.of(String.valueOf(smtpPassword)), true));
+        valueMap.put(EmailPropertyKeys.JAVAMAIL_EHLO_KEY.getPropertyKey(), new FieldValueModel(List.of(String.valueOf(smtpEhlo)), true));
+        valueMap.put(EmailPropertyKeys.JAVAMAIL_AUTH_KEY.getPropertyKey(), new FieldValueModel(List.of(String.valueOf(smtpAuth)), true));
+        valueMap.put(EmailPropertyKeys.JAVAMAIL_PORT_KEY.getPropertyKey(), new FieldValueModel(List.of(String.valueOf(smtpPort)), true));
+
+        valueMap.put(EmailDistributionUIConfig.KEY_SUBJECT_LINE, new FieldValueModel(List.of(SUBJECT_LINE), true));
+        valueMap.put(EmailDistributionUIConfig.KEY_PROJECT_OWNER_ONLY, new FieldValueModel(List.of("true"), true));
+        valueMap.put(EmailDistributionUIConfig.KEY_EMAIL_ADDRESSES, new FieldValueModel(List.of("noreply@blackducksoftware.com"), true));
+
+        final FieldModel model = new FieldModel("1L", EmailGroupChannel.COMPONENT_NAME, ConfigContextEnum.DISTRIBUTION.name(), valueMap);
+        return model;
     }
 
 }
