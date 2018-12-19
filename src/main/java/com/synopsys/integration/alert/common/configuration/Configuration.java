f5ad351c622c524a57216cbed87867804e606ebf
Gavin Killough <gavink@synopsys.com>
2018-12-19 09:55:24 -0500

    Require not null for CommonDistributionConfiguration constructor

--- src/main/java/com/synopsys/integration/alert/common/configuration/Configuration.java
+++ src/main/java/com/synopsys/integration/alert/common/configuration/Configuration.java
@@ -23,13 +23,15 @@
  */
 package com.synopsys.integration.alert.common.configuration;
 
+import javax.validation.constraints.NotNull;
+
 import com.synopsys.integration.alert.database.api.configuration.ConfigurationAccessor.ConfigurationModel;
 
 public class Configuration {
     private final Long id;
     private final FieldAccessor fieldAccessor;
 
-    public Configuration(final ConfigurationModel configurationModel) {
+    public Configuration(@NotNull final ConfigurationModel configurationModel) {
         fieldAccessor = new FieldAccessor(configurationModel.getCopyOfKeyToFieldMap());
         id = configurationModel.getConfigurationId();
     }
