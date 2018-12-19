522e438135b33785958e46337ed2870443bbcb86
psantos1113 <psantos@blackducksoftware.com>
2018-12-19 12:42:01 -0500

    add tests for the channel descriptors

--- src/test/java/com/synopsys/integration/alert/channel/hipchat/HipChatDescriptorTestIT.java
+++ src/test/java/com/synopsys/integration/alert/channel/hipchat/HipChatDescriptorTestIT.java
@@ -1,77 +1,109 @@
 package com.synopsys.integration.alert.channel.hipchat;
 
-public class HipChatDescriptorTestIT {//extends DescriptorTestConfigTest<HipChatDistributionConfig, HipChatDistributionConfigEntity, HipChatGlobalConfigEntity, HipChatEventProducer> {
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
-    //FIXME fix the tests
-    //    @Autowired
-    //    private HipChatDistributionRepositoryAccessor hipChatDistributionRepositoryAccessor;
-    //    @Autowired
-    //    private HipChatGlobalRepository hipChatRepository;
-    //    @Autowired
-    //    private HipChatDescriptor hipChatDescriptor;
-    //
-    //    @Override
-    //    @Test
-    //    public void testCreateChannelEvent() throws Exception {
-    //        final LinkableItem subTopic = new LinkableItem("subTopic", "Alert has sent this test message", null);
-    //        final AggregateMessageContent content = new AggregateMessageContent("testTopic", "", null, subTopic, Collections.emptyList());
-    //        final DatabaseEntity distributionEntity = getDistributionEntity();
-    //        final String roomId = "12345";
-    //        final Boolean notify = false;
-    //        final String color = "purple";
-    //        final HipChatDistributionConfig jobConfig = new HipChatDistributionConfig("1", roomId, notify, color, String.valueOf(distributionEntity.getId()), getDescriptor().getDestinationName(), "Test Job", "provider",
-    //            FrequencyType.DAILY.name(), "true", "", Collections.emptyList(), Collections.emptyList(), FormatType.DIGEST.name());
-    //
-    //        final HipChatChannelEvent channelEvent = this.channelEvent.createChannelEvent(jobConfig, content);
-    //
-    //        assertEquals(Long.valueOf(1L), channelEvent.getCommonDistributionConfigId());
-    //        assertEquals(36, channelEvent.getEventId().length());
-    //        assertEquals(getDescriptor().getDestinationName(), channelEvent.getDestination());
-    //        assertTrue(NumberUtils.toInt(roomId) == channelEvent.getRoomId());
-    //        assertEquals(notify, channelEvent.getNotify());
-    //        assertEquals(color, channelEvent.getColor());
-    //    }
-    //
-    //    @Override
-    //    public DatabaseEntity getDistributionEntity() {
-    //        final MockHipChatEntity mockHipChatEntity = new MockHipChatEntity();
-    //        final HipChatDistributionConfigEntity hipChatDistributionConfigEntity = mockHipChatEntity.createEntity();
-    //        return hipChatDistributionRepositoryAccessor.saveEntity(hipChatDistributionConfigEntity);
-    //    }
-    //
-    //    @Override
-    //    public HipChatEventProducer createChannelEvent() {
-    //        return new HipChatEventProducer();
-    //    }
-    //
-    //    @Override
-    //    public void cleanRepository() {
-    //        hipChatRepository.deleteAll();
-    //    }
-    //
-    //    @Override
-    //    public void cleanDistributionRepositories() {
-    //        hipChatDistributionRepositoryAccessor.deleteAll();
-    //    }
-    //
-    //    @Override
-    //    public void saveGlobalConfiguration() {
-    //        final HipChatGlobalConfigEntity globalEntity = new HipChatGlobalConfigEntity(properties.getProperty(TestPropertyKey.TEST_HIPCHAT_API_KEY), "");
-    //        hipChatRepository.save(globalEntity);
-    //    }
-    //
-    //    @Override
-    //    public ChannelDescriptor getDescriptor() {
-    //        return hipChatDescriptor;
-    //    }
-    //
-    //    @Override
-    //    public MockRestModelUtil<HipChatDistributionConfig> getFieldModel() {
-    //        final MockHipChatRestModel restModel = new MockHipChatRestModel();
-    //        restModel.setRoomId(properties.getProperty(TestPropertyKey.TEST_HIPCHAT_ROOM_ID));
-    //        restModel.setNotify(false);
-    //        restModel.setColor("random");
-    //        return restModel;
-    //    }
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import com.synopsys.integration.alert.channel.DescriptorTestConfigTest;
+import com.synopsys.integration.alert.channel.event.DistributionEvent;
+import com.synopsys.integration.alert.channel.hipchat.descriptor.HipChatDescriptor;
+import com.synopsys.integration.alert.common.configuration.FieldAccessor;
+import com.synopsys.integration.alert.common.descriptor.ChannelDescriptor;
+import com.synopsys.integration.alert.common.enumeration.ConfigContextEnum;
+import com.synopsys.integration.alert.common.enumeration.FormatType;
+import com.synopsys.integration.alert.common.exception.AlertDatabaseConstraintException;
+import com.synopsys.integration.alert.common.model.AggregateMessageContent;
+import com.synopsys.integration.alert.common.model.DateRange;
+import com.synopsys.integration.alert.common.model.LinkableItem;
+import com.synopsys.integration.alert.database.api.configuration.ConfigurationAccessor;
+import com.synopsys.integration.alert.database.api.configuration.ConfigurationFieldModel;
+import com.synopsys.integration.alert.mock.MockConfigurationModelFactory;
+import com.synopsys.integration.alert.provider.blackduck.BlackDuckProvider;
+import com.synopsys.integration.alert.provider.blackduck.descriptor.BlackDuckDescriptor;
+import com.synopsys.integration.alert.util.TestPropertyKey;
+import com.synopsys.integration.alert.web.model.FieldModel;
+import com.synopsys.integration.alert.web.model.FieldValueModel;
+import com.synopsys.integration.rest.RestConstants;
+
+public class HipChatDescriptorTestIT extends DescriptorTestConfigTest {
+    public static final String UNIT_TEST_JOB_NAME = "HipChatUnitTestJob";
+    @Autowired
+    private HipChatDescriptor hipChatDescriptor;
+
+    @BeforeEach
+    public void testSetup() throws Exception {
+        final String blackDuckTimeoutKey = BlackDuckDescriptor.KEY_BLACKDUCK_TIMEOUT;
+        final ConfigurationFieldModel blackDuckTimeoutField = ConfigurationFieldModel.create(blackDuckTimeoutKey);
+        blackDuckTimeoutField.setFieldValue(properties.getProperty(TestPropertyKey.TEST_BLACKDUCK_PROVIDER_TIMEOUT));
+
+        final String blackDuckApiKey = BlackDuckDescriptor.KEY_BLACKDUCK_API_KEY;
+        final ConfigurationFieldModel blackDuckApiField = ConfigurationFieldModel.create(blackDuckApiKey);
+        blackDuckApiField.setFieldValue(properties.getProperty(TestPropertyKey.TEST_BLACKDUCK_PROVIDER_API_KEY));
+
+        final String blackDuckProviderUrlKey = BlackDuckDescriptor.KEY_BLACKDUCK_URL;
+        final ConfigurationFieldModel blackDuckProviderUrlField = ConfigurationFieldModel.create(blackDuckProviderUrlKey);
+        blackDuckProviderUrlField.setFieldValue(properties.getProperty(TestPropertyKey.TEST_BLACKDUCK_PROVIDER_URL));
+
+        provider_global = configurationAccessor
+                              .createConfiguration(BlackDuckProvider.COMPONENT_NAME, ConfigContextEnum.GLOBAL, List.of(blackDuckTimeoutField, blackDuckApiField, blackDuckProviderUrlField));
+    }
+
+    @Override
+    public Optional<ConfigurationAccessor.ConfigurationModel> saveGlobalConfiguration() throws Exception {
+        final Map<String, String> valueMap = new HashMap<>();
+        final String apiToken = properties.getProperty(TestPropertyKey.TEST_HIPCHAT_API_KEY);
+        valueMap.put(HipChatDescriptor.KEY_API_KEY, apiToken);
+        final Map<String, ConfigurationFieldModel> fieldModelMap = MockConfigurationModelFactory.mapStringsToFields(valueMap);
+
+        return Optional.of(configurationAccessor.createConfiguration(HipChatChannel.COMPONENT_NAME, ConfigContextEnum.GLOBAL, fieldModelMap.values()));
+    }
+
+    @Override
+    public ConfigurationAccessor.ConfigurationModel saveDistributionConfiguration() throws Exception {
+        final List<ConfigurationFieldModel> models = new LinkedList<>();
+        models.addAll(MockConfigurationModelFactory.createCommonBlackDuckConfigurationFields(UNIT_TEST_JOB_NAME, HipChatChannel.COMPONENT_NAME));
+        models.addAll(MockConfigurationModelFactory.createHipChatConfigurationFields());
+        return configurationAccessor.createConfiguration(HipChatChannel.COMPONENT_NAME, ConfigContextEnum.DISTRIBUTION, models);
+    }
+
+    @Override
+    public DistributionEvent createChannelEvent() {
+        final LinkableItem subTopic = new LinkableItem("subTopic", "Alert has sent this test message", null);
+        final AggregateMessageContent content = new AggregateMessageContent("testTopic", "", null, subTopic, Collections.emptyList());
+        List<ConfigurationAccessor.ConfigurationModel> models = List.of();
+        try {
+            models = configurationAccessor.getConfigurationsByDescriptorName(HipChatChannel.COMPONENT_NAME);
+        } catch (final AlertDatabaseConstraintException e) {
+            e.printStackTrace();
+        }
+
+        final Map<String, ConfigurationFieldModel> fieldMap = new HashMap<>();
+        for (final ConfigurationAccessor.ConfigurationModel model : models) {
+            fieldMap.putAll(model.getCopyOfKeyToFieldMap());
+        }
+
+        final FieldAccessor fieldAccessor = new FieldAccessor(fieldMap);
+        final String createdAt = RestConstants.formatDate(DateRange.createCurrentDateTimestamp());
+        final DistributionEvent event = new DistributionEvent(String.valueOf(distribution_config.getConfigurationId()), HipChatChannel.COMPONENT_NAME, createdAt, BlackDuckProvider.COMPONENT_NAME, FormatType.DEFAULT.name(), content,
+            fieldAccessor);
+        return event;
+    }
+
+    @Override
+    public ChannelDescriptor getDescriptor() {
+        return hipChatDescriptor;
+    }
+
+    @Override
+    public FieldModel getFieldModel() {
+        final Map<String, FieldValueModel> valueMap = createFieldModelMap();
+        final FieldModel model = new FieldModel(String.valueOf(distribution_config.getConfigurationId()), HipChatChannel.COMPONENT_NAME, ConfigContextEnum.DISTRIBUTION.name(), valueMap);
+        return model;
+    }
 }
