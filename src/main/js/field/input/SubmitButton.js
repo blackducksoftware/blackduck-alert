f1ee0292fd84174e5e834b5b1ee0d199917e0d12
jrichard <jrichard@synopsys.com>
2018-12-21 13:27:17 -0500

    Fixing link errors on fields

--- src/main/js/field/input/SubmitButton.js
+++ src/main/js/field/input/SubmitButton.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const SubmitButton = ({onClick, children, id}) => (
+const SubmitButton = ({ onClick, children, id }) => (
     <button id={id} className="btn btn-md btn-primary" type="submit" onClick={onClick}>{children}</button>
 );
 
 SubmitButton.defaultProps = {
     children: 'Submit',
+    id: 'id',
     onClick: () => true
 };
 
 SubmitButton.propTypes = {
     children: PropTypes.string,
+    id: PropTypes.string,
     onClick: PropTypes.func
 };
 
