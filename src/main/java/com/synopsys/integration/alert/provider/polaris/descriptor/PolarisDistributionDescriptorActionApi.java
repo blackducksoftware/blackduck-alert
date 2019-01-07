5708f579bc49e8504a3f171fbd2cb21c2741b31c
Gavin Killough <gavink@synopsys.com>
2019-01-07 15:24:44 -0500

    Create required classes for Polaris provider (not implemented).

---
+++ src/main/java/com/synopsys/integration/alert/provider/polaris/descriptor/PolarisDistributionDescriptorActionApi.java
@@ -0,0 +1,23 @@
+package com.synopsys.integration.alert.provider.polaris.descriptor;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.synopsys.integration.alert.common.configuration.FieldAccessor;
+import com.synopsys.integration.alert.common.descriptor.config.context.DescriptorActionApi;
+import com.synopsys.integration.alert.web.model.TestConfigModel;
+import com.synopsys.integration.exception.IntegrationException;
+
+@Component
+public class PolarisDistributionDescriptorActionApi extends DescriptorActionApi {
+    @Override
+    public void validateConfig(final FieldAccessor fieldAccessor, final Map<String, String> fieldErrors) {
+        // FIXME implement
+    }
+
+    @Override
+    public void testConfig(final TestConfigModel testConfig) throws IntegrationException {
+        // FIXME implement
+    }
+}
