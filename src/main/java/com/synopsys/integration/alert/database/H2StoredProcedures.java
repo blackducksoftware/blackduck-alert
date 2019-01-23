a95f02b758fc962e71671b3b6667708eb5485844
Gavin Killough <gavink@synopsys.com>
2019-01-23 16:10:45 -0500

    Use stored procedure to replace field initialization in changelog

--- src/main/java/com/synopsys/integration/alert/database/H2StoredProcedures.java
+++ src/main/java/com/synopsys/integration/alert/database/H2StoredProcedures.java
@@ -28,24 +28,38 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import org.apache.commons.lang3.StringUtils;
+
 public final class H2StoredProcedures {
+    public static void defineField(final Connection connection, final String fieldKey, final Boolean sensitive, final String descriptorName, final String context) throws SQLException {
+        try (final Statement insertIntoDefinedFields = connection.createStatement()) {
+            insertIntoDefinedFields.executeUpdate("INSERT INTO ALERT.Defined_Fields (SOURCE_KEY, SENSITIVE) VALUES ('" + fieldKey + "', " + sensitive + ");");
+        }
+        try (final Statement insertIntoDescriptorFields = connection.createStatement()) {
+            insertIntoDescriptorFields.executeUpdate("INSERT INTO ALERT.DESCRIPTOR_FIELDS (DESCRIPTOR_ID, FIELD_ID) VALUES (GET_ID_FOR_REGISTERED_DESCRIPTOR_NAME('" + StringUtils.lowerCase(descriptorName) + "'), GET_LATEST_FIELD_ID());");
+        }
+        try (final Statement insertIntoFieldContexts = connection.createStatement()) {
+            insertIntoFieldContexts.executeUpdate("INSERT INTO ALERT.Field_Contexts (FIELD_ID, CONTEXT_ID) VALUES (GET_LATEST_FIELD_ID(), GET_ID_FOR_CONFIG_CONTEXT('" + StringUtils.upperCase(context) + "'));");
+        }
+    }
+
     public static Integer getIdForRegisteredDescriptorName(final Connection connection, final String descriptorName) throws SQLException {
-        return getFirstInt(connection, "SELECT id from alert.registered_descriptors WHERE registered_descriptors.name = '" + descriptorName + "' LIMIT 1;");
+        return getFirstInt(connection, "SELECT ID FROM ALERT.REGISTERED_DESCRIPTORS WHERE REGISTERED_DESCRIPTORS.NAME = '" + StringUtils.lowerCase(descriptorName) + "' LIMIT 1;");
     }
 
     public static Integer getLatestFieldId(final Connection connection) throws SQLException {
-        return getFirstInt(connection, "SELECT id FROM alert.defined_fields ORDER BY id DESC LIMIT 1;");
+        return getFirstInt(connection, "SELECT ID FROM ALERT.DEFINED_FIELDS ORDER BY ID DESC LIMIT 1;");
     }
 
     public static Integer getIdForConfigContext(final Connection connection, final String context) throws SQLException {
-        return getFirstInt(connection, "SELECT id from alert.config_contexts WHERE config_contexts.context = '" + context + "' LIMIT 1;");
+        return getFirstInt(connection, "SELECT ID FROM ALERT.CONFIG_CONTEXTS WHERE CONFIG_CONTEXTS.CONTEXT = '" + StringUtils.upperCase(context) + "' LIMIT 1;");
     }
 
     private static Integer getFirstInt(final Connection connection, final String sql) throws SQLException {
         try (final Statement statement = connection.createStatement()) {
             final ResultSet resultSet = statement.executeQuery(sql);
             if (resultSet.next()) {
-                return resultSet.getInt("id");
+                return resultSet.getInt("ID");
             } else {
                 throw new SQLException("Row does not exist");
             }
