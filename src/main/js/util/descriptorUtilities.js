d96944c7ee9f68c40a154463b08cae278ab884e4
psantos1113 <psantos@blackducksoftware.com>
2019-01-24 18:54:26 -0500

    Get the descriptors from alert and filter them.

---
+++ src/main/js/util/descriptorUtilities.js
@@ -0,0 +1,21 @@
+export const DESCRIPTOR_TYPE = {
+    PROVIDER: 'PROVIDER',
+    CHANNEL: 'CHANNEL',
+    COMPONENT: 'COMPONENT'
+}
+
+export const CONTEXT_TYPE = {
+    GLOBAL: 'GLOBAL',
+    DISTRIBUTION: 'DISTRIBUTION'
+}
+
+
+export function findDescriptorByTypeAndContext(descriptorList, descriptorType, context) {
+    if (!descriptorList) {
+        return null;
+    }
+    const resultList = descriptorList.filter((descriptor) => descriptor.type === descriptorType && descriptor.context === context);
+    if (!resultList) {
+        return null;
+    }
+}
