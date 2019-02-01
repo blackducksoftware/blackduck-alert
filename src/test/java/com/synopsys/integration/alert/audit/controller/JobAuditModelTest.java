dfc3046bb60bde993098c16054d78ab92aa2d19d
Gavin Killough <gavink@synopsys.com>
2019-02-01 13:39:11 -0500

    Refactor audit model names to better reflect their contents

--- src/test/java/com/synopsys/integration/alert/audit/controller/JobAuditModelTest.java
+++ src/test/java/com/synopsys/integration/alert/audit/controller/JobAuditModelTest.java
@@ -3,42 +3,42 @@ package com.synopsys.integration.alert.audit.controller;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
-import com.synopsys.integration.alert.audit.mock.MockJobModel;
-import com.synopsys.integration.alert.web.audit.JobModel;
+import com.synopsys.integration.alert.audit.mock.MockJobAuditModel;
+import com.synopsys.integration.alert.web.audit.JobAuditModel;
 import com.synopsys.integration.alert.web.model.RestModelTest;
 
-public class JobModelTest extends RestModelTest<JobModel> {
+public class JobAuditModelTest extends RestModelTest<JobAuditModel> {
 
     @Override
-    public MockJobModel getMockUtil() {
-        return new MockJobModel();
+    public MockJobAuditModel getMockUtil() {
+        return new MockJobAuditModel();
     }
 
     @Override
-    public Class<JobModel> getRestModelClass() {
-        return JobModel.class;
+    public Class<JobAuditModel> getRestModelClass() {
+        return JobAuditModel.class;
     }
 
     @Override
-    public void assertRestModelFieldsNull(final JobModel restModel) {
+    public void assertRestModelFieldsNull(final JobAuditModel restModel) {
         assertNull(restModel.getErrorMessage());
         assertNull(restModel.getErrorStackTrace());
         assertNull(restModel.getEventType());
         assertNull(restModel.getName());
-        assertNull(restModel.getJobAuditModel());
+        assertNull(restModel.getAuditJobStatusModel());
         assertNull(restModel.getConfigId());
         assertNull(restModel.getId());
     }
 
     @Override
-    public void assertRestModelFieldsFull(final JobModel restModel) {
+    public void assertRestModelFieldsFull(final JobAuditModel restModel) {
         assertEquals(getMockUtil().getErrorMessage(), restModel.getErrorMessage());
         assertEquals(getMockUtil().getErrorStackTrace(), restModel.getErrorStackTrace());
         assertEquals(getMockUtil().getEventType(), restModel.getEventType());
         assertEquals(getMockUtil().getName(), restModel.getName());
-        assertEquals(getMockUtil().getStatus(), restModel.getJobAuditModel().getStatus());
-        assertEquals(getMockUtil().getTimeAuditCreated(), restModel.getJobAuditModel().getTimeAuditCreated());
-        assertEquals(getMockUtil().getTimeLastSent(), restModel.getJobAuditModel().getTimeLastSent());
+        assertEquals(getMockUtil().getStatus(), restModel.getAuditJobStatusModel().getStatus());
+        assertEquals(getMockUtil().getTimeAuditCreated(), restModel.getAuditJobStatusModel().getTimeAuditCreated());
+        assertEquals(getMockUtil().getTimeLastSent(), restModel.getAuditJobStatusModel().getTimeLastSent());
         assertEquals(getMockUtil().getConfigId(), restModel.getConfigId());
         assertEquals(getMockUtil().getId().toString(), restModel.getId());
     }
