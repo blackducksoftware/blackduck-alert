d706fc3a0adb470b524b20cefdf6644b9014898e
Gavin Killough <gavink@synopsys.com>
2019-02-26 08:56:21 -0500

    Uncomment Polaris component annotations

--- src/main/java/com/synopsys/integration/alert/provider/polaris/PolarisProvider.java
+++ src/main/java/com/synopsys/integration/alert/provider/polaris/PolarisProvider.java
@@ -26,11 +26,13 @@ package com.synopsys.integration.alert.provider.polaris;
 import java.util.EnumSet;
 import java.util.Set;
 
+import org.springframework.stereotype.Component;
+
 import com.synopsys.integration.alert.common.enumeration.FormatType;
 import com.synopsys.integration.alert.common.provider.Provider;
 import com.synopsys.integration.alert.common.provider.ProviderContentType;
 
-// TODO exclude for now: @Component(PolarisProvider.COMPONENT_NAME)
+@Component(PolarisProvider.COMPONENT_NAME)
 public class PolarisProvider extends Provider {
     public static final String COMPONENT_NAME = "provider_polaris";
 
