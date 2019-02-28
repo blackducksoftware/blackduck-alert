0e128f2fba3fa2891fc468c767659dfce15d7018
jrichard <jrichard@synopsys.com>
2019-02-28 13:50:15 -0500

    Preparing for the field descriptions

--- src/main/js/field/LabeledField.js
+++ src/main/js/field/LabeledField.js
@@ -7,9 +7,14 @@ class LabeledField extends Component {
         return (
             <div className="form-group">
                 <label className="col-sm-3 col-form-label text-right">{this.props.label}</label>
+                {this.props.description &&
+                <div className="d-inline-flex">
+                    <span className="fa fa-question-circle" tabIndex="0" data-delay='{"show": 300, "hide": 100}' data-toggle="tooltip" title={this.props.description} />
+                </div>
+                }
                 {field}
                 {this.props.errorMessage &&
-                <div className="offset-sm-3 col-sm-9">
+                <div className="offset-sm-3 col-sm-8">
                     <p className="fieldError">{this.props.errorMessage}</p>
                 </div>
                 }
@@ -21,12 +26,14 @@ class LabeledField extends Component {
 LabeledField.propTypes = {
     field: PropTypes.node,
     label: PropTypes.string.isRequired,
+    description: PropTypes.string,
     errorMessage: PropTypes.string
 };
 
 LabeledField.defaultProps = {
     field: null,
-    errorMessage: null
+    errorMessage: null,
+    description: null
 };
 
 export default LabeledField;
