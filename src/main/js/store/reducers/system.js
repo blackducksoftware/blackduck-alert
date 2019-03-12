e341046395793aff4dc0df55f0dfad8e90af0bcf
jrichard <jrichard@synopsys.com>
2019-03-12 11:50:50 -0400

    Fixing settings so that a confirmation message appears when they are saved

--- src/main/js/store/reducers/system.js
+++ src/main/js/store/reducers/system.js
@@ -9,12 +9,12 @@ import {
     SYSTEM_SETUP_FETCHING,
     SYSTEM_SETUP_HIDE_RESET_PASSWORD_MODAL,
     SYSTEM_SETUP_PASSWORD_RESETTING,
+    SYSTEM_SETUP_SHOW_CONFIG,
     SYSTEM_SETUP_SHOW_RESET_PASSWORD_MODAL,
     SYSTEM_SETUP_UPDATE_ERROR,
     SYSTEM_SETUP_UPDATED,
     SYSTEM_SETUP_UPDATING
 } from 'store/actions/types';
-import { SYSTEM_SETUP_SHOW_CONFIG } from "../actions/types";
 
 const initialState = {
     fetching: false,
@@ -22,41 +22,45 @@ const initialState = {
     fetchingSetupStatus: '',
     updateStatus: '',
     latestMessages: [],
-    errorMessage: '',
+    errorMessage: null,
+    actionMessage: null,
     settingsData: {},
     systemInitialized: true,
     showPasswordResetModal: false,
-    error: {
-        message: ''
-    }
+    error: {}
 };
 
 const config = (state = initialState, action) => {
     switch (action.type) {
         case SYSTEM_LATEST_MESSAGES_FETCHING:
             return Object.assign({}, state, {
+                actionMessage: null,
                 fetching: true,
                 updateStatus: ''
             });
         case SYSTEM_LATEST_MESSAGES_FETCHED:
             return Object.assign({}, state, {
+                actionMessage: null,
                 fetching: false,
                 latestMessages: action.latestMessages,
                 updateStatus: ''
             });
         case SYSTEM_LATEST_MESSAGES_FETCH_ERROR:
             return Object.assign({}, state, {
+                actionMessage: null,
                 fetching: false,
                 updateStatus: ''
             });
         case SYSTEM_SETUP_FETCHING:
             return Object.assign({}, state, {
+                actionMessage: null,
                 resettingPassword: false,
                 fetchingSetupStatus: SYSTEM_SETUP_FETCHING,
                 updateStatus: 'FETCHING'
             });
         case SYSTEM_SETUP_FETCH_REDIRECTED:
             return Object.assign({}, state, {
+                actionMessage: null,
                 resettingPassword: false,
                 fetchingSetupStatus: '',
                 systemInitialized: true,
@@ -65,6 +69,7 @@ const config = (state = initialState, action) => {
             });
         case SYSTEM_SETUP_SHOW_CONFIG:
             return Object.assign({}, state, {
+                actionMessage: null,
                 resettingPassword: false,
                 systemInitialized: false,
                 fetchingSetupStatus: SYSTEM_SETUP_FETCHED,
@@ -74,6 +79,7 @@ const config = (state = initialState, action) => {
             });
         case SYSTEM_SETUP_FETCHED:
             return Object.assign({}, state, {
+                actionMessage: null,
                 resettingPassword: false,
                 fetchingSetupStatus: SYSTEM_SETUP_FETCHED,
                 updateStatus: 'FETCHED',
@@ -82,22 +88,25 @@ const config = (state = initialState, action) => {
             });
         case SYSTEM_SETUP_FETCH_ERROR:
             return Object.assign({}, state, {
+                actionMessage: null,
                 resettingPassword: false,
                 updateStatus: 'ERROR',
                 fetchingSetupStatus: SYSTEM_SETUP_FETCH_ERROR,
+                errorMessage: action.message,
                 error: {
-                    message: action.message,
                     ...action.errors
                 }
             });
         case SYSTEM_SETUP_UPDATING:
             return Object.assign({}, state, {
+                actionMessage: null,
                 resettingPassword: false,
                 fetchingSetupStatus: SYSTEM_SETUP_UPDATING,
                 updateStatus: 'UPDATING'
             });
         case SYSTEM_SETUP_UPDATED:
             return Object.assign({}, state, {
+                actionMessage: 'Update successful',
                 resettingPassword: false,
                 fetchingSetupStatus: SYSTEM_SETUP_UPDATED,
                 updateStatus: 'UPDATED',
@@ -105,11 +114,12 @@ const config = (state = initialState, action) => {
             });
         case SYSTEM_SETUP_UPDATE_ERROR:
             return Object.assign({}, state, {
+                actionMessage: null,
                 resettingPassword: false,
                 fetchingSetupStatus: SYSTEM_SETUP_UPDATE_ERROR,
                 updateStatus: 'ERROR',
+                errorMessage: action.message,
                 error: {
-                    message: action.message,
                     ...action.errors
                 }
             });
