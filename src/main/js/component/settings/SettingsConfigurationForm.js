dcb2a82515e7c8b0345c857d364ff725f1a2ad92
jrichard <jrichard@synopsys.com>
2019-04-02 16:56:03 -0400

    JS: fixing issue with the ldap authentication type

--- src/main/js/component/settings/SettingsConfigurationForm.js
+++ src/main/js/component/settings/SettingsConfigurationForm.js
@@ -133,9 +133,13 @@ class SettingsConfigurationForm extends Component {
         const { errorMessage, actionMessage } = this.props;
 
         const fieldModel = this.state.settingsData;
-        const selectedAuthenticationType = FieldModelUtilities.getFieldModelSingleValue(fieldModel, KEY_LDAP_AUTHENTICATION_TYPE);
+        let selectedAuthenticationType = FieldModelUtilities.getFieldModelSingleValue(fieldModel, KEY_LDAP_AUTHENTICATION_TYPE);
+        if (selectedAuthenticationType) {
+            selectedAuthenticationType = selectedAuthenticationType.toString().toLowerCase();
+        }
         const authenticationTypeOptions = this.getAuthenticationTypes();
-        const selectedAuthenticationOption = authenticationTypeOptions.filter(option => option.value === selectedAuthenticationType.toLowerCase());
+
+        const selectedAuthenticationOption = authenticationTypeOptions.filter(option => option.value === selectedAuthenticationType);
 
         const selectedReferral = FieldModelUtilities.getFieldModelSingleValue(fieldModel, KEY_LDAP_REFERRAL);
         const referralOptions = this.getReferralOptions();
