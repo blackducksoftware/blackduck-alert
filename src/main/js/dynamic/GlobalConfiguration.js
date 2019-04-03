d10fe55797264d9d90e7464edd026bd88c5a43a9
Brian Mandel <bamandel@gmail.com>
2019-04-03 13:43:12 -0400

    Made all my lets constant

--- src/main/js/dynamic/GlobalConfiguration.js
+++ src/main/js/dynamic/GlobalConfiguration.js
@@ -63,8 +63,8 @@ class GlobalConfiguration extends React.Component {
         event.preventDefault();
         event.stopPropagation();
         const fieldModel = this.state.currentConfig;
-        let emptyModel = !FieldModelUtilities.hasAnyValuesExcludingId(fieldModel);
-        let id = FieldModelUtilities.getFieldModelId(fieldModel);
+        const emptyModel = !FieldModelUtilities.hasAnyValuesExcludingId(fieldModel);
+        const id = FieldModelUtilities.getFieldModelId(fieldModel);
         if (emptyModel && id) {
             this.props.deleteConfig(id);
         } else {
--- src/main/js/providers/BlackDuckConfiguration.js
+++ src/main/js/providers/BlackDuckConfiguration.js
@@ -89,8 +89,8 @@ class BlackDuckConfiguration
         event.preventDefault();
         event.stopPropagation();
         const fieldModel = this.state.currentConfig;
-        let emptyModel = !FieldModelUtilities.hasAnyValuesExcludingId(fieldModel);
-        let id = FieldModelUtilities.getFieldModelId(fieldModel);
+        const emptyModel = !FieldModelUtilities.hasAnyValuesExcludingId(fieldModel);
+        const id = FieldModelUtilities.getFieldModelId(fieldModel);
         if (emptyModel && id) {
             this.props.deleteConfig(id);
         } else {
