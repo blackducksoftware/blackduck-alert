5a978fa646264f77052cb3199d8ad7e8421cd800
jrichard <jrichard@synopsys.com>
2019-04-09 11:58:07 -0400

    Making sure the button group is always on the screen when looking at a configuration

--- src/main/js/component/common/ConfigButtons.js
+++ src/main/js/component/common/ConfigButtons.js
@@ -6,7 +6,8 @@ import GeneralButton from 'field/input/GeneralButton';
 
 const ConfigButtons = props => (
     <div className="form-group">
-        <div className="d-inline-flex offset-sm-3 col-sm-8">
+        <div className="buttonGroupBuffer" />
+        <div className="buttonGroup d-inline-flex offset-sm-3 col-sm-8">
             <div className="progressContainer">
                 <div className="progressIcon">
                     {props.performingAction &&
