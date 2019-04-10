0ec48077d114b303c567f1b50644afd47196a717
psantos1113 <psantos@blackducksoftware.com>
2019-04-10 14:19:33 -0400

    refactor encapsulate the authentication code into an AuthenticationProvider for spring

--- src/main/java/com/synopsys/integration/alert/web/actions/LoginActions.java
+++ src/main/java/com/synopsys/integration/alert/web/actions/LoginActions.java
@@ -22,85 +22,28 @@
  */
 package com.synopsys.integration.alert.web.actions;
 
-import java.util.EnumSet;
-
-import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
 import org.springframework.stereotype.Component;
 
-import com.synopsys.integration.alert.common.exception.AlertLDAPConfigurationException;
-import com.synopsys.integration.alert.common.rest.model.UserModel;
-import com.synopsys.integration.alert.database.user.UserRole;
 import com.synopsys.integration.alert.web.model.LoginConfig;
-import com.synopsys.integration.alert.web.security.authentication.ldap.LdapManager;
+import com.synopsys.integration.alert.web.security.authentication.AlertAuthenticationProvider;
 
 @Component
 public class LoginActions {
-    private static final Logger logger = LoggerFactory.getLogger(LoginActions.class);
-    private final DaoAuthenticationProvider alertDatabaseAuthProvider;
-    private final LdapManager ldapManager;
+    private final AlertAuthenticationProvider authenticationProvider;
 
     @Autowired
-    public LoginActions(final DaoAuthenticationProvider alertDatabaseAuthProvider, final LdapManager ldapManager) {
-        this.alertDatabaseAuthProvider = alertDatabaseAuthProvider;
-        this.ldapManager = ldapManager;
+    public LoginActions(final AlertAuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
     }
 
     public boolean authenticateUser(final LoginConfig loginConfig) throws BadCredentialsException {
-        Authentication authentication;
-        authentication = performLdapAuthentication(loginConfig);
-        if (!authentication.isAuthenticated()) {
-            authentication = performDatabaseAuthentication(loginConfig);
-        }
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        return authentication.isAuthenticated() && isAuthorized(authentication);
-    }
-
-    private Authentication performDatabaseAuthentication(final LoginConfig loginConfig) {
-        logger.info("Attempting database authentication...");
         final Authentication pendingAuthentication = createUsernamePasswordAuthToken(loginConfig);
-        return alertDatabaseAuthProvider.authenticate(pendingAuthentication);
-    }
-
-    private Authentication performLdapAuthentication(final LoginConfig loginConfig) {
-        logger.info("Checking ldap based authentication...");
-        final Authentication pendingAuthentication = createUsernamePasswordAuthToken(loginConfig);
-        Authentication result;
-        if (ldapManager.isLdapEnabled()) {
-            logger.info("LDAP authentication enabled");
-            try {
-                final LdapAuthenticationProvider authenticationProvider = ldapManager.getAuthenticationProvider();
-                result = authenticationProvider.authenticate(pendingAuthentication);
-            } catch (final AlertLDAPConfigurationException ex) {
-                logger.error("LDAP Configuration error", ex);
-                result = pendingAuthentication;
-            } catch (final Exception ex) {
-                logger.error("LDAP Authentication error", ex);
-                result = pendingAuthentication;
-            }
-        } else {
-            logger.info("LDAP authentication disabled");
-            result = pendingAuthentication;
-        }
-        return result;
-    }
-
-    private boolean isAuthorized(final Authentication authentication) {
-        final EnumSet<UserRole> allowedRoles = EnumSet.allOf(UserRole.class);
-        return authentication.getAuthorities().stream()
-                   .map(GrantedAuthority::getAuthority)
-                   .filter(role -> role.startsWith(UserModel.ROLE_PREFIX))
-                   .map(role -> StringUtils.substringAfter(role, UserModel.ROLE_PREFIX))
-                   .anyMatch(roleName -> allowedRoles.contains(UserRole.valueOf(roleName)));
+        final Authentication authentication = authenticationProvider.authenticate(pendingAuthentication);
+        return authentication.isAuthenticated() && !authentication.getAuthorities().isEmpty();
     }
 
     private UsernamePasswordAuthenticationToken createUsernamePasswordAuthToken(final LoginConfig loginConfig) {
