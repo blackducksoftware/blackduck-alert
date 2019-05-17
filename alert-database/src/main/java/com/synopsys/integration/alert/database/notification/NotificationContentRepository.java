8c99e83a35daaf749259528d1a75bc56e9b23048
Gavin Killough <gavink@synopsys.com>
2019-05-17 15:16:01 -0400

    Only select columns relevant to return type

--- alert-database/src/main/java/com/synopsys/integration/alert/database/notification/NotificationContentRepository.java
+++ alert-database/src/main/java/com/synopsys/integration/alert/database/notification/NotificationContentRepository.java
@@ -31,8 +31,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.synopsys.integration.alert.common.rest.model.AlertNotificationWrapper;
 import com.synopsys.integration.alert.common.descriptor.config.ui.ChannelDistributionUIConfig;
+import com.synopsys.integration.alert.common.rest.model.AlertNotificationWrapper;
 
 public interface NotificationContentRepository extends JpaRepository<NotificationContent, Long> {
     @Query("SELECT entity FROM NotificationContent entity WHERE entity.createdAt BETWEEN ?1 AND ?2 ORDER BY created_at, provider_creation_time asc")
@@ -44,34 +44,40 @@ public interface NotificationContentRepository extends JpaRepository<Notificatio
     @Query(value = "SELECT entity FROM NotificationContent entity WHERE entity.id IN (SELECT notificationId FROM entity.auditNotificationRelations WHERE entity.id = notificationId)")
     Page<AlertNotificationWrapper> findAllSentNotifications(final Pageable pageable);
 
-    @Query(value = "SELECT DISTINCT entity FROM NotificationContent entity LEFT JOIN entity.auditNotificationRelations relation ON entity.id = relation.notificationId "
+    @Query(value = "SELECT DISTINCT "
+                       + "notificationRow.id, notificationRow.createdAt, notificationRow.provider, notificationRow.providerCreationTime, notificationRow.notificationType, notificationRow.content "
+                       + "FROM NotificationContent notificationRow "
+                       + "LEFT JOIN notificationRow.auditNotificationRelations relation ON notificationRow.id = relation.notificationId "
                        + "LEFT JOIN relation.auditEntryEntity auditEntry ON auditEntry.id = relation.auditEntryId "
                        + "LEFT JOIN ConfigGroupEntity configGroup ON auditEntry.commonConfigId = configGroup.jobId "
                        + "LEFT JOIN configGroup.descriptorConfigEntity descriptorConfig ON configGroup.configId = descriptorConfig.id "
                        + "LEFT JOIN descriptorConfig.fieldValueEntities fieldValue ON descriptorConfig.id = fieldValue.configId "
                        + "LEFT JOIN fieldValue.definedFieldEntity definedField ON fieldValue.fieldId = definedField.id "
-                       + "WHERE LOWER(entity.provider) LIKE %:searchTerm% OR "
-                       + "LOWER(entity.notificationType) LIKE %:searchTerm% OR "
-                       + "LOWER(entity.content) LIKE %:searchTerm% OR "
-                       + "LOWER(entity.createdAt) LIKE %:searchTerm% OR "
+                       + "WHERE LOWER(notificationRow.provider) LIKE %:searchTerm% OR "
+                       + "LOWER(notificationRow.notificationType) LIKE %:searchTerm% OR "
+                       + "LOWER(notificationRow.content) LIKE %:searchTerm% OR "
+                       + "LOWER(notificationRow.createdAt) LIKE %:searchTerm% OR "
                        + "LOWER(auditEntry.timeLastSent) LIKE %:searchTerm% OR "
                        + "LOWER(auditEntry.status) LIKE %:searchTerm% OR "
                        + "(definedField.key = '" + ChannelDistributionUIConfig.KEY_NAME + "' AND LOWER(fieldValue.value) LIKE %:searchTerm% ) OR "
                        + "(definedField.key = '" + ChannelDistributionUIConfig.KEY_CHANNEL_NAME + "' AND LOWER(fieldValue.value) LIKE %:searchTerm% )")
     Page<AlertNotificationWrapper> findMatchingNotification(@Param("searchTerm") String searchTerm, final Pageable pageable);
 
-    @Query(value = "SELECT DISTINCT entity FROM NotificationContent entity LEFT JOIN entity.auditNotificationRelations relation ON entity.id = relation.notificationId "
+    @Query(value = "SELECT DISTINCT "
+                       + "notificationRow.id, notificationRow.createdAt, notificationRow.provider, notificationRow.providerCreationTime, notificationRow.notificationType, notificationRow.content "
+                       + "FROM NotificationContent notificationRow "
+                       + "LEFT JOIN notificationRow.auditNotificationRelations relation ON notificationRow.id = relation.notificationId "
                        + "LEFT JOIN relation.auditEntryEntity auditEntry ON auditEntry.id = relation.auditEntryId "
                        + "LEFT JOIN ConfigGroupEntity configGroup ON auditEntry.commonConfigId = configGroup.jobId "
                        + "LEFT JOIN configGroup.descriptorConfigEntity descriptorConfig ON configGroup.configId = descriptorConfig.id "
                        + "LEFT JOIN descriptorConfig.fieldValueEntities fieldValue ON descriptorConfig.id = fieldValue.configId "
                        + "LEFT JOIN fieldValue.definedFieldEntity definedField ON fieldValue.fieldId = definedField.id "
-                       + "WHERE entity.id IN (SELECT notificationId FROM entity.auditNotificationRelations WHERE entity.id = notificationId) AND "
+                       + "WHERE notificationRow.id IN (SELECT notificationId FROM notificationRow.auditNotificationRelations WHERE notificationRow.id = notificationId) AND "
                        + "("
-                       + "LOWER(entity.provider) LIKE %:searchTerm% OR "
-                       + "LOWER(entity.notificationType) LIKE %:searchTerm% OR "
-                       + "LOWER(entity.content) LIKE %:searchTerm% OR "
-                       + "LOWER(entity.createdAt) LIKE %:searchTerm% OR "
+                       + "LOWER(notificationRow.provider) LIKE %:searchTerm% OR "
+                       + "LOWER(notificationRow.notificationType) LIKE %:searchTerm% OR "
+                       + "LOWER(notificationRow.content) LIKE %:searchTerm% OR "
+                       + "LOWER(notificationRow.createdAt) LIKE %:searchTerm% OR "
                        + "LOWER(auditEntry.timeLastSent) LIKE %:searchTerm% OR "
                        + "LOWER(auditEntry.status) LIKE %:searchTerm% OR "
                        + "(definedField.key = '" + ChannelDistributionUIConfig.KEY_NAME + "' AND LOWER(fieldValue.value) LIKE %:searchTerm% ) OR "
