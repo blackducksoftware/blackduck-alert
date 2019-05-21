61ee8d37a9198d158d5a6366a305721f480d3643
psantos1113 <psantos@blackducksoftware.com>
2019-05-21 16:44:51 -0400

    Feat(java): Implement the Jira channel basic object structure.

---
+++ src/main/java/com/synopsys/integration/alert/channel/jira/descriptor/JiraDescriptor.java
@@ -0,0 +1,43 @@
+/**
+ * blackduck-alert
+ *
+ * Copyright (c) 2019 Synopsys, Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.synopsys.integration.alert.channel.jira.descriptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.synopsys.integration.alert.channel.jira.JiraChannel;
+import com.synopsys.integration.alert.common.descriptor.ChannelDescriptor;
+
+@Component
+public class JiraDescriptor extends ChannelDescriptor {
+
+    public static final String JIRA_LABEL = "Jira";
+    public static final String JIRA_URL = "jira";
+    public static final String JIRA_ICON = "jira";
+    public static final String JIRA_DESCRIPTION = "This page allows you to configure the Jira server that Alert will send issue updates to.";
+
+    @Autowired
+    public JiraDescriptor(final JiraGlobalUIConfig globalUIConfig, final JiraDistributionUIConfig distributionUIConfig) {
+        super(JiraChannel.COMPONENT_NAME, distributionUIConfig, globalUIConfig);
+    }
+}
---
+++ src/main/java/com/synopsys/integration/alert/channel/jira/descriptor/JiraDistributionUIConfig.java
@@ -0,0 +1,47 @@
+/**
+ * blackduck-alert
+ *
+ * Copyright (c) 2019 Synopsys, Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.synopsys.integration.alert.channel.jira.descriptor;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import com.synopsys.integration.alert.channel.jira.JiraChannel;
+import com.synopsys.integration.alert.common.descriptor.DescriptorMap;
+import com.synopsys.integration.alert.common.descriptor.config.field.ConfigField;
+import com.synopsys.integration.alert.common.descriptor.config.ui.ChannelDistributionUIConfig;
+
+@Component
+public class JiraDistributionUIConfig extends ChannelDistributionUIConfig {
+    @Autowired
+    public JiraDistributionUIConfig(@Lazy final DescriptorMap descriptorMap) {
+        super(JiraChannel.COMPONENT_NAME, JiraDescriptor.JIRA_LABEL, JiraDescriptor.JIRA_URL, JiraDescriptor.JIRA_ICON, descriptorMap);
+    }
+
+    @Override
+    public List<ConfigField> createChannelDistributionFields() {
+        return List.of();
+    }
+}
