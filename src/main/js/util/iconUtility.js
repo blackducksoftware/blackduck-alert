30449fdb94bd78c0e081e12d89e8bee8fb83e4fd
psantos1113 <psantos@blackducksoftware.com>
2019-05-23 10:17:57 -0400

    Fix(js): Update the icon set delimiter and comments.

--- src/main/js/util/iconUtility.js
+++ src/main/js/util/iconUtility.js
@@ -1,9 +1,16 @@
+// Because Font Awesome now has different icon sets we need to create an array that includes the icon set.
+// the default set is fas the solid icons.  Brands are in the fab set.
+// if the parameter to this function contains a '/' we construct an array from the string splitting on '/' to denote the icon set.
+// if the parameter to this function does not contain a '/' then we use the default icon set.
+// Format:
+// <FA_SET>/<ICON_NAME> = ['<FA_SET>', '<ICON_NAME>']
+// <ICON_NAME> = ['fas', '<ICON_NAME>]'
 export function createIconPath(iconPathData) {
     let path = null;
     if (iconPathData) {
         path = ['fas'];
-        if (iconPathData.includes(',')) {
-            path = iconPathData.split(',');
+        if (iconPathData.includes('/')) {
+            path = iconPathData.split('/');
         } else {
             path.push(iconPathData);
         }
