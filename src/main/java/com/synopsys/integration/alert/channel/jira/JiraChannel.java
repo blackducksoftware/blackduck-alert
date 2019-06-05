1ca7237d2943eced71640a6afa4791ae2e8926cf
Brian Mandel <bmandel@blackducksoftware.com>
2019-06-05 17:00:53 -0400

    Started implementing Jira Cloud channel functionality.

--- src/main/java/com/synopsys/integration/alert/channel/jira/JiraChannel.java
+++ src/main/java/com/synopsys/integration/alert/channel/jira/JiraChannel.java
@@ -27,11 +27,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import com.google.gson.Gson;
+import com.synopsys.integration.alert.channel.jira.descriptor.JiraDescriptor;
 import com.synopsys.integration.alert.common.AlertProperties;
 import com.synopsys.integration.alert.common.channel.DistributionChannel;
 import com.synopsys.integration.alert.common.event.DistributionEvent;
+import com.synopsys.integration.alert.common.exception.AlertException;
 import com.synopsys.integration.alert.common.persistence.accessor.AuditUtility;
+import com.synopsys.integration.alert.common.persistence.accessor.FieldAccessor;
 import com.synopsys.integration.exception.IntegrationException;
+import com.synopsys.integration.jira.common.cloud.configuration.JiraServerConfig;
+import com.synopsys.integration.jira.common.cloud.configuration.JiraServerConfigBuilder;
+import com.synopsys.integration.jira.common.cloud.model.request.IssueCreationRequestModel;
+import com.synopsys.integration.jira.common.cloud.rest.JiraCloudHttpClient;
+import com.synopsys.integration.jira.common.cloud.rest.service.IssueService;
+import com.synopsys.integration.jira.common.cloud.rest.service.JiraCloudServiceFactory;
+import com.synopsys.integration.log.Slf4jIntLogger;
 
 @Component(value = JiraChannel.COMPONENT_NAME)
 public class JiraChannel extends DistributionChannel {
@@ -45,10 +55,33 @@ public class JiraChannel extends DistributionChannel {
     @Override
     public void sendMessage(final DistributionEvent event) throws IntegrationException {
         logger.info("Received event to send to Jira {}", event);
+
+        final FieldAccessor fieldAccessor = event.getFieldAccessor();
+        final JiraServerConfigBuilder jiraServerConfigBuilder = new JiraServerConfigBuilder();
+        final String jiraUrl = fieldAccessor.getString(JiraDescriptor.KEY_JIRA_URL).orElse(null);
+        final String accessToken = fieldAccessor.getString(JiraDescriptor.KEY_JIRA_ACCESS_TOKEN).orElse(null);
+        final String username = fieldAccessor.getString(JiraDescriptor.KEY_JIRA_USERNAME).orElse(null);
+        jiraServerConfigBuilder.setUrl(jiraUrl);
+        jiraServerConfigBuilder.setApiToken(accessToken);
+        jiraServerConfigBuilder.setAuthUserEmail(username);
+        try {
+            final JiraServerConfig config = jiraServerConfigBuilder.build();
+            final Slf4jIntLogger intLogger = new Slf4jIntLogger(logger);
+            final JiraCloudHttpClient jiraHttpClient = config.createJiraHttpClient(intLogger);
+            final JiraCloudServiceFactory jiraCloudServiceFactory = new JiraCloudServiceFactory(intLogger, jiraHttpClient, getGson());
+            final IssueService issueService = jiraCloudServiceFactory.createIssueService();
+        } catch (final IllegalArgumentException e) {
+            throw new AlertException("There was an issue building the configuration: " + e.getMessage());
+        }
     }
 
     @Override
     public String getDestinationName() {
         return COMPONENT_NAME;
     }
+
+    private IssueCreationRequestModel createRequest(final FieldAccessor fieldAccessor) {
+        return null;
+    }
+
 }
