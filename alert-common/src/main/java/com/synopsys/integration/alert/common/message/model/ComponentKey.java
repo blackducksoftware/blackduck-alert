288601d3496dce6148f25aa8523b0f206a923d2d
Gavin Killough <gavink@synopsys.com>
2019-06-17 09:05:32 -0400

    Feat: Add ComponentKey and begin rewriting collapsing logic

---
+++ alert-common/src/main/java/com/synopsys/integration/alert/common/message/model/ComponentKey.java
@@ -0,0 +1,62 @@
+package com.synopsys.integration.alert.common.message.model;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.synopsys.integration.alert.common.rest.model.AlertSerializableModel;
+
+public class ComponentKey extends AlertSerializableModel {
+    private static final char KEY_SEPARATOR = '_';
+
+    private final String category;
+    private final String componentName;
+    private final String componentValue;
+    private final String subComponentName;
+    private final String subComponentValue;
+    private final String additionalData;
+
+    public ComponentKey(final String category, final String componentName, final String componentValue, final String subComponentName, final String subComponentValue, final String additionalData) {
+        this.category = category;
+        this.componentName = componentName;
+        this.componentValue = componentValue;
+        this.subComponentName = subComponentName;
+        this.subComponentValue = subComponentValue;
+        this.additionalData = additionalData;
+    }
+
+    public String getKey() {
+        final List<String> keyParts = List.of(this.category, componentName, componentValue, subComponentName, subComponentValue, additionalData);
+        return StringUtils.join(keyParts, KEY_SEPARATOR);
+    }
+
+    public String prettyPrint() {
+        StringBuilder prettyPrintBuilder = new StringBuilder();
+        prettyPrintBuilder.append(category);
+        prettyPrintBuilder.append(" - ");
+        prettyPrintBuilder.append(componentName);
+        prettyPrintBuilder.append(": ");
+        prettyPrintBuilder.append(componentValue);
+        if (StringUtils.isNotBlank(subComponentName) && StringUtils.isNotBlank(subComponentValue)) {
+            prettyPrintBuilder.append(", ");
+            prettyPrintBuilder.append(subComponentName);
+            prettyPrintBuilder.append(": ");
+            prettyPrintBuilder.append(subComponentValue);
+        }
+        if (StringUtils.isNotBlank(additionalData)) {
+            prettyPrintBuilder.append(", ");
+            prettyPrintBuilder.append(additionalData);
+        }
+        return prettyPrintBuilder.toString();
+    }
+
+    @Override
+    public boolean equals(final Object otherObject) {
+        if (otherObject != null && otherObject instanceof ComponentKey) {
+            ComponentKey otherKey = (ComponentKey) otherObject;
+            return this.getKey().equals(otherKey.getKey());
+        }
+        return false;
+    }
+
+}
