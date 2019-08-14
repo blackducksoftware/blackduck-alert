3ed3b37f8aca2f1b0bd5909009a738e807ac767f
Brian Mandel <bmandel@blackducksoftware.com>
2019-08-14 12:51:43 -0400

    JS: Added some extra code to better show how we get the channel model and provider model from the job.

--- src/main/js/dynamic/DistributionConfiguration.js
+++ src/main/js/dynamic/DistributionConfiguration.js
@@ -66,8 +66,9 @@ class DistributionConfiguration extends Component {
                 this.loading = false;
                 const { job } = nextProps;
                 if (job && job.fieldModels) {
-                    const channelModel = nextProps.job.fieldModels.find(model => FieldModelUtilities.hasKey(model, KEY_NAME));
-                    const providerModel = nextProps.job.fieldModels.find(model => !FieldModelUtilities.hasKey(model, KEY_NAME));
+                    const channelModel = nextProps.job.fieldModels.find(model => FieldModelUtilities.hasKey(model, KEY_CHANNEL_NAME));
+                    const providerName = FieldModelUtilities.getFieldModelValues(channelModel, KEY_PROVIDER_NAME);
+                    const providerModel = nextProps.job.fieldModels.find(model => providerName === model.descriptorName);
                     const newChannel = this.props.descriptors.find(descriptor => descriptor.name === channelModel.descriptorName && descriptor.context === DescriptorUtilities.CONTEXT_TYPE.DISTRIBUTION);
                     const newProvider = this.props.descriptors.find(descriptor => descriptor.name === providerModel.descriptorName && descriptor.context === DescriptorUtilities.CONTEXT_TYPE.DISTRIBUTION);
 
