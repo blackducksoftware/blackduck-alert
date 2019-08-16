cf539282deac6050a8e023d33295195071e5af2e
Brian Mandel <bmandel@blackducksoftware.com>
2019-08-16 17:08:34 -0400

    JAVA: Added a new ConfigField that contains a mapping between a checkbox and any other fields. The UI will use this to hide other fields depending on if the box is checked.

---
+++ alert-common/src/main/java/com/synopsys/integration/alert/common/descriptor/config/field/HideCheckboxConfigField.java
@@ -0,0 +1,31 @@
+package com.synopsys.integration.alert.common.descriptor.config.field;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.synopsys.integration.alert.common.enumeration.FieldType;
+
+public class HideCheckboxConfigField extends CheckboxConfigField {
+    private List<String> relatedHiddenFields;
+
+    public static HideCheckboxConfigField create(final String key, final String label, final String description) {
+        return new HideCheckboxConfigField(key, label, description, false);
+    }
+
+    public HideCheckboxConfigField(final String key, final String label, final String description, final boolean required) {
+        super(key, label, description, FieldType.HIDE_CHECKBOX_INPUT, required);
+        relatedHiddenFields = new LinkedList<>();
+    }
+
+    public HideCheckboxConfigField addHiddenFieldKey(String key) {
+        relatedHiddenFields.add(key);
+        return this;
+    }
+
+    public HideCheckboxConfigField addHiddenFieldKeys(String... key) {
+        relatedHiddenFields.addAll(Stream.of(key).collect(Collectors.toList()));
+        return this;
+    }
+}
