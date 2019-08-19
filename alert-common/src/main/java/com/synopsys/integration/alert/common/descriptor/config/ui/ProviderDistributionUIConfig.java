68c02476b80ec2078669bb997d49e31ec0cba2fe
Brian Mandel <bmandel@blackducksoftware.com>
2019-08-19 13:59:55 -0400

    JAVA: Reverted ProviderDistributionUIConfig.java back to its original code for now so that no unforeseen issues occur.

--- alert-common/src/main/java/com/synopsys/integration/alert/common/descriptor/config/ui/ProviderDistributionUIConfig.java
+++ alert-common/src/main/java/com/synopsys/integration/alert/common/descriptor/config/ui/ProviderDistributionUIConfig.java
@@ -33,8 +33,8 @@ import java.util.stream.Stream;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.text.WordUtils;
 
+import com.synopsys.integration.alert.common.descriptor.config.field.CheckboxConfigField;
 import com.synopsys.integration.alert.common.descriptor.config.field.ConfigField;
-import com.synopsys.integration.alert.common.descriptor.config.field.HideCheckboxConfigField;
 import com.synopsys.integration.alert.common.descriptor.config.field.LabelValueSelectOption;
 import com.synopsys.integration.alert.common.descriptor.config.field.SelectConfigField;
 import com.synopsys.integration.alert.common.descriptor.config.field.TextInputConfigField;
@@ -84,9 +84,11 @@ public abstract class ProviderDistributionUIConfig extends UIConfig {
                 .sorted()
                 .collect(Collectors.toList()));
 
-        final ConfigField filterByProject = HideCheckboxConfigField.create(KEY_FILTER_BY_PROJECT, LABEL_FILTER_BY_PROJECT, DESCRIPTION_FILTER_BY_PROJECT)
-                                                .addHiddenFieldKeys(KEY_PROJECT_NAME_PATTERN, KEY_CONFIGURED_PROJECT);
+        final ConfigField filterByProject = CheckboxConfigField.create(KEY_FILTER_BY_PROJECT, LABEL_FILTER_BY_PROJECT, DESCRIPTION_FILTER_BY_PROJECT);
         final ConfigField projectNamePattern = TextInputConfigField.create(KEY_PROJECT_NAME_PATTERN, LABEL_PROJECT_NAME_PATTERN, DESCRIPTION_PROJECT_NAME_PATTERN, this::validateProjectNamePattern);
+
+        // TODO figure out how to create a project listing (Perhaps a new field type called table)
+        // TODO create a linkedField that is an endpoint the UI hits to generate a field
         final ConfigField configuredProject = SelectConfigField.createEmpty(KEY_CONFIGURED_PROJECT, LABEL_PROJECTS, "", this::validateConfiguredProject);
 
         final List<ConfigField> configFields = List.of(notificationTypesField, formatField, filterByProject, projectNamePattern, configuredProject);
