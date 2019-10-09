d3e2be0c230d963b5d912ae17f0c6b2ff71c5458
Brian Mandel <bmandel@blackducksoftware.com>
2019-10-09 17:11:18 -0400

    JAVA: Added a couple more methods to SettingsUtility that will be helpful when dealing with settings outside of the Descriptor sub project.

--- alert-common/src/main/java/com/synopsys/integration/alert/common/descriptor/accessor/SettingsUtility.java
+++ alert-common/src/main/java/com/synopsys/integration/alert/common/descriptor/accessor/SettingsUtility.java
@@ -1,17 +1,23 @@
 package com.synopsys.integration.alert.common.descriptor.accessor;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 import com.synopsys.integration.alert.common.exception.AlertException;
 import com.synopsys.integration.alert.common.persistence.model.ConfigurationFieldModel;
 import com.synopsys.integration.alert.common.persistence.model.ConfigurationModel;
+import com.synopsys.integration.alert.common.persistence.model.DefinedFieldModel;
 
 public interface SettingsUtility {
+
+    String getSettingsName();
 
     Optional<ConfigurationModel> getSettings() throws AlertException;
 
     ConfigurationModel saveSettings(Collection<ConfigurationFieldModel> fieldModels) throws AlertException;
 
     ConfigurationModel updateSettings(Long id, Collection<ConfigurationFieldModel> fieldModels) throws AlertException;
+
+    List<DefinedFieldModel> getSettingsFields() throws AlertException;
 }
--- src/main/java/com/synopsys/integration/alert/component/settings/DefaultSettingsUtility.java
+++ src/main/java/com/synopsys/integration/alert/component/settings/DefaultSettingsUtility.java
@@ -1,6 +1,7 @@
 package com.synopsys.integration.alert.component.settings;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,19 +11,28 @@ import com.synopsys.integration.alert.common.descriptor.accessor.SettingsUtility
 import com.synopsys.integration.alert.common.enumeration.ConfigContextEnum;
 import com.synopsys.integration.alert.common.exception.AlertException;
 import com.synopsys.integration.alert.common.persistence.accessor.ConfigurationAccessor;
+import com.synopsys.integration.alert.common.persistence.accessor.DescriptorAccessor;
 import com.synopsys.integration.alert.common.persistence.model.ConfigurationFieldModel;
 import com.synopsys.integration.alert.common.persistence.model.ConfigurationModel;
+import com.synopsys.integration.alert.common.persistence.model.DefinedFieldModel;
 import com.synopsys.integration.alert.component.settings.descriptor.SettingsDescriptorKey;
 
 @Component
 public class DefaultSettingsUtility implements SettingsUtility {
     private SettingsDescriptorKey settingsDescriptorKey;
     private ConfigurationAccessor configurationAccessor;
+    private DescriptorAccessor descriptorAccessor;
 
     @Autowired
-    public DefaultSettingsUtility(SettingsDescriptorKey settingsDescriptorKey, ConfigurationAccessor configurationAccessor) {
+    public DefaultSettingsUtility(SettingsDescriptorKey settingsDescriptorKey, ConfigurationAccessor configurationAccessor, final DescriptorAccessor descriptorAccessor) {
         this.settingsDescriptorKey = settingsDescriptorKey;
         this.configurationAccessor = configurationAccessor;
+        this.descriptorAccessor = descriptorAccessor;
+    }
+
+    @Override
+    public String getSettingsName() {
+        return settingsDescriptorKey.getUniversalKey();
     }
 
     @Override
@@ -38,5 +48,10 @@ public class DefaultSettingsUtility implements SettingsUtility {
     @Override
     public ConfigurationModel updateSettings(final Long id, final Collection<ConfigurationFieldModel> fieldModels) throws AlertException {
         return configurationAccessor.updateConfiguration(id, fieldModels);
+    }
+
+    @Override
+    public List<DefinedFieldModel> getSettingsFields() throws AlertException {
+        return descriptorAccessor.getFieldsForDescriptor(settingsDescriptorKey.getUniversalKey(), ConfigContextEnum.GLOBAL);
     }
 }
