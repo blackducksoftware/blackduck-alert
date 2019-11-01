5c290640c4ed14058ead969d7307f8c38261cba0
James Richard <jrichard@synopsys.com>
2019-11-01 16:18:42 -0400

    Fix: Fixing the imports

--- src/test/java/com/synopsys/integration/alert/provider/blackduck/mock/MockProviderDataAccessor.java
+++ src/test/java/com/synopsys/integration/alert/provider/blackduck/mock/MockProviderDataAccessor.java
@@ -7,7 +7,15 @@ import com.synopsys.integration.alert.database.api.DefaultProviderDataAccessor;
 import com.synopsys.integration.alert.provider.blackduck.BlackDuckProviderKey;
 import com.synopsys.integration.alert.provider.polaris.PolarisProviderKey;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
 
 public final class MockProviderDataAccessor extends DefaultProviderDataAccessor {
     private final Map<String, Set<ProviderProject>> providerProjectMap;
