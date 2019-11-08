54bfedaba42ccbc3deb5772e20b838a25ebf4149
Gavin Killough <gavink@synopsys.com>
2019-11-08 10:38:02 -0500

    Fix(Java): Call applyReadOnly in the constructor of ReadOnlyConfigField

--- alert-common/src/main/java/com/synopsys/integration/alert/common/descriptor/config/field/ReadOnlyConfigField.java
+++ alert-common/src/main/java/com/synopsys/integration/alert/common/descriptor/config/field/ReadOnlyConfigField.java
@@ -27,6 +27,7 @@ import com.synopsys.integration.alert.common.enumeration.FieldType;
 public class ReadOnlyConfigField extends ConfigField {
     public ReadOnlyConfigField(String key, String label, String description) {
         super(key, label, description, FieldType.READ_ONLY);
+        applyReadOnly(true);
     }
 
 }
