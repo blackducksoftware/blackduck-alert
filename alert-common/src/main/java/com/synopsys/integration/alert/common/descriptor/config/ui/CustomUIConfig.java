075bdadcb59bbe234db71743001f057629e96769
Brian Mandel <bmandel@blackducksoftware.com>
2019-11-12 17:57:11 -0500

    JAVA: Added a comment to CustomUIConfig explaining how to use the Class and what is expected of the user.

--- alert-common/src/main/java/com/synopsys/integration/alert/common/descriptor/config/ui/CustomUIConfig.java
+++ alert-common/src/main/java/com/synopsys/integration/alert/common/descriptor/config/ui/CustomUIConfig.java
@@ -26,9 +26,14 @@ import java.util.List;
 
 import com.synopsys.integration.alert.common.descriptor.config.field.ConfigField;
 
+/**
+ * By extending this class you are going to use a component predefined in tha javascript UI code rather than field generation.
+ * The javascript components need to be located in the js/component/dynamic/loaded directory.
+ * The javascript component that exists for this CustomUIConfig class needs to be registered in the DescriptorContentLoader.js file.
+ */
 public abstract class CustomUIConfig extends UIConfig {
 
-    public CustomUIConfig(final String label, final String description, final String urlName, final String componentPath) {
+    public CustomUIConfig(String label, String description, String urlName, String componentPath) {
         super(label, description, urlName, componentPath);
     }
 
