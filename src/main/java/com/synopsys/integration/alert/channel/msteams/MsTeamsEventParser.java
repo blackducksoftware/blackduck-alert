ef75766b43f3ce5970cdb8e91277ab32080ce2db
Brian Mandel <bmandel@blackducksoftware.com>
2019-11-18 13:02:11 -0500

    JAVA & JSON & FTL: Added the entire MSTeams Message parser code to be used in 5.1.0.

--- src/main/java/com/synopsys/integration/alert/channel/msteams/MsTeamsEventParser.java
+++ src/main/java/com/synopsys/integration/alert/channel/msteams/MsTeamsEventParser.java
@@ -23,12 +23,13 @@
 package com.synopsys.integration.alert.channel.msteams;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.synopsys.integration.alert.channel.util.FreemarkerTemplatingService;
-import com.synopsys.integration.alert.common.event.DistributionEvent;
 import com.synopsys.integration.exception.IntegrationException;
 
 import freemarker.cache.TemplateLoader;
@@ -37,6 +38,9 @@ import freemarker.template.Template;
 
 @Component
 public class MsTeamsEventParser {
+    // There is a size limit in the request size that is allowed (20KB). This text limit is meant to hopefully keep the message under that size limit
+    private static final int MAX_TEXT_LIMIT_REQUEST = 10000;
+
     private FreemarkerTemplatingService freemarkerTemplatingService;
     private Configuration freemarkerConfiguration;
     private Template msTeamsTemplate;
@@ -53,17 +57,48 @@ public class MsTeamsEventParser {
         }
     }
 
-    public String toJson(DistributionEvent distributionEvent) throws IntegrationException {
-        MsTeamsMessage msTeamsMessage = createMessage(distributionEvent);
-        return toJson(msTeamsMessage);
-    }
+    public List<MsTeamsMessage> splitMessages(MsTeamsMessage msTeamsMessage) {
+        List<MsTeamsMessage> splitMessages = new LinkedList<>();
+        int totalMessageSize = 0;
+        List<MsTeamsSection> msTeamsSections = new LinkedList<>();
+        for (MsTeamsSection section : msTeamsMessage.getSections()) {
+            MsTeamsSection msTeamsSection = new MsTeamsSection();
+            msTeamsSection.setSubTopic(section.getSubTopic());
+            List<String> componentsMessage = new LinkedList<>();
+            for (String message : section.getComponentsMessage()) {
+                int messageLength = message.length();
+                if (totalMessageSize + messageLength >= MAX_TEXT_LIMIT_REQUEST) {
+                    msTeamsSection.setComponentsMessage(componentsMessage);
+                    msTeamsSections.add(msTeamsSection);
+                    MsTeamsMessage newMessage = new MsTeamsMessage(msTeamsMessage.getTitle(), msTeamsMessage.getTopic(), msTeamsSections);
+                    splitMessages.add(newMessage);
+                    componentsMessage = new LinkedList<>();
+                    msTeamsSections = new LinkedList<>();
+                    msTeamsSection = new MsTeamsSection();
+                    msTeamsSection.setSubTopic(section.getSubTopic());
+                    totalMessageSize = 0;
+                }
+                componentsMessage.add(message);
+                totalMessageSize += messageLength;
+            }
+            msTeamsSection.setComponentsMessage(componentsMessage);
+            msTeamsSections.add(msTeamsSection);
+        }
 
-    public MsTeamsMessage createMessage(DistributionEvent distributionEvent) {
-        return distributionEvent
-                   .getContent()
-                   .getSubContent()
-                   .stream()
-                   .collect(MsTeamsMessage::new, MsTeamsMessage::addContent, MsTeamsMessage::addAllContent);
+        MsTeamsMessage newMessage = new MsTeamsMessage(msTeamsMessage.getTitle(), msTeamsMessage.getTopic(), msTeamsSections);
+        splitMessages.add(newMessage);
+
+        int messagesSize = splitMessages.size();
+        if (messagesSize > 1) {
+            for (int i = 0; i < messagesSize; i++) {
+                MsTeamsMessage message = splitMessages.get(i);
+                String title = message.getTitle();
+                String newTitle = String.format("%s (%s/%s)", title, i + 1, messagesSize);
+                message.setTitle(newTitle);
+            }
+        }
+
+        return splitMessages;
     }
 
     public String toJson(MsTeamsMessage msTeamsMessage) throws IntegrationException {
