54203144d5ef844dc16f68bb28875d13d1dd8439
Brian Mandel <bmandel@blackducksoftware.com>
2019-12-19 15:49:04 -0500

    JAVA: Modified the description of Digest and Summary to be more accurate about what they do.

--- alert-common/src/main/java/com/synopsys/integration/alert/common/enumeration/FormatType.java
+++ alert-common/src/main/java/com/synopsys/integration/alert/common/enumeration/FormatType.java
@@ -24,8 +24,9 @@ package com.synopsys.integration.alert.common.enumeration;
 
 public enum FormatType {
     DEFAULT("Default", "The message will contain all the relevant data found in your selected provider."),
-    DIGEST("Digest", "The message will contain a delta of the content found in your selected provider since it was last queried."),
-    SUMMARY("Summary", "The message contains only a small and summarized overview of what was found in your selected provider.");
+    DIGEST("Digest", "The message will contain a delta of the content found in your selected provider since it was last queried."
+                         + " Add and Delete operations will cancel each other out depending on the order they occurred."),
+    SUMMARY("Summary", "The message contains only a summarized form of the Digest data.");
 
     private String label;
     private String description;
