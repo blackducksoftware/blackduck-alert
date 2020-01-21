2143e4cf17adfcc6748eab7f536c2e068fd5a198
Martin Chomicki <martinch@synopsys.com>
2020-01-21 13:40:24 -0500

    test: Adding tests and increasing test coverage for Configuration.

---
+++ alert-common/src/test/java/com/synopsys/integration/alert/common/rest/model/ConfigurationTest.java
@@ -0,0 +1,33 @@
+package com.synopsys.integration.alert.common.rest.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.synopsys.integration.alert.common.persistence.accessor.FieldAccessor;
+import com.synopsys.integration.alert.common.persistence.model.ConfigurationFieldModel;
+
+public class ConfigurationTest {
+    @Test
+    public void getFieldAccessorTest() {
+        String fieldKey = "Key1";
+
+        ConfigurationFieldModel testConfigurationFieldModel = Mockito.mock(ConfigurationFieldModel.class);
+        Map<String, ConfigurationFieldModel> keyToFieldMapTest = new HashMap<>();
+        keyToFieldMapTest.put(fieldKey, testConfigurationFieldModel);
+
+        Configuration testConfig = new Configuration(keyToFieldMapTest);
+        FieldAccessor testFieldAccessor = testConfig.getFieldAccessor(); //Could also assert not null here
+        Optional<ConfigurationFieldModel> newConfigurationFieldModel = testFieldAccessor.getField(fieldKey);
+
+        assertTrue(newConfigurationFieldModel.isPresent());   //Checking if the Optional has a value
+        assertEquals(testConfigurationFieldModel, newConfigurationFieldModel.get());
+
+    }
+}
