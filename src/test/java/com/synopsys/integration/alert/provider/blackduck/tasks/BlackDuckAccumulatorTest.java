66181dd5adb3cf8d3969586e93650092407b190e
psantos1113 <psantos@synopsys.com>
2020-02-21 09:07:35 -0500

    test: Mock the proxy manager.

--- src/test/java/com/synopsys/integration/alert/provider/blackduck/tasks/BlackDuckAccumulatorTest.java
+++ src/test/java/com/synopsys/integration/alert/provider/blackduck/tasks/BlackDuckAccumulatorTest.java
@@ -25,21 +25,25 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.scheduling.TaskScheduler;
 
+import com.google.gson.Gson;
 import com.synopsys.integration.alert.common.exception.AlertDatabaseConstraintException;
 import com.synopsys.integration.alert.common.message.model.DateRange;
 import com.synopsys.integration.alert.common.persistence.accessor.ProviderTaskPropertiesAccessor;
+import com.synopsys.integration.alert.common.rest.ProxyManager;
 import com.synopsys.integration.alert.common.rest.model.AlertNotificationModel;
 import com.synopsys.integration.alert.database.api.DefaultNotificationManager;
 import com.synopsys.integration.alert.provider.blackduck.BlackDuckProperties;
 import com.synopsys.integration.alert.provider.blackduck.BlackDuckProviderKey;
 import com.synopsys.integration.alert.provider.blackduck.TestBlackDuckProperties;
 import com.synopsys.integration.alert.util.TestAlertProperties;
+import com.synopsys.integration.alert.util.TestProperties;
 import com.synopsys.integration.blackduck.api.manual.enumeration.NotificationType;
 import com.synopsys.integration.blackduck.api.manual.view.NotificationView;
 import com.synopsys.integration.blackduck.rest.BlackDuckHttpClient;
 import com.synopsys.integration.blackduck.service.BlackDuckServicesFactory;
 import com.synopsys.integration.blackduck.service.NotificationService;
 import com.synopsys.integration.rest.RestConstants;
+import com.synopsys.integration.rest.proxy.ProxyInfo;
 
 public class BlackDuckAccumulatorTest {
     private static final BlackDuckProviderKey BLACK_DUCK_PROVIDER_KEY = new BlackDuckProviderKey();
@@ -59,7 +63,9 @@ public class BlackDuckAccumulatorTest {
 
         TestAlertProperties testAlertProperties = new TestAlertProperties();
         testAlertProperties.setAlertConfigHome(testAccumulatorParent.getCanonicalPath());
-        testBlackDuckProperties = new TestBlackDuckProperties(testAlertProperties);
+        ProxyManager proxyManager = Mockito.mock(ProxyManager.class);
+        Mockito.when(proxyManager.createProxyInfo()).thenReturn(ProxyInfo.NO_PROXY_INFO);
+        testBlackDuckProperties = new TestBlackDuckProperties(new Gson(), testAlertProperties, new TestProperties(), proxyManager);
 
         notificationManager = Mockito.mock(DefaultNotificationManager.class);
         taskScheduler = Mockito.mock(TaskScheduler.class);
