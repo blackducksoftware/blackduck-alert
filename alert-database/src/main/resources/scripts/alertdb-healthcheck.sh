d860b91c5ec399c473e89b87b3402ea108af970b
psantos1113 <psantos@synopsys.com>
2020-04-16 16:16:15 -0400

    feat: Read secrets files for credentials in healthcheck.

--- alert-database/src/main/resources/scripts/alertdb-healthcheck.sh
+++ alert-database/src/main/resources/scripts/alertdb-healthcheck.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
-if psql "host=alertdb port=5432 dbname=postgres user=$POSTGRES_USER password=$POSTGRES_PASSWORD" -c '\l' |grep -q 'alertdb';
+dockerSecretDir=/run/secrets
+alertDatabaseUser="$POSTGRES_USER"
+alertDatabasePassword="$POSTGRES_PASSWORD"
+
+if [ -e $dockerSecretDir/ALERT_DB_USERNAME ];
+then
+  alertDatabaseUser=$(cat $dockerSecretDir/ALERT_DB_USERNAME)
+fi
+
+if [ -e $dockerSecretDir/ALERT_DB_PASSWORD ];
+then
+  alertDatabasePassword=$(cat $dockerSecretDir/ALERT_DB_PASSWORD)
+fi
+
+alertDatabaseConfig="host=alertdb port=5432 dbname=alertdb user=$alertDatabaseUser password=$alertDatabasePassword"
+
+if psql "${alertDatabaseConfig}" -c '\l' |grep -q 'alertdb';
 then
     exit 0
 else
