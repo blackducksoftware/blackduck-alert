b0362f5f1f5a705ac6e9adc6470dd0a70ad2d803
psantos1113 <psantos@synopsys.com>
2020-04-16 15:57:31 -0400

    Feat: Use the environment variables for the health check.

--- alert-database/src/main/resources/scripts/alertdb-healthcheck.sh
+++ alert-database/src/main/resources/scripts/alertdb-healthcheck.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if psql "host=alertdb port=5432 dbname=postgres user=${POSTGRES_USER} password=${POSTGRES_PASSWORD}" -c '\l' |grep -q 'alertdb';
+if psql "host=alertdb port=5432 dbname=postgres user=$POSTGRES_USER password=$POSTGRES_PASSWORD" -c '\l' |grep -q 'alertdb';
 then
     exit 0
 else
