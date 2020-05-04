5fe153921d441eb433459daec258a29d9a7446a4
psantos1113 <psantos@synopsys.com>
2020-05-04 12:45:22 -0400

    fix: Update debug test.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -394,7 +394,7 @@ validatePostgresDatabase() {
 
 postgresPrepare600Upgrade() {
     echo "Determining if preparation for 6.0.0 upgrade is necessary..."
-    CONTEXT_QUERY = `psql "${alertDatabaseConfig}" -c 'SELECT COUNT(CONTEXT) FROM Alert.Config_Contexts;'`
+    CONTEXT_QUERY= `psql "${alertDatabaseConfig}" -c 'SELECT COUNT(CONTEXT) FROM Alert.Config_Contexts;'`
     echo "Query Result $CONTEXT_QUERY"
     if psql "${alertDatabaseConfig}" -c 'SELECT COUNT(CONTEXT) FROM Alert.Config_Contexts;' | grep -q '2';
     then
