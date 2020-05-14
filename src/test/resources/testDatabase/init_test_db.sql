acbc70919abd04f1f83935f91be4b5335dae3c3f
psantos1113 <psantos@synopsys.com>
2020-05-14 09:12:16 -0400

    fix: Change init script to be run multiple times without failing.

--- src/test/resources/testDatabase/init_test_db.sql
+++ src/test/resources/testDatabase/init_test_db.sql
@@ -1,12 +1,10 @@
 DO $$
 BEGIN
-  CREATE ROLE sa LOGIN PASSWORD 'blackduck';
-  EXCEPTION WHEN DUPLICATE_OBJECT THEN
-  RAISE NOTICE 'not creating role my_role -- it already exists';
+    -- install dblink extension
+    CREATE EXTENSION IF NOT EXISTS dblink;
+    -- create role and database if there are errors they will be logged.
+    PERFORM dblink('user=test dbname=postgres', 'CREATE ROLE sa LOGIN PASSWORD ''blackduck'' ', FALSE);
+    PERFORM dblink('user=test dbname=postgres', 'CREATE DATABASE alertdb WITH OWNER sa', FALSE);
+    PERFORM dblink('user=test dbname=postgres', 'CREATE SCHEMA IF NOT EXISTS alert AUTHORIZATION sa', FALSE);
 END
 $$;
-
-SELECT 'CREATE DATABASE alertdb WITH OWNER sa'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname= 'alertdb');
-
-CREATE SCHEMA IF NOT EXISTS alert;
