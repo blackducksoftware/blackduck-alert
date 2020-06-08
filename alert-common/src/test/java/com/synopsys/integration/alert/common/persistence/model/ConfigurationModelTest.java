b47810c7a8dce5ae37d1f8df921ee3e3d081754e
Martin Chomicki <martinch@synopsys.com>
2020-06-08 09:31:44 -0400

    test: Increasing test coverage for ConfigurationModel

---
+++ alert-common/src/test/java/com/synopsys/integration/alert/common/persistence/model/ConfigurationModelTest.java
@@ -0,0 +1,80 @@
+package com.synopsys.integration.alert.common.persistence.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import com.synopsys.integration.alert.common.enumeration.ConfigContextEnum;
+import com.synopsys.integration.alert.common.persistence.model.mutable.ConfigurationModelMutable;
+
+public class ConfigurationModelTest {
+    private final Long descriptorId = 1L;
+    private final Long configurationId = 2L;
+    private final String createdAt = "createdAt-test";
+    private final String lastUpdated = "lastUpdated-test";
+    private final ConfigContextEnum configContextEnum = ConfigContextEnum.GLOBAL;
+    private final String fieldKey = "fieldKey";
+
+    @Test
+    public void getDescriptorIdTest() {
+        ConfigurationModel configurationModel = createConfigurationModel();
+        assertEquals(descriptorId, configurationModel.getDescriptorId());
+    }
+
+    @Test
+    public void getConfigurationIdTest() {
+        ConfigurationModel configurationModel = createConfigurationModel();
+        assertEquals(configurationId, configurationModel.getConfigurationId());
+    }
+
+    @Test
+    public void getCreatedAtTest() {
+        ConfigurationModel configurationModel = createConfigurationModel();
+        assertEquals(createdAt, configurationModel.getCreatedAt());
+    }
+
+    @Test
+    public void getLastUpdatedTest() {
+        ConfigurationModel configurationModel = createConfigurationModel();
+        assertEquals(lastUpdated, configurationModel.getLastUpdated());
+    }
+
+    @Test
+    public void getDescriptorContextTest() {
+        ConfigurationModel configurationModel = createConfigurationModel();
+        assertEquals(configContextEnum, configurationModel.getDescriptorContext());
+    }
+
+    @Test
+    public void getFieldTest() {
+        ConfigurationModel configurationModel = createConfigurationModel();
+        assertFalse(configurationModel.getField(fieldKey).isPresent());
+    }
+
+    @Test
+    public void getCopyOfFieldListTest() {
+        ConfigurationModel configurationModel = createConfigurationModel();
+        assertTrue(configurationModel.getCopyOfFieldList().isEmpty());
+    }
+
+    @Test
+    public void getCopyOfKeyToFieldMapTest() {
+        ConfigurationModel configurationModel = createConfigurationModel();
+        assertTrue(configurationModel.getCopyOfKeyToFieldMap().isEmpty());
+    }
+
+    @Test
+    public void createMutableCopyTest() {
+        ConfigurationModel configurationModel = createConfigurationModel();
+        ConfigurationModelMutable configurationModelMutable = configurationModel.createMutableCopy();
+        assertEquals(configurationModel, configurationModelMutable);
+        assertTrue(configurationModel != configurationModelMutable);
+    }
+
+    private ConfigurationModel createConfigurationModel() {
+        return new ConfigurationModel(descriptorId, configurationId, createdAt, lastUpdated, configContextEnum);
+    }
+}
+
