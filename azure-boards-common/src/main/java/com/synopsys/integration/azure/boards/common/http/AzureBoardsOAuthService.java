0612092c30865423240e7b50702ef04ba6c767e9
Gavin Killough <gavink@synopsys.com>
2020-06-30 11:03:23 -0400

    Feat: Create azure-boards-common and create OAuth 2.0 example

---
+++ azure-boards-common/src/main/java/com/synopsys/integration/azure/boards/common/http/AzureBoardsOAuthService.java
@@ -0,0 +1,86 @@
+package com.synopsys.integration.azure.boards.common.http;
+
+import java.io.IOException;
+import java.net.Proxy;
+import java.util.Optional;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
+import com.google.api.client.auth.oauth2.AuthorizationCodeRequestUrl;
+import com.google.api.client.auth.oauth2.AuthorizationCodeTokenRequest;
+import com.google.api.client.auth.oauth2.BearerToken;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.auth.oauth2.StoredCredential;
+import com.google.api.client.auth.oauth2.TokenResponse;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.Base64;
+import com.google.api.client.util.store.DataStoreFactory;
+import com.synopsys.integration.exception.IntegrationException;
+
+public class AzureBoardsOAuthService {
+    private final String authorizationServerUrl;
+    private final Proxy proxy;
+    private final DataStoreFactory dataStoreFactory;
+
+    public AzureBoardsOAuthService(String authorizationServerUrl, Proxy proxy, DataStoreFactory dataStoreFactory) {
+        this.authorizationServerUrl = sanitizedUrl(authorizationServerUrl);
+        this.proxy = proxy;
+        this.dataStoreFactory = dataStoreFactory;
+    }
+
+    public Optional<Credential> getExistingOAuthCredential(AuthorizationCodeFlow authorizationCodeFlow, String userId) throws IntegrationException {
+        try {
+            Credential storedCredential = authorizationCodeFlow.loadCredential(userId);
+            return Optional.ofNullable(storedCredential);
+        } catch (IOException e) {
+            throw new IntegrationException(e);
+        }
+    }
+
+    public AuthorizationCodeRequestUrl getAuthorizationUrl(AuthorizationCodeFlow authorizationCodeFlow, String redirectUri) {
+        return authorizationCodeFlow.newAuthorizationUrl()
+                   .setClientId(authorizationCodeFlow.getClientId())
+                   .setRedirectUri(redirectUri);
+    }
+
+    public Credential authorizeAndStoreCredential(AuthorizationCodeFlow authorizationCodeFlow, String userId, String authorizationCode) throws IntegrationException {
+        try {
+            AuthorizationCodeTokenRequest authorizationCodeTokenRequest = authorizationCodeFlow.newTokenRequest(authorizationCode);
+            TokenResponse tokenResponse = authorizationCodeTokenRequest.execute();
+            return authorizationCodeFlow.createAndStoreCredential(tokenResponse, userId);
+        } catch (IOException e) {
+            throw new IntegrationException(e);
+        }
+    }
+
+    private AuthorizationCodeFlow initializeFlow(String clientId) throws IOException {
+        return new AuthorizationCodeFlow.Builder(
+            BearerToken.authorizationHeaderAccessMethod(),
+            new NetHttpTransport.Builder().setProxy(proxy).build(),
+            JacksonFactory.getDefaultInstance(),
+            new GenericUrl(),
+            null,
+            clientId,
+            encode(authorizationServerUrl)
+        )
+                   .setCredentialDataStore(StoredCredential.getDefaultDataStore(dataStoreFactory))
+                   .build();
+    }
+
+    private String encode(String str) {
+        byte[] encodedBytes = Base64.encodeBase64(str.getBytes());
+        return new String(encodedBytes);
+    }
+
+    private String sanitizedUrl(String url) {
+        url = StringUtils.trim(url);
+        if (StringUtils.endsWith(url, "/")) {
+            return StringUtils.chomp(url);
+        }
+        return url;
+    }
+
+}
