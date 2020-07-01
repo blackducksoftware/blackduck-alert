817cb1964b4ac2c9431fcc64c705b482e92fdd02
Gavin Killough <gavink@synopsys.com>
2020-07-01 12:38:45 -0400

    Refactor(Azure Library): Rename work item classes and add license headers

---
+++ azure-boards-common/src/main/java/com/synopsys/integration/azure/boards/common/service/workitem/AzureWorkItemService.java
@@ -0,0 +1,47 @@
+/**
+ * azure-boards-common
+ *
+ * Copyright (c) 2020 Synopsys, Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.synopsys.integration.azure.boards.common.service.workitem;
+
+import com.synopsys.integration.azure.boards.common.http.AzureHttpService;
+import com.synopsys.integration.azure.boards.common.http.HttpServiceException;
+import com.synopsys.integration.azure.boards.common.util.AzureSpecTemplate;
+
+public class AzureWorkItemService {
+    public static final AzureSpecTemplate API_SPEC_ORGANIZATION_PROJECT_WORKITEMS_INDIVIDUAL = new AzureSpecTemplate("/{organization}/{project}/_apis/wit/workitems/{workitemId}");
+
+    private final AzureHttpService azureHttpService;
+
+    public AzureWorkItemService(AzureHttpService azureHttpService) {
+        this.azureHttpService = azureHttpService;
+    }
+
+    public WorkItemResponseModel getWorkItem(String organization, String project, Integer workItemId) throws HttpServiceException {
+        String requestSpec = API_SPEC_ORGANIZATION_PROJECT_WORKITEMS_INDIVIDUAL
+                                 .defineReplacement("{organization}", organization)
+                                 .defineReplacement("{project}", project)
+                                 .defineReplacement("{workitemId}", workItemId.toString())
+                                 .populateSpec();
+        return azureHttpService.get(requestSpec, WorkItemResponseModel.class);
+    }
+
+}
