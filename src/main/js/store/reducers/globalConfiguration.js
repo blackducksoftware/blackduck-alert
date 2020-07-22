ead27f35f41e7ec067cbbb5e67245b3ac13daec9
psantos1113 <psantos@synopsys.com>
2020-07-22 10:53:16 -0400

    fix: Update the forbidden error response to show banner, not logout.

--- src/main/js/store/reducers/globalConfiguration.js
+++ src/main/js/store/reducers/globalConfiguration.js
@@ -3,9 +3,11 @@ import {
     CONFIG_CLEAR_FIELD_ERRORS,
     CONFIG_DELETED,
     CONFIG_DELETING,
+    CONFIG_FETCH_ERROR,
     CONFIG_FETCHED,
     CONFIG_FETCHING,
     CONFIG_REFRESH,
+    CONFIG_REFRESH_ERROR,
     CONFIG_REFRESHING,
     CONFIG_TEST_FAILED,
     CONFIG_TEST_SUCCESS,
@@ -52,7 +54,11 @@ const globalConfiguration = (state = initialState, action) => {
                 config: action.config,
                 error: HTTPErrorUtils.createEmptyErrorObject()
             });
-
+        case CONFIG_FETCH_ERROR:
+        case CONFIG_REFRESH_ERROR:
+            return Object.assign({}, state, {
+                error: HTTPErrorUtils.createErrorObject(action)
+            });
         case CONFIG_REFRESHING:
             return Object.assign({}, state, {
                 fetching: true,
