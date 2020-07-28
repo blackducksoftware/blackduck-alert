8bddba5a1110cc973e8afa8a98859da0f2d409e1
psantos1113 <psantos@synopsys.com>
2020-07-28 10:08:12 -0400

    fix: Clear the value if the options are empty or value is missing.

--- src/main/js/field/EndpointSelectField.js
+++ src/main/js/field/EndpointSelectField.js
@@ -10,10 +10,10 @@ class EndpointSelectField extends Component {
         super(props);
 
         this.onSendClick = this.onSendClick.bind(this);
-
+        this.emptyFieldValue = this.emptyFieldValue.bind(this);
         this.state = ({
             options: [],
-            progress: false,
+            progress: false
         });
     }
 
@@ -35,7 +35,7 @@ class EndpointSelectField extends Component {
             success: false
         });
         const {
-            fieldKey, csrfToken, currentConfig, endpoint, requestedDataFieldKeys
+            fieldKey, csrfToken, currentConfig, endpoint, requestedDataFieldKeys, value
         } = this.props;
 
         const newFieldModel = FieldModelUtilities.createFieldModelFromRequestedFields(currentConfig, requestedDataFieldKeys);
@@ -45,8 +45,16 @@ class EndpointSelectField extends Component {
                 response.json().then((data) => {
                     const options = data.map(item => {
                         const dataValue = item.value;
-                        return { key: dataValue, label: item.label, value: dataValue };
+                        return {
+                            key: dataValue,
+                            label: item.label,
+                            value: dataValue
+                        };
                     });
+                    const selectedValues = options.filter(option => value.includes(option.value));
+                    if (options.length === 0 || selectedValues.length === 0) {
+                        this.emptyFieldValue();
+                    }
 
                     this.setState({
                         options,
@@ -54,20 +62,34 @@ class EndpointSelectField extends Component {
                     });
                 });
             } else {
-                response.json().then((data) => {
+                response.json()
+                .then((data) => {
                     this.setState({
                         options: [],
                         fieldError: data.message
-                    });
+                    }, this.emptyFieldValue);
                 });
             }
         });
     }
 
+    emptyFieldValue() {
+        const { fieldKey, onChange } = this.props;
+        const eventObject = {
+            target: {
+                name: fieldKey,
+                value: []
+            }
+        };
+
+        onChange(eventObject);
+    }
+
     render() {
         return (
             <div>
-                <DynamicSelectInput onChange={this.props.onChange} onFocus={this.onSendClick} options={this.state.options} {...this.props} />
+                <DynamicSelectInput onChange={this.props.onChange} onFocus={this.onSendClick}
+                                    options={this.state.options} {...this.props} />
             </div>
         );
     }
