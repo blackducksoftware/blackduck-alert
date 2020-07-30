a87d6ae46ff6144e5f0dc96dc1235b1d1a49eb58
psantos1113 <psantos@synopsys.com>
2020-07-30 13:08:40 -0400

    refactor: Handle is errorValue is null.

--- src/main/js/field/LabeledField.js
+++ src/main/js/field/LabeledField.js
@@ -21,7 +21,12 @@ class LabeledField extends Component {
 
         const baseClasses = `${labelClass} text-right`;
         const labelClasses = (required) ? `${baseClasses} required` : baseClasses;
-        const { severity, fieldMessage } = errorValue;
+        let severity = 'ERROR';
+        let fieldMessage = '';
+        if (errorValue) {
+            severity = errorValue.severity;
+            fieldMessage = errorValue.fieldMessage;
+        }
         const fieldErrorClass = severity === 'ERROR' ? 'fieldError' : 'fieldWarning';
         const errorMessage = severity === 'WARNING' ? `Warning: ${fieldMessage}` : fieldMessage;
         let descriptionField = null;
