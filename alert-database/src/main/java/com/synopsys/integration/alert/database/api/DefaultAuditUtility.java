116e5f4a1a221ad73f17a443138bf1e8bee9d68b
Gavin Killough <gavink@synopsys.com>
2020-08-04 13:14:48 -0400

    Fix(Audit): Fix unnecessary save for audit-notification relation to address persistence warning

--- alert-database/src/main/java/com/synopsys/integration/alert/database/api/DefaultAuditUtility.java
+++ alert-database/src/main/java/com/synopsys/integration/alert/database/api/DefaultAuditUtility.java
@@ -137,9 +137,10 @@ public class DefaultAuditUtility implements AuditUtility {
         for (Long notificationId : allMessageNotificationIds) {
             AuditEntryEntity auditEntryEntity = new AuditEntryEntity(jobId, DateUtils.createCurrentDateTimestamp(), null, null, null, null);
 
+            boolean didAuditEntryExist = false;
             if (null != existingNotificationIdToAuditId && !existingNotificationIdToAuditId.isEmpty()) {
                 Long auditEntryId = existingNotificationIdToAuditId.get(notificationId);
-                if (null != auditEntryId) {
+                if (didAuditEntryExist = null != auditEntryId) {
                     auditEntryEntity = auditEntryRepository.findById(auditEntryId).orElse(auditEntryEntity);
                 }
             }
@@ -148,8 +149,10 @@ public class DefaultAuditUtility implements AuditUtility {
             AuditEntryEntity savedAuditEntryEntity = auditEntryRepository.save(auditEntryEntity);
 
             notificationIdToAuditId.put(notificationId, savedAuditEntryEntity.getId());
-            AuditNotificationRelation auditNotificationRelation = new AuditNotificationRelation(savedAuditEntryEntity.getId(), notificationId);
-            auditNotificationRepository.save(auditNotificationRelation);
+            if (!didAuditEntryExist) {
+                AuditNotificationRelation auditNotificationRelation = new AuditNotificationRelation(savedAuditEntryEntity.getId(), notificationId);
+                auditNotificationRepository.save(auditNotificationRelation);
+            }
         }
         return notificationIdToAuditId;
     }
