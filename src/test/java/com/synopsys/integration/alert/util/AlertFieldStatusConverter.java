3a8e864c502e0bebc1dfded2e80246186490ed64
Martin Chomicki <martinch@synopsys.com>
2020-08-07 15:21:34 -0400

    refactor: Add new static method to DataStructureUtils

--- src/test/java/com/synopsys/integration/alert/util/AlertFieldStatusConverter.java
+++ src/test/java/com/synopsys/integration/alert/util/AlertFieldStatusConverter.java
@@ -2,21 +2,16 @@ package com.synopsys.integration.alert.util;
 
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import com.synopsys.integration.alert.common.descriptor.config.field.errors.AlertFieldStatus;
+import com.synopsys.integration.alert.common.util.DataStructureUtils;
 
-public class AlertFieldStatusConverter {
+public final class AlertFieldStatusConverter {
     public static final Map<String, AlertFieldStatus> convertToMap(List<AlertFieldStatus> statusList) {
-        return statusList
-                   .stream()
-                   .collect(Collectors.toMap(AlertFieldStatus::getFieldName, Function.identity()));
+        return DataStructureUtils.mapToValues(statusList, AlertFieldStatus::getFieldName);
     }
 
     public static final Map<String, String> convertToStringMap(List<AlertFieldStatus> statusList) {
-        return statusList
-                   .stream()
-                   .collect(Collectors.toMap(AlertFieldStatus::getFieldName, AlertFieldStatus::getFieldMessage));
+        return DataStructureUtils.mapToMap(statusList, AlertFieldStatus::getFieldName, AlertFieldStatus::getFieldMessage);
     }
 }
