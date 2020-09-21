0b4e6e746f14b2757de6d9f0b92aba8e74b11fe0
psantos1113 <psantos@synopsys.com>
2020-09-21 13:06:11 -0400

    refactor: Update the action to use the optional api.

--- src/main/java/com/synopsys/integration/alert/web/api/about/AboutActions.java
+++ src/main/java/com/synopsys/integration/alert/web/api/about/AboutActions.java
@@ -22,8 +22,6 @@
  */
 package com.synopsys.integration.alert.web.api.about;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
@@ -41,14 +39,9 @@ public class AboutActions {
     }
 
     public ActionResponse<AboutModel> getAboutModel() {
-        ActionResponse<AboutModel> response;
-        Optional<AboutModel> content = aboutReader.getAboutModel();
-        if (content.isEmpty()) {
-            response = new ActionResponse<>(HttpStatus.NOT_FOUND);
-        } else {
-            response = new ActionResponse<>(HttpStatus.OK, aboutReader.getAboutModel().orElse(null));
-        }
-        return response;
+        return aboutReader.getAboutModel()
+                   .map(content -> new ActionResponse<>(HttpStatus.OK, content))
+                   .orElse(new ActionResponse<>(HttpStatus.NOT_FOUND));
     }
 
 }
