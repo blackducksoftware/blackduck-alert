bbe575645b341760dfd54de979ac14e63c16a243
psantos1113 <psantos@synopsys.com>
2020-09-24 13:28:48 -0400

    refactor: Extend AlertSerializableModel.

--- src/main/java/com/synopsys/integration/alert/web/api/system/MultiSystemMessageModel.java
+++ src/main/java/com/synopsys/integration/alert/web/api/system/MultiSystemMessageModel.java
@@ -25,8 +25,9 @@ package com.synopsys.integration.alert.web.api.system;
 import java.util.List;
 
 import com.synopsys.integration.alert.common.persistence.model.SystemMessageModel;
+import com.synopsys.integration.alert.common.rest.model.AlertSerializableModel;
 
-public class MultiSystemMessageModel {
+public class MultiSystemMessageModel extends AlertSerializableModel {
     private List<SystemMessageModel> systemMessages;
 
     public MultiSystemMessageModel(List<SystemMessageModel> systemMessages) {
