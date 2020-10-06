c90355e2e577cd54c1aff2142a36bbdd5659692d
James Richard <jrichard@synopsys.com>
2020-10-06 12:41:12 -0400

    Refactor(test): Renaming the test, renaming the test method, refactoring the private test methods

--- src/test/java/com/synopsys/integration/alert/AlertSetupTest.java
+++ src/test/java/com/synopsys/integration/alert/AlertSetupTest.java
@@ -5,13 +5,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
-import org.apache.commons.lang.StringUtils;
 import org.junit.Ignore;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.synopsys.integration.alert.common.enumeration.ConfigContextEnum;
 import com.synopsys.integration.alert.common.rest.model.FieldModel;
 import com.synopsys.integration.alert.common.rest.model.FieldValueModel;
 import com.synopsys.integration.alert.provider.blackduck.BlackDuckProviderKey;
@@ -30,28 +30,26 @@ import com.synopsys.integration.rest.request.Request;
 import com.synopsys.integration.rest.response.Response;
 
 @Tag(TestTags.DEFAULT_PERFORMANCE)
-public class EndToEndTest {
+public class AlertSetupTest {
     private final IntLogger intLogger = new PrintStreamIntLogger(System.out, LogLevel.INFO);
     private final String BLACKDUCK_PROVIDER_KEY = new BlackDuckProviderKey().getUniversalKey();
-    private final String blackDuckProvider = System.getenv("ALERT_PROVIDER_BLACKDUCK_BLACKDUCK_URL");
+    private final String blackDuckProviderUrl = System.getenv("ALERT_PROVIDER_BLACKDUCK_BLACKDUCK_URL");
     private final String blackDuckApiToken = System.getenv("ALERT_PROVIDER_BLACKDUCK_BLACKDUCK_API_KEY");
     private final String blackDuckTimeout = System.getenv("ALERT_PROVIDER_BLACKDUCK_BLACKDUCK_TIMEOUT");
-    private final String blackDuckProviderName = blackDuckProvider + UUID.randomUUID();
+    private final String blackDuckProviderName = blackDuckProviderUrl + UUID.randomUUID();
 
     private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-    private String cookie = null;
-    private String csrfToken = null;
+    private IntHttpClient client = new IntHttpClient(intLogger, 60, true, ProxyInfo.NO_PROXY_INFO);
 
     @Test
     @Ignore
-    public void testEndToEnd() throws Exception {
-        IntHttpClient client = new IntHttpClient(intLogger, 60, true, ProxyInfo.NO_PROXY_INFO);
-        loginToAlert(client);
-        setupBlackDuck(client);
+    public void testConfigureRunningAlert() throws Exception {
+        loginToAlert();
+        setupBlackDuck();
     }
 
-    private void loginToAlert(IntHttpClient client) throws Exception {
+    private void loginToAlert() throws Exception {
         Request.Builder requestBuilder = createRequestBuilder("api/login");
         String loginBody = "{\"alertUsername\":\"sysadmin\",\"alertPassword\":\"blackduck\"}";
         BodyContent bodyContent = new StringBodyContent(loginBody);
@@ -63,25 +61,33 @@ public class EndToEndTest {
             intLogger.error("Could not log into Alert.");
             response.throwExceptionForError();
         }
-        csrfToken = response.getHeaderValue("X-CSRF-TOKEN");
-        cookie = response.getHeaderValue("Set-Cookie");
+        String csrfToken = response.getHeaderValue("X-CSRF-TOKEN");
+        String cookie = response.getHeaderValue("Set-Cookie");
+        client.addCommonRequestHeader("X-CSRF-TOKEN", csrfToken);
+        client.addCommonRequestHeader("Set-Cookie", cookie);
         intLogger.info("Logged into Alert.");
     }
 
-    private void setupBlackDuck(IntHttpClient client) throws Exception {
+    private void setupBlackDuck() throws Exception {
         Map<String, FieldValueModel> keyToValues = new HashMap<>();
         keyToValues.put("provider.common.config.enabled", new FieldValueModel(List.of("true"), true));
         keyToValues.put("provider.common.config.name", new FieldValueModel(List.of(blackDuckProviderName), true));
-        keyToValues.put("blackduck.url", new FieldValueModel(List.of(blackDuckProvider), true));
+        keyToValues.put("blackduck.url", new FieldValueModel(List.of(blackDuckProviderUrl), true));
         keyToValues.put("blackduck.api.key", new FieldValueModel(List.of(blackDuckApiToken), true));
         keyToValues.put("blackduck.timeout", new FieldValueModel(List.of(blackDuckTimeout), true));
-        FieldModel blackDuckProviderConfiguration = new FieldModel(BLACKDUCK_PROVIDER_KEY, "GLOBAL", keyToValues);
+        FieldModel blackDuckProviderConfiguration = new FieldModel(BLACKDUCK_PROVIDER_KEY, ConfigContextEnum.GLOBAL.name(), keyToValues);
 
         String blackDuckConfigBody = gson.toJson(blackDuckProviderConfiguration);
-        BodyContent bodyContent = new StringBodyContent(blackDuckConfigBody);
+        BodyContent requestBody = new StringBodyContent(blackDuckConfigBody);
+
+        validateBlackDuckConfiguration(requestBody);
+        createBlackDuckConfiguration(requestBody);
+    }
+
+    private void validateBlackDuckConfiguration(BodyContent requestBody) throws Exception {
         Request.Builder validationRequestBuilder = createRequestBuilder("api/configuration/validate");
         validationRequestBuilder.method(HttpMethod.POST);
-        validationRequestBuilder.bodyContent(bodyContent);
+        validationRequestBuilder.bodyContent(requestBody);
         Request validationRequest = validationRequestBuilder.build();
         Response validationResponse = client.execute(validationRequest);
         if (validationResponse.isStatusCodeError()) {
@@ -89,10 +95,12 @@ public class EndToEndTest {
             validationResponse.throwExceptionForError();
         }
         intLogger.info("Validated the Black Duck Configuration.");
+    }
 
+    private void createBlackDuckConfiguration(BodyContent requestBody) throws Exception {
         Request.Builder creationRequestBuilder = createRequestBuilder("api/configuration");
         creationRequestBuilder.method(HttpMethod.POST);
-        creationRequestBuilder.bodyContent(bodyContent);
+        creationRequestBuilder.bodyContent(requestBody);
         Request creationRequest = creationRequestBuilder.build();
         Response creationResponse = client.execute(creationRequest);
         if (creationResponse.isStatusCodeError()) {
@@ -105,12 +113,6 @@ public class EndToEndTest {
     private Request.Builder createRequestBuilder(String path) throws IntegrationException {
         Request.Builder requestBuilder = new Request.Builder();
         requestBuilder.url(new HttpUrl("https://localhost:8443/alert/" + path));
-        if (StringUtils.isNotBlank(csrfToken)) {
-            requestBuilder.addHeader("X-CSRF-TOKEN", csrfToken);
-        }
-        if (StringUtils.isNotBlank(cookie)) {
-            requestBuilder.addHeader("Cookie", cookie);
-        }
         return requestBuilder;
     }
 
