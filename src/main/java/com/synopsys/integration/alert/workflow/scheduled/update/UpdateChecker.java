2fd59c032f08cf830a9cba139121b9b95902a5a3
psantos1113 <psantos@synopsys.com>
2020-10-09 21:43:05 -0400

    chore: Fix imports for StringUtils.

--- src/main/java/com/synopsys/integration/alert/workflow/scheduled/update/UpdateChecker.java
+++ src/main/java/com/synopsys/integration/alert/workflow/scheduled/update/UpdateChecker.java
@@ -33,7 +33,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Stream;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
