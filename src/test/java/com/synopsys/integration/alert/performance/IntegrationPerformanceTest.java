3b101a1610c1c983b418ac6df8f12b295d29fc34
James Richard <jrichard@synopsys.com>
2020-10-14 11:33:15 -0400

    Updating postgres version for integration test. Fixing IntegrationPerformanceTest

--- src/test/java/com/synopsys/integration/alert/performance/IntegrationPerformanceTest.java
+++ src/test/java/com/synopsys/integration/alert/performance/IntegrationPerformanceTest.java
@@ -1,24 +1,15 @@
 package com.synopsys.integration.alert.performance;
 
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.synopsys.integration.alert.Application;
 import com.synopsys.integration.alert.ApplicationConfiguration;
 import com.synopsys.integration.alert.database.DatabaseDataSource;
@@ -26,14 +17,10 @@ import com.synopsys.integration.alert.performance.utility.AlertRequestUtility;
 import com.synopsys.integration.alert.performance.utility.IntegrationAlertRequestUtility;
 import com.synopsys.integration.alert.util.DescriptorMocker;
 
-@ExtendWith(SpringExtension.class)
+@SpringBootTest
 @ContextConfiguration(classes = { Application.class, ApplicationConfiguration.class, DatabaseDataSource.class, DescriptorMocker.class })
 @TestPropertySource(locations = "classpath:spring-test.properties")
 @WebAppConfiguration
-@Transactional
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@ActiveProfiles("alertdb")
-@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DirtiesContextTestExecutionListener.class, TransactionalTestExecutionListener.class, DbUnitTestExecutionListener.class })
 public abstract class IntegrationPerformanceTest extends BasePerformanceTest {
 
     @Autowired
@@ -47,4 +34,5 @@ public abstract class IntegrationPerformanceTest extends BasePerformanceTest {
 
         return new IntegrationAlertRequestUtility(intLogger, mockMvc);
     }
+
 }
