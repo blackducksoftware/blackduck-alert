38c41ada36aeb32d205eeb886cef815363c958d5
Gavin Killough <gavink@synopsys.com>
2020-12-01 12:25:07 -0500

    Fix(Job): Use constants where available

--- web/src/main/java/com/synopsys/integration/alert/web/api/job/JobFieldModelPopulationUtils.java
+++ web/src/main/java/com/synopsys/integration/alert/web/api/job/JobFieldModelPopulationUtils.java
@@ -30,6 +30,15 @@ import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
 
+import com.synopsys.integration.alert.channel.azure.boards.descriptor.AzureBoardsDescriptor;
+import com.synopsys.integration.alert.channel.email.descriptor.EmailDescriptor;
+import com.synopsys.integration.alert.channel.jira.cloud.descriptor.JiraCloudDescriptor;
+import com.synopsys.integration.alert.channel.jira.server.descriptor.JiraServerDescriptor;
+import com.synopsys.integration.alert.channel.msteams.descriptor.MsTeamsDescriptor;
+import com.synopsys.integration.alert.channel.slack.descriptor.SlackDescriptor;
+import com.synopsys.integration.alert.common.descriptor.ProviderDescriptor;
+import com.synopsys.integration.alert.common.descriptor.config.ui.ChannelDistributionUIConfig;
+import com.synopsys.integration.alert.common.descriptor.config.ui.ProviderDistributionUIConfig;
 import com.synopsys.integration.alert.common.enumeration.ConfigContextEnum;
 import com.synopsys.integration.alert.common.persistence.model.job.DistributionJobModel;
 import com.synopsys.integration.alert.common.persistence.model.job.details.AzureBoardsJobDetailsModel;
@@ -44,7 +53,7 @@ import com.synopsys.integration.alert.common.rest.model.FieldValueModel;
 import com.synopsys.integration.alert.common.rest.model.JobFieldModel;
 import com.synopsys.integration.alert.descriptor.api.BlackDuckProviderKey;
 
-public class JobFieldModelPopulationUtils {
+public final class JobFieldModelPopulationUtils {
     public static JobFieldModel createJobFieldModel(DistributionJobModel jobModel) {
         FieldModel providerFieldModel = new FieldModel(new BlackDuckProviderKey().getUniversalKey(), ConfigContextEnum.DISTRIBUTION.name(), new HashMap<>());
         populateProviderFields(providerFieldModel, jobModel);
@@ -62,21 +71,21 @@ public class JobFieldModelPopulationUtils {
         String providerCommonConfigId = Optional.ofNullable(jobModel.getBlackDuckGlobalConfigId())
                                             .map(String::valueOf)
                                             .orElse(null);
-        putField(providerFieldModel, "provider.common.config.id", providerCommonConfigId);
-        putField(providerFieldModel, "provider.distribution.processing.type", jobModel.getProcessingType().toString());
-        putField(providerFieldModel, "provider.distribution.notification.types", jobModel.getNotificationTypes());
+        putField(providerFieldModel, ProviderDescriptor.KEY_PROVIDER_CONFIG_ID, providerCommonConfigId);
+        putField(providerFieldModel, ProviderDistributionUIConfig.KEY_PROCESSING_TYPE, jobModel.getProcessingType().toString());
+        putField(providerFieldModel, ProviderDistributionUIConfig.KEY_NOTIFICATION_TYPES, jobModel.getNotificationTypes());
 
         boolean filterByProject = jobModel.isFilterByProject();
-        putField(providerFieldModel, "channel.common.filter.by.project", Boolean.toString(filterByProject));
+        putField(providerFieldModel, ProviderDistributionUIConfig.KEY_FILTER_BY_PROJECT, Boolean.toString(filterByProject));
 
         if (filterByProject) {
             jobModel.getProjectNamePattern()
                 .filter(StringUtils::isNotBlank)
-                .ifPresent(pattern -> putField(providerFieldModel, "channel.common.project.name.pattern", pattern));
+                .ifPresent(pattern -> putField(providerFieldModel, ProviderDistributionUIConfig.KEY_PROJECT_NAME_PATTERN, pattern));
 
             List<String> blackDuckProjectNames = jobModel.getProjectFilterProjectNames();
             if (!blackDuckProjectNames.isEmpty()) {
-                putField(providerFieldModel, "channel.common.configured.project", blackDuckProjectNames);
+                putField(providerFieldModel, ProviderDistributionUIConfig.KEY_CONFIGURED_PROJECT, blackDuckProjectNames);
             }
         }
 
@@ -93,11 +102,11 @@ public class JobFieldModelPopulationUtils {
 
     public static void populateChannelFields(FieldModel channelFieldModel, DistributionJobModel jobModel) {
         String channelDescriptorName = jobModel.getChannelDescriptorName();
-        putField(channelFieldModel, "channel.common.enabled", Boolean.toString(jobModel.isEnabled()));
-        putField(channelFieldModel, "channel.common.name", jobModel.getName());
-        putField(channelFieldModel, "channel.common.channel.name", channelDescriptorName);
-        putField(channelFieldModel, "channel.common.frequency", jobModel.getDistributionFrequency().name());
-        putField(channelFieldModel, "provider.distribution.processing.type", jobModel.getProcessingType().name());
+        putField(channelFieldModel, ChannelDistributionUIConfig.KEY_ENABLED, Boolean.toString(jobModel.isEnabled()));
+        putField(channelFieldModel, ChannelDistributionUIConfig.KEY_NAME, jobModel.getName());
+        putField(channelFieldModel, ChannelDistributionUIConfig.KEY_CHANNEL_NAME, channelDescriptorName);
+        putField(channelFieldModel, ChannelDistributionUIConfig.KEY_FREQUENCY, jobModel.getDistributionFrequency().name());
+        putField(channelFieldModel, ProviderDistributionUIConfig.KEY_PROCESSING_TYPE, jobModel.getProcessingType().name());
 
         DistributionJobDetailsModel jobDetails = jobModel.getDistributionJobDetails();
         if (jobDetails.isAzureBoardsDetails()) {
@@ -117,61 +126,61 @@ public class JobFieldModelPopulationUtils {
 
     private static void populateAzureBoardsFields(FieldModel channelFieldModel, AzureBoardsJobDetailsModel azureBoardsJobDetails) {
         if (null != azureBoardsJobDetails) {
-            putField(channelFieldModel, "channel.azure.boards.work.item.comment", Boolean.toString(azureBoardsJobDetails.isAddComments()));
-            putField(channelFieldModel, "channel.azure.boards.project", azureBoardsJobDetails.getProjectNameOrId());
-            putField(channelFieldModel, "channel.azure.boards.work.item.type", azureBoardsJobDetails.getWorkItemType());
-            putField(channelFieldModel, "channel.azure.boards.work.item.completed.state", azureBoardsJobDetails.getWorkItemCompletedState());
-            putField(channelFieldModel, "channel.azure.boards.work.item.reopen.state", azureBoardsJobDetails.getWorkItemReopenState());
+            putField(channelFieldModel, AzureBoardsDescriptor.KEY_WORK_ITEM_COMMENT, Boolean.toString(azureBoardsJobDetails.isAddComments()));
+            putField(channelFieldModel, AzureBoardsDescriptor.KEY_AZURE_PROJECT, azureBoardsJobDetails.getProjectNameOrId());
+            putField(channelFieldModel, AzureBoardsDescriptor.KEY_WORK_ITEM_TYPE, azureBoardsJobDetails.getWorkItemType());
+            putField(channelFieldModel, AzureBoardsDescriptor.KEY_WORK_ITEM_COMPLETED_STATE, azureBoardsJobDetails.getWorkItemCompletedState());
+            putField(channelFieldModel, AzureBoardsDescriptor.KEY_WORK_ITEM_REOPEN_STATE, azureBoardsJobDetails.getWorkItemReopenState());
         }
     }
 
     private static void populateEmailFields(FieldModel channelFieldModel, EmailJobDetailsModel emailJobDetails) {
         if (null != emailJobDetails) {
-            putField(channelFieldModel, "email.subject.line", emailJobDetails.getSubjectLine());
-            putField(channelFieldModel, "email.attachment.format", emailJobDetails.getAttachmentFileType());
-            putField(channelFieldModel, "project.owner.only", Boolean.toString(emailJobDetails.isProjectOwnerOnly()));
-            putField(channelFieldModel, "email.additional.addresses.only", Boolean.toString(emailJobDetails.isAdditionalEmailAddressesOnly()));
+            putField(channelFieldModel, EmailDescriptor.KEY_SUBJECT_LINE, emailJobDetails.getSubjectLine());
+            putField(channelFieldModel, EmailDescriptor.KEY_EMAIL_ATTACHMENT_FORMAT, emailJobDetails.getAttachmentFileType());
+            putField(channelFieldModel, EmailDescriptor.KEY_PROJECT_OWNER_ONLY, Boolean.toString(emailJobDetails.isProjectOwnerOnly()));
+            putField(channelFieldModel, EmailDescriptor.KEY_EMAIL_ADDITIONAL_ADDRESSES_ONLY, Boolean.toString(emailJobDetails.isAdditionalEmailAddressesOnly()));
 
             List<String> emailAddresses = emailJobDetails.getAdditionalEmailAddresses();
             if (!emailAddresses.isEmpty()) {
-                putField(channelFieldModel, "email.additional.addresses", emailAddresses);
+                putField(channelFieldModel, EmailDescriptor.KEY_EMAIL_ADDITIONAL_ADDRESSES, emailAddresses);
             }
         }
     }
 
     private static void populateJiraCloudFields(FieldModel channelFieldModel, JiraCloudJobDetailsModel jiraCloudJobDetails) {
         if (null != jiraCloudJobDetails) {
-            putField(channelFieldModel, "channel.jira.cloud.add.comments", Boolean.toString(jiraCloudJobDetails.isAddComments()));
-            putField(channelFieldModel, "channel.jira.cloud.issue.creator", jiraCloudJobDetails.getIssueCreatorEmail());
-            putField(channelFieldModel, "channel.jira.cloud.project.name", jiraCloudJobDetails.getProjectNameOrKey());
-            putField(channelFieldModel, "channel.jira.cloud.issue.type", jiraCloudJobDetails.getIssueType());
-            putField(channelFieldModel, "channel.jira.cloud.resolve.workflow", jiraCloudJobDetails.getResolveTransition());
-            putField(channelFieldModel, "channel.jira.cloud.reopen.workflow", jiraCloudJobDetails.getReopenTransition());
+            putField(channelFieldModel, JiraCloudDescriptor.KEY_ADD_COMMENTS, Boolean.toString(jiraCloudJobDetails.isAddComments()));
+            putField(channelFieldModel, JiraCloudDescriptor.KEY_ISSUE_CREATOR, jiraCloudJobDetails.getIssueCreatorEmail());
+            putField(channelFieldModel, JiraCloudDescriptor.KEY_JIRA_PROJECT_NAME, jiraCloudJobDetails.getProjectNameOrKey());
+            putField(channelFieldModel, JiraCloudDescriptor.KEY_ISSUE_TYPE, jiraCloudJobDetails.getIssueType());
+            putField(channelFieldModel, JiraCloudDescriptor.KEY_RESOLVE_WORKFLOW_TRANSITION, jiraCloudJobDetails.getResolveTransition());
+            putField(channelFieldModel, JiraCloudDescriptor.KEY_OPEN_WORKFLOW_TRANSITION, jiraCloudJobDetails.getReopenTransition());
         }
     }
 
     private static void populateJiraServerFields(FieldModel channelFieldModel, JiraServerJobDetailsModel jiraServerJobDetails) {
         if (null != jiraServerJobDetails) {
-            putField(channelFieldModel, "channel.jira.server.add.comments", Boolean.toString(jiraServerJobDetails.isAddComments()));
-            putField(channelFieldModel, "channel.jira.server.issue.creator", jiraServerJobDetails.getIssueCreatorUsername());
-            putField(channelFieldModel, "channel.jira.server.project.name", jiraServerJobDetails.getProjectNameOrKey());
-            putField(channelFieldModel, "channel.jira.server.issue.type", jiraServerJobDetails.getIssueType());
-            putField(channelFieldModel, "channel.jira.server.resolve.workflow", jiraServerJobDetails.getResolveTransition());
-            putField(channelFieldModel, "channel.jira.server.reopen.workflow", jiraServerJobDetails.getReopenTransition());
+            putField(channelFieldModel, JiraServerDescriptor.KEY_ADD_COMMENTS, Boolean.toString(jiraServerJobDetails.isAddComments()));
+            putField(channelFieldModel, JiraServerDescriptor.KEY_ISSUE_CREATOR, jiraServerJobDetails.getIssueCreatorUsername());
+            putField(channelFieldModel, JiraServerDescriptor.KEY_JIRA_PROJECT_NAME, jiraServerJobDetails.getProjectNameOrKey());
+            putField(channelFieldModel, JiraServerDescriptor.KEY_ISSUE_TYPE, jiraServerJobDetails.getIssueType());
+            putField(channelFieldModel, JiraServerDescriptor.KEY_RESOLVE_WORKFLOW_TRANSITION, jiraServerJobDetails.getResolveTransition());
+            putField(channelFieldModel, JiraServerDescriptor.KEY_OPEN_WORKFLOW_TRANSITION, jiraServerJobDetails.getReopenTransition());
         }
     }
 
     private static void populateMSTeamsField(FieldModel channelFieldModel, MSTeamsJobDetailsModel msTeamsJobDetails) {
         if (null != msTeamsJobDetails) {
-            putField(channelFieldModel, "channel.msteams.webhook", msTeamsJobDetails.getWebhook());
+            putField(channelFieldModel, MsTeamsDescriptor.KEY_WEBHOOK, msTeamsJobDetails.getWebhook());
         }
     }
 
     private static void populateSlackFields(FieldModel channelFieldModel, SlackJobDetailsModel slackJobDetails) {
         if (null != slackJobDetails) {
-            putField(channelFieldModel, "channel.slack.webhook", slackJobDetails.getWebhook());
-            putField(channelFieldModel, "channel.slack.channel.name", slackJobDetails.getChannelName());
-            putField(channelFieldModel, "channel.slack.channel.username", slackJobDetails.getChannelUsername());
+            putField(channelFieldModel, SlackDescriptor.KEY_WEBHOOK, slackJobDetails.getWebhook());
+            putField(channelFieldModel, SlackDescriptor.KEY_CHANNEL_NAME, slackJobDetails.getChannelName());
+            putField(channelFieldModel, SlackDescriptor.KEY_CHANNEL_USERNAME, slackJobDetails.getChannelUsername());
         }
     }
 
