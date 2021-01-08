674d5b6dbc2a38464782d8e32f12964e3ae8e80d
Brian Mandel <bmandel@blackducksoftware.com>
2021-01-08 13:27:02 -0500

    style(AutoActionable): Added comment that was previously there

--- alert-common/src/main/java/com/synopsys/integration/alert/common/channel/AutoActionable.java
+++ alert-common/src/main/java/com/synopsys/integration/alert/common/channel/AutoActionable.java
@@ -24,6 +24,9 @@ package com.synopsys.integration.alert.common.channel;
 
 import com.synopsys.integration.alert.descriptor.api.model.ChannelKey;
 
+/**
+ * This lets the DescriptorProcessor create the appropriate ChannelDistributionTestAction/ConfigurationAction combination when no customization is needed.
+ */
 public interface AutoActionable {
     ChannelKey getChannelKey();
 
