a653be5d3c9583ad911c24d358e71612635e5efd
Jake Mathews <jake.mathews.email@gmail.com>
2021-02-01 11:02:47 -0500

    test(issue-tracker): Added additional test to verify the description was being split properly.

--- alert-common/src/test/java/com/synopsys/integration/alert/common/channel/issuetracker/message/IssueTrackerMessageParserTest.java
+++ alert-common/src/test/java/com/synopsys/integration/alert/common/channel/issuetracker/message/IssueTrackerMessageParserTest.java
@@ -2,6 +2,7 @@ package com.synopsys.integration.alert.common.channel.issuetracker.message;
 
 import java.util.Set;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.synopsys.integration.alert.common.channel.issuetracker.enumeration.IssueOperation;
@@ -13,10 +14,29 @@ import com.synopsys.integration.alert.common.message.model.LinkableItem;
 class IssueTrackerMessageParserTest {
 
     @Test
-    void createIssueContentModel() throws AlertException {
+    void createIssueContentModelTest() throws AlertException {
         int titleSizeLimit = 10;
-        int messageSizeLimit = 20;
+        int messageSizeLimit = 10;
 
+        IssueContentModel issueContentModel = createIssueContentModel(titleSizeLimit, messageSizeLimit);
+        IssueContentLengthValidator issueContentLengthValidator = new IssueContentLengthValidator(titleSizeLimit, messageSizeLimit, messageSizeLimit);
+        issueContentLengthValidator.validateContentLength(issueContentModel);
+    }
+
+    @Test
+    void createIssueContentModelDescriptionSplitTest() throws AlertException {
+        int titleSizeLimit = 20;
+        int messageSizeLimit = 100;
+
+        IssueContentModel issueContentModel = createIssueContentModel(titleSizeLimit, messageSizeLimit);
+        IssueContentLengthValidator issueContentLengthValidator = new IssueContentLengthValidator(titleSizeLimit, messageSizeLimit, messageSizeLimit);
+        issueContentLengthValidator.validateContentLength(issueContentModel);
+
+        Assertions.assertTrue(issueContentModel.getDescription().length() < messageSizeLimit, "The description should be longer than the limit, thus resulting in it getting truncated to the under or at limit.");
+        Assertions.assertEquals(3, issueContentModel.getDescriptionComments().size(), "The number of the description components should be 3. First being the truncated description, the rest being additional description items.");
+    }
+
+    private IssueContentModel createIssueContentModel(int titleSizeLimit, int messageSizeLimit) throws AlertException {
         TestIssueMessageParser testIssueMessageParser = new TestIssueMessageParser(titleSizeLimit, messageSizeLimit);
         LinkableItem topic = new LinkableItem("topic", "https://example.topic.link");
         LinkableItem subtopic = new LinkableItem("subtopic", "https://example.subtopic.link");
@@ -31,10 +51,7 @@ class IssueTrackerMessageParserTest {
                                           .build();
         Set<ComponentItem> componentItems = Set.of(arbitraryItem);
 
-        IssueContentModel issueContentModel = testIssueMessageParser.createIssueContentModel("test-provider", IssueOperation.OPEN, topic, subtopic, componentItems, arbitraryItem);
-
-        IssueContentLengthValidator issueContentLengthValidator = new IssueContentLengthValidator(titleSizeLimit, messageSizeLimit, messageSizeLimit);
-        issueContentLengthValidator.validateContentLength(issueContentModel);
+        return testIssueMessageParser.createIssueContentModel("test-provider", IssueOperation.OPEN, topic, subtopic, componentItems, arbitraryItem);
     }
 
     private static class TestIssueMessageParser extends IssueTrackerMessageParser {
