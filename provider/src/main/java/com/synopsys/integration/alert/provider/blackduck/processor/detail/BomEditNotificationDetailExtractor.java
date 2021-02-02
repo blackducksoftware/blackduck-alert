dfce0acfd63f56ae96661bf4b0ea806a0765661e
Gavin Killough <gavink@synopsys.com>
2021-02-02 09:00:15 -0500

    Feat(Processing): Implement bom edit detail extractor

---
+++ provider/src/main/java/com/synopsys/integration/alert/provider/blackduck/processor/detail/BomEditNotificationDetailExtractor.java
@@ -0,0 +1,97 @@
+/*
+ * provider
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.synopsys.integration.alert.provider.blackduck.processor.detail;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.google.gson.Gson;
+import com.synopsys.integration.alert.common.persistence.accessor.ConfigurationAccessor;
+import com.synopsys.integration.alert.common.persistence.model.ConfigurationModel;
+import com.synopsys.integration.alert.common.rest.model.AlertNotificationModel;
+import com.synopsys.integration.alert.processor.api.filter.extractor.NotificationDetailExtractor;
+import com.synopsys.integration.alert.processor.api.filter.model.DetailedNotificationContent;
+import com.synopsys.integration.alert.provider.blackduck.BlackDuckProperties;
+import com.synopsys.integration.alert.provider.blackduck.factory.BlackDuckPropertiesFactory;
+import com.synopsys.integration.blackduck.api.generated.view.ProjectVersionView;
+import com.synopsys.integration.blackduck.api.generated.view.ProjectView;
+import com.synopsys.integration.blackduck.api.manual.component.BomEditNotificationContent;
+import com.synopsys.integration.blackduck.api.manual.enumeration.NotificationType;
+import com.synopsys.integration.blackduck.http.client.BlackDuckHttpClient;
+import com.synopsys.integration.blackduck.service.BlackDuckApiClient;
+import com.synopsys.integration.blackduck.service.BlackDuckServicesFactory;
+import com.synopsys.integration.exception.IntegrationException;
+import com.synopsys.integration.log.Slf4jIntLogger;
+import com.synopsys.integration.rest.HttpUrl;
+
+@Component
+public class BomEditNotificationDetailExtractor extends NotificationDetailExtractor<BomEditNotificationContent> {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final ConfigurationAccessor configurationAccessor;
+    private final BlackDuckPropertiesFactory blackDuckPropertiesFactory;
+
+    @Autowired
+    public BomEditNotificationDetailExtractor(Gson gson, ConfigurationAccessor configurationAccessor, BlackDuckPropertiesFactory blackDuckPropertiesFactory) {
+        super(NotificationType.BOM_EDIT, BomEditNotificationContent.class, gson);
+        this.configurationAccessor = configurationAccessor;
+        this.blackDuckPropertiesFactory = blackDuckPropertiesFactory;
+    }
+
+    @Override
+    protected List<DetailedNotificationContent> convertToFilterableNotificationWrapper(AlertNotificationModel alertNotificationModel, BomEditNotificationContent notificationContent) {
+        return retrieveProjectName(alertNotificationModel.getProviderConfigId(), notificationContent.getProjectVersion())
+                   .map(projectName -> DetailedNotificationContent.project(alertNotificationModel, notificationContent, projectName))
+                   .map(List::of)
+                   .orElse(List.of());
+    }
+
+    private Optional<String> retrieveProjectName(Long blackDuckConfigId, String projectVersionUrl) {
+        Optional<ConfigurationModel> optionalBlackDuckConfig = configurationAccessor.getConfigurationById(blackDuckConfigId);
+        if (optionalBlackDuckConfig.isPresent()) {
+            BlackDuckProperties properties = blackDuckPropertiesFactory.createProperties(optionalBlackDuckConfig.get());
+
+            Slf4jIntLogger intLogger = new Slf4jIntLogger(logger);
+            try {
+                BlackDuckHttpClient blackDuckHttpClient = properties.createBlackDuckHttpClient(intLogger);
+                BlackDuckServicesFactory blackDuckServicesFactory = properties.createBlackDuckServicesFactory(blackDuckHttpClient, intLogger);
+                BlackDuckApiClient blackDuckApiClient = blackDuckServicesFactory.getBlackDuckApiClient();
+
+                ProjectVersionView projectVersion = blackDuckApiClient.getResponse(new HttpUrl(projectVersionUrl), ProjectVersionView.class);
+                Optional<ProjectView> project = blackDuckApiClient.getResponse(projectVersion, ProjectVersionView.PROJECT_LINK_RESPONSE);
+                return project.map(ProjectView::getName);
+            } catch (IntegrationException e) {
+                logger.error("Failed to connect to BlackDuck. Config ID: {}", blackDuckConfigId, e);
+            }
+        } else {
+            logger.warn("No BlackDuck config with ID {} existed", blackDuckConfigId);
+        }
+        return Optional.empty();
+    }
+
+}
