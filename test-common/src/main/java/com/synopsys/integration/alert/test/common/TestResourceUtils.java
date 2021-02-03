ea5afea86f4ba14fe851c6524ff23a297351b724
Gavin Killough <gavink@synopsys.com>
2021-02-03 14:53:37 -0500

    Feat(test-common): Update test file resource loading

--- test-common/src/main/java/com/synopsys/integration/alert/test/common/TestResourceUtils.java
+++ test-common/src/main/java/com/synopsys/integration/alert/test/common/TestResourceUtils.java
@@ -22,38 +22,40 @@
  */
 package com.synopsys.integration.alert.test.common;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.util.Properties;
-import java.util.stream.Collectors;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.core.io.ClassPathResource;
 
-public class ResourceLoader {
+public final class TestResourceUtils {
     public static final String DEFAULT_PROPERTIES_FILE_LOCATION = "test.properties";
-    public static final File RESOURCE_DIR = new File(ResourceLoader.class.getProtectionDomain().getCodeSource().getLocation().getPath(), "../../../../src/test/resources/");
-    public static final String PROPERTIES_ENV_VARIABLE = "SPRING_APPLICATION_JSON";
+    public static final File BASE_TEST_RESOURCE_DIR = new File(TestResourceUtils.class.getProtectionDomain().getCodeSource().getLocation().getPath(), "../../../../src/test/resources/");
 
-    @SuppressWarnings("resource")
-    public String loadJsonResource(String resourceLocation) throws IOException {
-        File file = new File(RESOURCE_DIR, resourceLocation);
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        String contents = String.join("\n", reader.lines().collect(Collectors.toList()));
-        reader.close();
-        return contents;
+    /**
+     * @param resourcePath The path to the file resource. For example: If the file is in src/test/resources/dir1/dir2/file.ext, then use "dir1/dir2/file.ext"
+     * @return The file contents, never null
+     * @throws IOException Thrown by {@link FileUtils} if the file cannot be read
+     */
+    public static String readFileToString(String resourcePath) throws IOException {
+        ClassPathResource classPathResource = new ClassPathResource(resourcePath);
+        File jsonFile = classPathResource.getFile();
+        return FileUtils.readFileToString(jsonFile, Charset.defaultCharset());
     }
 
-    @SuppressWarnings("resource")
-    public Properties loadProperties(String resourceLocation) throws IOException {
+    public static Properties loadProperties(String resourceLocation) throws IOException {
         Properties properties = new Properties();
         ClassPathResource classPathResource = new ClassPathResource(resourceLocation);
         try (InputStream iStream = classPathResource.getInputStream()) {
             properties.load(iStream);
         }
         return properties;
+    }
+
+    private TestResourceUtils() {
     }
 
 }
