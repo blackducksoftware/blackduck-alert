55297ad2be542d6092a3309fe19607c34b8b7493
Jake Mathews <jake.mathews.email@gmail.com>
2021-02-08 15:31:28 -0500

    refactor(slack): Instantiated final map at declaration.

--- channel/src/main/java/com/synopsys/integration/alert/channel/slack2/SlackChannelMessageFormatter.java
+++ channel/src/main/java/com/synopsys/integration/alert/channel/slack2/SlackChannelMessageFormatter.java
@@ -35,13 +35,12 @@ import com.synopys.integration.alert.channel.api.convert.ChannelMessageFormatter
 public class SlackChannelMessageFormatter extends ChannelMessageFormatter {
     private static final int MAX_SLACK_MESSAGE_LENGTH = 3500;
     private static final String SLACK_LINE_SEPARATOR = "\n";
-    private static final Map<Character, String> SLACK_CHARACTER_ENCODING_MAP;
+    private static final Map<Character, String> SLACK_CHARACTER_ENCODING_MAP = new LinkedHashMap<>();
 
     private final MarkupEncoderUtil markupEncoderUtil;
 
     static {
         // Insertion order matters, so '&' must always be inserted first.
-        SLACK_CHARACTER_ENCODING_MAP = new LinkedHashMap<>();
         SLACK_CHARACTER_ENCODING_MAP.put('&', "&amp;");
         SLACK_CHARACTER_ENCODING_MAP.put('<', "&lt;");
         SLACK_CHARACTER_ENCODING_MAP.put('>', "&gt;");
