846ec507ea938d7a9fe025d80e2281ce30f514b1
Gavin Killough <gavink@synopsys.com>
2021-03-10 14:38:02 -0500

    Chore: Delete unused classes based on code-inspection in Intellij

--- alert-common/src/test/java/com/synopsys/integration/alert/common/channel/issuetracker/service/util/TestServerConfig.java
+++
@@ -1,7 +0,0 @@
-package com.synopsys.integration.alert.common.channel.issuetracker.service.util;
-
-import com.synopsys.integration.alert.common.channel.issuetracker.config.IssueTrackerServiceConfig;
-
-public class TestServerConfig implements IssueTrackerServiceConfig {
-
-}
