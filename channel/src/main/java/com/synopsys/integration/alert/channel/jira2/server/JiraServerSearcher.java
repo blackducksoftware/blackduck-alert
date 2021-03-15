c1d0fc1f7be3f29c7e7b35b3eee5399e2841ce1b
Jake Mathews <jake.mathews.email@gmail.com>
2021-03-15 16:35:41 -0400

    feat(jira-server): Implemented JiraServerSearcher and abstracted logic to JiraSearcher.

--- channel/src/main/java/com/synopsys/integration/alert/channel/jira2/server/JiraServerSearcher.java
+++ channel/src/main/java/com/synopsys/integration/alert/channel/jira2/server/JiraServerSearcher.java
@@ -5,23 +5,22 @@
  *
  * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
  */
-package com.synopsys.integration.alert.channel.jira2.cloud;
+package com.synopsys.integration.alert.channel.jira2.server;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
+import com.synopsys.integration.alert.channel.jira2.cloud.JiraIssueAlertPropertiesManager;
 import com.synopsys.integration.alert.channel.jira2.common.JiraSearcher;
 import com.synopsys.integration.alert.channel.jira2.common.model.JiraSearcherResponseModel;
 import com.synopsys.integration.exception.IntegrationException;
-import com.synopsys.integration.jira.common.cloud.model.IssueSearchResponseModel;
-import com.synopsys.integration.jira.common.cloud.service.IssueSearchService;
-import com.synopsys.integration.jira.common.model.components.IssueFieldsComponent;
+import com.synopsys.integration.jira.common.server.model.IssueSearchResponseModel;
+import com.synopsys.integration.jira.common.server.service.IssueSearchService;
 
-public class JiraCloudSearcher extends JiraSearcher {
+public class JiraServerSearcher extends JiraSearcher {
     private final IssueSearchService issueSearchService;
 
-    public JiraCloudSearcher(String jiraProjectKey, IssueSearchService issueSearchService, JiraIssueAlertPropertiesManager issuePropertiesManager) {
+    public JiraServerSearcher(String jiraProjectKey, IssueSearchService issueSearchService, JiraIssueAlertPropertiesManager issuePropertiesManager) {
         super(jiraProjectKey, issuePropertiesManager);
         this.issueSearchService = issueSearchService;
     }
@@ -32,13 +31,10 @@ public class JiraCloudSearcher extends JiraSearcher {
         return issueSearchResponseModel.getIssues()
                    .stream()
                    .map(issue -> {
-                       IssueFieldsComponent nullableIssueFields = issue.getFields();
-                       String existingIssueSummary = Optional.ofNullable(nullableIssueFields)
-                                                         .map(IssueFieldsComponent::getSummary)
-                                                         .orElse(issue.getKey());
-                       return new JiraSearcherResponseModel(issue.getSelf(), issue.getKey(), issue.getId(), existingIssueSummary);
+                       String issueUrl = issue.getFields().getIssueType().getSelf();
+                       String summary = issue.getFields().getSummary();
+                       return new JiraSearcherResponseModel(issueUrl, issue.getKey(), issue.getId(), summary);
                    })
                    .collect(Collectors.toList());
     }
-
 }
