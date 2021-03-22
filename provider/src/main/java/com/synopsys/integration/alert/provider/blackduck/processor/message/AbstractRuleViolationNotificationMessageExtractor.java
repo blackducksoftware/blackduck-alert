38bd498061b4205c63e49a2b9c5005843c015698
Gavin Killough <gavink@synopsys.com>
2021-03-22 12:17:22 -0400

    Fix(AbstractRuleViolationnotificationMessageExtractor): Handle BOM component 404

--- provider/src/main/java/com/synopsys/integration/alert/provider/blackduck/processor/message/AbstractRuleViolationNotificationMessageExtractor.java
+++ provider/src/main/java/com/synopsys/integration/alert/provider/blackduck/processor/message/AbstractRuleViolationNotificationMessageExtractor.java
@@ -10,6 +10,9 @@ package com.synopsys.integration.alert.provider.blackduck.processor.message;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.synopsys.integration.alert.common.enumeration.ItemOperation;
 import com.synopsys.integration.alert.descriptor.api.BlackDuckProviderKey;
 import com.synopsys.integration.alert.processor.api.extract.model.project.BomComponentDetails;
@@ -25,8 +28,11 @@ import com.synopsys.integration.blackduck.service.BlackDuckApiClient;
 import com.synopsys.integration.blackduck.service.BlackDuckServicesFactory;
 import com.synopsys.integration.exception.IntegrationException;
 import com.synopsys.integration.rest.HttpUrl;
+import com.synopsys.integration.rest.exception.IntegrationRestException;
 
 public abstract class AbstractRuleViolationNotificationMessageExtractor<T extends AbstractRuleViolationNotificationContent> extends AbstractBlackDuckComponentConcernMessageExtractor<T> {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
     private final ItemOperation itemOperation;
 
     public AbstractRuleViolationNotificationMessageExtractor(
@@ -46,13 +52,32 @@ public abstract class AbstractRuleViolationNotificationMessageExtractor<T extend
 
         List<BomComponentDetails> bomComponentDetails = new LinkedList<>();
         for (ComponentVersionStatus componentVersionStatus : notificationContent.getComponentVersionStatuses()) {
-            ProjectVersionComponentView bomComponent = blackDuckApiClient.getResponse(new HttpUrl(componentVersionStatus.getBomComponent()), ProjectVersionComponentView.class);
-            ComponentConcern policyConcern = BlackDuckMessageComponentConcernUtils.fromPolicyInfo(notificationContent.getPolicyInfo(), itemOperation);
-
-            BomComponentDetails componentVersionDetails = BlackDuckMessageBomComponentDetailsUtils.createBomComponentDetails(bomComponent, policyConcern, List.of());
+            BomComponentDetails componentVersionDetails = createBomComponentDetails(blackDuckApiClient, notificationContent, componentVersionStatus);
             bomComponentDetails.add(componentVersionDetails);
         }
         return bomComponentDetails;
+    }
+
+    private BomComponentDetails createBomComponentDetails(BlackDuckApiClient blackDuckApiClient, T notificationContent, ComponentVersionStatus componentVersionStatus) throws IntegrationException {
+        ComponentConcern policyConcern = BlackDuckMessageComponentConcernUtils.fromPolicyInfo(notificationContent.getPolicyInfo(), itemOperation);
+        try {
+            ProjectVersionComponentView bomComponent = blackDuckApiClient.getResponse(new HttpUrl(componentVersionStatus.getBomComponent()), ProjectVersionComponentView.class);
+            return BlackDuckMessageBomComponentDetailsUtils.createBomComponentDetails(bomComponent, policyConcern, List.of());
+        } catch (IntegrationRestException e) {
+            if (404 == e.getHttpStatusCode()) {
+                logger.debug("The BOM Component '{}[{}]' no longer exists", componentVersionStatus.getComponentName(), componentVersionStatus.getComponentVersionName());
+            } else {
+                throw e;
+            }
+        }
+        return BlackDuckMessageBomComponentDetailsUtils.createBomComponentDetails(
+            componentVersionStatus.getComponentName(),
+            componentVersionStatus.getComponent(),
+            componentVersionStatus.getComponentVersionName(),
+            componentVersionStatus.getComponentVersion(),
+            List.of(policyConcern),
+            List.of()
+        );
     }
 
 }
