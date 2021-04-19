2b519de1e483eebf35604b6758c93e11d725ba72
Gavin Killough <gavink@synopsys.com>
2021-04-19 08:56:37 -0400

    Feat(BlackDuck): Add BOM component details creator component to replace equivalent utils

--- provider/src/main/java/com/synopsys/integration/alert/provider/blackduck/processor/message/service/BlackDuckMessageBomComponentDetailsCreator.java
+++ provider/src/main/java/com/synopsys/integration/alert/provider/blackduck/processor/message/service/BlackDuckMessageBomComponentDetailsCreator.java
@@ -5,26 +5,29 @@
  *
  * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
  */
-package com.synopsys.integration.alert.provider.blackduck.processor.message.util;
+package com.synopsys.integration.alert.provider.blackduck.processor.message.service;
 
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nullable;
+import org.springframework.stereotype.Component;
 
 import com.synopsys.integration.alert.common.message.model.LinkableItem;
 import com.synopsys.integration.alert.processor.api.extract.model.project.BomComponentDetails;
 import com.synopsys.integration.alert.processor.api.extract.model.project.ComponentConcern;
 import com.synopsys.integration.alert.provider.blackduck.processor.message.BlackDuckMessageLabels;
+import com.synopsys.integration.alert.provider.blackduck.processor.message.util.BlackDuckMessageAttributesUtils;
+import com.synopsys.integration.alert.provider.blackduck.processor.message.util.BlackDuckMessageLinkUtils;
 import com.synopsys.integration.blackduck.api.generated.view.ProjectVersionComponentView;
 
-@Deprecated
-public final class BlackDuckMessageBomComponentDetailsUtils {
-    public static BomComponentDetails createBomComponentDetails(ProjectVersionComponentView bomComponent, ComponentConcern componentConcern, List<LinkableItem> additionalAttributes) {
+@Component
+public class BlackDuckMessageBomComponentDetailsCreator {
+    public BomComponentDetails createBomComponentDetails(ProjectVersionComponentView bomComponent, ComponentConcern componentConcern, List<LinkableItem> additionalAttributes) {
         return createBomComponentDetails(bomComponent, List.of(componentConcern), additionalAttributes);
     }
 
-    public static BomComponentDetails createBomComponentDetails(ProjectVersionComponentView bomComponent, List<ComponentConcern> componentConcerns, List<LinkableItem> additionalAttributes) {
+    public BomComponentDetails createBomComponentDetails(ProjectVersionComponentView bomComponent, List<ComponentConcern> componentConcerns, List<LinkableItem> additionalAttributes) {
         LinkableItem component;
         LinkableItem componentVersion = null;
 
@@ -45,7 +48,7 @@ public final class BlackDuckMessageBomComponentDetailsUtils {
         return new BomComponentDetails(component, componentVersion, componentConcerns, licenseInfo, usageInfo, additionalAttributes, issuesUrl);
     }
 
-    public static BomComponentDetails createBomComponentDetails(
+    public BomComponentDetails createMissingBomComponentDetails(
         String componentName,
         @Nullable String componentUrl,
         @Nullable String componentVersionName,
@@ -67,9 +70,6 @@ public final class BlackDuckMessageBomComponentDetailsUtils {
         String usageInfo = BlackDuckMessageLabels.VALUE_UNKNOWN_USAGE;
 
         return new BomComponentDetails(component, componentVersion, componentConcerns, licenseInfo, usageInfo, additionalAttributes, null);
-    }
-
-    private BlackDuckMessageBomComponentDetailsUtils() {
     }
 
 }
