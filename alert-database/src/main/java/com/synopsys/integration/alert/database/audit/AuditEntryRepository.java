ccc15aa67fb5c0742fbe9e3028e37ecf9a73c495
Gavin Killough <gavink@synopsys.com>
2021-04-29 09:55:06 -0400

    Feat(Audit): Add AuditEntryNotificationView

--- alert-database/src/main/java/com/synopsys/integration/alert/database/audit/AuditEntryRepository.java
+++ alert-database/src/main/java/com/synopsys/integration/alert/database/audit/AuditEntryRepository.java
@@ -7,16 +7,33 @@
  */
 package com.synopsys.integration.alert.database.audit;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AuditEntryRepository extends JpaRepository<AuditEntryEntity, Long> {
+    @Query(
+        "SELECT new com.synopsys.integration.alert.database.audit.AuditEntryNotificationView(audit.id, audit.commonConfigId, auditNotification.notificationId, audit.timeCreated, audit.timeLastSent, audit.status, audit.errorMessage, audit.errorStackTrace)"
+            + " FROM AuditEntryEntity audit"
+            + " LEFT JOIN audit.auditNotificationRelations auditNotification"
+            + " WHERE audit.commonConfigId = :jobId"
+            + " AND auditNotification.notificationId IN :notificationIds"
+            + " ORDER BY audit.timeLastSent DESC"
+    )
+    List<AuditEntryNotificationView> findByJobIdAndNotificationIds(@Param("jobId") UUID jobId, @Param("notificationIds") Collection<Long> notificationIds);
+
     Optional<AuditEntryEntity> findFirstByCommonConfigIdOrderByTimeLastSentDesc(UUID commonConfigId);
 
-    @Query(value = "SELECT entity FROM AuditEntryEntity entity INNER JOIN entity.auditNotificationRelations relation ON entity.id = relation.auditEntryId WHERE entity.commonConfigId = ?2 AND relation.notificationContent.id = ?1")
+    @Query("SELECT entity FROM AuditEntryEntity entity"
+               + " INNER JOIN entity.auditNotificationRelations relation ON entity.id = relation.auditEntryId"
+               + " WHERE entity.commonConfigId = ?2"
+               + " AND relation.notificationContent.id = ?1"
+    )
     Optional<AuditEntryEntity> findMatchingAudit(Long notificationId, UUID commonConfigId);
 
 }
