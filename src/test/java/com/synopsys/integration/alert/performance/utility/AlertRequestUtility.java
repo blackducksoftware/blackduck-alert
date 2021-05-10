8787783de4beefd76f884c483f4ef042810e013d
Gavin Killough <gavink@synopsys.com>
2021-05-10 11:46:25 -0400

    Fix(MultiFieldModel): Add empty constructor for serialization

--- src/test/java/com/synopsys/integration/alert/performance/utility/AlertRequestUtility.java
+++ src/test/java/com/synopsys/integration/alert/performance/utility/AlertRequestUtility.java
@@ -3,9 +3,10 @@ package com.synopsys.integration.alert.performance.utility;
 import com.synopsys.integration.exception.IntegrationException;
 
 public interface AlertRequestUtility {
-    public String executeGetRequest(String path, String error) throws IntegrationException;
+    String executeGetRequest(String path, String error) throws IntegrationException;
 
-    public String executePostRequest(String path, String requestBody, String error) throws IntegrationException;
+    String executePostRequest(String path, String requestBody, String error) throws IntegrationException;
 
-    public String executePutRequest(String path, String requestBody, String error) throws IntegrationException;
+    String executePutRequest(String path, String requestBody, String error) throws IntegrationException;
+
 }
