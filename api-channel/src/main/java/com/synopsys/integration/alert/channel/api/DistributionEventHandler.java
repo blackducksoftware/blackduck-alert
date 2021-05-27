2fe5c23800525c2299d50e586d10ae63d77bd29d
Gavin Killough <gavink@synopsys.com>
2021-05-27 09:43:11 -0400

    Chore: Add license headers

--- api-channel/src/main/java/com/synopsys/integration/alert/channel/api/DistributionEventHandler.java
+++ api-channel/src/main/java/com/synopsys/integration/alert/channel/api/DistributionEventHandler.java
@@ -1,3 +1,10 @@
+/*
+ * api-channel
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.alert.channel.api;
 
 import java.util.Optional;
--- channel/src/main/java/com/synopsys/integration/alert/channel/jira/server/distribution/JiraServerDistributionEventHandler.java
+++ channel/src/main/java/com/synopsys/integration/alert/channel/jira/server/distribution/JiraServerDistributionEventHandler.java
@@ -1,3 +1,10 @@
+/*
+ * channel
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.alert.channel.jira.server.distribution;
 
 import org.springframework.beans.factory.annotation.Autowired;
