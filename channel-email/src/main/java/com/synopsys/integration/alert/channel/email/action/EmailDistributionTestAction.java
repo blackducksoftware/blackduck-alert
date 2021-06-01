f8ffefee894ce8f56775b7d21d0ce085e23f6fc0
Gavin Killough <gavink@synopsys.com>
2021-06-01 14:00:56 -0400

    Fix(Email): Make subject line optional

--- channel-email/src/main/java/com/synopsys/integration/alert/channel/email/action/EmailDistributionTestAction.java
+++ channel-email/src/main/java/com/synopsys/integration/alert/channel/email/action/EmailDistributionTestAction.java
@@ -14,9 +14,9 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.synopsys.integration.alert.api.common.model.exception.AlertException;
 import com.synopsys.integration.alert.channel.api.action.DistributionChannelMessageTestAction;
 import com.synopsys.integration.alert.channel.email.distribution.EmailChannel;
-import com.synopsys.integration.alert.api.common.model.exception.AlertException;
 import com.synopsys.integration.alert.common.persistence.model.job.DistributionJobModel;
 import com.synopsys.integration.alert.common.persistence.model.job.details.DistributionJobDetailsModel;
 import com.synopsys.integration.alert.common.persistence.model.job.details.EmailJobDetailsModel;
@@ -45,7 +45,7 @@ public class EmailDistributionTestAction extends DistributionChannelMessageTestA
 
         return new EmailJobDetailsModel(
             testJobModel.getJobId(),
-            originalEmailJobDetails.getSubjectLine(),
+            originalEmailJobDetails.getSubjectLine().orElse(null),
             false,
             true,
             originalEmailJobDetails.getAttachmentFileType(),
