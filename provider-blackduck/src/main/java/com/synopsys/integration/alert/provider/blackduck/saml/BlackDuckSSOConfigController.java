9180c6c0ccdc859fef169929615d52dd6e8c259f
Gavin Killough <gavink@synopsys.com>
2021-06-04 15:02:50 -0400

    Feat(BlackDuck): Create SSO configuration REST controller

---
+++ provider-blackduck/src/main/java/com/synopsys/integration/alert/provider/blackduck/saml/BlackDuckSSOConfigController.java
@@ -0,0 +1,35 @@
+/*
+ * provider-blackduck
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
+package com.synopsys.integration.alert.provider.blackduck.saml;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.synopsys.integration.alert.common.action.ActionResponse;
+import com.synopsys.integration.alert.common.rest.ResponseFactory;
+
+@RestController
+@RequestMapping("/api/blackduck/{blackDuckConfigId}/sso/configuration")
+public class BlackDuckSSOConfigController {
+    private final BlackDuckSSOConfigActions blackDuckSSOConfigActions;
+
+    @Autowired
+    public BlackDuckSSOConfigController(BlackDuckSSOConfigActions blackDuckSSOConfigActions) {
+        this.blackDuckSSOConfigActions = blackDuckSSOConfigActions;
+    }
+
+    @GetMapping
+    public BlackDuckSSOConfig getBlackDuckSSOConfig(@PathVariable Long blackDuckConfigId) {
+        ActionResponse<BlackDuckSSOConfig> configActionResponse = blackDuckSSOConfigActions.retrieveBlackDuckSSOConfig(blackDuckConfigId);
+        return ResponseFactory.createContentResponseFromAction(configActionResponse);
+    }
+
+}
