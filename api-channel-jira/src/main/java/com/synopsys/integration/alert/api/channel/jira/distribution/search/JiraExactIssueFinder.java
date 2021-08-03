65eb2edfb6c6b6be842e067467ed0fc236b8ff24
Gavin Killough <gavink@synopsys.com>
2021-08-03 09:03:52 -0400

    Feat(api-channel-jira): Implement JiraSearcherFactory for creating IssueTrackerSearcherV2

---
+++ api-channel-jira/src/main/java/com/synopsys/integration/alert/api/channel/jira/distribution/search/JiraExactIssueFinder.java
@@ -0,0 +1,72 @@
+/*
+ * api-channel-jira
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
+package com.synopsys.integration.alert.api.channel.jira.distribution.search;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.synopsys.integration.alert.api.channel.issue.model.IssueBomComponentDetails;
+import com.synopsys.integration.alert.api.channel.issue.model.IssuePolicyDetails;
+import com.synopsys.integration.alert.api.channel.issue.model.ProjectIssueModel;
+import com.synopsys.integration.alert.api.channel.issue.search.ExactIssueFinder;
+import com.synopsys.integration.alert.api.channel.issue.search.ExistingIssueDetails;
+import com.synopsys.integration.alert.api.channel.issue.search.IssueCategoryRetriever;
+import com.synopsys.integration.alert.api.channel.issue.search.enumeration.IssueCategory;
+import com.synopsys.integration.alert.api.common.model.exception.AlertException;
+import com.synopsys.integration.alert.common.message.model.LinkableItem;
+import com.synopsys.integration.alert.processor.api.extract.model.project.ComponentConcernType;
+
+public class JiraExactIssueFinder implements ExactIssueFinder<String> {
+    private final String jiraProjectKey;
+    private final JqlQueryExecutor jqlQueryExecutor;
+    private final JiraIssueSearchResultCreator searchResultCreator;
+    private final IssueCategoryRetriever issueCategoryRetriever;
+
+    public JiraExactIssueFinder(String jiraProjectKey, JqlQueryExecutor jqlQueryExecutor, JiraIssueSearchResultCreator searchResultCreator, IssueCategoryRetriever issueCategoryRetriever) {
+        this.jiraProjectKey = jiraProjectKey;
+        this.jqlQueryExecutor = jqlQueryExecutor;
+        this.searchResultCreator = searchResultCreator;
+        this.issueCategoryRetriever = issueCategoryRetriever;
+    }
+
+    @Override
+    public List<ExistingIssueDetails<String>> findExistingIssuesByProjectIssueModel(ProjectIssueModel projectIssueModel) throws AlertException {
+        LinkableItem provider = projectIssueModel.getProvider();
+        LinkableItem project = projectIssueModel.getProject();
+        IssueBomComponentDetails bomComponent = projectIssueModel.getBomComponentDetails();
+
+        ComponentConcernType concernType = ComponentConcernType.VULNERABILITY;
+        String policyName = null;
+
+        Optional<IssuePolicyDetails> policyDetails = projectIssueModel.getPolicyDetails();
+        Optional<String> optionalPolicyName = policyDetails.map(IssuePolicyDetails::getName);
+        if (optionalPolicyName.isPresent()) {
+            concernType = ComponentConcernType.POLICY;
+            policyName = optionalPolicyName.get();
+        }
+
+        String jqlString = JqlStringCreator.createBlackDuckComponentConcernIssuesSearchString(
+            jiraProjectKey,
+            provider,
+            project,
+            projectIssueModel.getProjectVersion().orElse(null),
+            bomComponent.getComponent(),
+            bomComponent.getComponentVersion().orElse(null),
+            concernType,
+            policyName
+        );
+
+        IssueCategory issueCategory = issueCategoryRetriever.retrieveIssueCategoryFromComponentConcernType(concernType);
+        return jqlQueryExecutor.executeQuery(jqlString)
+                   .stream()
+                   .map(jiraSearcherResponseModel -> searchResultCreator.createExistingIssueDetails(jiraSearcherResponseModel, issueCategory))
+                   .collect(Collectors.toList());
+    }
+
+}
