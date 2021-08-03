c8bbabbf027653233fecf5f30b0cf4b409db5fb1
Gavin Killough <gavink@synopsys.com>
2021-08-03 09:52:01 -0400

    Feat(Jira): Use new issue search framework

--- api-channel-issue-tracker/src/main/java/com/synopsys/integration/alert/api/channel/issue/search/IssueTrackerSearcher.java
+++ api-channel-issue-tracker/src/main/java/com/synopsys/integration/alert/api/channel/issue/search/IssueTrackerSearcher.java
@@ -19,7 +19,8 @@ import com.synopsys.integration.alert.processor.api.extract.model.project.BomCom
 import com.synopsys.integration.alert.processor.api.extract.model.project.ProjectMessage;
 
 // TODO remove in favor of IssueTrackerSearcherV2
-public abstract class IssueTrackerSearcher<T extends Serializable> {
+@Deprecated
+public class IssueTrackerSearcher<T extends Serializable> {
     private final IssueTrackerSearcherV2<T> issueTrackerSearcherV2;
 
     public IssueTrackerSearcher(IssueTrackerSearcherV2<T> issueTrackerSearcherV2) {
@@ -40,12 +41,24 @@ public abstract class IssueTrackerSearcher<T extends Serializable> {
         return issueTrackerSearcherV2.findIssues(projectMessage);
     }
 
-    protected abstract List<ProjectIssueSearchResult<T>> findProjectIssues(ProviderDetails providerDetails, LinkableItem project) throws AlertException;
+    @Deprecated
+    protected List<ProjectIssueSearchResult<T>> findProjectIssues(ProviderDetails providerDetails, LinkableItem project) throws AlertException {
+        throw new UnsupportedOperationException("Deprecated for replacement");
+    }
 
-    protected abstract List<ProjectIssueSearchResult<T>> findProjectVersionIssues(ProviderDetails providerDetails, LinkableItem project, LinkableItem projectVersion) throws AlertException;
+    @Deprecated
+    protected List<ProjectIssueSearchResult<T>> findProjectVersionIssues(ProviderDetails providerDetails, LinkableItem project, LinkableItem projectVersion) throws AlertException {
+        throw new UnsupportedOperationException("Deprecated for replacement");
+    }
 
-    protected abstract List<ProjectIssueSearchResult<T>> findIssuesByComponent(ProviderDetails providerDetails, LinkableItem project, LinkableItem projectVersion, BomComponentDetails bomComponent) throws AlertException;
+    @Deprecated
+    protected List<ProjectIssueSearchResult<T>> findIssuesByComponent(ProviderDetails providerDetails, LinkableItem project, LinkableItem projectVersion, BomComponentDetails bomComponent) throws AlertException {
+        throw new UnsupportedOperationException("Deprecated for replacement");
+    }
 
-    protected abstract List<ExistingIssueDetails<T>> findExistingIssuesByProjectIssueModel(ProjectIssueModel projectIssueModel) throws AlertException;
+    @Deprecated
+    protected List<ExistingIssueDetails<T>> findExistingIssuesByProjectIssueModel(ProjectIssueModel projectIssueModel) throws AlertException {
+        throw new UnsupportedOperationException("Deprecated for replacement");
+    }
 
 }
