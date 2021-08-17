315e77c05248b0e1fec5f4013abf3e724c0d2f46
Gavin Killough <gavink@synopsys.com>
2021-08-17 11:06:22 -0400

    Test(JiraJobDetailsExtractor)

--- api-channel-jira/src/main/java/com/synopsys/integration/alert/api/channel/jira/action/JiraJobDetailsExtractor.java
+++ api-channel-jira/src/main/java/com/synopsys/integration/alert/api/channel/jira/action/JiraJobDetailsExtractor.java
@@ -16,13 +16,13 @@ import com.synopsys.integration.alert.common.persistence.model.ConfigurationFiel
 import com.synopsys.integration.alert.common.persistence.model.job.details.JiraJobCustomFieldModel;
 import com.synopsys.integration.alert.common.persistence.model.job.details.processor.DistributionJobDetailsExtractor;
 import com.synopsys.integration.alert.common.persistence.model.job.details.processor.DistributionJobFieldExtractor;
-import com.synopsys.integration.alert.descriptor.api.model.ChannelKey;
+import com.synopsys.integration.alert.descriptor.api.model.IssueTrackerChannelKey;
 
 public abstract class JiraJobDetailsExtractor extends DistributionJobDetailsExtractor {
     private final DistributionJobFieldExtractor fieldExtractor;
     private final Gson gson;
 
-    public JiraJobDetailsExtractor(ChannelKey channelKey, DistributionJobFieldExtractor fieldExtractor, Gson gson) {
+    public JiraJobDetailsExtractor(IssueTrackerChannelKey channelKey, DistributionJobFieldExtractor fieldExtractor, Gson gson) {
         super(channelKey);
         this.fieldExtractor = fieldExtractor;
         this.gson = gson;
@@ -31,8 +31,8 @@ public abstract class JiraJobDetailsExtractor extends DistributionJobDetailsExtr
     protected List<JiraJobCustomFieldModel> extractJiraFieldMappings(String fieldKey, Map<String, ConfigurationFieldModel> configuredFieldsMap) {
         List<String> fieldMappingStrings = fieldExtractor.extractFieldValues(fieldKey, configuredFieldsMap);
         return fieldMappingStrings.stream()
-                   .map(fieldMappingString -> gson.fromJson(fieldMappingString, JiraJobCustomFieldModel.class))
-                   .collect(Collectors.toList());
+            .map(fieldMappingString -> gson.fromJson(fieldMappingString, JiraJobCustomFieldModel.class))
+            .collect(Collectors.toList());
     }
 
 }
