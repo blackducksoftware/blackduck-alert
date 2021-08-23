3ef9be3525c93956e8457519005fc33b81581d57
Martin Chomicki <martinch@synopsys.com>
2021-08-23 15:18:37 -0400

    test: Add unit tests for PolicyOverrideNotificationMessageExtractor

--- provider-blackduck/src/test/java/com/synopsys/integration/alert/provider/blackduck/processor/message/PolicyOverrideNotificationMessageExtractorTest.java
+++ provider-blackduck/src/test/java/com/synopsys/integration/alert/provider/blackduck/processor/message/PolicyOverrideNotificationMessageExtractorTest.java
@@ -1,29 +1,31 @@
 package com.synopsys.integration.alert.provider.blackduck.processor.message;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.http.HttpStatus;
 
 import com.synopsys.integration.alert.common.message.model.LinkableItem;
 import com.synopsys.integration.alert.descriptor.api.BlackDuckProviderKey;
-import com.synopsys.integration.alert.processor.api.extract.model.ProviderDetails;
 import com.synopsys.integration.alert.processor.api.extract.model.project.BomComponentDetails;
+import com.synopsys.integration.alert.processor.api.extract.model.project.ComponentConcern;
+import com.synopsys.integration.alert.processor.api.extract.model.project.ComponentConcernSeverity;
+import com.synopsys.integration.alert.processor.api.extract.model.project.ComponentPolicy;
 import com.synopsys.integration.alert.processor.api.extract.model.project.ComponentUpgradeGuidance;
 import com.synopsys.integration.alert.processor.api.extract.model.project.ComponentVulnerabilities;
-import com.synopsys.integration.alert.processor.api.extract.model.project.MessageReason;
-import com.synopsys.integration.alert.processor.api.extract.model.project.ProjectMessage;
 import com.synopsys.integration.alert.provider.blackduck.processor.NotificationExtractorBlackDuckServicesFactoryCache;
 import com.synopsys.integration.alert.provider.blackduck.processor.message.service.BlackDuckMessageBomComponentDetailsCreator;
 import com.synopsys.integration.alert.provider.blackduck.processor.message.service.BlackDuckMessageBomComponentDetailsCreatorFactory;
 import com.synopsys.integration.alert.provider.blackduck.processor.message.service.BomComponent404Handler;
-import com.synopsys.integration.alert.provider.blackduck.processor.model.BomEditWithProjectNameNotificationContent;
+import com.synopsys.integration.alert.provider.blackduck.processor.message.service.policy.BlackDuckPolicyComponentConcernCreator;
+import com.synopsys.integration.alert.provider.blackduck.processor.message.service.policy.BlackDuckPolicySeverityConverter;
+import com.synopsys.integration.alert.provider.blackduck.processor.model.PolicyOverrideUniquePolicyNotificationContent;
 import com.synopsys.integration.blackduck.api.generated.view.ProjectVersionComponentVersionView;
-import com.synopsys.integration.blackduck.api.manual.component.BomEditNotificationContent;
+import com.synopsys.integration.blackduck.api.manual.component.PolicyInfo;
 import com.synopsys.integration.blackduck.service.BlackDuckApiClient;
 import com.synopsys.integration.blackduck.service.BlackDuckServicesFactory;
 import com.synopsys.integration.exception.IntegrationException;
@@ -31,14 +33,11 @@ import com.synopsys.integration.rest.HttpMethod;
 import com.synopsys.integration.rest.HttpUrl;
 import com.synopsys.integration.rest.exception.IntegrationRestException;
 
-public class BomEditNotificationMessageExtractorTest {
-    private static final ProviderDetails PROVIDER_DETAILS = new ProviderDetails(15L, new LinkableItem("Provider", "A provider", "https://provider-url"));
-    private static final LinkableItem PROJECT_ITEM = new LinkableItem("Project", "A project", "https://project-url");
-    private static final LinkableItem PROJECT_VERSION_ITEM = new LinkableItem("Project Version", "2.3.4-RC", "https://project-version-url");
-
+public class PolicyOverrideNotificationMessageExtractorTest {
     private static final LinkableItem COMPONENT = new LinkableItem("Component", "A BOM component");
     private static final LinkableItem COMPONENT_VERSION = new LinkableItem("Component Version", "0.8.7");
     private static final ComponentVulnerabilities COMPONENT_VULNERABILITIES = ComponentVulnerabilities.none();
+    private static final ComponentPolicy COMPONENT_POLICY = new ComponentPolicy("policyName", ComponentConcernSeverity.BLOCKER, true, false, "A Policy Description", "category");
     private static final LinkableItem LICENSE = new LinkableItem("License", "A software license", "https://license-url");
     private static final String USAGE = "Some generic usage";
     private static final String ISSUES_URL = "https://issues-url";
@@ -50,47 +49,78 @@ public class BomEditNotificationMessageExtractorTest {
     private final BlackDuckMessageBomComponentDetailsCreatorFactory detailsCreatorFactory = Mockito.mock(BlackDuckMessageBomComponentDetailsCreatorFactory.class);
     private final BomComponent404Handler bomComponent404Handler = new BomComponent404Handler();
 
-    @Test
-    public void createProjectMessageTest() {
-        BomEditNotificationMessageExtractor bomEditNotificationMessageExtractor = new BomEditNotificationMessageExtractor(providerKey, servicesFactoryCache, detailsCreatorFactory, bomComponent404Handler);
+    private final BlackDuckPolicySeverityConverter blackDuckPolicySeverityConverter = new BlackDuckPolicySeverityConverter();
+    private final BlackDuckPolicyComponentConcernCreator blackDuckPolicyComponentConcernCreator = new BlackDuckPolicyComponentConcernCreator(blackDuckPolicySeverityConverter);
 
-        ProjectMessage projectMessage = bomEditNotificationMessageExtractor.createProjectMessage(PROVIDER_DETAILS, PROJECT_ITEM, PROJECT_VERSION_ITEM, List.of(bomComponentDetails));
+    private final PolicyOverrideUniquePolicyNotificationContent policyOverrideUniquePolicyNotificationContent = createPolicyOverrideUniquePolicyNotificationContent();
+    private PolicyOverrideNotificationMessageExtractor policyOverrideNotificationMessageExtractor;
 
-        assertEquals(MessageReason.COMPONENT_UPDATE, projectMessage.getMessageReason());
-        assertTrue(projectMessage.getBomComponents().contains(bomComponentDetails));
+    @BeforeEach
+    public void init() {
+        policyOverrideNotificationMessageExtractor = new PolicyOverrideNotificationMessageExtractor(
+            providerKey,
+            servicesFactoryCache,
+            blackDuckPolicyComponentConcernCreator,
+            detailsCreatorFactory,
+            bomComponent404Handler
+        );
     }
 
     @Test
     public void createBomComponentDetailsTest() throws IntegrationException {
-        BomEditNotificationMessageExtractor bomEditNotificationMessageExtractor = new BomEditNotificationMessageExtractor(providerKey, servicesFactoryCache, detailsCreatorFactory, bomComponent404Handler);
-        BomEditWithProjectNameNotificationContent notificationContent = createBomEditWithProjectNameNotificationContent();
         BlackDuckServicesFactory blackDuckServicesFactory = createBlackDuckServicesFactoryWithMocks();
 
         BlackDuckMessageBomComponentDetailsCreator bomComponentDetailsCreator = Mockito.mock(BlackDuckMessageBomComponentDetailsCreator.class);
         Mockito.when(detailsCreatorFactory.createBomComponentDetailsCreator(Mockito.any())).thenReturn(bomComponentDetailsCreator);
-        Mockito.when(bomComponentDetailsCreator.createBomComponentDetails(Mockito.any(), Mockito.anyList(), Mockito.any(), Mockito.anyList())).thenReturn(bomComponentDetails);
+        Mockito.when(bomComponentDetailsCreator.createBomComponentDetails(Mockito.any(), (ComponentConcern) Mockito.any(), Mockito.eq(ComponentUpgradeGuidance.none()), Mockito.anyList())).thenReturn(bomComponentDetails);
 
-        List<BomComponentDetails> bomComponentDetailsList = bomEditNotificationMessageExtractor.createBomComponentDetails(notificationContent, blackDuckServicesFactory);
+        List<BomComponentDetails> bomComponentDetailsList = policyOverrideNotificationMessageExtractor.createBomComponentDetails(policyOverrideUniquePolicyNotificationContent, blackDuckServicesFactory);
         assertTrue(bomComponentDetailsList.contains(bomComponentDetails));
     }
 
     @Test
     public void createBomComponentDetailsExceptionTest() throws IntegrationException {
-        BomEditNotificationMessageExtractor bomEditNotificationMessageExtractor = new BomEditNotificationMessageExtractor(providerKey, servicesFactoryCache, detailsCreatorFactory, bomComponent404Handler);
-        BomEditWithProjectNameNotificationContent notificationContent = createBomEditWithProjectNameNotificationContent();
         BlackDuckServicesFactory blackDuckServicesFactory = createBlackDuckServicesFactoryWithMocks();
 
         BlackDuckMessageBomComponentDetailsCreator bomComponentDetailsCreator = Mockito.mock(BlackDuckMessageBomComponentDetailsCreator.class);
         Mockito.when(detailsCreatorFactory.createBomComponentDetailsCreator(Mockito.any())).thenReturn(bomComponentDetailsCreator);
-
         Mockito.doThrow(new IntegrationRestException(HttpMethod.GET, new HttpUrl("https://google.com"), HttpStatus.NOT_FOUND.value(), "httpStatusMessageTest", "httpResponseContentTest", "IntegrationRestExceptionForAlertTest"))
-            .when(bomComponentDetailsCreator).createBomComponentDetails(Mockito.any(), Mockito.anyList(), Mockito.any(), Mockito.anyList());
+            .when(bomComponentDetailsCreator).createBomComponentDetails(Mockito.any(), (ComponentConcern) Mockito.any(), Mockito.any(), Mockito.anyList());
 
         Mockito.when(bomComponentDetailsCreator.createMissingBomComponentDetails(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyList(), Mockito.any(), Mockito.anyList()))
             .thenReturn(bomComponentDetails);
 
-        List<BomComponentDetails> bomComponentDetailsList = bomEditNotificationMessageExtractor.createBomComponentDetails(notificationContent, blackDuckServicesFactory);
+        List<BomComponentDetails> bomComponentDetailsList = policyOverrideNotificationMessageExtractor.createBomComponentDetails(policyOverrideUniquePolicyNotificationContent, blackDuckServicesFactory);
         assertTrue(bomComponentDetailsList.contains(bomComponentDetails));
+    }
+
+    private PolicyOverrideUniquePolicyNotificationContent createPolicyOverrideUniquePolicyNotificationContent() {
+        String projectName = "Project";
+        String projectVersionName = "ProjectVersionName";
+        String projectVersion = "http://projectVersionUrl";
+        String componentName = "ComponentName";
+        String componentVersionName = "ComponentVersionName";
+        String firstName = "firstName";
+        String lastName = "lastName";
+        //TODO: set the policy info
+        PolicyInfo policyInfo = new PolicyInfo();
+        String policy = "http://policyUrl";
+        String bomComponentVersionPolicyStatus = "BomComponentVersionPolicyStatus";
+        String bomComponent = "http://bomComponentUrl";
+
+        return new PolicyOverrideUniquePolicyNotificationContent(
+            projectName,
+            projectVersionName,
+            projectVersion,
+            componentName,
+            componentVersionName,
+            firstName,
+            lastName,
+            policyInfo,
+            policy,
+            bomComponentVersionPolicyStatus,
+            bomComponent
+        );
     }
 
     private BlackDuckServicesFactory createBlackDuckServicesFactoryWithMocks() throws IntegrationException {
@@ -104,22 +134,12 @@ public class BomEditNotificationMessageExtractorTest {
         return blackDuckServicesFactory;
     }
 
-    private BomEditWithProjectNameNotificationContent createBomEditWithProjectNameNotificationContent() {
-        BomEditNotificationContent bomEditNotificationContent = new BomEditNotificationContent();
-        bomEditNotificationContent.setProjectVersion(PROJECT_VERSION_ITEM.getLabel());
-        bomEditNotificationContent.setBomComponent("http://bomComponentUrl");
-        bomEditNotificationContent.setComponentName(COMPONENT.getLabel());
-        bomEditNotificationContent.setComponentVersionName(COMPONENT_VERSION.getLabel());
-
-        return new BomEditWithProjectNameNotificationContent(bomEditNotificationContent, PROJECT_ITEM.getLabel(), PROJECT_VERSION_ITEM.getLabel());
-    }
-
     private BomComponentDetails createBomComponentDetails() {
         return new BomComponentDetails(
             COMPONENT,
             COMPONENT_VERSION,
             COMPONENT_VULNERABILITIES,
-            List.of(),
+            List.of(COMPONENT_POLICY),
             List.of(),
             LICENSE,
             USAGE,
@@ -128,4 +148,5 @@ public class BomEditNotificationMessageExtractorTest {
             ISSUES_URL
         );
     }
+
 }
