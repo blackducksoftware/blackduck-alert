d72671648be87e96d7b487789c71d373f7bed33c
Martin Chomicki <martinch@synopsys.com>
2021-08-24 15:47:21 -0400

    test: Improve PolicyOverrideNotificationMessageExtractorTest to use less mocks

--- provider-blackduck/src/test/java/com/synopsys/integration/alert/provider/blackduck/processor/message/PolicyOverrideNotificationMessageExtractorTest.java
+++ provider-blackduck/src/test/java/com/synopsys/integration/alert/provider/blackduck/processor/message/PolicyOverrideNotificationMessageExtractorTest.java
@@ -13,14 +13,10 @@ import org.springframework.http.HttpStatus;
 import com.synopsys.integration.alert.common.message.model.LinkableItem;
 import com.synopsys.integration.alert.descriptor.api.BlackDuckProviderKey;
 import com.synopsys.integration.alert.processor.api.extract.model.project.BomComponentDetails;
-import com.synopsys.integration.alert.processor.api.extract.model.project.ComponentConcern;
 import com.synopsys.integration.alert.processor.api.extract.model.project.ComponentConcernSeverity;
 import com.synopsys.integration.alert.processor.api.extract.model.project.ComponentPolicy;
-import com.synopsys.integration.alert.processor.api.extract.model.project.ComponentUpgradeGuidance;
-import com.synopsys.integration.alert.processor.api.extract.model.project.ComponentVulnerabilities;
 import com.synopsys.integration.alert.provider.blackduck.processor.NotificationExtractorBlackDuckServicesFactoryCache;
 import com.synopsys.integration.alert.provider.blackduck.processor.message.service.BlackDuckComponentVulnerabilityDetailsCreator;
-import com.synopsys.integration.alert.provider.blackduck.processor.message.service.BlackDuckMessageBomComponentDetailsCreator;
 import com.synopsys.integration.alert.provider.blackduck.processor.message.service.BlackDuckMessageBomComponentDetailsCreatorFactory;
 import com.synopsys.integration.alert.provider.blackduck.processor.message.service.BomComponent404Handler;
 import com.synopsys.integration.alert.provider.blackduck.processor.message.service.policy.BlackDuckComponentPolicyDetailsCreatorFactory;
@@ -49,64 +45,36 @@ public class PolicyOverrideNotificationMessageExtractorTest {
     private static final LinkableItem COMPONENT = new LinkableItem("Component", "A BOM component");
     private static final LinkableItem COMPONENT_VERSION = new LinkableItem("Component Version", "0.8.7");
     private static final String COMPONENT_VERSION_URL = "http://componentVersionUrl";
-    private static final ComponentVulnerabilities COMPONENT_VULNERABILITIES = ComponentVulnerabilities.none();
     private static final ComponentPolicy COMPONENT_POLICY = new ComponentPolicy("policyName", ComponentConcernSeverity.BLOCKER, true, false, "A Policy Description", "category");
-    private static final LinkableItem LICENSE = new LinkableItem("License", "A software license", "https://license-url");
-    private static final String USAGE = "Some generic usage";
-    private static final String ISSUES_URL = "https://issues-url";
-
-    private final BomComponentDetails bomComponentDetails = createBomComponentDetails();
-
-    private final BlackDuckProviderKey providerKey = new BlackDuckProviderKey();
-    private final NotificationExtractorBlackDuckServicesFactoryCache servicesFactoryCache = Mockito.mock(NotificationExtractorBlackDuckServicesFactoryCache.class);
-    private final BlackDuckMessageBomComponentDetailsCreatorFactory detailsCreatorFactory = Mockito.mock(BlackDuckMessageBomComponentDetailsCreatorFactory.class);
-    private final BomComponent404Handler bomComponent404Handler = new BomComponent404Handler();
-
-    private final BlackDuckPolicySeverityConverter blackDuckPolicySeverityConverter = new BlackDuckPolicySeverityConverter();
-    private final BlackDuckPolicyComponentConcernCreator blackDuckPolicyComponentConcernCreator = new BlackDuckPolicyComponentConcernCreator(blackDuckPolicySeverityConverter);
+    private static final String LICENSE_DISPLAY = "licenseDisplay";
 
     private final PolicyOverrideUniquePolicyNotificationContent policyOverrideUniquePolicyNotificationContent = createPolicyOverrideUniquePolicyNotificationContent();
-    private PolicyOverrideNotificationMessageExtractor policyOverrideNotificationMessageExtractor;
+    private PolicyOverrideNotificationMessageExtractor extractor;
 
     @BeforeEach
     public void init() {
-        policyOverrideNotificationMessageExtractor = new PolicyOverrideNotificationMessageExtractor(
-            providerKey,
-            servicesFactoryCache,
-            blackDuckPolicyComponentConcernCreator,
-            detailsCreatorFactory,
-            bomComponent404Handler
-        );
-    }
+        BlackDuckProviderKey providerKey = new BlackDuckProviderKey();
+        NotificationExtractorBlackDuckServicesFactoryCache servicesFactoryCache = Mockito.mock(NotificationExtractorBlackDuckServicesFactoryCache.class);
+        BomComponent404Handler bomComponent404Handler = new BomComponent404Handler();
 
-    @Test
-    public void createBomComponentDetailsTest() throws IntegrationException {
-        BlackDuckServicesFactory blackDuckServicesFactory = createBlackDuckServicesFactoryWithMocks();
+        BlackDuckPolicySeverityConverter blackDuckPolicySeverityConverter = new BlackDuckPolicySeverityConverter();
+        BlackDuckPolicyComponentConcernCreator blackDuckPolicyComponentConcernCreator = new BlackDuckPolicyComponentConcernCreator(blackDuckPolicySeverityConverter);
 
-        BlackDuckMessageBomComponentDetailsCreator bomComponentDetailsCreator = Mockito.mock(BlackDuckMessageBomComponentDetailsCreator.class);
-        Mockito.when(detailsCreatorFactory.createBomComponentDetailsCreator(Mockito.any())).thenReturn(bomComponentDetailsCreator);
-        Mockito.when(bomComponentDetailsCreator.createBomComponentDetails(Mockito.any(), (ComponentConcern) Mockito.any(), Mockito.eq(ComponentUpgradeGuidance.none()), Mockito.anyList())).thenReturn(bomComponentDetails);
-
-        List<BomComponentDetails> bomComponentDetailsList = policyOverrideNotificationMessageExtractor.createBomComponentDetails(policyOverrideUniquePolicyNotificationContent, blackDuckServicesFactory);
-        assertTrue(bomComponentDetailsList.contains(bomComponentDetails));
-    }
-
-    @Test
-    public void createBomComponentDetailsREVISITEDTest() throws IntegrationException {
-        //TODO: Perhaps we can improve this test with a concrete implementation of the BlackDuckMessageBomComponentDetailsCreator
-        //  we already have a policySeverityConverter, we can use that here
         BlackDuckComponentVulnerabilityDetailsCreator vulnerabilityDetailsCreator = new BlackDuckComponentVulnerabilityDetailsCreator();
         BlackDuckComponentPolicyDetailsCreatorFactory blackDuckComponentPolicyDetailsCreatorFactory = new BlackDuckComponentPolicyDetailsCreatorFactory(blackDuckPolicySeverityConverter);
         BlackDuckMessageBomComponentDetailsCreatorFactory newDetailsCreatorFactory = new BlackDuckMessageBomComponentDetailsCreatorFactory(vulnerabilityDetailsCreator, blackDuckComponentPolicyDetailsCreatorFactory);
 
-        PolicyOverrideNotificationMessageExtractor extractor = new PolicyOverrideNotificationMessageExtractor(
+        extractor = new PolicyOverrideNotificationMessageExtractor(
             providerKey,
             servicesFactoryCache,
             blackDuckPolicyComponentConcernCreator,
             newDetailsCreatorFactory,
             bomComponent404Handler
         );
+    }
 
+    @Test
+    public void createBomComponentDetailsTest() throws IntegrationException {
         BlackDuckServicesFactory blackDuckServicesFactory = Mockito.mock(BlackDuckServicesFactory.class);
         BlackDuckApiClient blackDuckApiClient = Mockito.mock(BlackDuckApiClient.class);
         Mockito.when(blackDuckServicesFactory.getBlackDuckApiClient()).thenReturn(blackDuckApiClient);
@@ -130,43 +98,49 @@ public class PolicyOverrideNotificationMessageExtractorTest {
         List<BomComponentDetails> bomComponentDetailsList = extractor.createBomComponentDetails(policyOverrideUniquePolicyNotificationContent, blackDuckServicesFactory);
 
         assertEquals(1, bomComponentDetailsList.size());
-        BomComponentDetails bomComponentDetails = bomComponentDetailsList.get(0);
-        assertEquals(COMPONENT, bomComponentDetails.getComponent());
-        assertEquals(1, bomComponentDetails.getComponentConcerns().size());
-        assertTrue(bomComponentDetails.getComponentVersion().isPresent());
-        assertEquals(COMPONENT_VERSION.getValue(), bomComponentDetails.getComponentVersion().get().getValue());
-        assertEquals("licenseDisplay", bomComponentDetails.getLicense().getValue());
-        assertEquals(UsageType.DYNAMICALLY_LINKED.prettyPrint(), bomComponentDetails.getUsage());
-        assertEquals(1, bomComponentDetails.getAdditionalAttributes().size());
+        BomComponentDetails testBomComponentDetails = bomComponentDetailsList.get(0);
+        assertEquals(COMPONENT, testBomComponentDetails.getComponent());
+        assertEquals(1, testBomComponentDetails.getComponentConcerns().size());
+        assertTrue(testBomComponentDetails.getComponentVersion().isPresent());
+        assertEquals(COMPONENT_VERSION.getValue(), testBomComponentDetails.getComponentVersion().get().getValue());
+        assertEquals(LICENSE_DISPLAY, testBomComponentDetails.getLicense().getValue());
+        assertEquals(UsageType.DYNAMICALLY_LINKED.prettyPrint(), testBomComponentDetails.getUsage());
+        assertEquals(1, testBomComponentDetails.getAdditionalAttributes().size());
 
-        assertEquals(1, bomComponentDetails.getRelevantPolicies().size());
-        ComponentPolicy testComponentPolicy = bomComponentDetails.getRelevantPolicies().get(0);
+        assertEquals(1, testBomComponentDetails.getRelevantPolicies().size());
+        ComponentPolicy testComponentPolicy = testBomComponentDetails.getRelevantPolicies().get(0);
         assertTrue(testComponentPolicy.getCategory().isPresent());
         assertEquals(PolicyRuleCategoryType.UNCATEGORIZED.toString(), testComponentPolicy.getCategory().get());
     }
 
     @Test
-    public void createBomComponentDetailsExceptionTest() throws IntegrationException {
-        BlackDuckServicesFactory blackDuckServicesFactory = createBlackDuckServicesFactoryWithMocks();
+    public void createBomComponentDetailsMissingBomComponentTest() throws IntegrationException {
+        BlackDuckServicesFactory blackDuckServicesFactory = Mockito.mock(BlackDuckServicesFactory.class);
+        BlackDuckApiClient blackDuckApiClient = Mockito.mock(BlackDuckApiClient.class);
+        Mockito.when(blackDuckServicesFactory.getBlackDuckApiClient()).thenReturn(blackDuckApiClient);
 
-        BlackDuckMessageBomComponentDetailsCreator bomComponentDetailsCreator = Mockito.mock(BlackDuckMessageBomComponentDetailsCreator.class);
-        Mockito.when(detailsCreatorFactory.createBomComponentDetailsCreator(Mockito.any())).thenReturn(bomComponentDetailsCreator);
         Mockito.doThrow(new IntegrationRestException(HttpMethod.GET, new HttpUrl("https://google.com"), HttpStatus.NOT_FOUND.value(), "httpStatusMessageTest", "httpResponseContentTest", "IntegrationRestExceptionForAlertTest"))
-            .when(bomComponentDetailsCreator).createBomComponentDetails(Mockito.any(), (ComponentConcern) Mockito.any(), Mockito.any(), Mockito.anyList());
+            .when(blackDuckApiClient).getResponse(Mockito.any(), Mockito.any());
 
-        Mockito.when(bomComponentDetailsCreator.createMissingBomComponentDetails(Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyString(), Mockito.anyList(), Mockito.any(), Mockito.anyList()))
-            .thenReturn(bomComponentDetails);
+        List<BomComponentDetails> bomComponentDetailsList = extractor.createBomComponentDetails(policyOverrideUniquePolicyNotificationContent, blackDuckServicesFactory);
 
-        List<BomComponentDetails> bomComponentDetailsList = policyOverrideNotificationMessageExtractor.createBomComponentDetails(policyOverrideUniquePolicyNotificationContent, blackDuckServicesFactory);
-        assertTrue(bomComponentDetailsList.contains(bomComponentDetails));
+        assertEquals(1, bomComponentDetailsList.size());
+        BomComponentDetails testBomComponentDetails = bomComponentDetailsList.get(0);
+        assertEquals(COMPONENT, testBomComponentDetails.getComponent());
+        assertEquals(1, testBomComponentDetails.getComponentConcerns().size());
+        assertTrue(testBomComponentDetails.getComponentVersion().isPresent());
+        assertEquals(COMPONENT_VERSION.getValue(), testBomComponentDetails.getComponentVersion().get().getValue());
+        assertTrue(testBomComponentDetails.getRelevantPolicies().isEmpty());
+        assertEquals(BlackDuckMessageLabels.VALUE_UNKNOWN_LICENSE, testBomComponentDetails.getLicense().getValue());
+        assertEquals(BlackDuckMessageLabels.VALUE_UNKNOWN_USAGE, testBomComponentDetails.getUsage());
     }
 
     private PolicyOverrideUniquePolicyNotificationContent createPolicyOverrideUniquePolicyNotificationContent() {
         String projectName = "Project";
         String projectVersionName = "ProjectVersionName";
         String projectVersion = "http://projectVersionUrl";
-        String componentName = "ComponentName";
-        String componentVersionName = "ComponentVersionName";
+        String componentName = COMPONENT.getValue();
+        String componentVersionName = COMPONENT_VERSION.getValue();
         String firstName = "firstName";
         String lastName = "lastName";
         String policy = "http://policyUrl";
@@ -202,7 +176,7 @@ public class PolicyOverrideNotificationMessageExtractorTest {
 
         ProjectVersionComponentVersionLicensesView projectVersionComponentVersionLicensesView = new ProjectVersionComponentVersionLicensesView();
         projectVersionComponentVersionLicensesView.setLicense("http://licenseLink");
-        projectVersionComponentVersionLicensesView.setLicenseDisplay("licenseDisplay");
+        projectVersionComponentVersionLicensesView.setLicenseDisplay(LICENSE_DISPLAY);
         projectVersionComponentVersionView.setLicenses(List.of(projectVersionComponentVersionLicensesView));
 
         ResourceLink resourceLink = new ResourceLink();
@@ -215,31 +189,4 @@ public class PolicyOverrideNotificationMessageExtractorTest {
 
         return projectVersionComponentVersionView;
     }
-
-    private BlackDuckServicesFactory createBlackDuckServicesFactoryWithMocks() throws IntegrationException {
-        BlackDuckServicesFactory blackDuckServicesFactory = Mockito.mock(BlackDuckServicesFactory.class);
-        BlackDuckApiClient blackDuckApiClient = Mockito.mock(BlackDuckApiClient.class);
-        Mockito.when(blackDuckServicesFactory.getBlackDuckApiClient()).thenReturn(blackDuckApiClient);
-
-        ProjectVersionComponentVersionView projectVersionComponentVersionView = new ProjectVersionComponentVersionView();
-        Mockito.when(blackDuckApiClient.getResponse(Mockito.any(), Mockito.any())).thenReturn(projectVersionComponentVersionView);
-
-        return blackDuckServicesFactory;
-    }
-
-    private BomComponentDetails createBomComponentDetails() {
-        return new BomComponentDetails(
-            COMPONENT,
-            COMPONENT_VERSION,
-            COMPONENT_VULNERABILITIES,
-            List.of(COMPONENT_POLICY),
-            List.of(),
-            LICENSE,
-            USAGE,
-            ComponentUpgradeGuidance.none(),
-            List.of(),
-            ISSUES_URL
-        );
-    }
-
 }
