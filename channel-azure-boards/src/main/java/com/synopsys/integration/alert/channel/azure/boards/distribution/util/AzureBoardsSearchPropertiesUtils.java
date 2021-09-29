f677291b311de3ade028cee6ca911ac6df7ccd40
Brian Mandel <bmandel@blackducksoftware.com>
2021-09-29 10:25:27 -0400

    fix(AzureBoardsSearchPropertiesUtils.java): added a limit of 128 character to the custom fields in Azure as that is the ADO limit

--- channel-azure-boards/src/main/java/com/synopsys/integration/alert/channel/azure/boards/distribution/util/AzureBoardsSearchPropertiesUtils.java
+++ channel-azure-boards/src/main/java/com/synopsys/integration/alert/channel/azure/boards/distribution/util/AzureBoardsSearchPropertiesUtils.java
@@ -7,6 +7,7 @@
  */
 package com.synopsys.integration.alert.channel.azure.boards.distribution.util;
 
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nullable;
 
 import com.synopsys.integration.alert.common.message.model.LinkableItem;
@@ -14,6 +15,8 @@ import com.synopsys.integration.alert.common.message.model.LinkableItem;
 public final class AzureBoardsSearchPropertiesUtils {
     public static final String URL_DELIMITER = "|";
     public static final String LINKABLE_ITEM_DELIMITER = ":";
+
+    private static final int MAX_KEY_LENGTH = 128;
 
     public static String createProviderKey(String providerName, String providerUrl) {
         StringBuilder providerKeyBuilder = new StringBuilder();
@@ -28,8 +31,10 @@ public final class AzureBoardsSearchPropertiesUtils {
             return null;
         }
 
+        String truncatedLabel = StringUtils.truncate(linkableItem.getLabel(), MAX_KEY_LENGTH - LINKABLE_ITEM_DELIMITER.length());
+
         StringBuilder linkableItemBuilder = new StringBuilder();
-        linkableItemBuilder.append(linkableItem.getLabel());
+        linkableItemBuilder.append(truncatedLabel);
         linkableItemBuilder.append(LINKABLE_ITEM_DELIMITER);
         linkableItemBuilder.append(linkableItem.getValue());
         linkableItem.getUrl()
@@ -37,7 +42,9 @@ public final class AzureBoardsSearchPropertiesUtils {
                 linkableItemBuilder.append(URL_DELIMITER);
                 linkableItemBuilder.append(url);
             });
-        return linkableItemBuilder.toString();
+
+        // Per this issue https://github.com/MicrosoftDocs/azure-devops-docs/issues/5890, it appears that we want to limit field content size to 256.
+        return StringUtils.truncate(linkableItemBuilder.toString(), MAX_KEY_LENGTH);
     }
 
     private AzureBoardsSearchPropertiesUtils() {
