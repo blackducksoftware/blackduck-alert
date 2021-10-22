8dfb017eba67d3ddb3cd516526cdf99f6147edb4
psantos1113 <psantos@synopsys.com>
2021-10-22 15:56:34 -0400

    test: Add a test for another complex query.

--- azure-boards-common/src/test/java/com/synopsys/integration/azure/boards/common/service/WorkItemQueryTest.java
+++ azure-boards-common/src/test/java/com/synopsys/integration/azure/boards/common/service/WorkItemQueryTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 
 import com.synopsys.integration.azure.boards.common.service.query.fluent.WorkItemQuery;
 import com.synopsys.integration.azure.boards.common.service.query.fluent.WorkItemQueryWhereOperator;
+import com.synopsys.integration.azure.boards.common.service.workitem.response.WorkItemResponseFields;
 
 public class WorkItemQueryTest {
     @Test
@@ -48,18 +49,44 @@ public class WorkItemQueryTest {
                                          + " AND ( [Something] Does Not Contain 'forbidden phrase' AND [Something Else] <> '' )"
                                          + " ORDER BY [Other Field]";
         WorkItemQuery workItemQuery = WorkItemQuery
-                                          .select("Field")
-                                          .fromWorkItems()
-                                          .whereGroup("Field", WorkItemQueryWhereOperator.EQ, "a value")
-                                          .or("Other Field", WorkItemQueryWhereOperator.CONTAINS, "something else")
-                                          .endGroup()
-                                          .and("Thing", WorkItemQueryWhereOperator.WAS_EVER, "not a thing")
-                                          .beginGroup()
-                                          .and("Something", WorkItemQueryWhereOperator.DOES_NOT_CONTAIN, "forbidden phrase")
-                                          .and("Something Else", WorkItemQueryWhereOperator.NOT_EQUALS, null)
-                                          .orderBy("Other Field")
-                                          .build();
+            .select("Field")
+            .fromWorkItems()
+            .whereGroup("Field", WorkItemQueryWhereOperator.EQ, "a value")
+            .or("Other Field", WorkItemQueryWhereOperator.CONTAINS, "something else")
+            .endGroup()
+            .and("Thing", WorkItemQueryWhereOperator.WAS_EVER, "not a thing")
+            .beginGroup()
+            .and("Something", WorkItemQueryWhereOperator.DOES_NOT_CONTAIN, "forbidden phrase")
+            .and("Something Else", WorkItemQueryWhereOperator.NOT_EQUALS, null)
+            .orderBy("Other Field")
+            .build();
         assertEquals(expectedQueryString, workItemQuery.toString());
+    }
+
+    @Test
+    public void whereComplexGroupingTest() {
+        String expectedQueryString = "SELECT [System.Id]"
+                                         + " FROM WorkItems"
+                                         + " WHERE [System.TeamProject] = 'projectName'"
+                                         + " AND ( [Custom.AlertProviderKey] = 'providerKey|providerUrl' OR [Custom.AlertProviderKey] Contains 'providerKey' )"
+                                         + " AND [Custom.AlertTopicKey] = 'topicKey'"
+                                         + " ORDER BY [topicKey]";
+
+        String systemIdFieldName = WorkItemResponseFields.System_Id.getFieldName();
+        String teamProjectFieldName = WorkItemResponseFields.System_TeamProject.getFieldName();
+        WorkItemQuery workItemQuery = WorkItemQuery
+            .select(systemIdFieldName)
+            .fromWorkItems()
+            .where(teamProjectFieldName, WorkItemQueryWhereOperator.EQ, "projectName")
+            .beginGroup()
+            .and("Custom.AlertProviderKey", WorkItemQueryWhereOperator.EQ, "providerKey|providerUrl")
+            .or("Custom.AlertProviderKey", WorkItemQueryWhereOperator.CONTAINS, "providerKey")
+            .endGroup()
+            .and("Custom.AlertTopicKey", WorkItemQueryWhereOperator.EQ, "topicKey")
+            .orderBy("topicKey")
+            .build();
+        assertEquals(expectedQueryString, workItemQuery.toString());
+
     }
 
 }
