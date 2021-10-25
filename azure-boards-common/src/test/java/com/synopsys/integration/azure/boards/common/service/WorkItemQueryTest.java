e9c4edcb7b107ed565c371464057a1847abdb643
psantos1113 <psantos@synopsys.com>
2021-10-25 15:03:00 -0400

    test: Update the tests for the query where clause.

--- azure-boards-common/src/test/java/com/synopsys/integration/azure/boards/common/service/WorkItemQueryTest.java
+++ azure-boards-common/src/test/java/com/synopsys/integration/azure/boards/common/service/WorkItemQueryTest.java
@@ -31,12 +31,13 @@ public class WorkItemQueryTest {
     public void whereGroupTest() {
         String expectedQueryString = "SELECT [Field] FROM WorkItems WHERE ( [Field] = 'a value' OR [Other Field] Contains 'something else' ) ORDER BY [Other Field]";
         WorkItemQuery workItemQuery = WorkItemQuery
-                                          .select("Field")
-                                          .fromWorkItems()
-                                          .whereGroup("Field", WorkItemQueryWhereOperator.EQ, "a value")
-                                          .or("Other Field", WorkItemQueryWhereOperator.CONTAINS, "something else")
-                                          .orderBy("Other Field")
-                                          .build();
+            .select("Field")
+            .fromWorkItems()
+            .whereGroup("Field", WorkItemQueryWhereOperator.EQ, "a value")
+            .or("Other Field", WorkItemQueryWhereOperator.CONTAINS, "something else")
+            .endGroupToWhereClause()
+            .orderBy("Other Field")
+            .build();
         assertEquals(expectedQueryString, workItemQuery.toString());
     }
 
@@ -53,11 +54,12 @@ public class WorkItemQueryTest {
             .fromWorkItems()
             .whereGroup("Field", WorkItemQueryWhereOperator.EQ, "a value")
             .or("Other Field", WorkItemQueryWhereOperator.CONTAINS, "something else")
-            .endGroup()
+            .endGroupToWhereClause()
             .and("Thing", WorkItemQueryWhereOperator.WAS_EVER, "not a thing")
-            .beginGroup()
-            .and("Something", WorkItemQueryWhereOperator.DOES_NOT_CONTAIN, "forbidden phrase")
+            .beginAndGroup()
+            .condition("Something", WorkItemQueryWhereOperator.DOES_NOT_CONTAIN, "forbidden phrase")
             .and("Something Else", WorkItemQueryWhereOperator.NOT_EQUALS, null)
+            .endGroupToWhereClause()
             .orderBy("Other Field")
             .build();
         assertEquals(expectedQueryString, workItemQuery.toString());
@@ -68,7 +70,9 @@ public class WorkItemQueryTest {
         String expectedQueryString = "SELECT [System.Id]"
                                          + " FROM WorkItems"
                                          + " WHERE [System.TeamProject] = 'projectName'"
-                                         + " AND ( [Custom.AlertProviderKey] = 'providerKey|providerUrl' OR [Custom.AlertProviderKey] Contains 'providerKey' )"
+                                         + " AND ( ( [Custom.AlertProviderKey] = 'providerKey' AND [Custom.AlertProviderUrl] = 'providerUrl' )"
+                                         + " OR [Custom.AlertProviderKey] = 'providerKey|providerUrl' )"
+                                         + " OR ( [Custom.AlertProviderKey] Contains 'provide' )"
                                          + " AND [Custom.AlertTopicKey] = 'topicKey'"
                                          + " ORDER BY [topicKey]";
 
@@ -78,10 +82,46 @@ public class WorkItemQueryTest {
             .select(systemIdFieldName)
             .fromWorkItems()
             .where(teamProjectFieldName, WorkItemQueryWhereOperator.EQ, "projectName")
+            .beginAndGroup()
             .beginGroup()
-            .and("Custom.AlertProviderKey", WorkItemQueryWhereOperator.EQ, "providerKey|providerUrl")
-            .or("Custom.AlertProviderKey", WorkItemQueryWhereOperator.CONTAINS, "providerKey")
+            .condition("Custom.AlertProviderKey", WorkItemQueryWhereOperator.EQ, "providerKey")
+            .and("Custom.AlertProviderUrl", WorkItemQueryWhereOperator.EQ, "providerUrl")
             .endGroup()
+            .or("Custom.AlertProviderKey", WorkItemQueryWhereOperator.EQ, "providerKey|providerUrl")
+            .endGroupToWhereClause()
+            .beginOrGroup()
+            .condition("Custom.AlertProviderKey", WorkItemQueryWhereOperator.CONTAINS, "provide")
+            .endGroupToWhereClause()
+            .and("Custom.AlertTopicKey", WorkItemQueryWhereOperator.EQ, "topicKey")
+            .orderBy("topicKey")
+            .build();
+        assertEquals(expectedQueryString, workItemQuery.toString());
+
+    }
+
+    @Test
+    public void whereGroupComplexGroupingTest() {
+        String expectedQueryString = "SELECT [System.Id]"
+                                         + " FROM WorkItems"
+                                         + " WHERE ( [System.TeamProject] = 'projectName'"
+                                         + " AND ( ( [Custom.AlertProviderKey] = 'providerKey' AND [Custom.AlertProviderUrl] = 'providerUrl' )"
+                                         + " OR [Custom.AlertProviderKey] = 'providerKey|providerUrl' ) )"
+                                         + " AND [Custom.AlertTopicKey] = 'topicKey'"
+                                         + " ORDER BY [topicKey]";
+
+        String systemIdFieldName = WorkItemResponseFields.System_Id.getFieldName();
+        String teamProjectFieldName = WorkItemResponseFields.System_TeamProject.getFieldName();
+        WorkItemQuery workItemQuery = WorkItemQuery
+            .select(systemIdFieldName)
+            .fromWorkItems()
+            .whereGroup(teamProjectFieldName, WorkItemQueryWhereOperator.EQ, "projectName")
+            .beginAndGroup()
+            .beginGroup()
+            .condition("Custom.AlertProviderKey", WorkItemQueryWhereOperator.EQ, "providerKey")
+            .and("Custom.AlertProviderUrl", WorkItemQueryWhereOperator.EQ, "providerUrl")
+            .endGroup()
+            .or("Custom.AlertProviderKey", WorkItemQueryWhereOperator.EQ, "providerKey|providerUrl")
+            .endGroupToWhereClause()
             .and("Custom.AlertTopicKey", WorkItemQueryWhereOperator.EQ, "topicKey")
             .orderBy("topicKey")
             .build();
