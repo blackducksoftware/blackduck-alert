8a6d902a9ccbc43f76256b7f3605c6dc41168999
psantos1113 <psantos@blackducksoftware.com>
2021-11-02 11:38:07 -0400

    test: Add extractor tests. (#1881)

--- provider-blackduck/src/test/java/com/synopsys/integration/alert/provider/blackduck/processor/message/ComponentUnknownVersionExtractorTest.java
+++ provider-blackduck/src/test/java/com/synopsys/integration/alert/provider/blackduck/processor/message/ComponentUnknownVersionExtractorTest.java
@@ -1,32 +1,46 @@
 package com.synopsys.integration.alert.provider.blackduck.processor.message;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.http.HttpStatus;
 
 import com.synopsys.integration.alert.common.message.model.LinkableItem;
 import com.synopsys.integration.alert.descriptor.api.BlackDuckProviderKey;
+import com.synopsys.integration.alert.processor.api.extract.model.project.BomComponentDetails;
+import com.synopsys.integration.alert.processor.api.extract.model.project.ComponentUpgradeGuidance;
 import com.synopsys.integration.alert.provider.blackduck.processor.NotificationExtractorBlackDuckServicesFactoryCache;
 import com.synopsys.integration.alert.provider.blackduck.processor.message.service.BlackDuckComponentVulnerabilityDetailsCreator;
 import com.synopsys.integration.alert.provider.blackduck.processor.message.service.BlackDuckMessageBomComponentDetailsCreatorFactory;
 import com.synopsys.integration.alert.provider.blackduck.processor.message.service.BomComponent404Handler;
 import com.synopsys.integration.alert.provider.blackduck.processor.message.service.policy.BlackDuckComponentPolicyDetailsCreatorFactory;
 import com.synopsys.integration.alert.provider.blackduck.processor.message.service.policy.BlackDuckPolicySeverityConverter;
+import com.synopsys.integration.alert.provider.blackduck.processor.model.ComponentUnknownVersionWithStatusNotificationContent;
 import com.synopsys.integration.blackduck.api.core.ResourceMetadata;
 import com.synopsys.integration.blackduck.api.generated.component.ProjectVersionComponentVersionLicensesView;
 import com.synopsys.integration.blackduck.api.generated.enumeration.UsageType;
 import com.synopsys.integration.blackduck.api.generated.view.ProjectVersionComponentVersionView;
-import com.synopsys.integration.blackduck.api.manual.component.ComponentUnknownVersionNotificationContent;
+import com.synopsys.integration.blackduck.api.manual.enumeration.ComponentUnknownVersionStatus;
+import com.synopsys.integration.blackduck.service.BlackDuckApiClient;
+import com.synopsys.integration.blackduck.service.BlackDuckServicesFactory;
 import com.synopsys.integration.exception.IntegrationException;
+import com.synopsys.integration.rest.HttpMethod;
 import com.synopsys.integration.rest.HttpUrl;
+import com.synopsys.integration.rest.exception.IntegrationRestException;
 
 public class ComponentUnknownVersionExtractorTest {
-    private static final LinkableItem COMPONENT = new LinkableItem("Component", "A BOM component");
-    private static final LinkableItem COMPONENT_VERSION = new LinkableItem("Component Version", "0.8.7");
+    private static final LinkableItem COMPONENT = new LinkableItem("Component", "A BOM component", "http://bomComponentUrl?q=componentOrVersionName:A%20BOM%20component");
+    private static final LinkableItem COMPONENT_VERSION = new LinkableItem("Component Version", "Unknown Version");
     private static final String COMPONENT_VERSION_URL = "http://componentVersionUrl";
     private static final String LICENSE_DISPLAY = "licenseDisplay";
 
+    private final ComponentUnknownVersionWithStatusNotificationContent componentUnknownVersionNotificationStatus = createNotificationContent();
     private ComponentUnknownVersionExtractor extractor;
 
     @BeforeEach
@@ -48,7 +62,7 @@ public class ComponentUnknownVersionExtractorTest {
             bomComponent404Handler
         );
     }
-/*
+
     @Test
     public void createBomComponentDetailsTest() throws IntegrationException {
         BlackDuckServicesFactory blackDuckServicesFactory = Mockito.mock(BlackDuckServicesFactory.class);
@@ -58,31 +72,23 @@ public class ComponentUnknownVersionExtractorTest {
         ProjectVersionComponentVersionView projectVersionComponentVersionView = createProjectVersionComponentVersionView();
         Mockito.when(blackDuckApiClient.getResponse(Mockito.any(), Mockito.any())).thenReturn(projectVersionComponentVersionView);
 
-        ComponentUnknownVersionNotificationUserView componentUnknownVersionNotificationUserView = new ComponentUnknownVersionNotificationUserView();
         ResourceMetadata meta = new ResourceMetadata();
-        meta.setHref(new HttpUrl("https://someUrlComponentUnknownVersionView"));
-        Mockito.when(blackDuckApiClient.getAllResponses(Mockito.eq(projectVersionComponentVersionView.metaPolicyRulesLink()))).thenReturn(List.of(componentPolicyRulesView));
+        meta.setHref(new HttpUrl("http://bomComponentUrl"));
 
-        List<BomComponentDetails> bomComponentDetailsList = extractor.createBomComponentDetails(componentUnknownVersionNotificationUserView, blackDuckServicesFactory);
+        List<BomComponentDetails> bomComponentDetailsList = extractor.createBomComponentDetails(componentUnknownVersionNotificationStatus, blackDuckServicesFactory);
 
         assertEquals(1, bomComponentDetailsList.size());
         BomComponentDetails testBomComponentDetails = bomComponentDetailsList.get(0);
         assertEquals(COMPONENT, testBomComponentDetails.getComponent());
-        assertEquals(1, testBomComponentDetails.getComponentConcerns().size());
+        assertEquals(4, testBomComponentDetails.getComponentConcerns().size());
         assertTrue(testBomComponentDetails.getComponentVersion().isPresent());
-        assertEquals(COMPONENT_VERSION.getValue(), testBomComponentDetails.getComponentVersion().get().getValue());
         assertEquals(LICENSE_DISPLAY, testBomComponentDetails.getLicense().getValue());
         assertEquals(UsageType.DYNAMICALLY_LINKED.prettyPrint(), testBomComponentDetails.getUsage());
-        assertEquals(1, testBomComponentDetails.getAdditionalAttributes().size());
+        assertEquals(0, testBomComponentDetails.getAdditionalAttributes().size());
 
         ComponentUpgradeGuidance componentUpgradeGuidance = testBomComponentDetails.getComponentUpgradeGuidance();
         assertFalse(componentUpgradeGuidance.getLongTermUpgradeGuidance().isPresent());
         assertFalse(componentUpgradeGuidance.getShortTermUpgradeGuidance().isPresent());
-
-        assertEquals(1, testBomComponentDetails.getRelevantPolicies().size());
-        ComponentPolicy testComponentPolicy = testBomComponentDetails.getRelevantPolicies().get(0);
-        assertTrue(testComponentPolicy.getCategory().isPresent());
-        assertEquals(PolicyRuleCategoryType.UNCATEGORIZED.toString(), testComponentPolicy.getCategory().get());
     }
 
     @Test
@@ -94,49 +100,41 @@ public class ComponentUnknownVersionExtractorTest {
         Mockito.doThrow(new IntegrationRestException(HttpMethod.GET, new HttpUrl("https://google.com"), HttpStatus.NOT_FOUND.value(), "httpStatusMessageTest", "httpResponseContentTest", "IntegrationRestExceptionForAlertTest"))
             .when(blackDuckApiClient).getResponse(Mockito.any(), Mockito.any());
 
-        List<BomComponentDetails> bomComponentDetailsList = extractor.createBomComponentDetails(policyOverrideUniquePolicyNotificationContent, blackDuckServicesFactory);
+        List<BomComponentDetails> bomComponentDetailsList = extractor.createBomComponentDetails(componentUnknownVersionNotificationStatus, blackDuckServicesFactory);
 
         assertEquals(1, bomComponentDetailsList.size());
         BomComponentDetails testBomComponentDetails = bomComponentDetailsList.get(0);
         assertEquals(COMPONENT, testBomComponentDetails.getComponent());
-        assertEquals(1, testBomComponentDetails.getComponentConcerns().size());
-        assertTrue(testBomComponentDetails.getComponentVersion().isPresent());
-        assertEquals(COMPONENT_VERSION.getValue(), testBomComponentDetails.getComponentVersion().get().getValue());
-        assertTrue(testBomComponentDetails.getRelevantPolicies().isEmpty());
+        assertEquals(4, testBomComponentDetails.getComponentConcerns().size());
+        assertFalse(testBomComponentDetails.getComponentVersion().isPresent());
         assertEquals(BlackDuckMessageLabels.VALUE_UNKNOWN_LICENSE, testBomComponentDetails.getLicense().getValue());
         assertEquals(BlackDuckMessageLabels.VALUE_UNKNOWN_USAGE, testBomComponentDetails.getUsage());
     }
- */
 
-    private ComponentUnknownVersionNotificationContent createNotificationContent() {
+    private ComponentUnknownVersionWithStatusNotificationContent createNotificationContent() {
         String projectName = "Project";
         String projectVersionName = "ProjectVersionName";
         String projectVersion = "http://projectVersionUrl";
         String componentName = COMPONENT.getValue();
-        String componentVersionName = COMPONENT_VERSION.getValue();
-        String bomComponentVersionPolicyStatus = "BomComponentVersionPolicyStatus";
         String bomComponent = "http://bomComponentUrl";
 
-        ComponentUnknownVersionNotificationContent content = new ComponentUnknownVersionNotificationContent();
-        content.setComponent(COMPONENT_VERSION_URL);
-        content.setBomComponent(bomComponent);
-        content.setComponentName(componentName);
-        content.setProjectName(projectName);
-        content.setProjectVersionName(projectVersionName);
-        content.setProjectVersion(projectVersion);
-        content.setCriticalVulnerabilityCount(1);
-        content.setCriticalVulnerabilityVersionName("critical vulnerability name");
-        content.setCriticalVulnerabilityVersion("http://criticalVulnVersionURL");
-        content.setHighVulnerabilityCount(2);
-        content.setHighVulnerabilityVersionName("critical vulnerability name");
-        content.setHighVulnerabilityVersion("http://criticalVulnVersionURL");
-        content.setMediumVulnerabilityCount(3);
-        content.setMediumVulnerabilityVersionName("critical vulnerability name");
-        content.setMediumVulnerabilityVersion("http://criticalVulnVersionURL");
-        content.setLowVulnerabilityCount(4);
-        content.setLowVulnerabilityVersionName("critical vulnerability name");
-        content.setLowVulnerabilityVersion("http://criticalVulnVersionURL");
-        return content;
+        return new ComponentUnknownVersionWithStatusNotificationContent(projectName, projectVersionName, projectVersion,
+            componentName,
+            bomComponent,
+            COMPONENT.getUrl().orElse(null),
+            1,
+            "http://criticalVulnVersionURL",
+            "critical vulnerability name",
+            2,
+            "http://criticalVulnVersionURL",
+            "critical vulnerability name",
+            3,
+            "http://criticalVulnVersionURL",
+            "critical vulnerability name",
+            4,
+            "http://criticalVulnVersionURL",
+            "critical vulnerability name",
+            ComponentUnknownVersionStatus.FOUND);
     }
 
     private ProjectVersionComponentVersionView createProjectVersionComponentVersionView() throws IntegrationException {
@@ -153,7 +151,8 @@ public class ComponentUnknownVersionExtractorTest {
         projectVersionComponentVersionView.setLicenses(List.of(projectVersionComponentVersionLicensesView));
 
         ResourceMetadata meta = new ResourceMetadata();
-        meta.setHref(new HttpUrl("https://someUrl"));
+        meta.setHref(new HttpUrl("http://bomComponentUrl"));
+        meta.setLinks(List.of());
         projectVersionComponentVersionView.setMeta(meta);
 
         return projectVersionComponentVersionView;
