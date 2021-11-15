d06822e734a5d960ab97fb4ac9ad96eb03a352c4
psantos1113 <psantos@blackducksoftware.com>
2021-11-15 14:25:10 -0500

    Ps email config entities (#1893)
    * feat: Add JPA classes for the email configuration table.
    
    * refactor: Add serial ids and copyrights.

---
+++ alert-database/src/main/java/com/synopsys/integration/alert/database/email/EmailConfigurationRepository.java
@@ -0,0 +1,16 @@
+/*
+ * alert-database
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
+package com.synopsys.integration.alert.database.email;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmailConfigurationRepository extends JpaRepository<EmailConfigurationEntity, UUID> {
+
+}
