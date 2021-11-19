fa6f30d4315b74f3ae29059d6f9c2c9a869262b7
psantos1113 <psantos@synopsys.com>
2021-11-19 14:06:24 -0500

    test: Add a test for the encryption settings.

--- component/src/test/java/com/synopsys/integration/alert/component/settings/environment/EncryptionSettingsEnvironmentHandlerTest.java
+++ component/src/test/java/com/synopsys/integration/alert/component/settings/environment/EncryptionSettingsEnvironmentHandlerTest.java
@@ -1,2 +1,48 @@
-package com.synopsys.integration.alert.component.settings.environment;public class EncryptionSettingsEnvironmentHandlerTest {
+package com.synopsys.integration.alert.component.settings.environment;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Properties;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.core.env.Environment;
+
+import com.synopsys.integration.alert.common.rest.AlertRestConstants;
+import com.synopsys.integration.alert.environment.EnvironmentVariableUtility;
+import com.synopys.integration.alert.test.common.environment.TestEnvironmentHandlerWrapper;
+
+public class EncryptionSettingsEnvironmentHandlerTest {
+
+    @Test
+    public void testEncryptionSetInEnvironment() {
+        Environment environment = Mockito.mock(Environment.class);
+        Set<String> variableNames = Set.of(EncryptionSettingsEnvironmentHandler.ENCRYPTION_PASSWORD_KEY, EncryptionSettingsEnvironmentHandler.ENCRYPTION_SALT_KEY);
+        for (String variableName : variableNames) {
+            Mockito.when(environment.containsProperty(Mockito.eq(variableName))).thenReturn(Boolean.TRUE);
+            Mockito.when(environment.getProperty(Mockito.eq(variableName))).thenReturn("a value");
+        }
+        EnvironmentVariableUtility environmentVariableUtility = new EnvironmentVariableUtility(environment);
+        TestEnvironmentHandlerWrapper handler = new TestEnvironmentHandlerWrapper(new EncryptionSettingsEnvironmentHandler(environmentVariableUtility));
+        handler.updateFromEnvironment();
+        assertTrue(handler.hasUpdateOccurred());
+        Properties updatedProperties = handler.getUpdatedProperties().orElseThrow(() -> new AssertionError("Properties should exist"));
+        assertFalse(updatedProperties.isEmpty());
+        assertEquals(AlertRestConstants.MASKED_VALUE, updatedProperties.getProperty(EncryptionSettingsEnvironmentHandler.ENCRYPTION_PASSWORD_KEY));
+        assertEquals(AlertRestConstants.MASKED_VALUE, updatedProperties.getProperty(EncryptionSettingsEnvironmentHandler.ENCRYPTION_SALT_KEY));
+    }
+
+    @Test
+    public void testEncryptionMissingFromEnvironment() {
+        Environment environment = Mockito.mock(Environment.class);
+        EnvironmentVariableUtility environmentVariableUtility = new EnvironmentVariableUtility(environment);
+        TestEnvironmentHandlerWrapper handler = new TestEnvironmentHandlerWrapper(new EncryptionSettingsEnvironmentHandler(environmentVariableUtility));
+        handler.updateFromEnvironment();
+        assertFalse(handler.hasUpdateOccurred());
+        assertTrue(handler.getUpdatedProperties().stream()
+            .allMatch(Properties::isEmpty));
+    }
 }
