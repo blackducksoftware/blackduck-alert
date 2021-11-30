357f9c2a5d760cd61cffe149b2dafe8a5467d7fe
psantos1113 <psantos@synopsys.com>
2021-11-30 08:45:41 -0500

    chore: Add copyright header.

--- src/main/java/com/synopsys/integration/alert/configuration/BrokerServiceDependentTask.java
+++ src/main/java/com/synopsys/integration/alert/configuration/BrokerServiceDependentTask.java
@@ -1,3 +1,10 @@
+/*
+ * blackduck-alert
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.alert.configuration;
 
 import java.time.LocalDateTime;
