b696ad0d96fbbe8cdfdc949ee7f119121793b3f5
psantos1113 <psantos@synopsys.com>
2021-12-02 11:18:39 -0500

    fix: Notification receiver to process in thread.

--- alert-common/src/main/java/com/synopsys/integration/alert/common/event/EventManager.java
+++ alert-common/src/main/java/com/synopsys/integration/alert/common/event/EventManager.java
@@ -8,13 +8,7 @@
 package com.synopsys.integration.alert.common.event;
 
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
@@ -23,16 +17,13 @@ import com.synopsys.integration.alert.common.ContentConverter;
 
 @Component
 public class EventManager {
-    private final Logger logger = LoggerFactory.getLogger(EventManager.class);
     private final JmsTemplate jmsTemplate;
     private final ContentConverter contentConverter;
-    private final ExecutorService eventDispatcher;
 
     @Autowired
     public EventManager(ContentConverter contentConverter, JmsTemplate jmsTemplate) {
         this.contentConverter = contentConverter;
         this.jmsTemplate = jmsTemplate;
-        this.eventDispatcher = Executors.newSingleThreadExecutor();
     }
 
     public void sendEvents(List<? extends AlertEvent> eventList) {
@@ -42,22 +33,9 @@ public class EventManager {
     }
 
     public void sendEvent(AlertEvent event) {
-        Future<Void> eventDispatched = eventDispatcher.submit(() -> {
-            String destination = event.getDestination();
-            String jsonMessage = contentConverter.getJsonString(event);
-            jmsTemplate.convertAndSend(destination, jsonMessage);
-            return null;
-        });
-
-        try {
-            // wait for event to be dispatched
-            eventDispatched.get();
-        } catch (InterruptedException ex) {
-            Thread.currentThread().interrupt();
-        } catch (ExecutionException ex) {
-            logger.error("Error dispatching event: {}", event);
-            logger.error("Cause", ex);
-        }
+        String destination = event.getDestination();
+        String jsonMessage = contentConverter.getJsonString(event);
+        jmsTemplate.convertAndSend(destination, jsonMessage);
     }
 
 }
