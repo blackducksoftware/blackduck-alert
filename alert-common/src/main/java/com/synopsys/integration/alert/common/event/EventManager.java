cc902fbc6df3f8e5f9d1c756c5c07d852bfe2cc9
psantos1113 <psantos@synopsys.com>
2021-12-02 09:54:38 -0500

    fix: Make the event manager dispatch on a thread.

--- alert-common/src/main/java/com/synopsys/integration/alert/common/event/EventManager.java
+++ alert-common/src/main/java/com/synopsys/integration/alert/common/event/EventManager.java
@@ -8,7 +8,13 @@
 package com.synopsys.integration.alert.common.event;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
@@ -17,13 +23,16 @@ import com.synopsys.integration.alert.common.ContentConverter;
 
 @Component
 public class EventManager {
+    private final Logger logger = LoggerFactory.getLogger(EventManager.class);
     private final JmsTemplate jmsTemplate;
     private final ContentConverter contentConverter;
+    private final ExecutorService eventDispatcher;
 
     @Autowired
     public EventManager(ContentConverter contentConverter, JmsTemplate jmsTemplate) {
         this.contentConverter = contentConverter;
         this.jmsTemplate = jmsTemplate;
+        this.eventDispatcher = Executors.newSingleThreadExecutor();
     }
 
     public void sendEvents(List<? extends AlertEvent> eventList) {
@@ -33,9 +42,22 @@ public class EventManager {
     }
 
     public void sendEvent(AlertEvent event) {
-        String destination = event.getDestination();
-        String jsonMessage = contentConverter.getJsonString(event);
-        jmsTemplate.convertAndSend(destination, jsonMessage);
+        Future<Void> eventDispatched = eventDispatcher.submit(() -> {
+            String destination = event.getDestination();
+            String jsonMessage = contentConverter.getJsonString(event);
+            jmsTemplate.convertAndSend(destination, jsonMessage);
+            return null;
+        });
+
+        try {
+            // wait for event to be dispatched
+            eventDispatched.get();
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        } catch (ExecutionException ex) {
+            logger.error("Error dispatching event: {}", event);
+            logger.error("Cause", ex);
+        }
     }
 
 }
