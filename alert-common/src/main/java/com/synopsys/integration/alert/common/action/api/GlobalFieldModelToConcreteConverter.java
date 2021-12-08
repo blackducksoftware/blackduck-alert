1fbe0112885c10a7379b3c3dd5576587bd6a606c
psantos1113 <psantos@synopsys.com>
2021-12-08 13:02:18 -0500

    chore: add copyright

--- alert-common/src/main/java/com/synopsys/integration/alert/common/action/api/GlobalFieldModelToConcreteConverter.java
+++ alert-common/src/main/java/com/synopsys/integration/alert/common/action/api/GlobalFieldModelToConcreteConverter.java
@@ -1,3 +1,10 @@
+/*
+ * alert-common
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.alert.common.action.api;
 
 import com.synopsys.integration.alert.api.common.model.AlertSerializableModel;
