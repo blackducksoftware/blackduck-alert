bd4a7d16a2b7ae064b087f38541e5778ab19c588
Martin Chomicki <martinch@synopsys.com>
2021-12-09 14:37:06 -0500

    feat: create new unique ConfigurationAccessor and StaticConfigResourceController for endpoints with only a single configuration

---
+++ alert-common/src/main/java/com/synopsys/integration/alert/common/persistence/accessor/UniqueConfigurationAccessor.java
@@ -0,0 +1,26 @@
+/*
+ * alert-common
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
+package com.synopsys.integration.alert.common.persistence.accessor;
+
+import java.util.Optional;
+
+import com.synopsys.integration.alert.api.common.model.AlertSerializableModel;
+import com.synopsys.integration.alert.api.common.model.exception.AlertConfigurationException;
+
+public interface UniqueConfigurationAccessor<T extends AlertSerializableModel> {
+    String DEFAULT_CONFIGURATION_NAME = "default-configuration";
+
+    Optional<T> getConfiguration();
+
+    T createConfiguration(T configuration) throws AlertConfigurationException;
+
+    T updateConfiguration(T configuration) throws AlertConfigurationException;
+
+    void deleteConfiguration();
+
+}
