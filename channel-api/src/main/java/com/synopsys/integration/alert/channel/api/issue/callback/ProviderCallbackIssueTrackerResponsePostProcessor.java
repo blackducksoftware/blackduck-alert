9f72f0879706b669880c09cc9c7f907d14ed4510
psantos1113 <psantos@blackducksoftware.com>
2021-12-17 12:18:02 -0500

    fix: Call the callback handler directly. (#1947)

--- channel-api/src/main/java/com/synopsys/integration/alert/channel/api/issue/callback/ProviderCallbackIssueTrackerResponsePostProcessor.java
+++ channel-api/src/main/java/com/synopsys/integration/alert/channel/api/issue/callback/ProviderCallbackIssueTrackerResponsePostProcessor.java
@@ -18,22 +18,28 @@ import org.springframework.stereotype.Component;
 import com.synopsys.integration.alert.channel.api.issue.IssueTrackerResponsePostProcessor;
 import com.synopsys.integration.alert.channel.api.issue.model.IssueTrackerIssueResponseModel;
 import com.synopsys.integration.alert.channel.api.issue.model.IssueTrackerResponse;
-import com.synopsys.integration.alert.common.event.EventManager;
 import com.synopsys.integration.alert.common.event.IssueTrackerCallbackEvent;
+import com.synopsys.integration.alert.common.workflow.IssueTrackerCallbackHandler;
 
 @Component
 public class ProviderCallbackIssueTrackerResponsePostProcessor implements IssueTrackerResponsePostProcessor {
-    private final EventManager eventManager;
+    private final IssueTrackerCallbackHandler handler;
 
     @Autowired
-    public ProviderCallbackIssueTrackerResponsePostProcessor(EventManager eventManager) {
-        this.eventManager = eventManager;
+    public ProviderCallbackIssueTrackerResponsePostProcessor(IssueTrackerCallbackHandler handler) {
+        this.handler = handler;
     }
 
     @Override
     public <T extends Serializable> void postProcess(IssueTrackerResponse<T> response) {
         List<IssueTrackerCallbackEvent> callbackEvents = createCallbackEvents(response);
-        eventManager.sendEvents(callbackEvents);
+        // This code is executed from inside an event handler for an issue tracker channel.
+        // In the short term we cannot post an event from inside an event handler because it is a consumer of events.
+        // no memory can be freed for other consumers to run since a consumer is blocked trying to produce an event onto a queue.
+        // call the handler directly for now which will make the same post-processing calls in the same thread then the consumer thread should free up.
+        for (IssueTrackerCallbackEvent event : callbackEvents) {
+            handler.handleEvent(event);
+        }
     }
 
     private <T extends Serializable> List<IssueTrackerCallbackEvent> createCallbackEvents(IssueTrackerResponse<T> issueTrackerResponse) {
