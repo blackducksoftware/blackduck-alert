73df0eb770285fc6b829f579d69cd3ddce638d92
Martin Chomicki <martinch@synopsys.com>
2021-12-22 11:05:06 -0500

    test: Add test coverage for ProxyConfigurationModelSaveActions

--- component/src/test/java/com/synopsys/integration/alert/component/settings/convert/ProxyConfigurationModelSaveActionsTest.java
+++ component/src/test/java/com/synopsys/integration/alert/component/settings/convert/ProxyConfigurationModelSaveActionsTest.java
@@ -1,10 +1,13 @@
 package com.synopsys.integration.alert.component.settings.convert;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.jupiter.api.Test;
@@ -45,11 +48,12 @@ class ProxyConfigurationModelSaveActionsTest {
     private final FilePersistenceUtil filePersistenceUtil = new FilePersistenceUtil(alertProperties, gson);
     private final EncryptionUtility encryptionUtility = new EncryptionUtility(alertProperties, filePersistenceUtil);
     private final AuthorizationManager authorizationManager = createAuthorizationManager();
+    private final ProxyConfigurationModelConverter converter = new ProxyConfigurationModelConverter();
     private final SettingsProxyValidator validator = new SettingsProxyValidator();
 
     @Test
     void getDescriptorKeyTest() {
-        ProxyConfigurationModelSaveActions saveActions = new ProxyConfigurationModelSaveActions(null);
+        ProxyConfigurationModelSaveActions saveActions = new ProxyConfigurationModelSaveActions(null, null);
         assertEquals(settingsDescriptorKey, saveActions.getDescriptorKey());
     }
 
@@ -73,11 +77,180 @@ class ProxyConfigurationModelSaveActionsTest {
 
         SettingsProxyConfigAccessor configurationAccessor = new SettingsProxyConfigAccessor(encryptionUtility, settingsProxyConfigurationRepository, nonProxyHostsConfigurationRepository);
         SettingsProxyCrudActions settingsProxyCrudActions = new SettingsProxyCrudActions(authorizationManager, configurationAccessor, validator, settingsDescriptorKey);
-        ProxyConfigurationModelSaveActions saveActions = new ProxyConfigurationModelSaveActions(settingsProxyCrudActions);
+        ProxyConfigurationModelSaveActions saveActions = new ProxyConfigurationModelSaveActions(converter, settingsProxyCrudActions);
         saveActions.createConcreteModel(createDefaultConfigurationModel());
 
         SettingsProxyConfigurationEntity actualEntity = savedEntity.get();
         assertEquals(TEST_PROXY_HOST, actualEntity.getHost());
+        assertEquals(Integer.valueOf(TEST_PROXY_PORT), actualEntity.getPort());
+        assertEquals(TEST_PROXY_USERNAME, actualEntity.getUsername());
+        assertEquals(TEST_PROXY_PASSWORD, encryptionUtility.decrypt(actualEntity.getPassword()));
+
+        NonProxyHostConfigurationEntity actualNonProxyHosts = savedNonProxyHostEntity.get();
+        assertNotNull(actualNonProxyHosts);
+        assertEquals(TEST_PROXY_NON_PROXY_HOST, actualNonProxyHosts.getHostnamePattern());
+    }
+
+    @Test
+    void createInvalidConversionTest() {
+        AtomicReference<SettingsProxyConfigurationEntity> savedEntity = new AtomicReference<>();
+        AtomicReference<NonProxyHostConfigurationEntity> savedNonProxyHostEntity = new AtomicReference<>();
+        SettingsProxyConfigurationRepository settingsProxyConfigurationRepository = Mockito.mock(SettingsProxyConfigurationRepository.class);
+        NonProxyHostsConfigurationRepository nonProxyHostsConfigurationRepository = Mockito.mock(NonProxyHostsConfigurationRepository.class);
+        Mockito.when(settingsProxyConfigurationRepository.save(Mockito.any(SettingsProxyConfigurationEntity.class))).thenAnswer(invocation -> {
+            savedEntity.set(invocation.getArgument(0));
+            return savedEntity.get();
+        });
+        Mockito.when(nonProxyHostsConfigurationRepository.saveAll(Mockito.any(List.class))).thenAnswer(invocation -> {
+            Iterable<NonProxyHostConfigurationEntity> iterable = invocation.getArgument(0);
+            for (NonProxyHostConfigurationEntity entity : iterable) {
+                savedNonProxyHostEntity.set(entity);
+            }
+            return List.of(savedNonProxyHostEntity.get());
+        });
+
+        SettingsProxyConfigAccessor configurationAccessor = new SettingsProxyConfigAccessor(encryptionUtility, settingsProxyConfigurationRepository, nonProxyHostsConfigurationRepository);
+        SettingsProxyCrudActions settingsProxyCrudActions = new SettingsProxyCrudActions(authorizationManager, configurationAccessor, validator, settingsDescriptorKey);
+        ProxyConfigurationModelSaveActions saveActions = new ProxyConfigurationModelSaveActions(converter, settingsProxyCrudActions);
+        ConfigurationModel configurationModel = createDefaultConfigurationModel();
+        updateField(configurationModel, ProxyConfigurationModelConverter.FIELD_KEY_PORT, "badport");
+        saveActions.createConcreteModel(configurationModel);
+
+        SettingsProxyConfigurationEntity actualEntity = savedEntity.get();
+        NonProxyHostConfigurationEntity actualNonProxyHosts = savedNonProxyHostEntity.get();
+        assertNull(actualEntity);
+        assertNull(actualNonProxyHosts);
+    }
+
+    @Test
+    void updateTest() {
+        AtomicReference<SettingsProxyConfigurationEntity> savedEntity = new AtomicReference<>();
+        AtomicReference<NonProxyHostConfigurationEntity> savedNonProxyHostEntity = new AtomicReference<>();
+        SettingsProxyConfigurationRepository settingsProxyConfigurationRepository = Mockito.mock(SettingsProxyConfigurationRepository.class);
+        NonProxyHostsConfigurationRepository nonProxyHostsConfigurationRepository = Mockito.mock(NonProxyHostsConfigurationRepository.class);
+        Mockito.when(settingsProxyConfigurationRepository.save(Mockito.any(SettingsProxyConfigurationEntity.class))).thenAnswer(invocation -> {
+            savedEntity.set(invocation.getArgument(0));
+            return savedEntity.get();
+        });
+
+        Mockito.when(settingsProxyConfigurationRepository.findByName(Mockito.anyString())).thenAnswer(invocation -> Optional.ofNullable(savedEntity.get()));
+
+        Mockito.when(nonProxyHostsConfigurationRepository.saveAll(Mockito.any(List.class))).thenAnswer(invocation -> {
+            Iterable<NonProxyHostConfigurationEntity> iterable = invocation.getArgument(0);
+            for (NonProxyHostConfigurationEntity entity : iterable) {
+                savedNonProxyHostEntity.set(entity);
+            }
+            return List.of(savedNonProxyHostEntity.get());
+        });
+
+        SettingsProxyConfigAccessor configurationAccessor = new SettingsProxyConfigAccessor(encryptionUtility, settingsProxyConfigurationRepository, nonProxyHostsConfigurationRepository);
+        SettingsProxyCrudActions settingsProxyCrudActions = new SettingsProxyCrudActions(authorizationManager, configurationAccessor, validator, settingsDescriptorKey);
+        ProxyConfigurationModelSaveActions saveActions = new ProxyConfigurationModelSaveActions(converter, settingsProxyCrudActions);
+        String newHost = "updatedHost";
+        String newPort = "1234";
+        ConfigurationModel configurationModel = createDefaultConfigurationModel();
+        saveActions.createConcreteModel(configurationModel);
+
+        updateField(configurationModel, ProxyConfigurationModelConverter.FIELD_KEY_HOST, newHost);
+        updateField(configurationModel, ProxyConfigurationModelConverter.FIELD_KEY_PORT, newPort);
+
+        saveActions.updateConcreteModel(configurationModel);
+
+        SettingsProxyConfigurationEntity actualEntity = savedEntity.get();
+        assertEquals(newHost, actualEntity.getHost());
+        assertEquals(Integer.valueOf(newPort), actualEntity.getPort());
+        assertEquals(TEST_PROXY_USERNAME, actualEntity.getUsername());
+        assertEquals(TEST_PROXY_PASSWORD, encryptionUtility.decrypt(actualEntity.getPassword()));
+
+        NonProxyHostConfigurationEntity actualNonProxyHosts = savedNonProxyHostEntity.get();
+        assertNotNull(actualNonProxyHosts);
+        assertEquals(TEST_PROXY_NON_PROXY_HOST, actualNonProxyHosts.getHostnamePattern());
+    }
+
+    @Test
+    void updateInvalidConversionTest() {
+        AtomicReference<SettingsProxyConfigurationEntity> savedEntity = new AtomicReference<>();
+        AtomicReference<NonProxyHostConfigurationEntity> savedNonProxyHostEntity = new AtomicReference<>();
+        SettingsProxyConfigurationRepository settingsProxyConfigurationRepository = Mockito.mock(SettingsProxyConfigurationRepository.class);
+        NonProxyHostsConfigurationRepository nonProxyHostsConfigurationRepository = Mockito.mock(NonProxyHostsConfigurationRepository.class);
+        Mockito.when(settingsProxyConfigurationRepository.save(Mockito.any(SettingsProxyConfigurationEntity.class))).thenAnswer(invocation -> {
+            savedEntity.set(invocation.getArgument(0));
+            return savedEntity.get();
+        });
+
+        Mockito.when(settingsProxyConfigurationRepository.findByName(Mockito.anyString())).thenAnswer(invocation -> Optional.ofNullable(savedEntity.get()));
+
+        Mockito.when(nonProxyHostsConfigurationRepository.saveAll(Mockito.any(List.class))).thenAnswer(invocation -> {
+            Iterable<NonProxyHostConfigurationEntity> iterable = invocation.getArgument(0);
+            for (NonProxyHostConfigurationEntity entity : iterable) {
+                savedNonProxyHostEntity.set(entity);
+            }
+            return List.of(savedNonProxyHostEntity.get());
+        });
+
+        SettingsProxyConfigAccessor configurationAccessor = new SettingsProxyConfigAccessor(encryptionUtility, settingsProxyConfigurationRepository, nonProxyHostsConfigurationRepository);
+        SettingsProxyCrudActions settingsProxyCrudActions = new SettingsProxyCrudActions(authorizationManager, configurationAccessor, validator, settingsDescriptorKey);
+        ProxyConfigurationModelSaveActions saveActions = new ProxyConfigurationModelSaveActions(converter, settingsProxyCrudActions);
+        String newHost = "updatedHost";
+        ConfigurationModel configurationModel = createDefaultConfigurationModel();
+
+        saveActions.createConcreteModel(configurationModel);
+
+        updateField(configurationModel, ProxyConfigurationModelConverter.FIELD_KEY_HOST, newHost);
+        updateField(configurationModel, ProxyConfigurationModelConverter.FIELD_KEY_PORT, "badPort");
+
+        saveActions.updateConcreteModel(configurationModel);
+
+        // Assert that the update failed and the original created values are used.
+        SettingsProxyConfigurationEntity actualEntity = savedEntity.get();
+        assertEquals(TEST_PROXY_HOST, actualEntity.getHost());
+        assertEquals(Integer.valueOf(TEST_PROXY_PORT), actualEntity.getPort());
+        assertEquals(TEST_PROXY_USERNAME, actualEntity.getUsername());
+        assertEquals(TEST_PROXY_PASSWORD, encryptionUtility.decrypt(actualEntity.getPassword()));
+    }
+
+    @Test
+    void deleteTest() {
+        AtomicReference<SettingsProxyConfigurationEntity> savedEntity = new AtomicReference<>();
+        AtomicReference<NonProxyHostConfigurationEntity> savedNonProxyHostEntity = new AtomicReference<>();
+        SettingsProxyConfigurationRepository settingsProxyConfigurationRepository = Mockito.mock(SettingsProxyConfigurationRepository.class);
+        NonProxyHostsConfigurationRepository nonProxyHostsConfigurationRepository = Mockito.mock(NonProxyHostsConfigurationRepository.class);
+        Mockito.when(settingsProxyConfigurationRepository.save(Mockito.any(SettingsProxyConfigurationEntity.class))).thenAnswer(invocation -> {
+            savedEntity.set(invocation.getArgument(0));
+            return savedEntity.get();
+        });
+
+        Mockito.when(settingsProxyConfigurationRepository.findByName(Mockito.anyString())).thenAnswer(invocation -> Optional.ofNullable(savedEntity.get()));
+        Mockito.doAnswer(invocation -> {
+            savedEntity.set(null);
+            savedNonProxyHostEntity.set(null);
+            return null;
+        }).when(settingsProxyConfigurationRepository).deleteByName(Mockito.anyString());
+
+        Mockito.when(nonProxyHostsConfigurationRepository.saveAll(Mockito.any(List.class))).thenAnswer(invocation -> {
+            Iterable<NonProxyHostConfigurationEntity> iterable = invocation.getArgument(0);
+            for (NonProxyHostConfigurationEntity entity : iterable) {
+                savedNonProxyHostEntity.set(entity);
+            }
+            return List.of(savedNonProxyHostEntity.get());
+        });
+
+        SettingsProxyConfigAccessor configurationAccessor = new SettingsProxyConfigAccessor(encryptionUtility, settingsProxyConfigurationRepository, nonProxyHostsConfigurationRepository);
+        SettingsProxyCrudActions settingsProxyCrudActions = new SettingsProxyCrudActions(authorizationManager, configurationAccessor, validator, settingsDescriptorKey);
+        ProxyConfigurationModelSaveActions saveActions = new ProxyConfigurationModelSaveActions(converter, settingsProxyCrudActions);
+        ConfigurationModel configurationModel = createDefaultConfigurationModel();
+        saveActions.createConcreteModel(configurationModel);
+
+        SettingsProxyConfigurationEntity actualEntity = savedEntity.get();
+        NonProxyHostConfigurationEntity actualNonProxyHosts = savedNonProxyHostEntity.get();
+        assertNotNull(actualEntity);
+        assertNotNull(actualNonProxyHosts);
+
+        saveActions.deleteConcreteModel(configurationModel);
+        actualEntity = savedEntity.get();
+        actualNonProxyHosts = savedNonProxyHostEntity.get();
+        assertNull(actualEntity);
+        assertNull(actualNonProxyHosts);
     }
 
     private AuthorizationManager createAuthorizationManager() {
@@ -109,5 +282,9 @@ class ProxyConfigurationModelSaveActionsTest {
         fieldValuesMap.put(ProxyConfigurationModelConverter.FIELD_KEY_NON_PROXY_HOSTS, nonProxyHostField);
 
         return new ConfigurationModel(1L, 1L, "", "", ConfigContextEnum.GLOBAL, fieldValuesMap);
+    }
+
+    private void updateField(ConfigurationModel configurationModel, String key, String value) {
+        configurationModel.getField(key).ifPresent(field -> field.setFieldValue(value));
     }
 }
