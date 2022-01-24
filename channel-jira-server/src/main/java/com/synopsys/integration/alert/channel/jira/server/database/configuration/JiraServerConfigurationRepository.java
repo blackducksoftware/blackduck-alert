8913297f7e011bafa5130e324d57678be5c3741b
psantos1113 <psantos@blackducksoftware.com>
2022-01-24 08:11:40 -0500

    jira server db (#1990)
    * feat: Create liquibase and database tables for jira server channel.
    
    * feat: Add config entity and repository
    
    * feat: Add accessor class.
    
    * test: add unit test for the accessor.
    
    * feat: Add migration of old jira data.
    
    * chore: fix copyright and import.
    
    * feat: Add the 6.10 db changelog
    
    * feat: Update the migration script.

---
+++ channel-jira-server/src/main/java/com/synopsys/integration/alert/channel/jira/server/database/configuration/JiraServerConfigurationRepository.java
@@ -0,0 +1,17 @@
+/*
+ * channel-jira-server
+ *
+ * Copyright (c) 2022 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
+package com.synopsys.integration.alert.channel.jira.server.database.configuration;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JiraServerConfigurationRepository extends JpaRepository<JiraServerConfigurationEntity, UUID> {
+    Optional<JiraServerConfigurationEntity> findByName(String name);
+}
