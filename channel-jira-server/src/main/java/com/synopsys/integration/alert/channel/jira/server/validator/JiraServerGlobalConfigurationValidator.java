8b840579ac018dd74ed7cdf3e3f3b41d7ea37091
Martin Chomicki <martinch@synopsys.com>
2022-01-24 11:11:35 -0500

    feat: Add new validation for JiraServerGlobalConfig

---
+++ channel-jira-server/src/main/java/com/synopsys/integration/alert/channel/jira/server/validator/JiraServerGlobalConfigurationValidator.java
@@ -0,0 +1,50 @@
+package com.synopsys.integration.alert.channel.jira.server.validator;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+
+import com.synopsys.integration.alert.channel.jira.server.model.JiraServerGlobalConfigModel;
+import com.synopsys.integration.alert.common.descriptor.config.field.errors.AlertFieldStatus;
+import com.synopsys.integration.alert.common.descriptor.config.field.errors.AlertFieldStatusMessages;
+import com.synopsys.integration.alert.common.rest.model.ValidationResponseModel;
+
+@Component
+public class JiraServerGlobalConfigurationValidator {
+
+    public ValidationResponseModel validate(JiraServerGlobalConfigModel model) {
+        Set<AlertFieldStatus> statuses = new HashSet<>();
+
+        if (StringUtils.isBlank(model.getName())) {
+            statuses.add(AlertFieldStatus.error("name", AlertFieldStatusMessages.REQUIRED_FIELD_MISSING));
+        }
+        if (StringUtils.isBlank(model.getUrl())) {
+            statuses.add(AlertFieldStatus.error("url", AlertFieldStatusMessages.REQUIRED_FIELD_MISSING));
+        }
+        if (StringUtils.isNotBlank(model.getUrl())) {
+            try {
+                new URL(model.getUrl());
+            } catch (MalformedURLException e) {
+                statuses.add(AlertFieldStatus.error("url", e.getMessage()));
+            }
+        }
+        if (StringUtils.isBlank(model.getUserName())) {
+            statuses.add(AlertFieldStatus.error("username", AlertFieldStatusMessages.REQUIRED_FIELD_MISSING));
+        }
+
+        boolean isPasswordSet = model.getPasswordSet().isEmpty() || model.getPasswordSet().filter(Boolean.FALSE::equals).isPresent();
+        if (model.getPassword().isEmpty() && isPasswordSet) {
+            statuses.add(AlertFieldStatus.error("password", AlertFieldStatusMessages.REQUIRED_FIELD_MISSING));
+        }
+
+        if (!statuses.isEmpty()) {
+            return ValidationResponseModel.fromStatusCollection(statuses);
+        }
+
+        return ValidationResponseModel.success();
+    }
+}
