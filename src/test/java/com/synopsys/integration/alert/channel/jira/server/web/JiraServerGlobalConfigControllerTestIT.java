3c45309fc685cfe47d6b5644e2148ededfc400e8
Brian Mandel <bmandel@blackducksoftware.com>
2022-01-26 11:09:14 -0500

    fix(JiraServerGlobalConfigControllerTestIT.java): Enabled the test and fixed issues with properly verifying the endpoints exist.

--- src/test/java/com/synopsys/integration/alert/channel/jira/server/web/JiraServerGlobalConfigControllerTestIT.java
+++ src/test/java/com/synopsys/integration/alert/channel/jira/server/web/JiraServerGlobalConfigControllerTestIT.java
@@ -1,8 +1,11 @@
 package com.synopsys.integration.alert.channel.jira.server.web;
 
 import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -12,20 +15,24 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.google.gson.Gson;
+import com.synopsys.integration.alert.channel.jira.server.database.accessor.JiraServerGlobalConfigAccessor;
 import com.synopsys.integration.alert.channel.jira.server.model.JiraServerGlobalConfigModel;
 import com.synopsys.integration.alert.common.rest.AlertRestConstants;
 import com.synopsys.integration.alert.common.util.DateUtils;
 import com.synopsys.integration.alert.util.AlertIntegrationTest;
 import com.synopsys.integration.alert.util.AlertIntegrationTestConstants;
 
-@Disabled("Actions have not yet been implemented, must wait for those before tests can be ran")
+@Transactional
 @AlertIntegrationTest
 public class JiraServerGlobalConfigControllerTestIT {
     private static final MediaType MEDIA_TYPE = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), StandardCharsets.UTF_8);
@@ -33,6 +40,9 @@ public class JiraServerGlobalConfigControllerTestIT {
 
     @Autowired
     private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private JiraServerGlobalConfigAccessor jiraServerGlobalConfigAccessor;
 
     @Autowired
     private Gson gson;
@@ -47,7 +57,8 @@ public class JiraServerGlobalConfigControllerTestIT {
     @Test
     @WithMockUser(roles = AlertIntegrationTestConstants.ROLE_ALERT_ADMIN)
     public void verifyGetOneEndpointTest() throws Exception {
-        String urlPath = REQUEST_URL + UUID.randomUUID();
+        JiraServerGlobalConfigModel jiraServerGlobalConfigModel = saveConfigModel(createConfigModel(UUID.randomUUID()));
+        String urlPath = REQUEST_URL + "/" + jiraServerGlobalConfigModel.getId();
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get(urlPath)
             .with(SecurityMockMvcRequestPostProcessors.user("admin").roles(AlertIntegrationTestConstants.ROLE_ALERT_ADMIN))
             .with(SecurityMockMvcRequestPostProcessors.csrf());
@@ -75,34 +86,35 @@ public class JiraServerGlobalConfigControllerTestIT {
             .with(SecurityMockMvcRequestPostProcessors.user("admin").roles(AlertIntegrationTestConstants.ROLE_ALERT_ADMIN))
             .with(SecurityMockMvcRequestPostProcessors.csrf());
 
-        UUID uuid = UUID.randomUUID();
-        JiraServerGlobalConfigModel configModel = createConfigModel(uuid);
+        JiraServerGlobalConfigModel configModel = createConfigModel(null);
         request.content(gson.toJson(configModel));
         request.contentType(MEDIA_TYPE);
 
-        mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().isOk());
+        ResultActions resultActions = mockMvc.perform(request);
+        resultActions.andExpect(MockMvcResultMatchers.status().isCreated());
     }
 
     @Test
     @WithMockUser(roles = AlertIntegrationTestConstants.ROLE_ALERT_ADMIN)
     public void verifyUpdateEndpointTest() throws Exception {
-        UUID uuid = UUID.randomUUID();
-        JiraServerGlobalConfigModel configModel = createConfigModel(uuid);
-        String urlPath = REQUEST_URL + uuid;
+        JiraServerGlobalConfigModel configModel = createConfigModel(UUID.randomUUID());
+        JiraServerGlobalConfigModel jiraServerGlobalConfigModel = saveConfigModel(configModel);
+
+        String urlPath = REQUEST_URL + "/" + jiraServerGlobalConfigModel.getId();
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders.put(urlPath)
             .with(SecurityMockMvcRequestPostProcessors.user("admin").roles(AlertIntegrationTestConstants.ROLE_ALERT_ADMIN))
             .with(SecurityMockMvcRequestPostProcessors.csrf());
 
-        request.content(gson.toJson(configModel));
+        request.content(gson.toJson(jiraServerGlobalConfigModel));
         request.contentType(MEDIA_TYPE);
 
-        mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().isOk());
+        mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().isNoContent());
     }
 
     @Test
     @WithMockUser(roles = AlertIntegrationTestConstants.ROLE_ALERT_ADMIN)
     public void verifyValidateEndpointTest() throws Exception {
-        String urlPath = REQUEST_URL;
+        String urlPath = REQUEST_URL + "/validate";
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(urlPath)
             .with(SecurityMockMvcRequestPostProcessors.user("admin").roles(AlertIntegrationTestConstants.ROLE_ALERT_ADMIN))
             .with(SecurityMockMvcRequestPostProcessors.csrf());
@@ -118,21 +130,21 @@ public class JiraServerGlobalConfigControllerTestIT {
     @Test
     @WithMockUser(roles = AlertIntegrationTestConstants.ROLE_ALERT_ADMIN)
     public void verifyDeleteEndpointTest() throws Exception {
-        UUID uuid = UUID.randomUUID();
-        JiraServerGlobalConfigModel configModel = createConfigModel(uuid);
+        JiraServerGlobalConfigModel jiraServerGlobalConfigModel = saveConfigModel(createConfigModel(UUID.randomUUID()));
 
-        String urlPath = REQUEST_URL + uuid;
-        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(urlPath)
+        String urlPath = REQUEST_URL + "/" + jiraServerGlobalConfigModel.getId();
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.delete(urlPath)
             .with(SecurityMockMvcRequestPostProcessors.user("admin").roles(AlertIntegrationTestConstants.ROLE_ALERT_ADMIN))
             .with(SecurityMockMvcRequestPostProcessors.csrf());
 
-        mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().isOk());
+        mockMvc.perform(request).andExpect(MockMvcResultMatchers.status().isNoContent());
     }
 
+    @Disabled("Test action not yet implemented")
     @Test
     @WithMockUser(roles = AlertIntegrationTestConstants.ROLE_ALERT_ADMIN)
     public void verifyTestEndpointTest() throws Exception {
-        String urlPath = REQUEST_URL + "test";
+        String urlPath = REQUEST_URL + "/test";
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(urlPath)
             .with(SecurityMockMvcRequestPostProcessors.user("admin").roles(AlertIntegrationTestConstants.ROLE_ALERT_ADMIN))
             .with(SecurityMockMvcRequestPostProcessors.csrf());
@@ -148,13 +160,14 @@ public class JiraServerGlobalConfigControllerTestIT {
     @Test
     @WithMockUser(roles = AlertIntegrationTestConstants.ROLE_ALERT_ADMIN)
     public void verifyDisablePluginEndpointTest() throws Exception {
-        String urlPath = REQUEST_URL + "disable-plugin";
+        String urlPath = REQUEST_URL + "/disable-plugin";
         MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(urlPath)
             .with(SecurityMockMvcRequestPostProcessors.user("admin").roles(AlertIntegrationTestConstants.ROLE_ALERT_ADMIN))
             .with(SecurityMockMvcRequestPostProcessors.csrf());
 
         UUID uuid = UUID.randomUUID();
-        JiraServerGlobalConfigModel configModel = createConfigModel(uuid);
+        // Need to just verify the endpoint exists and not actually connect to Jira
+        JiraServerGlobalConfigModel configModel = createConfigModel(uuid, "badUrl");
         request.content(gson.toJson(configModel));
         request.contentType(MEDIA_TYPE);
 
@@ -162,13 +175,22 @@ public class JiraServerGlobalConfigControllerTestIT {
     }
 
     private JiraServerGlobalConfigModel createConfigModel(UUID uuid) {
-        return new JiraServerGlobalConfigModel(
-            uuid.toString(),
+        return createConfigModel(uuid, "https://google.com");
+    }
+
+    private JiraServerGlobalConfigModel createConfigModel(UUID uuid, String url) {
+        JiraServerGlobalConfigModel jiraServerGlobalConfigModel = new JiraServerGlobalConfigModel(
+            (null != uuid) ? uuid.toString() : null,
             "Configuration name",
-            DateUtils.createCurrentDateString(DateUtils.AUDIT_DATE_FORMAT),
-            DateUtils.createCurrentDateString(DateUtils.AUDIT_DATE_FORMAT),
-            "url",
+            url,
             "username"
         );
+        jiraServerGlobalConfigModel.setPassword("password");
+        return jiraServerGlobalConfigModel;
+    }
+
+    private JiraServerGlobalConfigModel saveConfigModel(JiraServerGlobalConfigModel jiraServerGlobalConfigModel) {
+        JiraServerGlobalConfigModel configuration = jiraServerGlobalConfigAccessor.createConfiguration(jiraServerGlobalConfigModel);
+        return configuration;
     }
 }
