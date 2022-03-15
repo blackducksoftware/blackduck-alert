6daa7692f44ccdb12671c8715ac9bf215ebed2fb
psantos1113 <psantos@synopsys.com>
2022-03-15 16:50:10 -0400

    Update to next version (#2051)
    * chore: Update headers.
    
    * build: Update to version 6.11.0.

--- channel-email/src/main/java/com/synopsys/integration/alert/channel/email/environment/EmailEnvironmentVariableHandler.java
+++ channel-email/src/main/java/com/synopsys/integration/alert/channel/email/environment/EmailEnvironmentVariableHandler.java
@@ -1,3 +1,10 @@
+/*
+ * channel-email
+ *
+ * Copyright (c) 2022 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.alert.channel.email.environment;
 
 import java.util.HashMap;
