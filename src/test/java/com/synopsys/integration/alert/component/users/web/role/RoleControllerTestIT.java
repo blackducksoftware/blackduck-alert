47d25373990d7dc9fe001d9d7c799d03ad0a028e
psantos1113 <psantos@synopsys.com>
2022-04-08 09:42:15 -0400

    chore: Fix the imports from gson library.

--- src/test/java/com/synopsys/integration/alert/component/users/web/role/RoleControllerTestIT.java
+++ src/test/java/com/synopsys/integration/alert/component/users/web/role/RoleControllerTestIT.java
@@ -22,9 +22,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
-import org.testcontainers.shaded.com.google.common.reflect.TypeToken;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.synopsys.integration.alert.database.user.RoleRepository;
 import com.synopsys.integration.alert.descriptor.api.BlackDuckProviderKey;
 import com.synopsys.integration.alert.util.AlertIntegrationTest;
