bc5c04750f5b671d9b0a9bc8bf657faa89002d04
psantos1113 <psantos@synopsys.com>
2022-05-18 15:24:38 -0400

    refactor: Rename rabbitmq environment variables for containers.

--- docker/blackduck-alert-rabbitmq/generate-config.sh
+++ docker/blackduck-alert-rabbitmq/generate-config.sh
@@ -3,16 +3,16 @@
 rabbitHome=/etc/rabbitmq
 blackduckSecurityHome=/opt/blackduck/rabbitmq/security
 confFile="${rabbitHome}/rabbitmq.conf"
-isSsl=${ALERT_RABBIT_SSL:-false}
-clusterSize=${ALERT_RABBIT_CLUSTER_SIZE:-1}
-clusterType=${ALERT_RABBIT_CLUSTER_TYPE:-compose}
+isSsl=${ALERT_RABBITMQ_SSL:-false}
+clusterSize=${ALERT_RABBITMQ_CLUSTER_SIZE:-1}
+clusterType=${ALERT_RABBITMQ_CLUSTER_TYPE:-compose}
 
 cat >> ${confFile} << EOL
 loopback_users.guest = false
 total_memory_available_override_value = 1GB
 log.console = true
-default_user = ${ALERT_RABBIT_USER:-sysadmin}
-default_pass = ${ALERT_RABBIT_PASSWORD:-blackduck}
+default_user = ${ALERT_RABBITMQ_USER:-sysadmin}
+default_pass = ${ALERT_RABBITMQ_PASSWORD:-blackduck}
 default_vhost = blackduck-alert
 queue_master_locator=min-masters
 prometheus.return_per_object_metrics = true
@@ -21,7 +21,7 @@ EOL
 
 if [ "$isSsl" = 'true' ] ; then
   cat  >> ${confFile} << EOL
-listeners.ssl.default = 5671
+listeners.ssl.default = ${ALERT_RABBITMQ_PORT}
 ssl_options.cacertfile = "${blackduckSecurityHome}/root.crt"
 ssl_options.certfile = "${blackduckSecurityHome}/rabbitmq-server.crt"
 ssl_options.fail_if_no_peer_cert = false
@@ -29,10 +29,14 @@ ssl_options.keyfile = "${blackduckSecurityHome}/rabbitmq-server.key"
 ssl_options.verify = verify_peer
 management.ssl.port = 15671
 EOL
+else
+  cat >> ${confFile} << EOL
+listeners.tcp.default = ${ALERT_RABBITMQ_PORT}
+EOL
 fi
 
 if [ "${clusterType}" = 'kubernetes' ] ; then
-  clusterName=${ALERT_RABBIT_CLUSTER_NAME:-cluster.local}
+  clusterName=${ALERT_RABBITMQ_CLUSTER_NAME:-cluster.local}
   cat  >> ${confFile} << EOL
 cluster_formation.peer_discovery_backend  = rabbit_peer_discovery_k8s
 cluster_formation.k8s.host = kubernetes.default.svc.${clusterName}
