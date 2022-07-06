e42b06a9068379bc80a9c048a50b88447cc38ac8
psantos1113 <psantos@synopsys.com>
2022-07-06 09:40:02 -0400

    IALERT-3099: Send the event to indicate jobs are mapped to notifications (#2150)
    * fix: Make the DateRange abstract.
    
    * test: Update test for the abstract method change.
    
    * fix: Calculate the time range and send events.
    
    * chore: Add comment to getDateRange().
    
    * chore: Reformat whitespace of constructor variables.

--- component/src/main/java/com/synopsys/integration/alert/component/scheduling/workflow/DailyTask.java
+++ component/src/main/java/com/synopsys/integration/alert/component/scheduling/workflow/DailyTask.java
@@ -7,16 +7,21 @@
  */
 package com.synopsys.integration.alert.component.scheduling.workflow;
 
+import java.time.OffsetDateTime;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Component;
 
+import com.synopsys.integration.alert.api.event.EventManager;
 import com.synopsys.integration.alert.api.task.TaskManager;
 import com.synopsys.integration.alert.common.enumeration.FrequencyType;
+import com.synopsys.integration.alert.common.message.model.DateRange;
 import com.synopsys.integration.alert.common.persistence.accessor.ConfigurationModelConfigurationAccessor;
 import com.synopsys.integration.alert.common.persistence.accessor.JobAccessor;
 import com.synopsys.integration.alert.common.persistence.accessor.NotificationAccessor;
 import com.synopsys.integration.alert.common.persistence.model.ConfigurationFieldModel;
+import com.synopsys.integration.alert.common.util.DateUtils;
 import com.synopsys.integration.alert.component.scheduling.descriptor.SchedulingDescriptor;
 import com.synopsys.integration.alert.component.scheduling.descriptor.SchedulingDescriptorKey;
 import com.synopsys.integration.alert.component.tasks.ProcessingTask;
@@ -38,9 +43,10 @@ public class DailyTask extends ProcessingTask {
         NotificationMappingProcessor notificationMappingProcessor,
         TaskManager taskManager,
         ConfigurationModelConfigurationAccessor configurationModelConfigurationAccessor,
-        JobAccessor jobAccessor
+        JobAccessor jobAccessor,
+        EventManager eventManager
     ) {
-        super(taskScheduler, taskManager, notificationAccessor, notificationMappingProcessor, jobAccessor, FrequencyType.DAILY);
+        super(taskScheduler, taskManager, notificationAccessor, notificationMappingProcessor, jobAccessor, FrequencyType.DAILY, eventManager);
         this.schedulingDescriptorKey = schedulingDescriptorKey;
         this.configurationModelConfigurationAccessor = configurationModelConfigurationAccessor;
     }
@@ -48,12 +54,26 @@ public class DailyTask extends ProcessingTask {
     @Override
     public String scheduleCronExpression() {
         String dailySavedCronValue = configurationModelConfigurationAccessor.getConfigurationsByDescriptorKey(schedulingDescriptorKey)
-                                         .stream()
-                                         .findFirst()
-                                         .flatMap(configurationModel -> configurationModel.getField(SchedulingDescriptor.KEY_DAILY_PROCESSOR_HOUR_OF_DAY))
-                                         .flatMap(ConfigurationFieldModel::getFieldValue)
-                                         .orElse(String.valueOf(DEFAULT_HOUR_OF_DAY));
+            .stream()
+            .findFirst()
+            .flatMap(configurationModel -> configurationModel.getField(SchedulingDescriptor.KEY_DAILY_PROCESSOR_HOUR_OF_DAY))
+            .flatMap(ConfigurationFieldModel::getFieldValue)
+            .orElse(String.valueOf(DEFAULT_HOUR_OF_DAY));
         return String.format(CRON_FORMAT, dailySavedCronValue);
     }
 
+    @Override
+    public DateRange getDateRange() {
+        // the hour of the daily task is configurable so zero out the minutes, seconds, and nanoseconds.
+        OffsetDateTime endDate = DateUtils.createCurrentDateTimestamp()
+            .withMinute(0)
+            .withSecond(0)
+            .withNano(0);
+        OffsetDateTime startDate = endDate.minusDays(1)
+            .withMinute(0)
+            .withSecond(0)
+            .withNano(0);
+
+        return DateRange.of(startDate, endDate);
+    }
 }
