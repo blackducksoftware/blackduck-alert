43b8f3d3fadcfbf61c0852e6f19e70f655c28a6e
seifert <seifert@synopsys.com>
2022-08-04 17:21:13 -0400

    Creating Table component, Search capability, various cells, ToggleSwitch for autorefresh

---
+++ ui/src/main/js/common/component/table/cell/TableHeaderCell.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import classNames from 'classnames';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({
+    headerCell: {
+        fontWeight: 'bold',
+        '& > button': {
+            background: 'none',
+            border: 0,
+            fontWeight: 'bold',
+            minHeight: '34px',
+            textAlign: 'left',
+            width: '100%'
+        }
+    },
+    right: {
+        textAlign: 'right',
+        paddingRight: '15px'
+    },
+    center: {
+        textAlign: 'center'
+    },
+    sortableCell: {
+        '&:hover': {
+            borderBottom: [1, 'solid', '#787884']
+        }
+    }
+});
+
+
+const TableHeaderCell = ({ label, sortable, settings }) => {
+    const classes = useStyles();
+    const cellStyle = classNames(classes.headerCell, {
+        [classes.sortableCell]: sortable,
+        [classes.right]: settings?.alignment === 'right',
+        [classes.center]: settings?.alignment === 'center'
+    });
+
+    return (
+        <th className={cellStyle} >
+            { sortable ? (
+                <button>
+                    {label}
+                </button>
+            ) : <>{label}</> }
+            
+        </th>
+    );
+};
+
+export default TableHeaderCell;
