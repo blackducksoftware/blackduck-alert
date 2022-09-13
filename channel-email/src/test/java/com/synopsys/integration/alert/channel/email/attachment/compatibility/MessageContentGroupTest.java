59ef60d05fece0d8ee5c195c7cc713929f6bf244
Dana Maxfield <danam@synopsys.com>
2022-09-13 15:44:01 -0400

    test:Add coverage for MessageContentGroup

---
+++ channel-email/src/test/java/com/synopsys/integration/alert/channel/email/attachment/compatibility/MessageContentGroupTest.java
@@ -0,0 +1,137 @@
+package com.synopsys.integration.alert.channel.email.attachment.compatibility;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.LinkedHashSet;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.synopsys.integration.alert.common.enumeration.ItemOperation;
+import com.synopsys.integration.alert.common.message.model.LinkableItem;
+
+public class MessageContentGroupTest {
+    public static final String PROVIDER_NAME = "test-providerName";
+    public static final String PROVIDER_CONFIG_NAME = "test-providerConfigName";
+    public static final String PROVIDER_URL = "test-providerUrl";
+    public static final LinkableItem PROVIDER_LINKABLE_ITEM = new LinkableItem(PROVIDER_NAME, PROVIDER_CONFIG_NAME, PROVIDER_URL);
+
+    public static final String TOPIC_NAME = "test-topicName";
+    public static final String TOPIC_VALUE = "test-topicValue";
+    public static final String TOPIC_URL = "test-topicUrl";
+    public static final LinkableItem TOPIC_LINKABLE_ITEM = new LinkableItem(TOPIC_NAME, TOPIC_VALUE, TOPIC_URL);
+
+    public final ProviderMessageContent.Builder validBuilder = new ProviderMessageContent.Builder();
+
+    @BeforeEach
+    public void init() {
+        validBuilder.applyProvider(PROVIDER_NAME, 12345L, PROVIDER_CONFIG_NAME, PROVIDER_URL)
+            .applyTopic(TOPIC_NAME, TOPIC_VALUE, TOPIC_URL);
+    }
+
+    @Test
+    public void addOneValidProviderMessageTest() {
+        MessageContentGroup messageContentGroup = new MessageContentGroup();
+        assertNull(messageContentGroup.getCommonProvider());
+        assertNull(messageContentGroup.getCommonTopic());
+        assertEquals(0, messageContentGroup.getSubContent().size());
+        assertTrue(messageContentGroup.isEmpty());
+
+        ProviderMessageContent providerMessageContent = assertDoesNotThrow(validBuilder::build);
+
+        messageContentGroup.add(providerMessageContent);
+        assertEquals(PROVIDER_LINKABLE_ITEM, messageContentGroup.getCommonProvider());
+        assertEquals(TOPIC_LINKABLE_ITEM, messageContentGroup.getCommonTopic());
+        assertTrue(messageContentGroup.getSubContent().contains(providerMessageContent));
+        assertFalse(messageContentGroup.isEmpty());
+    }
+
+    @Test
+    public void addMultipleProviderMessageGoodValueTest() {
+        ProviderMessageContent providerMessageContent1 = assertDoesNotThrow(validBuilder::build);
+        validBuilder.applyAction(ItemOperation.ADD);
+        ProviderMessageContent providerMessageContent2 = assertDoesNotThrow(validBuilder::build);
+        LinkedHashSet<ProviderMessageContent> providerMessageContents = new LinkedHashSet<>();
+        providerMessageContents.add(providerMessageContent1);
+        providerMessageContents.add(providerMessageContent2);
+
+        MessageContentGroup messageContentGroup = new MessageContentGroup();
+        messageContentGroup.addAll(providerMessageContents);
+        assertTrue(messageContentGroup.getSubContent().contains(providerMessageContent1));
+        assertTrue(messageContentGroup.getSubContent().contains(providerMessageContent2));
+    }
+
+    @Test
+    public void addMultipleProviderMessageBadValueTest() {
+        ProviderMessageContent providerMessageContent1 = assertDoesNotThrow(validBuilder::build);
+        validBuilder.applyTopic(TOPIC_NAME, "cause failure", TOPIC_URL);
+        ProviderMessageContent providerMessageContent2 = assertDoesNotThrow(validBuilder::build);
+
+        MessageContentGroup messageContentGroup = new MessageContentGroup();
+        messageContentGroup.add(providerMessageContent1);
+        assertThrows(IllegalArgumentException.class, () -> messageContentGroup.add(providerMessageContent2));
+    }
+
+    @Test
+    public void commonTopicUrlBothNullTest() {
+        validBuilder.applyTopicUrl(null);
+        ProviderMessageContent providerMessageContent1 = assertDoesNotThrow(validBuilder::build);
+        validBuilder.applyTopicUrl(null);
+        ProviderMessageContent providerMessageContent2 = assertDoesNotThrow(validBuilder::build);
+
+        MessageContentGroup messageContentGroup = new MessageContentGroup();
+        messageContentGroup.add(providerMessageContent1);
+        assertEquals(Optional.empty(), messageContentGroup.getCommonTopic().getUrl());
+        messageContentGroup.add(providerMessageContent2);
+        assertEquals(Optional.empty(), messageContentGroup.getCommonTopic().getUrl());
+    }
+
+    @Test
+    public void commonTopicUrlBothPresentTest() {
+        validBuilder.applyTopicUrl(TOPIC_URL);
+        ProviderMessageContent providerMessageContent1 = assertDoesNotThrow(validBuilder::build);
+        validBuilder.applyTopicUrl("valid url");
+        ProviderMessageContent providerMessageContent2 = assertDoesNotThrow(validBuilder::build);
+
+        MessageContentGroup messageContentGroup = new MessageContentGroup();
+        messageContentGroup.add(providerMessageContent1);
+        assertEquals(TOPIC_URL, messageContentGroup.getCommonTopic().getUrl().orElse(null));
+        messageContentGroup.add(providerMessageContent2);
+        assertEquals(TOPIC_URL, messageContentGroup.getCommonTopic().getUrl().orElse(null));
+    }
+
+    @Test
+    public void commonTopicUrlSecondNullTest() {
+        validBuilder.applyTopicUrl(TOPIC_URL);
+        ProviderMessageContent providerMessageContent1 = assertDoesNotThrow(validBuilder::build);
+        validBuilder.applyTopicUrl(null);
+        ProviderMessageContent providerMessageContent2 = assertDoesNotThrow(validBuilder::build);
+
+        MessageContentGroup messageContentGroup = new MessageContentGroup();
+        messageContentGroup.add(providerMessageContent1);
+        assertEquals(TOPIC_URL, messageContentGroup.getCommonTopic().getUrl().orElse(null));
+        messageContentGroup.add(providerMessageContent2);
+        assertEquals(TOPIC_URL, messageContentGroup.getCommonTopic().getUrl().orElse(null));
+    }
+
+    @Test
+    public void commonTopicUrlFirstNullTest() {
+        validBuilder.applyTopicUrl(null);
+        ProviderMessageContent providerMessageContent1 = assertDoesNotThrow(validBuilder::build);
+        String topicUrl = "should see this";
+        validBuilder.applyTopicUrl(topicUrl);
+        ProviderMessageContent providerMessageContent2 = assertDoesNotThrow(validBuilder::build);
+
+        MessageContentGroup messageContentGroup = new MessageContentGroup();
+        messageContentGroup.add(providerMessageContent1);
+        assertEquals(Optional.empty(), messageContentGroup.getCommonTopic().getUrl());
+        messageContentGroup.add(providerMessageContent2);
+        assertEquals(topicUrl, messageContentGroup.getCommonTopic().getUrl().orElse(null));
+    }
+}
