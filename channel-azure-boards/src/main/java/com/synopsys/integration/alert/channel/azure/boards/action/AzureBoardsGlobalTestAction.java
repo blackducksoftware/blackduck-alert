50f51910c126126688661d0b499b66c1ee239b7e
Michael Au <mau@synopsys.com>
2022-10-12 15:55:22 -0400

    FEAT: > Add AzureBoardsGlobalTestAction > Checkout AzureBoardsPropertiesLegacy.fromGlobalConfigurationModel from mc_IALERT-3183_Azure_OAuth_actions > Hook validation and test actions to Azure Boards controller

---
+++ channel-azure-boards/src/main/java/com/synopsys/integration/alert/channel/azure/boards/action/AzureBoardsGlobalTestAction.java
@@ -0,0 +1,105 @@
+package com.synopsys.integration.alert.channel.azure.boards.action;
+
+import com.google.gson.Gson;
+import com.synopsys.integration.alert.api.common.model.ValidationResponseModel;
+import com.synopsys.integration.alert.channel.azure.boards.AzureBoardsPropertiesLegacy;
+import com.synopsys.integration.alert.channel.azure.boards.AzureRedirectUrlCreator;
+import com.synopsys.integration.alert.channel.azure.boards.database.accessor.AzureBoardsGlobalConfigAccessor;
+import com.synopsys.integration.alert.channel.azure.boards.model.AzureBoardsGlobalConfigModel;
+import com.synopsys.integration.alert.channel.azure.boards.oauth.storage.AzureBoardsCredentialDataStoreFactory;
+import com.synopsys.integration.alert.channel.azure.boards.validator.AzureBoardsGlobalConfigurationValidator;
+import com.synopsys.integration.alert.common.action.ActionResponse;
+import com.synopsys.integration.alert.common.action.ValidationActionResponse;
+import com.synopsys.integration.alert.common.enumeration.ConfigContextEnum;
+import com.synopsys.integration.alert.common.message.model.ConfigurationTestResult;
+import com.synopsys.integration.alert.common.rest.api.ConfigurationTestHelper;
+import com.synopsys.integration.alert.common.rest.api.ConfigurationValidationHelper;
+import com.synopsys.integration.alert.common.rest.proxy.ProxyManager;
+import com.synopsys.integration.alert.common.security.authorization.AuthorizationManager;
+import com.synopsys.integration.alert.descriptor.api.model.ChannelKeys;
+import com.synopsys.integration.azure.boards.common.http.AzureApiVersionAppender;
+import com.synopsys.integration.azure.boards.common.http.AzureHttpRequestCreatorFactory;
+import com.synopsys.integration.azure.boards.common.http.AzureHttpService;
+import com.synopsys.integration.azure.boards.common.service.project.AzureProjectService;
+import com.synopsys.integration.exception.IntegrationException;
+import com.synopsys.integration.rest.proxy.ProxyInfo;
+import org.apache.commons.lang3.BooleanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Supplier;
+
+@Component
+public class AzureBoardsGlobalTestAction {
+    private final AzureBoardsGlobalConfigurationValidator validator;
+    private final AzureBoardsGlobalConfigAccessor configurationAccessor;
+    private final AzureBoardsCredentialDataStoreFactory azureBoardsCredentialDataStoreFactory;
+    private final AzureRedirectUrlCreator azureRedirectUrlCreator;
+
+    private final ConfigurationValidationHelper validationHelper;
+    private final ConfigurationTestHelper testHelper;
+    private final Gson gson;
+    private final ProxyManager proxyManager;
+
+    @Autowired
+    public AzureBoardsGlobalTestAction(
+        AuthorizationManager authorizationManager,
+        AzureBoardsGlobalConfigurationValidator validator,
+        AzureBoardsGlobalConfigAccessor configurationAccessor,
+        AzureBoardsCredentialDataStoreFactory azureBoardsCredentialDataStoreFactory,
+        AzureRedirectUrlCreator azureRedirectUrlCreator,
+        Gson gson,
+        ProxyManager proxyManager
+    ) {
+        this.testHelper = new ConfigurationTestHelper(authorizationManager, ConfigContextEnum.GLOBAL, ChannelKeys.JIRA_SERVER);
+        this.validationHelper = new ConfigurationValidationHelper(authorizationManager, ConfigContextEnum.GLOBAL, ChannelKeys.JIRA_SERVER);
+        this.proxyManager = proxyManager;
+        this.gson = gson;
+
+        this.azureBoardsCredentialDataStoreFactory = azureBoardsCredentialDataStoreFactory;
+        this.azureRedirectUrlCreator = azureRedirectUrlCreator;
+        this.validator = validator;
+        this.configurationAccessor = configurationAccessor;
+    }
+
+    public ActionResponse<ValidationResponseModel> testWithPermissionCheck(AzureBoardsGlobalConfigModel requestResource) {
+        Supplier<ValidationActionResponse> validationSupplier = () -> validationHelper.validate(() -> validator.validate(requestResource, requestResource.getId()));
+        return testHelper.test(validationSupplier, () -> testConfigModelContent(requestResource));
+    }
+
+    public ConfigurationTestResult testConfigModelContent(AzureBoardsGlobalConfigModel azureBoardsGlobalConfigModel) {
+        try {
+            boolean isAppIdSet = BooleanUtils.toBoolean(azureBoardsGlobalConfigModel.getIsAppIdSet().orElse(Boolean.FALSE));
+            boolean isClientSecretSet = BooleanUtils.toBoolean(azureBoardsGlobalConfigModel.getIsClientSecretSet().orElse(Boolean.FALSE));
+            if (isAppIdSet || isClientSecretSet) {
+                Optional<AzureBoardsGlobalConfigModel> accessorModel = configurationAccessor.getConfiguration(UUID.fromString(azureBoardsGlobalConfigModel.getId()));
+
+                if (isAppIdSet) {
+                    accessorModel.flatMap(AzureBoardsGlobalConfigModel::getAppId).ifPresent(azureBoardsGlobalConfigModel::setAppId);
+                }
+                if (isClientSecretSet) {
+                    accessorModel.flatMap(AzureBoardsGlobalConfigModel::getClientSecret).ifPresent(azureBoardsGlobalConfigModel::setClientSecret);
+                }
+            }
+
+            AzureBoardsPropertiesLegacy azureBoardsProperties = AzureBoardsPropertiesLegacy.fromGlobalConfigurationModel(
+                azureBoardsCredentialDataStoreFactory,
+                azureRedirectUrlCreator.createOAuthRedirectUri(),
+                azureBoardsGlobalConfigModel
+            );
+            AzureHttpService azureHttpService = createAzureHttpService(azureBoardsProperties);
+            AzureProjectService azureProjectService = new AzureProjectService(azureHttpService, new AzureApiVersionAppender());
+            azureProjectService.getProjects(azureBoardsGlobalConfigModel.getOrganizationName());
+        } catch (IntegrationException ex) {
+            return ConfigurationTestResult.failure("Global Test Action failed testing Azure Boards connection." + ex.getMessage());
+        }
+        return ConfigurationTestResult.success("Successfully connected to Azure instance.");
+    }
+
+    private AzureHttpService createAzureHttpService(AzureBoardsPropertiesLegacy azureBoardsProperties) throws IntegrationException {
+        ProxyInfo proxy = proxyManager.createProxyInfoForHost(AzureHttpRequestCreatorFactory.DEFAULT_BASE_URL);
+        return azureBoardsProperties.createAzureHttpService(proxy, gson);
+    }
+}
