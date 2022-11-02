c4244729fa0b7f330122179956bc738b3276c26d
Dana Maxfield <danam@synopsys.com>
2022-11-02 15:41:11 -0400

    feat: implement validate() in controller

--- channel-github/src/main/java/com/synopsys/integration/alert/channel/github/web/GitHubGlobalConfigController.java
+++ channel-github/src/main/java/com/synopsys/integration/alert/channel/github/web/GitHubGlobalConfigController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.synopsys.integration.alert.api.common.model.ValidationResponseModel;
 import com.synopsys.integration.alert.channel.github.action.GitHubGlobalCrudActions;
+import com.synopsys.integration.alert.channel.github.action.GitHubGlobalValidationAction;
 import com.synopsys.integration.alert.channel.github.model.GitHubGlobalConfigModel;
 import com.synopsys.integration.alert.common.rest.AlertRestConstants;
 import com.synopsys.integration.alert.common.rest.ResponseFactory;
@@ -20,46 +21,48 @@ import com.synopsys.integration.alert.common.rest.model.AlertPagedModel;
 @RequestMapping(AlertRestConstants.GITHUB_CONFIGURATION_PATH)
 public class GitHubGlobalConfigController implements StaticConfigResourceController<GitHubGlobalConfigModel>,
     ValidateController<GitHubGlobalConfigModel>, ReadPageController<AlertPagedModel<GitHubGlobalConfigModel>> {
+    private final GitHubGlobalValidationAction gitHubGlobalValidationAction;
     private final GitHubGlobalCrudActions configActions;
 
     @Autowired
-    public GitHubGlobalConfigController(final GitHubGlobalCrudActions configActions) {
+    public GitHubGlobalConfigController(GitHubGlobalValidationAction gitHubGlobalValidationAction, GitHubGlobalCrudActions configActions) {
+        this.gitHubGlobalValidationAction = gitHubGlobalValidationAction;
         this.configActions = configActions;
     }
 
     @Override
     public AlertPagedModel<GitHubGlobalConfigModel> getPage(
-        final Integer pageNumber,
-        final Integer pageSize,
-        final String searchTerm,
-        final String sortName,
-        final String sortOrder
+        Integer pageNumber,
+        Integer pageSize,
+        String searchTerm,
+        String sortName,
+        String sortOrder
     ) {
         return ResponseFactory.createContentResponseFromAction(configActions.getPaged(pageNumber, pageSize, searchTerm, sortName, sortOrder));
     }
 
     @Override
-    public GitHubGlobalConfigModel create(final GitHubGlobalConfigModel resource) {
+    public GitHubGlobalConfigModel create(GitHubGlobalConfigModel resource) {
         return ResponseFactory.createContentResponseFromAction(configActions.create(resource));
     }
 
     @Override
-    public GitHubGlobalConfigModel getOne(final UUID id) {
+    public GitHubGlobalConfigModel getOne(UUID id) {
         return ResponseFactory.createContentResponseFromAction(configActions.getOne(id));
     }
 
     @Override
-    public void update(final UUID id, final GitHubGlobalConfigModel resource) {
+    public void update(UUID id, GitHubGlobalConfigModel resource) {
         ResponseFactory.createContentResponseFromAction(configActions.update(id, resource));
     }
 
     @Override
-    public void delete(final UUID id) {
+    public void delete(UUID id) {
         ResponseFactory.createContentResponseFromAction(configActions.delete(id));
     }
 
     @Override
-    public ValidationResponseModel validate(final GitHubGlobalConfigModel requestBody) {
-        return ValidationResponseModel.success();
+    public ValidationResponseModel validate(GitHubGlobalConfigModel requestBody) {
+        return ResponseFactory.createContentResponseFromAction(gitHubGlobalValidationAction.validate(requestBody));
     }
 }
