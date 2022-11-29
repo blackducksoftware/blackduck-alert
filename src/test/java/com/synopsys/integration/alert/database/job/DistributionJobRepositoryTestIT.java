dcd62319ca441770064612a2855fffc1a3f64f19
Michael Au <mau@synopsys.com>
2022-11-29 14:55:58 -0500

    Reduce lines and add accessor cleanups afterEach for sanity

--- src/test/java/com/synopsys/integration/alert/database/job/DistributionJobRepositoryTestIT.java
+++ src/test/java/com/synopsys/integration/alert/database/job/DistributionJobRepositoryTestIT.java
@@ -7,6 +7,7 @@ import com.synopsys.integration.alert.common.persistence.model.job.DistributionJ
 import com.synopsys.integration.alert.common.persistence.model.job.details.DistributionJobDetailsModel;
 import com.synopsys.integration.alert.common.persistence.model.job.details.EmailJobDetailsModel;
 import com.synopsys.integration.alert.common.rest.model.AlertNotificationModel;
+import com.synopsys.integration.alert.common.rest.model.AlertPagedModel;
 import com.synopsys.integration.alert.common.util.DateUtils;
 import com.synopsys.integration.alert.database.api.DefaultNotificationAccessor;
 import com.synopsys.integration.alert.database.api.StaticJobAccessor;
@@ -38,34 +39,39 @@ class DistributionJobRepositoryTestIT {
     @Autowired
     private DefaultNotificationAccessor defaultNotificationAccessor;
 
+    private static final String JOB_NAME = "test job";
+    private static final long PROVIDER_AND_BDCONFIG_ID = 1L;  // This is also notification.providerConfigId
+
     @AfterEach
     public void cleanup() {
         distributionJobRepository.deleteAll();
+
+        PageRequest notificationPages = defaultNotificationAccessor.getPageRequestForNotifications(0, 10, null, null);
+        Page<AlertNotificationModel> notifications = defaultNotificationAccessor.findAll(notificationPages, false);
+        notifications.get().forEach(defaultNotificationAccessor::deleteNotification);
+
+        AlertPagedModel<DistributionJobModel> jobPages = staticJobAccessor.getPageOfJobs(0, 10);
+        jobPages.getModels().stream()
+            .map(DistributionJobModel::getJobId)
+            .forEach(staticJobAccessor::deleteJob);
     }
 
     @Test
     @Transactional
     void findAndSortReturnsNotificationsOnNoFilters() {
-        long providerAndBDConfigId = 1L;  // This is also notification.providerConfigId
-
         // Create a JobsDetailEntity and the DistributionJobEntity
-        FrequencyType frequency = FrequencyType.REAL_TIME;
         EmailJobDetailsModel emailJobDetailsModel = createEmailJobDetails();
         DistributionJobRequestModel distributionJobModel = createDistributionJobRequestModel(
-            frequency,
-            providerAndBDConfigId,
             List.of(NotificationType.VULNERABILITY.name(), NotificationType.RULE_VIOLATION.name()),
             Collections.emptyList(),
             Collections.emptyList(),
             emailJobDetailsModel
         );
 
-        DistributionJobModel createdJob = staticJobAccessor.createJob(distributionJobModel);
-        UUID createdJobId = createdJob.getJobId();
+        UUID createdJobId = staticJobAccessor.createJob(distributionJobModel).getJobId();
 
         // Create the NotificationEntity
-        AlertNotificationModel alertNotificationModel = createAlertNotificationModel(providerAndBDConfigId, NotificationType.VULNERABILITY.name());
-
+        AlertNotificationModel alertNotificationModel = createAlertNotificationModel(NotificationType.VULNERABILITY.name());
         List<AlertNotificationModel> alertNotificationModels = defaultNotificationAccessor.saveAllNotifications(
             List.of(alertNotificationModel)
         );
@@ -73,8 +79,8 @@ class DistributionJobRepositoryTestIT {
         // Filter and find
         Page<FilteredDistributionJob> filteredDistributionJobPage = distributionJobRepository.findAndSortMatchingEnabledJobsByFilteredNotification(
             alertNotificationModels.get(0).getId(),
-            providerAndBDConfigId,
-            List.of(frequency.name()),
+            PROVIDER_AND_BDCONFIG_ID,
+            List.of(FrequencyType.REAL_TIME.name()),
             Collections.emptySet(),
             null,
             null,
@@ -82,34 +88,27 @@ class DistributionJobRepositoryTestIT {
         );
 
         assertEquals(1L, filteredDistributionJobPage.getTotalElements());
-        FilteredDistributionJob firstFilteredDistributionJob = filteredDistributionJobPage.stream().findFirst().orElseThrow();
-        assertEquals(createdJobId, firstFilteredDistributionJob.getJobId());
-        assertEquals(alertNotificationModels.get(0).getId(), firstFilteredDistributionJob.getNotificationId());
+        assertFilteredPageMatchesJobAndNotificationId(filteredDistributionJobPage, createdJobId, alertNotificationModels);
     }
 
     @Test
     @Transactional
     void findAndSortReturnsNotificationsOnPolicyFilter() {
-        long providerAndBDConfigId = 1L;
         List<String> policyFilterNames = List.of("Policy1");
 
         // Create a JobsDetailEntity and the DistributionJobEntity
-        FrequencyType frequency = FrequencyType.REAL_TIME;
         EmailJobDetailsModel emailJobDetailsModel = createEmailJobDetails();
         DistributionJobRequestModel distributionJobModel = createDistributionJobRequestModel(
-            frequency,
-            providerAndBDConfigId,
-            List.of(NotificationType.VULNERABILITY.name(), NotificationType.RULE_VIOLATION.name()),
+            List.of(NotificationType.RULE_VIOLATION.name()),
             policyFilterNames,
             Collections.emptyList(),
             emailJobDetailsModel
         );
 
-        DistributionJobModel createdJob = staticJobAccessor.createJob(distributionJobModel);
-        UUID createdJobId = createdJob.getJobId();
+        UUID createdJobId = staticJobAccessor.createJob(distributionJobModel).getJobId();
 
         // Create the NotificationEntity
-        AlertNotificationModel alertNotificationModel = createAlertNotificationModel(providerAndBDConfigId, NotificationType.RULE_VIOLATION.name());
+        AlertNotificationModel alertNotificationModel = createAlertNotificationModel(NotificationType.RULE_VIOLATION.name());
 
         List<AlertNotificationModel> alertNotificationModels = defaultNotificationAccessor.saveAllNotifications(
             List.of(alertNotificationModel)
@@ -118,8 +117,8 @@ class DistributionJobRepositoryTestIT {
         // Filter and find with matching policy
         Page<FilteredDistributionJob> filteredPageWithWantedPolicy = distributionJobRepository.findAndSortMatchingEnabledJobsByFilteredNotification(
             alertNotificationModels.get(0).getId(),
-            providerAndBDConfigId,
-            List.of(frequency.name()),
+            PROVIDER_AND_BDCONFIG_ID,
+            List.of(FrequencyType.REAL_TIME.name()),
             Collections.emptySet(),
             new HashSet<>(policyFilterNames),
             null,
@@ -127,15 +126,13 @@ class DistributionJobRepositoryTestIT {
         );
 
         assertEquals(1L, filteredPageWithWantedPolicy.getTotalElements());
-        FilteredDistributionJob firstFilteredDistributionJob = filteredPageWithWantedPolicy.stream().findFirst().orElseThrow();
-        assertEquals(createdJobId, firstFilteredDistributionJob.getJobId());
-        assertEquals(alertNotificationModels.get(0).getId(), firstFilteredDistributionJob.getNotificationId());
+        assertFilteredPageMatchesJobAndNotificationId(filteredPageWithWantedPolicy, createdJobId, alertNotificationModels);
 
         // Filter and find with non-matching policy
         Page<FilteredDistributionJob> filteredPageWithUnwantedPolicy = distributionJobRepository.findAndSortMatchingEnabledJobsByFilteredNotification(
             alertNotificationModels.get(0).getId(),
-            providerAndBDConfigId,
-            List.of(frequency.name()),
+            PROVIDER_AND_BDCONFIG_ID,
+            List.of(FrequencyType.REAL_TIME.name()),
             Collections.emptySet(),
             new HashSet<>(List.of("Policy2")),
             null,
@@ -148,26 +145,21 @@ class DistributionJobRepositoryTestIT {
     @Test
     @Transactional
     void findAndSortReturnsNotificationsOnSeverityFilter() {
-        long providerAndBDConfigId = 1L;
         List<String> vulnerabilitySeverityFilterNames = List.of(VulnerabilitySeverityType.CRITICAL.name());
 
         // Create a JobsDetailEntity and the DistributionJobEntity
-        FrequencyType frequency = FrequencyType.REAL_TIME;
         EmailJobDetailsModel emailJobDetailsModel = createEmailJobDetails();
         DistributionJobRequestModel distributionJobModel = createDistributionJobRequestModel(
-            frequency,
-            providerAndBDConfigId,
-            List.of(NotificationType.VULNERABILITY.name(), NotificationType.RULE_VIOLATION.name()),
+            List.of(NotificationType.VULNERABILITY.name()),
             Collections.emptyList(),
             vulnerabilitySeverityFilterNames,
             emailJobDetailsModel
         );
 
-        DistributionJobModel createdJob = staticJobAccessor.createJob(distributionJobModel);
-        UUID createdJobId = createdJob.getJobId();
+        UUID createdJobId = staticJobAccessor.createJob(distributionJobModel).getJobId();
 
         // Create the NotificationEntity
-        AlertNotificationModel alertNotificationModel = createAlertNotificationModel(providerAndBDConfigId, NotificationType.VULNERABILITY.name());
+        AlertNotificationModel alertNotificationModel = createAlertNotificationModel(NotificationType.VULNERABILITY.name());
 
         List<AlertNotificationModel> alertNotificationModels = defaultNotificationAccessor.saveAllNotifications(
             List.of(alertNotificationModel)
@@ -176,58 +168,49 @@ class DistributionJobRepositoryTestIT {
         // Filter and find with matching severity
         Page<FilteredDistributionJob> filteredPageWithWantedSeverity = distributionJobRepository.findAndSortMatchingEnabledJobsByFilteredNotification(
             alertNotificationModels.get(0).getId(),
-            providerAndBDConfigId,
-            List.of(frequency.name()),
+            PROVIDER_AND_BDCONFIG_ID,
+            List.of(FrequencyType.REAL_TIME.name()),
             Collections.emptySet(),
             null,
             new HashSet<>(vulnerabilitySeverityFilterNames),
             PageRequest.of(0, 10)
         );
-
         assertEquals(1L, filteredPageWithWantedSeverity.getTotalElements());
-        FilteredDistributionJob firstFilteredDistributionJob = filteredPageWithWantedSeverity.stream().findFirst().orElseThrow();
-        assertEquals(createdJobId, firstFilteredDistributionJob.getJobId());
-        assertEquals(alertNotificationModels.get(0).getId(), firstFilteredDistributionJob.getNotificationId());
+        assertFilteredPageMatchesJobAndNotificationId(filteredPageWithWantedSeverity, createdJobId, alertNotificationModels);
 
         // Filter and find with non-matching severity
         Page<FilteredDistributionJob> filteredPageWithUnwantedSeverity = distributionJobRepository.findAndSortMatchingEnabledJobsByFilteredNotification(
             alertNotificationModels.get(0).getId(),
-            providerAndBDConfigId,
-            List.of(frequency.name()),
+            PROVIDER_AND_BDCONFIG_ID,
+            List.of(FrequencyType.REAL_TIME.name()),
             Collections.emptySet(),
             null,
             new HashSet<>(List.of(VulnerabilitySeverityType.LOW.name())),
             PageRequest.of(0, 10)
         );
-
         assertEquals(0L, filteredPageWithUnwantedSeverity.getTotalElements());
     }
 
     @Test
     @Transactional
     void findAndSortReturnsNotificationsOnSeverityAndPolicyFilter() {
-        long providerAndBDConfigId = 1L;
         List<String> policyFilterNames = List.of("Policy1");
         List<String> vulnerabilitySeverityFilterNames = List.of(VulnerabilitySeverityType.CRITICAL.name());
 
         // Create a JobsDetailEntity and the DistributionJobEntity
-        FrequencyType frequency = FrequencyType.REAL_TIME;
         EmailJobDetailsModel emailJobDetailsModel = createEmailJobDetails();
         DistributionJobRequestModel distributionJobModel = createDistributionJobRequestModel(
-            frequency,
-            providerAndBDConfigId,
             List.of(NotificationType.VULNERABILITY.name(), NotificationType.RULE_VIOLATION.name()),
             policyFilterNames,
             vulnerabilitySeverityFilterNames,
             emailJobDetailsModel
         );
 
-        DistributionJobModel createdJob = staticJobAccessor.createJob(distributionJobModel);
-        UUID createdJobId = createdJob.getJobId();
+        UUID createdJobId = staticJobAccessor.createJob(distributionJobModel).getJobId();
 
         // Create the NotificationEntity - 2 here to simulate policy AND severity violation
-        AlertNotificationModel alertNotificationModel1 = createAlertNotificationModel(providerAndBDConfigId, NotificationType.VULNERABILITY.name());
-        AlertNotificationModel alertNotificationModel2 = createAlertNotificationModel(providerAndBDConfigId, NotificationType.RULE_VIOLATION.name());
+        AlertNotificationModel alertNotificationModel1 = createAlertNotificationModel(NotificationType.VULNERABILITY.name());
+        AlertNotificationModel alertNotificationModel2 = createAlertNotificationModel(NotificationType.RULE_VIOLATION.name());
 
         List<AlertNotificationModel> alertNotificationModels = defaultNotificationAccessor.saveAllNotifications(
             List.of(alertNotificationModel1, alertNotificationModel2)
@@ -236,55 +219,49 @@ class DistributionJobRepositoryTestIT {
         // Filter and find with matching severity and policy on a notification
         Page<FilteredDistributionJob> filteredPageWithWantedSeverityAndPolicy = distributionJobRepository.findAndSortMatchingEnabledJobsByFilteredNotification(
             alertNotificationModels.get(0).getId(), // Notification 0
-            providerAndBDConfigId,
-            List.of(frequency.name()),
+            PROVIDER_AND_BDCONFIG_ID,
+            List.of(FrequencyType.REAL_TIME.name()),
             Collections.emptySet(),
             new HashSet<>(policyFilterNames),
             new HashSet<>(vulnerabilitySeverityFilterNames),
             PageRequest.of(0, 10)
         );
-
         assertEquals(1L, filteredPageWithWantedSeverityAndPolicy.getTotalElements());
-        FilteredDistributionJob firstFilteredDistributionJob = filteredPageWithWantedSeverityAndPolicy.stream().findFirst().orElseThrow();
-        assertEquals(createdJobId, firstFilteredDistributionJob.getJobId());
-        assertEquals(alertNotificationModels.get(0).getId(), firstFilteredDistributionJob.getNotificationId());
+        assertFilteredPageMatchesJobAndNotificationId(filteredPageWithWantedSeverityAndPolicy, createdJobId, alertNotificationModels);
 
         // Filter and find with non-matching severity and policy on the other notification
         Page<FilteredDistributionJob> filteredPageWithUnwantedSeverityAndPolicy = distributionJobRepository.findAndSortMatchingEnabledJobsByFilteredNotification(
             alertNotificationModels.get(1).getId(), // Notification 1
-            providerAndBDConfigId,
-            List.of(frequency.name()),
+            PROVIDER_AND_BDCONFIG_ID,
+            List.of(FrequencyType.REAL_TIME.name()),
             Collections.emptySet(),
             new HashSet<>(List.of("Policy2")),
             new HashSet<>(List.of(VulnerabilitySeverityType.LOW.name())),
             PageRequest.of(0, 10)
         );
-
         assertEquals(0L, filteredPageWithUnwantedSeverityAndPolicy.getTotalElements());
 
         // Extra scenarios checks here for policy and sev dependency along with non-existence and emptiness
         Page<FilteredDistributionJob> filteredPageWithWantedSeverityAndNullPolicy = distributionJobRepository.findAndSortMatchingEnabledJobsByFilteredNotification(
             alertNotificationModels.get(0).getId(), // Notification 0
-            providerAndBDConfigId,
-            List.of(frequency.name()),
+            PROVIDER_AND_BDCONFIG_ID,
+            List.of(FrequencyType.REAL_TIME.name()),
             Collections.emptySet(),
             null,
             new HashSet<>(vulnerabilitySeverityFilterNames),
             PageRequest.of(0, 10)
         );
-
         assertEquals(1L, filteredPageWithWantedSeverityAndNullPolicy.getTotalElements());
 
         Page<FilteredDistributionJob> filteredPageWithEmptySeverityAndWantedPolicy = distributionJobRepository.findAndSortMatchingEnabledJobsByFilteredNotification(
             alertNotificationModels.get(1).getId(), // Notification 1
-            providerAndBDConfigId,
-            List.of(frequency.name()),
+            PROVIDER_AND_BDCONFIG_ID,
+            List.of(FrequencyType.REAL_TIME.name()),
             Collections.emptySet(),
             new HashSet<>(policyFilterNames),
             new HashSet<>(),
             PageRequest.of(0, 10)
         );
-
         assertEquals(1L, filteredPageWithEmptySeverityAndWantedPolicy.getTotalElements());
     }
 
@@ -299,15 +276,15 @@ class DistributionJobRepositoryTestIT {
         );
     }
 
-    private DistributionJobRequestModel createDistributionJobRequestModel(FrequencyType frequency, long providerAndBDConfigId, List<String> notificationTypes, List<String> policyFilterPolicyNames, List<String> vulnerabilityFilterSeverityNames, DistributionJobDetailsModel distributionJobDetails) {
+    private DistributionJobRequestModel createDistributionJobRequestModel(List<String> notificationTypes, List<String> policyFilterPolicyNames, List<String> vulnerabilityFilterSeverityNames, DistributionJobDetailsModel distributionJobDetails) {
         return new DistributionJobRequestModel(
             true,
-            "test job",
-            frequency,
+            JOB_NAME,
+            FrequencyType.REAL_TIME,
             ProcessingType.DEFAULT,
             ChannelKeys.EMAIL.getUniversalKey(),
             UUID.randomUUID(),
-            providerAndBDConfigId,
+            PROVIDER_AND_BDCONFIG_ID,
             false,
             null,
             null,
@@ -319,11 +296,11 @@ class DistributionJobRepositoryTestIT {
         );
     }
 
-    private AlertNotificationModel createAlertNotificationModel(long providerAndBDConfigId, String notificationType) {
+    private AlertNotificationModel createAlertNotificationModel(String notificationType) {
         OffsetDateTime currentDateTimestamp = DateUtils.createCurrentDateTimestamp();
         return new AlertNotificationModel(
             null,
-            providerAndBDConfigId,
+            PROVIDER_AND_BDCONFIG_ID,
             "provider",
             "providerConfigName",
             notificationType,
@@ -332,5 +309,11 @@ class DistributionJobRepositoryTestIT {
             currentDateTimestamp,
             false
         );
+    }
+
+    private void assertFilteredPageMatchesJobAndNotificationId(Page<FilteredDistributionJob> filteredPage, UUID createdJobId, List<AlertNotificationModel> alertNotificationModels) {
+        FilteredDistributionJob firstFilteredDistributionJob = filteredPage.stream().findFirst().orElseThrow();
+        assertEquals(createdJobId, firstFilteredDistributionJob.getJobId());
+        assertEquals(alertNotificationModels.get(0).getId(), firstFilteredDistributionJob.getNotificationId());
     }
 }
