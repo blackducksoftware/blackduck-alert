5f593b95d9de13c0935186674e83ab38f885b9a6
Michael Au <mau@synopsys.com>
2022-12-15 15:57:03 -0500

    Create SAML controller

---
+++ component-saml/src/main/java/com/synopsys/integration/alert/component/saml/web/SAMLGlobalConfigController.java
@@ -0,0 +1,42 @@
+package com.synopsys.integration.alert.component.saml.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.synopsys.integration.alert.api.common.model.ValidationResponseModel;
+import com.synopsys.integration.alert.common.rest.AlertRestConstants;
+import com.synopsys.integration.alert.common.rest.api.ValidateController;
+import com.synopsys.integration.alert.common.rest.api.StaticUniqueConfigResourceController;
+import com.synopsys.integration.alert.component.saml.model.SAMLGlobalConfigModel;
+
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(AlertRestConstants.SETTINGS_PROXY_PATH) // TODO figure path
+public class SAMLGlobalConfigController implements StaticUniqueConfigResourceController<SAMLGlobalConfigModel>, ValidateController<SAMLGlobalConfigModel> {
+    @Override
+    public SAMLGlobalConfigModel create(SAMLGlobalConfigModel resource) {
+        return new SAMLGlobalConfigModel(UUID.randomUUID().toString());
+    }
+
+    @Override
+    public SAMLGlobalConfigModel getOne() {
+        return new SAMLGlobalConfigModel(UUID.randomUUID().toString());
+    }
+
+    @Override
+    public void update(SAMLGlobalConfigModel resource) {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public ValidationResponseModel validate(SAMLGlobalConfigModel requestBody) {
+        return new ValidationResponseModel("Success", Map.of());
+    }
+}
