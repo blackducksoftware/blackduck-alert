d568b2d1c72b05f692a194dceccd965b17764c6e
Michael Au <mau@synopsys.com>
2022-12-20 17:02:24 -0500

    Remove todos, add autowired constructor

--- authentication-saml/src/main/java/com/synopsys/integration/alert/authentication/saml/web/SAMLGlobalConfigController.java
+++ authentication-saml/src/main/java/com/synopsys/integration/alert/authentication/saml/web/SAMLGlobalConfigController.java
@@ -1,21 +1,26 @@
 package com.synopsys.integration.alert.authentication.saml.web;
 
+import com.synopsys.integration.alert.api.common.model.ValidationResponseModel;
 import com.synopsys.integration.alert.authentication.saml.model.SAMLGlobalConfigModel;
-import com.synopsys.integration.alert.component.authentication.descriptor.AuthenticationDescriptor;
+import com.synopsys.integration.alert.common.rest.AlertRestConstants;
+import com.synopsys.integration.alert.common.rest.api.StaticUniqueConfigResourceController;
+import com.synopsys.integration.alert.common.rest.api.ValidateController;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.synopsys.integration.alert.api.common.model.ValidationResponseModel;
-import com.synopsys.integration.alert.common.rest.AlertRestConstants;
-import com.synopsys.integration.alert.common.rest.api.ValidateController;
-import com.synopsys.integration.alert.common.rest.api.StaticUniqueConfigResourceController;
 
 import java.util.Map;
 import java.util.UUID;
 
 @RestController
-@RequestMapping(AlertRestConstants.SAML_PATH) // TODO Create AlertRestConstants once path is confirmed
+@RequestMapping(AlertRestConstants.SAML_PATH)
 public class SAMLGlobalConfigController implements StaticUniqueConfigResourceController<SAMLGlobalConfigModel>, ValidateController<SAMLGlobalConfigModel> {
+    @Autowired
+    public SAMLGlobalConfigController() {
+        // TODO Wire actions
+    }
+
     @Override
     public SAMLGlobalConfigModel create(SAMLGlobalConfigModel resource) {
         return new SAMLGlobalConfigModel(UUID.randomUUID().toString());
