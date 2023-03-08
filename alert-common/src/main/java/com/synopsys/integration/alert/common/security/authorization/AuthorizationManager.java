0bb1c599bdddd7c49403d8e6b6c9deb32d38fe92
Michael Au <mau@synopsys.com>
2023-03-08 11:37:48 -0500

    Remove userdetails import

--- alert-common/src/main/java/com/synopsys/integration/alert/common/security/authorization/AuthorizationManager.java
+++ alert-common/src/main/java/com/synopsys/integration/alert/common/security/authorization/AuthorizationManager.java
@@ -22,7 +22,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import com.synopsys.integration.alert.api.common.model.exception.AlertConfigurationException;
