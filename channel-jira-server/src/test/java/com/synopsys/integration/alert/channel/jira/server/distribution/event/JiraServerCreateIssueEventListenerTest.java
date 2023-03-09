49ccb24d34351293f723636463b547fa8359f4b0
psantos1113 <psantos@synopsys.com>
2023-03-09 17:01:11 -0500

    test: Add tests for jira server and jira cloud.

--- channel-jira-server/src/test/java/com/synopsys/integration/alert/channel/jira/server/distribution/event/JiraServerCreateIssueEventListenerTest.java
+++ channel-jira-server/src/test/java/com/synopsys/integration/alert/channel/jira/server/distribution/event/JiraServerCreateIssueEventListenerTest.java
@@ -1,5 +1,9 @@
 package com.synopsys.integration.alert.channel.jira.server.distribution.event;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.Instant;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
@@ -10,22 +14,30 @@ import org.springframework.amqp.core.Message;
 
 import com.google.gson.Gson;
 import com.synopsys.integration.alert.api.channel.issue.model.IssueCreationModel;
+import com.synopsys.integration.alert.api.distribution.execution.ExecutingJob;
 import com.synopsys.integration.alert.api.distribution.execution.ExecutingJobManager;
+import com.synopsys.integration.alert.api.distribution.execution.ExecutingJobStage;
+import com.synopsys.integration.alert.api.distribution.execution.JobStage;
 import com.synopsys.integration.alert.api.event.EventManager;
 import com.synopsys.integration.alert.common.message.model.LinkableItem;
+import com.synopsys.integration.alert.common.persistence.accessor.JobCompletionStatusModelAccessor;
 import com.synopsys.integration.alert.descriptor.api.model.ChannelKeys;
 
 class JiraServerCreateIssueEventListenerTest {
     private final Gson gson = new Gson();
 
     @Test
-    void onMessageTest() {
-        UUID parentEventId = UUID.randomUUID();
-        UUID jobExecutionId = UUID.randomUUID();
+    void onMessageTestJob() {
+
         UUID jobId = UUID.randomUUID();
         Set<Long> notificationIds = Set.of(1L, 2L, 3L);
         EventManager eventManager = Mockito.mock(EventManager.class);
-        ExecutingJobManager executingJobManager = Mockito.mock(ExecutingJobManager.class);
+        JobCompletionStatusModelAccessor jobCompletionStatusModelAccessor = Mockito.mock(JobCompletionStatusModelAccessor.class);
+        ExecutingJobManager executingJobManager = new ExecutingJobManager(jobCompletionStatusModelAccessor);
+
+        ExecutingJob executingJob = executingJobManager.startJob(jobId, notificationIds.size());
+        UUID jobExecutionId = executingJob.getExecutionId();
+        executingJobManager.incrementProcessedNotificationCount(jobExecutionId, notificationIds.size());
 
         LinkableItem provider = new LinkableItem("provider", "test-provider");
         IssueCreationModel issueCreationModel = IssueCreationModel.simple("title", "description", List.of(), provider);
@@ -52,6 +64,95 @@ class JiraServerCreateIssueEventListenerTest {
         Message message = new Message(gson.toJson(event).getBytes());
         listener.onMessage(message);
 
-        //TODO add assertions
+        assertTrue(executingJobManager.getExecutingJob(jobExecutionId).isEmpty());
+    }
+
+    @Test
+    void onMessageJobWithRemainingEventsFinishedTest() {
+
+        UUID jobId = UUID.randomUUID();
+        Set<Long> notificationIds = Set.of(1L, 2L, 3L);
+        EventManager eventManager = Mockito.mock(EventManager.class);
+        JobCompletionStatusModelAccessor jobCompletionStatusModelAccessor = Mockito.mock(JobCompletionStatusModelAccessor.class);
+        ExecutingJobManager executingJobManager = new ExecutingJobManager(jobCompletionStatusModelAccessor);
+
+        ExecutingJob executingJob = executingJobManager.startJob(jobId, notificationIds.size());
+        UUID jobExecutionId = executingJob.getExecutionId();
+        executingJobManager.incrementProcessedNotificationCount(jobExecutionId, notificationIds.size());
+        executingJobManager.incrementRemainingEvents(jobExecutionId, 1);
+
+        LinkableItem provider = new LinkableItem("provider", "test-provider");
+        IssueCreationModel issueCreationModel = IssueCreationModel.simple("title", "description", List.of(), provider);
+        JiraServerCreateIssueEvent event = new JiraServerCreateIssueEvent(
+            "destination",
+            jobExecutionId,
+            jobId,
+            notificationIds,
+            issueCreationModel
+        );
+
+        JiraServerCreateIssueEventHandler handler = Mockito.spy(new JiraServerCreateIssueEventHandler(
+            eventManager,
+            gson,
+            null,
+            null,
+            null,
+            null,
+            executingJobManager
+        ));
+        Mockito.doNothing().when(handler).handleEvent(event);
+
+        JiraServerCreateIssueEventListener listener = new JiraServerCreateIssueEventListener(gson, ChannelKeys.JIRA_SERVER, handler);
+        Message message = new Message(gson.toJson(event).getBytes());
+        listener.onMessage(message);
+
+        assertTrue(executingJobManager.getExecutingJob(jobExecutionId).isEmpty());
+    }
+
+    @Test
+    void onMessageJobWithRemainingEventsTest() {
+
+        UUID jobId = UUID.randomUUID();
+        Set<Long> notificationIds = Set.of(1L, 2L, 3L);
+        EventManager eventManager = Mockito.mock(EventManager.class);
+        JobCompletionStatusModelAccessor jobCompletionStatusModelAccessor = Mockito.mock(JobCompletionStatusModelAccessor.class);
+        ExecutingJobManager executingJobManager = new ExecutingJobManager(jobCompletionStatusModelAccessor);
+
+        ExecutingJob executingJob = executingJobManager.startJob(jobId, notificationIds.size());
+        UUID jobExecutionId = executingJob.getExecutionId();
+        executingJobManager.startStage(jobExecutionId, JobStage.ISSUE_CREATION, Instant.now());
+        executingJobManager.incrementProcessedNotificationCount(jobExecutionId, notificationIds.size());
+        executingJobManager.incrementRemainingEvents(jobExecutionId, 2);
+
+        LinkableItem provider = new LinkableItem("provider", "test-provider");
+        IssueCreationModel issueCreationModel = IssueCreationModel.simple("title", "description", List.of(), provider);
+        JiraServerCreateIssueEvent event = new JiraServerCreateIssueEvent(
+            "destination",
+            jobExecutionId,
+            jobId,
+            notificationIds,
+            issueCreationModel
+        );
+
+        JiraServerCreateIssueEventHandler handler = Mockito.spy(new JiraServerCreateIssueEventHandler(
+            eventManager,
+            gson,
+            null,
+            null,
+            null,
+            null,
+            executingJobManager
+        ));
+        Mockito.doNothing().when(handler).handleEvent(event);
+
+        JiraServerCreateIssueEventListener listener = new JiraServerCreateIssueEventListener(gson, ChannelKeys.JIRA_SERVER, handler);
+        Message message = new Message(gson.toJson(event).getBytes());
+        listener.onMessage(message);
+
+        executingJob = executingJobManager.getExecutingJob(jobExecutionId).orElseThrow(() -> new AssertionError("Expected to find and executing job but none found."));
+        ExecutingJobStage jobStage = executingJob.getStage(JobStage.ISSUE_CREATION)
+            .orElseThrow(() -> new AssertionError(String.format("Expected stage %s not found", JobStage.ISSUE_CREATION)));
+        assertNotNull(jobStage.getStart());
+        assertTrue(jobStage.getEnd().isEmpty());
     }
 }
