b2fa5a453722a23d79e8ac51285656ccd4ccb43c
seifert <seifert@synopsys.com>
2023-03-24 09:31:50 -0400

    Running lint

--- ui/src/main/js/common/component/modal/Modal.js
+++ ui/src/main/js/common/component/modal/Modal.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { createUseStyles } from 'react-jss';
 import ModalFooter from 'common/component/modal/ModalFooter';
@@ -40,7 +41,6 @@ const useStyles = createUseStyles({
     }
 });
 
-
 const Modal = ({ isOpen, size, title, closeModal, children, handleCancel, handleSubmit, submitText }) => {
     const classes = useStyles();
 
@@ -77,6 +77,22 @@ const Modal = ({ isOpen, size, title, closeModal, children, handleCancel, handle
             </div>
         </div>
     );
+};
+
+Modal.defaultProps = {
+    open: true,
+    size: 'md'
+};
+
+Modal.propTypes = {
+    isOpen: PropTypes.bool,
+    size: PropTypes.oneOf(['sm', 'md', 'lg']),
+    title: PropTypes.string,
+    closeModal: PropTypes.func,
+    handleCancel: PropTypes.func,
+    handleSubmit: PropTypes.func,
+    showLoader: PropTypes.bool,
+    submitText: PropTypes.string
 };
 
 export default Modal;
