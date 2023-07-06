62d2f660ce1f6c806f4f2d39e1768ded4b8e775a
seifert <seifert@synopsys.com>
2023-07-06 10:35:40 -0400

    Updating modal overflow in body

--- ui/src/main/js/common/component/modal/Modal.js
+++ ui/src/main/js/common/component/modal/Modal.js
@@ -43,7 +43,7 @@ const useStyles = createUseStyles((theme) => ({
         position: 'relative'
     },
     modalBody: {
-        overflowY: 'visible',
+        overflowY: 'auto',
         maxHeight: 'calc(100vh - 355px)',
         padding: ['16px', 0]
     }
