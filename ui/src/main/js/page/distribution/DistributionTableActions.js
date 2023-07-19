21bc26a8410da2934d1d9d144de13a39d9d6f438
Michael Au <mau@synopsys.com>
2023-07-18 22:00:46 -0400

    Hotfix pass in setParamsConfig to distribution delete

--- ui/src/main/js/page/distribution/DistributionTableActions.js
+++ ui/src/main/js/page/distribution/DistributionTableActions.js
@@ -8,7 +8,7 @@ import { useHistory } from 'react-router-dom/cjs/react-router-dom';
 import { fetchDistribution } from 'store/actions/distribution';
 import { useDispatch, useSelector } from 'react-redux';
 
-const DistributionTableActions = ({ data, selected, setSelected, paramsConfig }) => {
+const DistributionTableActions = ({ data, selected, setSelected, paramsConfig, setParamsConfig }) => {
     const dispatch = useDispatch();
     const history = useHistory();
     const { fetching } = useSelector((state) => state.distribution);
