dea3c7a9bce223770586649dfa5d254813306e74
Dana Maxfield <danam@synopsys.com>
2023-12-18 13:12:49 -0500

    IALERT-3300 - Rename package for api-descriptor

--- api-descriptor/src/main/java/com/synopsys/integration/alert/api/descriptor/MsTeamsKey.java
+++ api-descriptor/src/main/java/com/synopsys/integration/alert/api/descriptor/MsTeamsKey.java
@@ -5,11 +5,10 @@
  *
  * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
  */
-package com.synopsys.integration.alert.descriptor.api;
+package com.synopsys.integration.alert.api.descriptor;
 
+import com.synopsys.integration.alert.api.descriptor.model.ChannelKey;
 import org.springframework.stereotype.Component;
-
-import com.synopsys.integration.alert.descriptor.api.model.ChannelKey;
 
 @Component
 public final class MsTeamsKey extends ChannelKey {
