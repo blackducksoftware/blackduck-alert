91e8a45c4105df749c7cfff918b0e406d8d702e7
Michael Au <mau@synopsys.com>
2024-01-25 14:59:21 -0500

    Remove new line comment

--- alert-database/src/main/java/com/synopsys/integration/alert/database/certificates/CustomCertificateRepository.java
+++ alert-database/src/main/java/com/synopsys/integration/alert/database/certificates/CustomCertificateRepository.java
@@ -3,8 +3,7 @@
  *
  * Copyright (c) 2022 Synopsys, Inc.
  *
- * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All
- * rights reserved worldwide.
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
  */
 package com.synopsys.integration.alert.database.certificates;
 
