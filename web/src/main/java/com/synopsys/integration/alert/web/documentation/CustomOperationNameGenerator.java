793675a7528ffe45709eda27c3e1af8007bcc6f0
psantos1113 <psantos@synopsys.com>
2024-02-14 11:56:37 -0500

    IALERT-3627: add a comment and unit test for the generator.

--- web/src/main/java/com/synopsys/integration/alert/web/documentation/CustomOperationNameGenerator.java
+++ web/src/main/java/com/synopsys/integration/alert/web/documentation/CustomOperationNameGenerator.java
@@ -12,6 +12,29 @@ import org.springframework.web.method.HandlerMethod;
 
 import io.swagger.v3.oas.models.Operation;
 
+/* SpringDocs will generate an operation ID, which is an optional field for OpenAPI compliant REST APIs,
+  when the UI for the REST API documentation is accessed.
+  The operation ID that is generated is unique, because according to the OpenAPI Spec it must be,
+  however, it is the method name plus a numeric suffix i.e. getOne, getOne1, ..., getOne15.
+  Alert uses an interface to ensure a consistent REST API for users of Alert and developers.
+  The method names are the same across multiple controllers due to using the interfaces to ensure a consistent contract first REST API design.
+  The names that are generated provide little context to the method when the numeric suffixes are added, because the method names across
+  controllers are not unique.
+  If a new controller is added and is processed before another controller that previously existed,
+  then the numeric suffix used on the operation ID may be different for a previously existing controller.
+
+  Use this class to take the class name and the method name to create an operation ID that has a name that has more contextual meaning.
+  Generate the operation ID using 3 components to the operation ID in the form: <prefix><context><suffix> where:
+  1. prefix = api
+  2. context = first operation tag name
+  3. suffix = method name
+
+  Examples:
+    - apiAboutGetAbout
+    - apiAuditEntryGetOne
+    - apiAuditEntryLegacyGet
+    - apiCertificatesGetOne
+ */
 public class CustomOperationNameGenerator implements GlobalOperationCustomizer {
     public static final String TAG_KEYWORD_CONTROLLER = "controller";
     public static final String TAG_NAME_COMPONENT_DELIMETER = "-";
