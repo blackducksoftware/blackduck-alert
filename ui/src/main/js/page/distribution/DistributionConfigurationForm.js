290272ad1980c2e9cdd03c69fd6f1d806126329e
bseifert14 <ben.seifert14@gmail.com>
2024-11-11 15:45:09 -0500

    IALERT-3765: Project Filter Request (#2643)
    * Adding conditional statement to update project filter data object
    
    * Adding in removed line regarding the fieldConfigProjects
    
    ---------
    
    Co-authored-by: seifert <seifert@blackduck.com>

--- ui/src/main/js/page/distribution/DistributionConfigurationForm.js
+++ ui/src/main/js/page/distribution/DistributionConfigurationForm.js
@@ -67,12 +67,18 @@ const DistributionConfigurationForm = ({
         let configuredProviderProjects = [];
 
         const fieldConfiguredProjects = FieldModelUtilities.getFieldModelValues(providerConfig, DISTRIBUTION_COMMON_FIELD_KEYS.configuredProjects);
-        if (fieldConfiguredProjects && fieldConfiguredProjects.length > 0) {
+        
+        // Determine if fieldConfiguredProjects has shape { label: projectName, href: projectHREF }
+        //      If true change shape to { name: projectName, href: projectHREF, missing: false }
+        //      If false, data shape is OK
+        if (fieldConfiguredProjects.some(project => project.hasOwnProperty('label'))) {
             configuredProviderProjects = fieldConfiguredProjects.map((selectedValue) => ({
                 name: selectedValue.label,
                 href: selectedValue.value,
                 missing: false
             }));
+        } else {
+            configuredProviderProjects = fieldConfiguredProjects;
         }
 
         const providerConfigToSave = FieldModelUtilities.updateFieldModelValues(providerConfig, DISTRIBUTION_COMMON_FIELD_KEYS.configuredProjects, []);
