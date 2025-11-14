a15157797c41134f1d0222923fe2083b8b962b9c
Dana Maxfield <danam@blackduck.com>
2025-11-14 14:19:59 -0500

    IALERT-3912 - Add in missing copyright header

--- api-channel-issue-tracker/src/main/java/com/blackduck/integration/alert/api/channel/issue/tracker/IssueTrackerMessageProcessor.java
+++ api-channel-issue-tracker/src/main/java/com/blackduck/integration/alert/api/channel/issue/tracker/IssueTrackerMessageProcessor.java
@@ -1,3 +1,10 @@
+/*
+ * blackduck-alert
+ *
+ * Copyright (c) 2024 Black Duck Software, Inc.
+ *
+ * Use subject to the terms and conditions of the Black Duck Software End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.blackduck.integration.alert.api.channel.issue.tracker;
 
 import java.io.Serializable;
