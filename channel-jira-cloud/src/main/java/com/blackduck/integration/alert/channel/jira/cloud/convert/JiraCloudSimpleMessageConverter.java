6dbe5f983c0bd085e561bc9aef5d8769765ac8fb
Dana Maxfield <danam@blackduck.com>
2025-11-14 14:33:20 -0500

    IALERT-3912 - Correct copyright

--- channel-jira-cloud/src/main/java/com/blackduck/integration/alert/channel/jira/cloud/convert/JiraCloudSimpleMessageConverter.java
+++ channel-jira-cloud/src/main/java/com/blackduck/integration/alert/channel/jira/cloud/convert/JiraCloudSimpleMessageConverter.java
@@ -1,7 +1,7 @@
 /*
  * blackduck-alert
  *
- * Copyright (c) 2024 Black Duck Software, Inc.
+ * Copyright (c) 2025 Black Duck Software, Inc.
  *
  * Use subject to the terms and conditions of the Black Duck Software End User Software License and Maintenance Agreement. All rights reserved worldwide.
  */
