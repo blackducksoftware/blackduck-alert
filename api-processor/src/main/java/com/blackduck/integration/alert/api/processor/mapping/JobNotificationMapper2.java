fcbbcf3cdc9589b30a4c3a7e5d42c3c2ca8f225b
martinch <martinch@blackduck.com>
2026-01-15 09:23:29 -0500

    Add additional debug logging when mapping jobs to notifications

--- api-processor/src/main/java/com/blackduck/integration/alert/api/processor/mapping/JobNotificationMapper2.java
+++ api-processor/src/main/java/com/blackduck/integration/alert/api/processor/mapping/JobNotificationMapper2.java
@@ -12,6 +12,8 @@ import java.util.List;
 import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -27,6 +29,7 @@ import com.blackduck.integration.alert.common.rest.model.AlertPagedDetails;
 
 @Component
 public class JobNotificationMapper2 {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     private final ProcessingJobAccessor2 processingJobAccessor;
     private final JobNotificationMappingAccessor jobNotificationMappingAccessor;
 
@@ -83,14 +86,22 @@ public class JobNotificationMapper2 {
             pageNumber,
             pageSize
         );
+        // TODO: Investigate reasoning for usage of Optional in FilteredDistributionJobRequestModel.notificationId
+        Long notificationId = filteredDistributionJobRequestModel.getNotificationId().orElse(0L);
+        logger.debug("Beginning job to notification mapping for correlationId: [{}] for notification: [{}]", correlationId, notificationId);
+        boolean doAnyJobsApply = false;
         while (jobs.getCurrentPage() <= jobs.getTotalPages()) {
             List<JobToNotificationMappingModel> mappings = new LinkedList<>();
             for (SimpleFilteredDistributionJobResponseModel job : jobs.getModels()) {
                 if (JobNotificationFilterUtils.doesProjectApplyToJob(job, projectName, projectVersionName)) {
+                    doAnyJobsApply = true;
                     mappings.add(new JobToNotificationMappingModel(correlationId, job.getJobId(), job.getNotificationId()));
                 }
             }
             if (!mappings.isEmpty()) {
+                for (JobToNotificationMappingModel mapping : mappings) {
+                    logger.debug("Mapping jobId: [{}] to notificationId: [{}] for correlationId: [{}]", mapping.getJobId(), mapping.getNotificationId(), mapping.getCorrelationId());
+                }
                 jobNotificationMappingAccessor.addJobMappings(mappings);
             }
             pageNumber++;
@@ -100,5 +111,9 @@ public class JobNotificationMapper2 {
                 pageSize
             );
         }
+        if (!doAnyJobsApply) {
+            logger.debug("No jobs to notification mapping for notification: [{}]", notificationId);
+        }
+        logger.debug("Completed job to notification mapping for correlationId: [{}] for notification: [{}]", correlationId, notificationId);
     }
 }
