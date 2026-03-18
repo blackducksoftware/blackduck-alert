4b8e021712b0c2c88e400f127907eac47ef838a4
seifert <seifert@blackduck.com>
2026-03-18 18:02:24 -0400

    Creating common ViewTabs compoenent that should create a design mimicking the Hub/SCA tabs

---
+++ ui/src/main/js/common/component/navigation/ViewTabs.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import * as PropTypes from 'prop-types';
+import { createUseStyles } from 'react-jss';
+import { Tabs } from 'react-bootstrap';
+
+const useStyles = createUseStyles(theme => ({
+    tabNavigation: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        '& .tab-content': {
+            width: '100%',
+            marginTop: '20px'
+        }
+    },
+    tabContainer: {
+        display: 'inline-flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 'fit-content',
+        margin: 'auto',
+        borderBottom: 'none',
+        '& .nav-item': {
+            flex: '0 0 10rem'
+        },
+        '& .nav-link': {
+            width: '100%',
+            textAlign: 'center',
+            borderRadius: '20px',
+            color: 'black',
+            backgroundColor: theme.colors.grey.lightGrey,
+            border: `1px solid ${theme.colors.grey.lightGrey}`
+        },
+        '& .nav-link.active': {
+            color: theme.colors.white.default,
+            backgroundColor: 'black',
+            border: '1px solid black'
+        },
+        '& .nav-item:not(:first-child) .nav-link': {
+            borderBottomLeftRadius: 0,
+            borderTopLeftRadius: 0
+        },
+        '& .nav-item:not(:last-child) .nav-link': {
+            borderBottomRightRadius: 0,
+            borderTopRightRadius: 0
+        }
+    }
+}));
+const ViewTabs = ({ children, defaultActiveKey = 1, id }) => {
+    const classes = useStyles();
+    return (
+        <div className={classes.tabNavigation}>
+            <Tabs defaultActiveKey={defaultActiveKey} id={id} className={classes.tabContainer}>
+                {children}
+            </Tabs>
+        </div>
+    );
+};
+
+ViewTabs.propTypes = {
+    children: PropTypes.node.isRequired,
+    defaultActiveKey: PropTypes.number,
+    id: PropTypes.string.isRequired,
+};
+
+export default ViewTabs;
--- ui/src/main/js/page/certificates/CertificatesPageLayout.js
+++ ui/src/main/js/page/certificates/CertificatesPageLayout.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import { Tab } from 'react-bootstrap';
 import PageHeader from 'common/component/navigation/PageHeader';
+import ViewTabs from '../../common/component/navigation/ViewTabs';
 import CertificatesTable from 'page/certificates/CertificatesTable';
 import MTLSCertificateLayout from 'page/certificates/MTLSCertificateLayout';
 import { CERTIFICATE_INFO } from 'page/certificates/CertificateModel';
-import { Tab, Tabs } from 'react-bootstrap';
 
 const CertificatesPageLayout = ({ csrfToken, errorHandler }) => {
     return (
@@ -15,7 +15,7 @@ const CertificatesPageLayout = ({ csrfToken, errorHandler }) => {
                 description="This page allows you to configure certificates for Alert to establish secure communication."
                 icon="award"
             />
-            <Tabs defaultActiveKey={1} id="certificate-tabs">
+            <ViewTabs defaultActiveKey={1} id="certificate-tabs">
                 <Tab eventKey={1} title="Server">
                     <CertificatesTable />
                 </Tab>
@@ -25,7 +25,7 @@ const CertificatesPageLayout = ({ csrfToken, errorHandler }) => {
                         errorHandler={errorHandler}
                     />
                 </Tab>
-            </Tabs>
+            </ViewTabs>
         </div>
     );
 };
