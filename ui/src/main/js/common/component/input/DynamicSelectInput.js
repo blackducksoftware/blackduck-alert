8e8ce5015ea27664e06258af1972675c62784a2c
bseifert14 <ben.seifert14@gmail.com>
2026-08-25 11:24:55 -0400

    IALERT-4059: (UI) Filter by Project Issues (#2798)
    * IALERT-4059: Allow Filter By Project field to be fully clickable by the user; Fix expected proptype for the filter by project modal; Fix the table loader call; Fix the Select All for the filter by project table
    
    * IALERT-4059: Set the channel selector dropdown to open above so that it is not cut off
    
    * IALERT-4059: Minor fixes related to copilor review
    
    ---------
    
    Co-authored-by: seifert <seifert@blackduck.com>

--- ui/src/main/js/common/component/input/DynamicSelectInput.js
+++ ui/src/main/js/common/component/input/DynamicSelectInput.js
@@ -42,7 +42,8 @@ const DynamicSelectInput = ({
     creatable,
     maxMenuHeight,
     customVal,
-    customSelect
+    customSelect,
+    menuPlacement = 'auto'
 }) => {
     const classes = useStyles({ width });
     const selectedOptions = options.filter((option) => value.includes(option.value));
@@ -142,7 +143,7 @@ const DynamicSelectInput = ({
             isDisabled={readOnly}
             noOptionsMessage={() => 'No options available'}
             onFocus={onFocus}
-            menuPlacement="auto"
+            menuPlacement={menuPlacement}
             maxMenuHeight={maxMenuHeight || 250}
             styles={selectStyles}
         />
@@ -211,7 +212,8 @@ DynamicSelectInput.propTypes = {
     tooltipDescription: PropTypes.string,
     customVal: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
     customSelect: PropTypes.element,
-    width: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+    width: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    menuPlacement: PropTypes.oneOf(['auto', 'top', 'bottom'])
 };
 
 export default DynamicSelectInput;
--- ui/src/main/js/common/component/table/Table.js
+++ ui/src/main/js/common/component/table/Table.js
@@ -65,7 +65,7 @@ const Table = ({
                 <TableSkeleton />
             )}
 
-            { (!tableData || tableData?.length === 0) && (
+            { (!isLoading && (!tableData || tableData?.length === 0)) && (
                 <EmptyTableView emptyTableConfig={emptyTableConfig} />
             )}
 
